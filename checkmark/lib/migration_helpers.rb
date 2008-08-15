0d6b54b8bf25830684565639c7219e2135c6224d
Geoffrey Flores <g6flores@cdf.toronto.edu>
2008-08-15 02:00:51 +0000

    Added method for declaring foreign keys without cascading delete

--- checkmark/lib/migration_helpers.rb
+++ checkmark/lib/migration_helpers.rb
@@ -5,10 +5,18 @@ module MigrationHelpers
   def foreign_key(from_table, from_column, to_table)
     constraint_name = "fk_#{from_table}_#{to_table}"
     
-    execute %{alter table #{from_table} 
-              add constraint #{constraint_name} 
-              foreign key (#{from_column}) 
-              references #{to_table} (id) on delete cascade}
+    execute %{alter table #{from_table} add constraint #{constraint_name} 
+              foreign key (#{from_column}) references #{to_table} (id) on delete cascade}
+  end
+  
+  # Applies foreign key constraints of the form:
+  #   constraint fk_#{from_table}_#{to_table) foreign key from_column references to_table(id)
+  # with no cascading delete
+  def foreign_key_no_delete(from_table, from_column, to_table)
+    constraint_name = "fk_#{from_table}_#{to_table}"
+    
+    execute %{alter table #{from_table} add constraint #{constraint_name} 
+              foreign key (#{from_column}) references #{to_table} (id)}
   end
   
 end
