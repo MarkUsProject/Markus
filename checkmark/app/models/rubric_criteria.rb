7331784c5b5063d977b46bb149f8b66930fff6e7
Mike Conley <mike.d.conley@gmail.com>
2008-10-22 05:45:33 +0000

    Added primitive slider functionality to Rubric Manager UI.

--- checkmark/app/models/rubric_criteria.rb
+++ checkmark/app/models/rubric_criteria.rb
@@ -1,7 +1,7 @@
 class RubricCriteria < ActiveRecord::Base
   belongs_to  :assignment
-  validates_associated      :assignment, :message => 'association is not strong with you'
+  validates_associated      :assignment, :message => 'association is not strong with an assignment'
   validates_presence_of :name, :weight, :assignment_id
-  validates_numericality_of :assignment_id, :only_integer => true, :greater_than => 0, :message => "can only be whole number greater than 0."
-  validates_numericality_of :weight, :message => "must be a number.", :less_than_or_equal_to => 1.0, :greater_than => 0.0
+  validates_numericality_of :assignment_id, :only_integer => true, :greater_than => 0, :message => "can only be whole number greater than 0"
+  validates_numericality_of :weight, :message => "must be a number greater than 0.0 and less than 1.0", :less_than_or_equal_to => 1.0, :greater_than => 0.0
 end
