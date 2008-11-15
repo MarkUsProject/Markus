fd0ee01dc16a1765af7a4ce3416eb622ecacbfc9
Mike Conley <mike.d.conley@gmail.com>
2008-11-15 15:55:43 +0000

    Controller with modify method, showcasing AJAX difficulties outlined in Nov 14 blog entry

--- checkmark/app/controllers/rubrics_controller.rb
+++ checkmark/app/controllers/rubrics_controller.rb
@@ -1,8 +1,12 @@
 class RubricsController < ApplicationController
+  #TODO:  Remove this?  Security hole?
+  skip_before_filter :verify_authenticity_token
+  
   def index
     @assignment = Assignment.find(params[:id])
   end
-  def get
-    
+  
+  def modify
+      render :text => params.inspect   
   end
 end
