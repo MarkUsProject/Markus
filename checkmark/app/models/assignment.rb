49f75634302ca842ac18532d90daabcb6205e4d1
Mike Conley <mike.d.conley@gmail.com>
2008-11-19 18:13:10 +0000

    Rubrics modify view, showcasing AJAX difficulties outlined in Nov 14 blog entry

--- checkmark/app/models/assignment.rb
+++ checkmark/app/models/assignment.rb
@@ -1,7 +1,7 @@
 class Assignment < ActiveRecord::Base
   
   has_and_belongs_to_many :groups
-  
+  has_many :rubric_criterias
   has_many  :assignment_files
   validates_associated :assignment_files
   
