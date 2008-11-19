96929a3ed7be3bdd13bd096634fb9bc706a48888
Mike Conley <mike.d.conley@gmail.com>
2008-11-19 18:13:16 +0000

    Rubrics modify view, showcasing AJAX difficulties outlined in Nov 14 blog entry

--- checkmark/app/models/rubric_criteria.rb
+++ checkmark/app/models/rubric_criteria.rb
@@ -1,5 +1,6 @@
 class RubricCriteria < ActiveRecord::Base
   belongs_to  :assignment
+  has_many :rubric_levels
   validates_associated      :assignment, :message => 'association is not strong with an assignment'
   validates_presence_of :name, :weight, :assignment_id
   validates_numericality_of :assignment_id, :only_integer => true, :greater_than => 0, :message => "can only be whole number greater than 0"
