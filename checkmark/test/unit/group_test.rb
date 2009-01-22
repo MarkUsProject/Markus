6e8444d5bf94c98cfa05f8587a1f278eddc25204
Catherine Fawcett <g7fawcet@cdf.toronto.edu>
2009-01-22 16:33:17 +0000

    Added the following unit tests:
    test_reject_invalid_no_user
    test_reject_invalid_status
    test_remove_member_in_group
    test_reject_status
    test_pending?

--- checkmark/test/unit/group_test.rb
+++ checkmark/test/unit/group_test.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class GroupTest < ActiveSupport::TestCase
   
-  fixtures  :users, :groups, :assignments
+  fixtures  :users, :groups, :assignments, :memberships
   
   # Test if assignments can fetch the group for a user
   def test_inviter
@@ -20,12 +20,21 @@ class GroupTest < ActiveSupport::TestCase
     assert_equal student5, student2.group_for(a1).inviter
   end
   
+  def test_pending?
+  	group = groups(:group3)
+  	student5 = users(:student5)
+  	student2 = users(:student2)
+  	
+  	assert !group.pending?(student5)
+  	assert group.pending?(student2)
+  end
+  
   # Test if status is correctly fetched for each user
   def test_status
     group = groups(:group3)
     assert_nil group.status(users(:student4))  # not in group
-    assert "inviter", group.status(users(:student5))
-    assert "pending", group.status(users(:student2))
+    assert_equal "inviter", group.status(users(:student5))
+    assert_equal "pending", group.status(users(:student2))
   end
   
   # Test if accept changes user status from pending to accepted
@@ -33,9 +42,9 @@ class GroupTest < ActiveSupport::TestCase
     group = groups(:group3)
     student2 = users(:student2)
     
-    assert "pending", group.status(student2)
+    assert_equal "pending", group.status(student2)
     assert group.accept(student2)
-    assert "accepted", group.status(student2)
+    assert_equal "accepted", group.status(student2)
   end
   
   # Tests if accept raises an error if a user already in group accepts an invite
@@ -56,15 +65,47 @@ class GroupTest < ActiveSupport::TestCase
     end
   end
   
-  # Test if accept changes user status from pending to accepted
+  # Test if reject removes the user from the group.
   def test_reject
     group = groups(:group3)
     student2 = users(:student2)
     
-    assert "pending", group.status(student2)
     assert group.members.include?(student2)
     assert group.reject(student2)
     assert !group.members.include?(student2)
+  end
+  
+  # Tests if reject changes a user status to reject.
+  def test_reject_status
+  	group = groups(:group3)
+    student2 = users(:student2)
+    
+    assert_equal "pending", group.status(student2)
+    assert group.reject(student2)
+    assert_equal "rejected", group.status(student2)
+
+    assert "accepted", group.status(student2)
+    assert "pending", group.status(student2)
+  end
+  
+  # Test if reject raises an error if a user was not invited.
+  def test_reject_invalid_no_user
+  	group = groups(:group3)
+  	student4 = users(:student4)
+  	
+  	assert_raise RuntimeError do
+  		group.reject(student4) # Student 4 was not invited.
+  	end
+  end
+  
+  # Test if reject raises an error if rejecting a user who's status is not pending.
+  def test_reject_invalid_status
+  	group = groups(:group3)
+  	student5 = users(:student5)
+  	
+  	assert_raise RuntimeError do
+  		group.reject(student5) # Student 5 is not pending.
+  	end
   end
   
   def test_add_member_valid
@@ -74,7 +115,7 @@ class GroupTest < ActiveSupport::TestCase
     
     assert_nil group.errors.on_base, group.errors.on_base
     assert group.save, group.errors.on_base
-    assert "pending", group.status(student)
+    assert_equal "pending", group.status(student)
   end
   
   def test_add_member_invalid
@@ -97,6 +138,23 @@ class GroupTest < ActiveSupport::TestCase
     group.invite(['admin']) # not a student
     assert !group.errors.on_base.empty?
   end
+  
+  # Test if a user can be removed from a group.
+  def test_remove_member_in_group
+  	group = groups(:group3)
+  	student = users(:student5) # In group 3.
+  	m = memberships(:student5_group3)
+  	group.remove_member(m)
+  	
+  	assert !group.members.include?(student)
+  end
+  
+  def test_individual?
+  	group = group(:group3)
+  	
+  end
+  
+  
   
 end
 
