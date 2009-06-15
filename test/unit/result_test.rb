9249a1a08e37769a6b4620322b3ed63d5be1791d
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2009-06-15 15:31:36 +0000

    refs #188 -- Result tests pass.

--- test/unit/result_test.rb
+++ test/unit/result_test.rb
@@ -1,17 +1,11 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require 'shoulda'
 
 class ResultTest < ActiveSupport::TestCase
-  fixtures :assignments, :rubric_criterias,  :submissions, :results, :marks, :extra_marks
+  fixtures :assignments, :rubric_criteria,  :submissions, :results, :marks, :extra_marks
 
+  should_have_many :marks
+  should_have_many :extra_marks
+  should_validate_presence_of :marking_state
   # Replace this with your real tests.
-  def test_calculate_total
-    result = results(:r1)
-    total = result.calculate_total
-    assert_equal 30, total
-    bonus_marks = result.get_bonus_marks
-    assert_equal 5, bonus_marks
-    deductions = result.get_deductions
-    assert_equal deductions, -9
-  end
-
 end
