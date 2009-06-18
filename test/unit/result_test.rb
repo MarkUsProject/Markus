3863c26a6a19dea40e0e340fefadb4fd5de682d8
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2009-06-18 18:51:29 +0000

    the populate file for one assignment

--- test/unit/result_test.rb
+++ test/unit/result_test.rb
@@ -8,4 +8,9 @@ class ResultTest < ActiveSupport::TestCase
   should_have_many :extra_marks
   should_validate_presence_of :marking_state
   # Replace this with your real tests.
+
+  def test_get_subtotal
+    result = results(:result_1)
+    assert_equal(10, result.get_subtotal, "Subtotal should be equal to 10")
+  end
 end
