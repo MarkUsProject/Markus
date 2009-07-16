57018b26e8922b27780c0504e3ec1ddb7436c727
Severin Gehwolf <Severin.Gehwolf@utoronto.ca>
2009-07-16 19:01:30 +0000

    hacky rename of checkmark_helper

---
+++ app/helpers/main_helper.rb
@@ -0,0 +1,21 @@
+module MainHelper
+  
+  protected
+  
+  # Sets current user for this session
+  def current_user=(user)
+    session[:uid] = (user.blank? || !user.kind_of?(User)) ? nil : user.id 
+  end
+  
+  def get_blank_message(blank_login, blank_password)
+    return "" unless blank_login || blank_password
+    
+    message = "Your "
+    message += "CDF login " if blank_login
+    message += "and " if blank_login && blank_password
+    message += "password " if blank_password
+    
+    message + "must not be blank."
+  end
+  
+end
