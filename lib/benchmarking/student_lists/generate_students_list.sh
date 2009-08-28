e7e9a00c3ca0f59adea00623f02690945f477a8b
Severin Gehwolf <Severin.Gehwolf@utoronto.ca>
2009-08-28 13:14:35 +0000

    Ignore: just new lists for load-testing

--- lib/benchmarking/student_lists/generate_students_list.sh
+++ lib/benchmarking/student_lists/generate_students_list.sh
@@ -3,7 +3,7 @@
 STUDENT_LIST_BASE_NAME="student_list"
 for i in `seq 5`; do
 	# write 5 chunks of student lists
-	for j in `seq $(( $(( $(($i-1)) * 1000 )) + 1 )) $(($i * 1000))`; do
+	for j in `seq $(( $(( $(($i-1)) * 100 )) + 1 )) $(($i * 100))`; do
 		echo "student_${j},first_name_${j},last_name_${j}" >> "${STUDENT_LIST_BASE_NAME}_upload_${i}.csv"
 		echo "student_${j}" >> "${STUDENT_LIST_BASE_NAME}_${i}.txt"
 	done
