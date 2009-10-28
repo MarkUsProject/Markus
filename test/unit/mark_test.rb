bed9610fd9a5ab273e46dc696ba0a87e976216c1
Simon Lavigne-Giroux <simon.lg@gmail.com>
2009-10-28 20:54:14 +0000

    Ticket #447 - Update the unit tests for the mark model

--- test/unit/mark_test.rb
+++ test/unit/mark_test.rb
@@ -6,10 +6,21 @@ class MarkTest < ActiveSupport::TestCase
   should_belong_to :rubric_criterion
   should_belong_to :result
   should_validate_presence_of :result_id, :rubric_criterion_id
+  
+  should_allow_values_for :mark, 0, 1, 2, 3, 4
+  should_not_allow_values_for :mark, -2, -1, 5, 6
 
-#  def test_get_mark
-#     mark = marks(:mark_2)
-#     assert_equal(6, mark.get_mark)
-#  end
+  should_allow_values_for :result_id, 1, 2, 3
+  should_not_allow_values_for :result_id, -2, -1, 0
+
+  should_allow_values_for :rubric_criterion_id, 1, 2, 3
+  should_not_allow_values_for :rubric_criterion_id, -2, -1, 0
+
+  should_validate_uniqueness_of :rubric_criterion_id, :scoped_to => :result_id
+  
+  def test_get_mark
+     mark = marks(:mark_1)
+     assert_equal(2, mark.get_mark)
+  end
 
 end
