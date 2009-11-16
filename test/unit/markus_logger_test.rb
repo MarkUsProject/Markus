6b98b36ccc4ab7281b468a0576b8b8df2ca13015
Severin Gehwolf <Severin.Gehwolf@utoronto.ca>
2009-11-16 23:17:32 +0000

    Fix erroneous require line in markus_logger_test.rb

--- test/unit/markus_logger_test.rb
+++ test/unit/markus_logger_test.rb
@@ -1,4 +1,4 @@
-require 'test/unit'
+require File.dirname(__FILE__) + '/../test_helper'
 require File.join(File.dirname(__FILE__),'/../../app/models/markus_logger')
 require 'rubygems'
 require 'shoulda'
