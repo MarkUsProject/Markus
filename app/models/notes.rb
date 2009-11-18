42c89cdf3eabffb775854f7de85fc388ce9806c3
Fernando Garces <hades.ares@gmail.com>
2009-11-18 20:04:07 +0000

    typo :note_message => :notes_message

--- app/models/notes.rb
+++ app/models/notes.rb
@@ -2,6 +2,6 @@ class Notes < ActiveRecord::Base
   belongs_to :grouping
   belongs_to :user, :foreign_key => :creator_id
   
-  validates_presence_of :note_message, :grouping_id, :creator_id, :type_association
+  validates_presence_of :notes_message, :grouping_id, :creator_id, :type_association
   validates_associated :grouping, :user
 end
