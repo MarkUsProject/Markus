3947312fc3eadc861239e866a63b42133d413b16
Fernando Garces <hades.ares@gmail.com>
2009-11-18 19:50:17 +0000

    :null => false removed

--- db/migrate/20091118064643_rename_columns_note.rb
+++ db/migrate/20091118064643_rename_columns_note.rb
@@ -1,11 +1,11 @@
 class RenameColumnsNote < ActiveRecord::Migration
   def self.up
-    rename_column :notes, :type, :type_association, :null => false
-    rename_column :notes, :message, :notes_message, :null => false
+    rename_column :notes, :type, :type_association
+    rename_column :notes, :message, :notes_message
   end
   
   def self.down
-    rename_column :notes, :type_association, :type, :null => false
-    rename_column :notes, :notes_message, :message, :null => false
+    rename_column :notes, :type_association, :type
+    rename_column :notes, :notes_message, :message
   end
 end
