bcd868d0705ed975ade346925087a1b84fd8b412
Fernando Garces <hades.ares@gmail.com>
2009-11-18 19:25:27 +0000

    rename doesn't exist. rename_column used instead

--- db/migrate/20091118064643_rename_columns_note.rb
+++ db/migrate/20091118064643_rename_columns_note.rb
@@ -1,11 +1,11 @@
 class RenameColumnsNote < ActiveRecord::Migration
   def self.up
-    rename :notes, :type, :type_association, :null => false
-    rename :notes, :message, :notes_message, :null => false
+    rename_column :notes, :type, :type_association, :null => false
+    rename_column :notes, :message, :notes_message, :null => false
   end
   
   def self.down
-    rename :notes, :type_association, :type, :null => false
-    rename :notes, :notes_message, :message, :null => false
+    rename_column :notes, :type_association, :type, :null => false
+    rename_column :notes, :notes_message, :message, :null => false
   end
 end
