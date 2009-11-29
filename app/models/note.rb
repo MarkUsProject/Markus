945dd41c2b2b35ae2d80b16f11334330d3f998dc
Fernando Garces <hades.ares@gmail.com>
2009-11-29 04:37:48 +0000

    Rename Notes model to singular

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,4 @@
-class Notes < ActiveRecord::Base
+class Note < ActiveRecord::Base
   belongs_to :user, :foreign_key => :creator_id
   belongs_to :noteable, :polymorphic => true
   
--- test/unit/note_test.rb
+++ test/unit/note_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 require 'shoulda'
 
-class NotesTest < ActiveSupport::TestCase
+class NoteTest < ActiveSupport::TestCase
   should_validate_presence_of :notes_message, :creator_id, :noteable
   should_belong_to :noteable, :user
   
