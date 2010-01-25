4fd0c1fba23333cab0665e1853365ba7aabfc12b
Brian Xu <baxzhejiang@gmail.com>
2010-01-25 10:09:54 +0000

    Closing ticket 570

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -32,7 +32,32 @@ class UserTest < ActiveSupport::TestCase
   end
   
   # User creation validations --------------------------------------------
-  
+
+  # test if user_name, last_name, first_name are stripped correctly
+  context "a user" do   
+    setup do 
+      @unspaceduser_name = 'ausername'
+      @unspacedfirst_name = 'afirstname'
+      @unspacedlast_name = 'alastname'
+      new_user = { 
+        :user_name => '   ausername   ',
+        :last_name => '   alastname  ', 
+        :first_name => '   afirstname ', 
+        :type => 'Student'
+      }
+      @user = Student.new(new_user)
+      
+    end
+    
+    should "strip all strings with white space" do
+      assert @user.save
+      assert_nil User.find_by_user_name('   ausername   ')
+      assert_equal @user.user_name, @unspaceduser_name
+      assert_equal @user.first_name, @unspacedfirst_name
+      assert_equal @user.last_name, @unspacedlast_name
+    end
+  end
+
   
   # test if User validates uniqueness of user_name and user_number
   def test_validates_uniq
