2d84a6910a86ad558bbffedceaf1b7ed9e4284e4
Brian Xu <baxzhejiang@gmail.com>
2010-02-26 06:16:42 +0000

    CLosing Ticket #531 Graders need a way of un-setting Criteria

--- app/models/mark.rb
+++ app/models/mark.rb
@@ -8,7 +8,7 @@ class Mark < ActiveRecord::Base
   belongs_to :result
   validates_presence_of :result_id, :markable_id, :markable_type
   validates_numericality_of :result_id, :only_integer => true, :greater_than => 0, :message => "result_id must be an id that is an integer greater than 0"
-  validates_numericality_of :mark, :only_integer => true, :greater_than => -1, :less_than => 5, :message => "Mark must be an integer between 0 and 4"
+  validates_numericality_of :mark, :allow_nil => true, :only_integer => true, :greater_than => -1, :less_than => 5, :message => "Mark must be an integer between 0 and 4"
   validates_numericality_of :markable_id, :only_integer => true, :greater_than => 0, :message => "Criterion must be an id that is an integer greater than 0"
   validates_uniqueness_of :markable_id, :scope => [:result_id, :markable_type]
 
