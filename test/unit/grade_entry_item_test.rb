60e0a01ae845a18c680cb363ed016a3f73415d1b
Farah Juma <farahjuma@gmail.com>
2010-03-02 00:34:54 +0000

    Ticket #608: Fixing the test failures related to "out_of" for GradeEntryItems.

--- test/unit/grade_entry_item_test.rb
+++ test/unit/grade_entry_item_test.rb
@@ -1,13 +1,17 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require File.join(File.dirname(__FILE__),'/../blueprints/blueprints')
+require File.join(File.dirname(__FILE__), '/../blueprints/helper')
 require 'shoulda'
 
 # Tests for GradeEntryItems
 class GradeEntryItemTest < ActiveSupport::TestCase
-  fixtures :all
-  should_belong_to :grade_entry_form
   
-  # Not yet
-  #should_have_many :grades
+  def setup
+    clear_fixtures
+  end
+  
+  should_belong_to :grade_entry_form
+  should_have_many :grades
   
   should_validate_presence_of :name, :out_of
   should_validate_uniqueness_of :name, :scoped_to => :grade_entry_form_id, 
@@ -21,15 +25,15 @@ class GradeEntryItemTest < ActiveSupport::TestCase
   # Make sure different grade entry forms can have grade entry items 
   # with the same name
   should "allow same column name for different grade entry forms" do
-    column = grade_entry_items(:grade_entry_item_1)
-    assert_equal(grade_entry_forms(:grade_entry_form_1), column.grade_entry_form, 
-                 "Expected the grade entry item to be associated with the grade_entry_form_1 fixture") 
-
+    grade_entry_form_1 = GradeEntryForm.make
+    grade_entry_form_2 = GradeEntryForm.make
+    column = grade_entry_form_1.grade_entry_items.make(:name => "Q1")
+    
     # Re-use the column name for a different grade entry form
     dup_column = GradeEntryItem.new
     dup_column.name = column.name
     dup_column.out_of = column.out_of
-    dup_column.grade_entry_form = grade_entry_forms(:grade_entry_form_2)
+    dup_column.grade_entry_form = grade_entry_form_2
     
     assert dup_column.valid?
   end
