1a9d7466381d8516470cfd2a0d82a5c42ddfdddc
Brian Xu <baxzhejiang@gmail.com>
2010-04-02 21:00:28 +0000

    Mark test updated for flexible scheme

--- test/unit/mark_test.rb
+++ test/unit/mark_test.rb
@@ -7,8 +7,8 @@ class MarkTest < ActiveSupport::TestCase
   should_belong_to :result
   should_validate_presence_of :result_id, :markable_id, :markable_type
   
-  should_allow_values_for :mark, 0, 1, 2, 3, 4
-  should_not_allow_values_for :mark, -2, -1, 5, 6
+  should_allow_values_for :mark, 0, 1, 2, 3, 4, nil
+  should_not_allow_values_for :mark, 'a', 'abc', 'b'
 
   should_allow_values_for :result_id, 1, 2, 3
   should_not_allow_values_for :result_id, -2, -1, 0
@@ -38,6 +38,42 @@ class MarkTest < ActiveSupport::TestCase
        
     should "return the good value" do
       assert_equal(4, @mark.get_mark)
+    end
+  end
+  
+  context "An invalid mark asociated with RubricCriterion" do
+    setup do
+      @mark = marks(:mark_1)
+    end
+    
+    should "return error info for mark = -1" do
+      @mark.mark = -1
+      assert_equal(false,@mark.save)
+      assert_equal('Mark '+ I18n.t("mark.error.validate_rubric"), @mark.errors.full_messages.join)
+    end
+    
+    should "return error info for mark = 5" do
+      @mark.mark = 5
+      assert_equal(false,@mark.save)
+      assert_equal('Mark '+ I18n.t("mark.error.validate_rubric"), @mark.errors.full_messages.join)
+    end
+  end
+  
+  context "An invalid mark asociated with FlexibleCriterion" do
+    setup do
+      @mark = marks(:flexible_mark)
+    end
+    
+    should "return error info for mark = -1" do
+      @mark.mark = -1
+      assert_equal(false,@mark.save)
+      assert_equal('Mark '+ I18n.t("mark.error.validate_flexible"), @mark.errors.full_messages.join)
+    end
+    
+    should "return error info for mark > max" do
+      @mark.mark = 11
+      assert_equal(false,@mark.save)
+      assert_equal('Mark '+ I18n.t("mark.error.validate_flexible"), @mark.errors.full_messages.join)
     end
   end
 end
