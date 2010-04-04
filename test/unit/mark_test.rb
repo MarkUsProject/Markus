15bddcc7895ce1e763a93530fcd0388446353a73
Bryan Shen <bryanshen005@gmail.com>
2010-04-04 19:55:48 +0000

    Tests for flexible marking scheme

--- test/unit/mark_test.rb
+++ test/unit/mark_test.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + '/../blueprints/helper'
 require 'shoulda'
 
 class MarkTest < ActiveSupport::TestCase
@@ -7,9 +8,6 @@ class MarkTest < ActiveSupport::TestCase
   should_belong_to :result
   should_validate_presence_of :result_id, :markable_id, :markable_type
   
-  should_allow_values_for :mark, 0, 1, 2, 3, 4
-  should_not_allow_values_for :mark, -2, -1, 5, 6
-
   should_allow_values_for :result_id, 1, 2, 3
   should_not_allow_values_for :result_id, -2, -1, 0
 
@@ -23,9 +21,23 @@ class MarkTest < ActiveSupport::TestCase
   
   context "A mark asociated with RubricCriterion" do   
     setup do
-      @mark = marks(:mark_1)
+      @mark = Mark.make(:markable_type => "RubricCriterion", :mark => 2)
     end
-       
+      
+    should "allow valid values" do
+      values = [0, 2, 3, 4]
+      values.each do |val|
+        assert(@mark.update_attributes(:mark => val), val.to_s)  
+      end
+    end
+    
+    should "not allow invalid values" do
+      values = [-1, 5, -10, 10]
+      values.each do |val|
+        assert(!@mark.update_attributes(:mark => val), val.to_s)
+      end
+    end
+    
     should "return the good value" do
       assert_equal(2, @mark.get_mark)
     end
@@ -33,9 +45,24 @@ class MarkTest < ActiveSupport::TestCase
   
   context "A mark asociated with FlexibleCriterion" do   
     setup do
-      @mark = marks(:flexible_mark)
+      # max of flexible criterion is 10 in blueprint
+      @mark = Mark.make(:markable_type => "FlexibleCriterion", :mark => 4)
     end
        
+    should "allow valid values" do
+      values = [0, 1, 6, 9, 10]
+      values.each do |val|
+        assert(@mark.update_attributes(:mark => val), val.to_s)  
+      end
+    end
+    
+    should "not allow invalid values" do
+      values = [-1, -2, -5, 11, 12, 20]
+      values.each do |val|
+        assert(!@mark.update_attributes(:mark => val), val.to_s)
+      end
+    end
+    
     should "return the good value" do
       assert_equal(4, @mark.get_mark)
     end
