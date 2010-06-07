7fbc424f96ccd9df5e17d20ae18c1b46fe3231fd
Diane Tam <diane.tam@gmail.com>
2010-06-07 21:16:20 +0000

    modified class rules

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,6 +1,5 @@
 class Period < ActiveRecord::Base
-  #validates_presence_of     :start_time, :end_time, :submission_rule_id
-  #validates_presence_of     :type, :deduction
-  #validates_associated      :submission_rule_id
+  validates_presence_of     :hours
+  validates_associated      :submission_rule
   belongs_to :submission_rule, :polymorphic => true
 end
