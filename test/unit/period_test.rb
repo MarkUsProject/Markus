1988c2249e6609de38c178d4394fca61325eb6f3
Diane Tam <diane.tam@gmail.com>
2010-06-08 20:56:13 +0000

    prevent negative hours from being entered, added supporting test cases

--- test/unit/period_test.rb
+++ test/unit/period_test.rb
@@ -8,30 +8,52 @@ require 'shoulda'
 class PeriodTest < ActiveSupport::TestCase
   should_validate_presence_of :hours
   should_belong_to :submission_rule
-  
+
   def setup
     clear_fixtures
-  end  
-  
-  context "Valid grace period" do   
-    setup do
-      @period = Period.make
-	end
-	
-    should "return true for valid grace periods" do
-	  assert @period.valid?
-    end		
   end
 
-  context "Invalid grace period" do   
+  context "A valid grace period" do
     setup do
-	  @period = Period.make
-	end
-	
-    should "return false for invalid grace periods" do
-	  @period.hours = nil
-	  assert !@period.valid?, "period expected to be invalid when hours is set to nil"
-    end		
+      @period = Period.make
+    end
+
+    should "return true when a positive hour value is entered" do
+      assert @period.valid?
+    end
   end
-      
+
+  context "A valid grace period" do
+    setup do
+      @period = Period.make
+    end
+
+    should "return true when zero hours is entered" do
+      @period.hours = 0
+      assert @period.valid?
+    end
+  end
+
+  context "An invalid grace period" do
+    setup do
+      @period = Period.make
+    end
+
+    should "return false when nil hours is entered" do
+      @period.hours = nil
+      assert !@period.valid?, "period expected to be invalid when hours is set to nil"
+    end
+  end
+
+  context "An invalid grace period" do
+    setup do
+      @period = Period.make
+    end
+
+    should "return false when a negative hour value is entered" do
+      @period.hours = -10
+      assert !@period.valid?, "period expected to be invalid when hours is negative"
+    end
+  end
+
 end
