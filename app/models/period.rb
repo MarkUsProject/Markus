3c62c2372d3652da610b6e84208012ac4b2d6e03
Diane Tam <diane.tam@gmail.com>
2010-06-08 20:56:13 +0000

    prevent negative hours from being entered, added supporting test cases

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,5 +1,6 @@
 class Period < ActiveRecord::Base
   validates_presence_of     :hours
   validates_associated      :submission_rule
+  validates_numericality_of :hours, :greater_than_or_equal_to => 0
   belongs_to :submission_rule, :polymorphic => true
 end
