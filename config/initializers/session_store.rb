d7b38203f56bc82eb2de9c0555a8e8f387bc83c3
Benjamin Vialle <benjaminvialle@gmail.com>
2010-07-12 13:15:29 +0000

    [Ticket #169] Session Storage: ActiveRecord -> Cookies

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,17 @@
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+#
+# Please make sure, :session_key is named uniquely if you are hosting
+# several MarkUs instances on one machine. Also, make sure you are changing
+# the :secret string to something else than you find below.
+ActionController::Base.session = {
+  :key => '_markus_session',
+  :secret      => '650d281667d8011a3a6ad6dd4b5d4f9ddbce14a7d78b107812dbb40b24e234256ab2c5572c8196cf6cde6b85942688b6bfd337ffa0daee648d04e1674cf1fdf6'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
