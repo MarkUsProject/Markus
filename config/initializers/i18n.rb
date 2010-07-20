fdd2d82693901cd50e50c3a8b28732a2f4e81646
Benjamin Vialle <benjaminvialle@gmail.com>
2010-07-20 17:34:43 +0000

    [Ticket #661] [i18n] Internationalisation of the calendar. First commit

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,7 +1,7 @@
 require "date"
 # set English as default
 I18n.default_locale = 'en'
-#I18n.locale = 'en' # Calendar Date Select
+I18n.locale = 'en' 
 
 # location where the language files go
 LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
