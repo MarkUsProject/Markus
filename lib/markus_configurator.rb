45ad57ac081ae1ce9ba2c5a53df75620c632f243
Anton Braverman <anton.braverman@utoronto.ca>
2010-08-05 13:27:36 +0000

    closing ticket 679

--- lib/markus_configurator.rb
+++ lib/markus_configurator.rb
@@ -16,33 +16,61 @@ module MarkusConfigurator
   end
   
   def markus_config_repository_storage
-    return REPOSITORY_STORAGE || File.join(RAILS_ROOT, "repositories")
+    if defined? REPOSITORY_STORAGE
+      return REPOSITORY_STORAGE
+    else
+      return File.join(RAILS_ROOT, "repositories")
+    end
   end
 
   def markus_config_pdf_storage
-    return PDF_STORAGE || File.join(RAILS_ROOT, "converted_pdf_dir")
+    if defined? PDF_STORAGE
+      return PDF_STORAGE
+    else
+      return File.join(RAILS_ROOT, "converted_pdf_dir")
+    end
   end
 
   def markus_config_pdf_conv_memory_allowance
-    return PDF_CONV_MEMORY_ALLOWANCE || 100
+    if defined? PDF_CONV_MEMORY_ALLOWANCE
+      return PDF_CONV_MEMORY_ALLOWANCE
+    else
+      return 100
+    end
   end
 
   def markus_config_pdf_support
-    return PDF_SUPPORT || false
+    if defined? PDF_SUPPORT
+      return PDF_SUPPORT
+    else
+      return false
+    end
   end
 
   def markus_config_repository_type
-    return REPOSITORY_TYPE || "svn"
+    if defined? REPOSITORY_TYPE
+      return REPOSITORY_TYPE
+    else
+      return "svn"
+    end
   end
   
   def markus_config_repository_external_base_url
-    return REPOSITORY_EXTERNAL_BASE_URL || "http://www.example.com/svn"
+    if defined? REPOSITORY_EXTERNAL_BASE_URL
+      return REPOSITORY_EXTERNAL_BASE_URL
+    else
+      return "http://www.example.com/svn"
+    end
   end
   
   def markus_config_repository_external_submits_only?
     case markus_config_repository_type
       when "svn"
-        retval = (REPOSITORY_EXTERNAL_SUBMITS_ONLY || false)
+        if defined? REPOSITORY_EXTERNAL_SUBMITS_ONLY
+          retval = REPOSITORY_EXTERNAL_SUBMITS_ONLY
+        else
+          retval = false
+        end
       else
         retval = false
     end
@@ -50,56 +78,104 @@ module MarkusConfigurator
   end
   
   def markus_config_repository_permission_file
-    return REPOSITORY_PERMISSION_FILE || File.join(markus_config_repository_storage, "svn_authz")
+    if defined? REPOSITORY_PERMISSION_FILE
+      return REPOSITORY_PERMISSION_FILE
+    else
+      return File.join(markus_config_repository_storage, "svn_authz")
+    end
   end
 
   def markus_config_course_name
-    return COURSE_NAME || "CSC199: Example Course Name"
+    if defined? COURSE_NAME
+      return COURSE_NAME
+    else
+      return "CSC199: Example Course Name"
+    end
   end
 
   def markus_config_logout_redirect
-    return LOGOUT_REDIRECT || "DEFAULT"
+    if defined? LOGOUT_REDIRECT
+      return LOGOUT_REDIRECT
+    else
+      return "DEFAULT"
+    end
   end
 
   def markus_config_remote_user_auth
-    return REMOTE_USER_AUTH || false
+    if defined? REMOTE_USER_AUTH
+      return REMOTE_USER_AUTH
+    else
+      return false
+    end
   end
 
   #Repository for the test framework
   #Students file will be compiled, executed and tested in this repository
   def markus_config_test_framework_repository
-    return TEST_FRAMEWORK_REPOSITORY || File.join(RAILS_ROOT, "test-framework")
+    if defined? TEST_FRAMEWORK_REPOSITORY
+      return TEST_FRAMEWORK_REPOSITORY
+    else
+      return File.join(RAILS_ROOT, "test-framework")
+    end
   end
 
   ######################################
   # MarkusLogger configuration
   ######################################
   def markus_config_validate_file
-    return VALIDATE_FILE || "#{RAILS_ROOT}./config/dummy_validate.sh"
+    if defined? VALIDATE_FILE
+      return VALIDATE_FILE
+    else
+      return "#{RAILS_ROOT}./config/dummy_validate.sh"
+    end
   end
 
   def markus_config_logging_rotate_by_interval
-    return MARKUS_LOGGING_ROTATE_BY_INTERVAL || false
+    if defined? MARKUS_LOGGING_ROTATE_BY_INTERVAL
+      return MARKUS_LOGGING_ROTATE_BY_INTERVAL
+    else
+      return false
+    end
   end
   
   def markus_config_logging_size_threshold
-    return MARKUS_LOGGING_SIZE_THRESHOLD || (1024 * 10**6)
+    if defined? MARKUS_LOGGING_SIZE_THRESHOLD
+      return MARKUS_LOGGING_SIZE_THRESHOLD
+    else
+      return (1024 * 10**6)
+    end
   end
   
   def markus_config_logging_rotate_interval
-    return MARKUS_LOGGING_ROTATE_INTERVAL || 'daily'
+    if defined? MARKUS_LOGGING_ROTATE_INTERVAL
+      return MARKUS_LOGGING_ROTATE_INTERVAL
+    else
+      return 'daily'
+    end
   end
   
   def markus_config_logging_logfile
-    return MARKUS_LOGGING_LOGFILE || File.join(RAILS_ROOT, "log", "#{RAILS_ENV}_info.log")
+    if defined? MARKUS_LOGGING_LOGFILE
+      return MARKUS_LOGGING_LOGFILE
+    else
+      return File.join(RAILS_ROOT, "log", "#{RAILS_ENV}_info.log")
+    end
   end
   
   def markus_config_logging_errorlogfile
-    return MARKUS_LOGGING_ERRORLOGFILE || File.join(RAILS_ROOT, "log", "#{RAILS_ENV}_error.log")
+    if defined? MARKUS_LOGGING_ERRORLOGFILE
+      return MARKUS_LOGGING_ERRORLOGFILE
+    else
+      return File.join(RAILS_ROOT, "log", "#{RAILS_ENV}_error.log")
+    end
   end
   
   def markus_config_logging_num_oldfiles
-    return MARKUS_LOGGING_OLDFILES || 10
+    if defined? MARKUS_LOGGING_OLDFILES
+      return MARKUS_LOGGING_OLDFILES
+    else
+      return 10
+    end
   end
 
 end
