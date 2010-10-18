13569ee40b037abd1d5f17afbdea620eb6d5f616
Severin Gehwolf <Severin.Gehwolf@utoronto.ca>
2010-10-18 00:58:23 -0400

    Add language chooser to MarkUs.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,10 +1,14 @@
 require "date"
+
 # set English as default
 I18n.default_locale = 'en'
 I18n.locale = 'en' 
 
 # location where the language files go
-LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
+LOCALES_DIRECTORY = File.join(RAILS_ROOT, "config", "locales")
+
+language_files = Dir.glob(File.join(LOCALES_DIRECTORY, "*.yml" ))
+AVAILABLE_LANGS = language_files.collect{ |file| File.basename(file).chomp(".yml") }
 
 # Languages available in MarkUs
 # Change the value of I18n.default_locale
