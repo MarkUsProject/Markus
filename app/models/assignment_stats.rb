bdc7c48fc7f344e36d7fce0843eae1647f915096
Horatiu Halmaghi <hhalmaghi@gmail.com>
2010-11-23 23:59:40 -0800

    - changed the refresh function to refresh_grade_distribution - removed unnecessary fixture

--- app/models/assignment_stats.rb
+++ app/models/assignment_stats.rb
@@ -4,7 +4,7 @@ class AssignmentStats < ActiveRecord::Base
   belongs_to  :assignment
 
   # Update the cached grade distribution
-  def refresh
+  def refresh_grade_distribution
     self.grade_distribution_percentage = self.assignment.grade_distribution_as_percentage.to_csv
     self.save
   end
