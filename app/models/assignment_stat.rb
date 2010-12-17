2d3ab1764faf2c5c5611b1d47fa0a5c08b4633df
Horatiu Halmaghi <hhalmaghi@gmail.com>
2010-12-16 20:45:08 -0800

    - Integrated Bluff graphs into the dashboard

--- app/models/assignment_stat.rb
+++ app/models/assignment_stat.rb
@@ -8,4 +8,10 @@ class AssignmentStat < ActiveRecord::Base
     self.grade_distribution_percentage = self.assignment.grade_distribution_as_percentage.to_csv
     self.save
   end
+
+  # Returns an array containing the grade distribution as percentage
+  # Used by Bluff to draw the graphs
+  def grade_distribution_array
+    return self.grade_distribution_percentage.parse_csv.map{ |x| x.to_i }.to_json
+  end
 end
