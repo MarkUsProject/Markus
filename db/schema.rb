77ba8e3a6cea024cfa06aa7fba810dd0be0be4b9
Benjamin Vialle <benjaminvialle@gmail.com>
2010-12-21 13:02:00 +0100

    Updated schema.rb for Rails3

--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,11 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
@@ -57,7 +58,7 @@ ActiveRecord::Schema.define(:version => 20101116004008) do
 
   create_table "assignment_stats", :force => true do |t|
     t.integer "assignment_id"
-    t.text    "grade_distribution_percentage", :default => "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n"
+    t.text    "grade_distribution_percentage", :default => "'0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\n'"
   end
 
   create_table "assignments", :force => true do |t|
