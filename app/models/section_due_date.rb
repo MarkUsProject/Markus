5d3f6c56a5162ba10ada15faf1e4fbb71dba2c02
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-01-09 16:04:27 +0000

    A few updates with Severin's remark

--- app/models/section_due_date.rb
+++ app/models/section_due_date.rb
@@ -5,10 +5,10 @@ class SectionDueDate < ActiveRecord::Base
 
   # returns the dute date for a section and an assignment
   def self.due_date?(section, assignment)
-     d = SectionDueDate.find_by_assignment_id_and_section_id(assignment.id,
-     section.id);
-     if assignment.section_due_dates_type && !d.nil? && !d.due_date.nil?
-       return d.due_date
+    d = SectionDueDate.find_by_assignment_id_and_section_id(assignment.id,
+    section.id);
+    if assignment.section_due_dates_type && !d.nil? && !d.due_date.nil?
+      return d.due_date
     else
       return assignment.due_date
     end
