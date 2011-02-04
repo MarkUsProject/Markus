bd97ac909fc2fe1f45d711432f427c527a351688
Benjamin Vialle <benjaminvialle@gmail.com>
2011-02-04 19:13:30 +0100

    Changed before_validation_on_create(arguments) to before_validation(arguments, :on => :create)

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,7 +5,7 @@ require 'fileutils' # FileUtils used here
 # Use Assignment.submission_by(user) to retrieve the correct submission.
 class Submission < ActiveRecord::Base
   after_create :create_result
-  before_validation_on_create :bump_old_submissions
+  before_validation(:bump_old_submissions, :on => :create)
 
   validates_numericality_of :submission_version, :only_integer => true
   belongs_to :grouping
@@ -16,7 +16,7 @@ class Submission < ActiveRecord::Base
   belongs_to :remark_result, :class_name => "Result"
 
   validates_associated :remark_result
-  
+
   def self.create_by_timestamp(grouping, timestamp)
      if !timestamp.kind_of? Time
        raise "Expected a timestamp of type Time"
@@ -27,7 +27,7 @@ class Submission < ActiveRecord::Base
      repo.close
      return submission
   end
-  
+
   def self.create_by_revision_number(grouping, revision_number)
     repo = grouping.group.repo
     revision = repo.get_revision(revision_number)
@@ -35,7 +35,7 @@ class Submission < ActiveRecord::Base
     repo.close
     return submission
   end
-  
+
   def self.generate_new_submission(grouping, revision)
      new_submission = Submission.new
      new_submission.grouping = grouping
@@ -43,7 +43,7 @@ class Submission < ActiveRecord::Base
      new_submission.submission_version_used = true
      new_submission.revision_timestamp = revision.timestamp
      new_submission.revision_number = revision.revision_number
-     
+
      new_submission.transaction do
        begin
          new_submission.populate_with_submission_files(revision)
@@ -54,20 +54,20 @@ class Submission < ActiveRecord::Base
      end
      return new_submission
   end
-  
+
   # For group submissions, actions here must only be accessible to members
   # that has inviter or accepted status. This check is done when fetching 
   # the user or group submission from an assignment (see controller).
-  
+
   # Handles file submissions. Late submissions have a status of "late"
   def submit(user, file, submission_time, sdir=SUBMISSIONS_PATH)
     filename = file.original_filename
-    
+
     # create a backup if file already exists
     dir = submit_dir(sdir)
     filepath = File.join(dir, filename)
     create_backup(filename, sdir) if File.exists?(filepath)
-    
+
     # create a submission_file record
     submission_file = submission_files.create do |f|
       f.user = user
@@ -75,12 +75,12 @@ class Submission < ActiveRecord::Base
       f.submitted_at = submission_time
       f.submission_file_status = "late" if assignment.due_date < submission_time
     end
-    
+
     # upload file contents to file system
     File.open(filepath, "wb") { |f| f.write(file.read) } if submission_file.save
     return submission_file
   end
-  
+
   # Delete all records of filename in submissions and store in backup folder
   # (for now, called "BACKUP")
   def remove_file(filename)
@@ -88,18 +88,18 @@ class Submission < ActiveRecord::Base
     files = submission_files.all(:conditions => ["filename = ?", filename])
     return unless files && !files.empty?
     files.each { |f| f.destroy }  # destroy all records first
-    
+
     _adir = submit_dir
     backup_dir = File.join(_adir, "BACKUP")
     FileUtils.mkdir_p(backup_dir)
-    
+
     source_file = File.join(_adir, filename)
     dest_file = File.join(backup_dir, filename)
     FileUtils.mv(source_file, dest_file, :force => true)
   end
-  
-  
-  # Query functions -------------------------------------------------------  
+
+
+  # Query functions -------------------------------------------------------
   # Figure out which assignment this submission is for
   def assignment
     return self.grouping.assignment
@@ -110,13 +110,13 @@ class Submission < ActiveRecord::Base
   end
 
   # Helper methods
-  def populate_with_submission_files(revision, path="/") 
+  def populate_with_submission_files(revision, path="/")
     # Remember that assignments have folders within repositories - these
     # will be "spoofed" as root...
     if path == '/'
       path = assignment.repository_folder
     end
-    
+
     # First, go through directories...
     directories = revision.directories_at_path(path)
     directories.each do |directory_name, directory|
@@ -147,9 +147,9 @@ class Submission < ActiveRecord::Base
       return nil
     end
   end
-  
+
   private
-  
+
   def create_result
     result = Result.new
     self.result = result
