6edc349108a7fc6cfa506b73b4866b2194f729b9
Bertan Guven <bertanguven@gmail.com>
2011-02-05 14:42:22 -0500

    Added time stamp functionality.Also modified the *.yml files to reflect the appropiate language. Deleted server_time view file.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -115,11 +115,7 @@ class SubmissionsController < ApplicationController
   end
  
   def file_manager
-    # Figure out the current time of the server Markus is residing on and
-    # and display it to the user.
-    @current_time = Time.now.ctime 
     @assignment = Assignment.find(params[:id])
-
     @grouping = current_user.accepted_grouping_for(@assignment.id)
 
     if @grouping.nil?
