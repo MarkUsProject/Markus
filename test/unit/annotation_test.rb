081b1ae0688153937e48dee0978388b59625cedd
Benjamin Vialle <benjaminvialle@gmail.com>
2011-02-07 10:40:50 +0100

    Closes #243 We migrate test files to Machinist and Shoulda

--- test/unit/annotation_test.rb
+++ test/unit/annotation_test.rb
@@ -1,76 +1,31 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require File.join(File.dirname(__FILE__),'/../blueprints/blueprints')
+require File.join(File.dirname(__FILE__), '..', 'blueprints', 'helper')
+require 'shoulda'
 
 class AnnotationTest < ActiveSupport::TestCase
-  fixtures :all
-  # Test that Annotation without line_start are not valid
-  def test_no_line_start
-    text_no_line_start = create_text_no_attr(:line_start);
-    assert !text_no_line_start.valid?
-  end
 
-  # Test that Annotation without line_end are not valid
-  def test_no_line_end
-    text_no_line_start = create_text_no_attr(:line_end);
-    assert !text_no_line_start.valid?
-  end
+  context "A good Annotation model" do
 
-  #Description Id must be an integer greater or equal to 0
-  def test_annotation_text_id_range
-    annotation_text_id_range = create_text_no_attr(nil)
+    should belong_to :submission_file
+    should belong_to :annotation_text
 
-    bad = %w{ 'string', -0.1, -1, 0}
+    should validate_presence_of :submission_file
+    should validate_presence_of :annotation_text
+    should validate_presence_of :annotation_number
 
-    bad.each do |id|
+    should validate_numericality_of :annotation_text_id
+    should validate_numericality_of :submission_file_id
+    should validate_numericality_of :annotation_number
 
-        annotation_text_id_range.annotation_text_id = id
-        assert !annotation_text_id_range.valid?
+    should_not allow_value(-rand(100)-1).for(:annotation_text_id)
+    should_not allow_value(-rand(100)-1).for(:submission_file_id)
+    should_not allow_value(-rand(100)-1).for(:annotation_number)
 
-    end
+    should allow_value("ImageAnnotation").for(:type)
+    should allow_value("TextAnnotation").for(:type)
+    should_not allow_value("OtherAnnotation").for(:type)
 
   end
 
-  #Description Id must be an integer greater or equal to 0
-  def test_submissionfile_id_range
-    submissionfile_id_range = create_text_no_attr(nil)
-
-    bad = %w{ 'string', -0.1, -1, 0}
-
-    bad.each do |id|
-
-        submissionfile_id_range.submission_file_id = id
-        assert !submissionfile_id_range.valid?
-
-    end
-
-  end
-
-
-  #Test that Annotation assigned to non-existant Description is not valid
-  def test_annotation_text_id_dne
-    annotation_text_id_dne = create_text_no_attr(nil)
-    annotation_text_id_dne.annotation_text = AnnotationText.new
-    assert !annotation_text_id_dne.save
-  end
-
-  #Test that Annotation assigned to non-existant File is not valid
-
-  def test_submission_file_id_dne
-    submission_file_id_dne = create_text_no_attr(nil)
-    submission_file_id_dne.submission_file = SubmissionFile.new
-    assert !submission_file_id_dne.save
-  end
-
-
-  # Helper method for test_validate_presence_of to create a annotation without
-  # the specified attribute. if attr == nil then all attributes are included
-  def create_text_no_attr(attr)
-    new_annotation = {
-      :line_start => 1,
-      :line_end => 10,
-      :annotation_text_id => 1,
-      :submission_file_id => 1
-    }
-    new_annotation.delete(attr) if attr
-    TextAnnotation.new(new_annotation)
-  end
 end
