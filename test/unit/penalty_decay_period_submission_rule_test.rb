70928654987fb7d7ffeafcb825617dcd94df6027
KarelKahula <umkahula@cc.umanitoba.ca>
2011-03-04 15:12:31 -0600

    finally implemented the decay period submission rule. Tests are slowly forming something representable.

---
+++ test/unit/penalty_decay_period_submission_rule_test.rb
@@ -0,0 +1,23 @@
+#require File.dirname(__FILE__) + '/../test_helper'
+#require 'shoulda'
+#require 'time-warp'
+
+#class PenaltyDecayPeriodSubmissionRuleTest < ActiveSupport::TestCase
+#
+#  fixtures :all
+#  context "Marks decay with a penalty deducted every interval over a period of time."
+#    setup do
+#      setup_group_fixture_repos
+#      @grou = groups(:group_1)
+#      @grouping = @grouping.assignment
+#      penalty_decay_period_submission_rule = PenaltyDecayPeriodSubmissionRule.new
+#      @assignment.replace_submission_rule(penalty_decay_period_submission_rule)
+#      penalty_decay_period_submission_rule.save
+      
+      # On July 1 at 1PM, the instructor sets up the course...
+#    end
+    
+#    teardown do
+#      destory_repos
+#    end
+#end
