72846c4fe92c412cfd4af1d6d1381ccecf2422c0
Danesh Dadachanji <g9dadach@cdf.toronto.edu>
2011-05-17 20:20:43 -0400

    Revamped update_marking_state to use validation in the result model, then showing an error depending on the result.save passing.

--- app/models/result.rb
+++ app/models/result.rb
@@ -11,17 +11,25 @@ class Result < ActiveRecord::Base
   has_many :extra_marks
   has_one :remarked_submission, :foreign_key => :remark_result_id
 
+
+  validate :check_criterion_filled
   validates_presence_of :marking_state
   validates_inclusion_of :marking_state, :in => [Result::MARKING_STATES[:complete],
     Result::MARKING_STATES[:partial],   Result::MARKING_STATES[:unmarked]]
   validates_numericality_of :total_mark, :greater_than_or_equal_to => 0
   before_update  :unrelease_partial_results
 
+  def check_criterion_filled
+    # Check that the marking state is not no mark is nil or
+    return @result.nil? ||
+    @result.marking_state != Result::MARKING_STATES[:complete] ||
+        !@result.marks.find_by_mark(nil)
+  end
   # calculate the total mark for this assignment
   def update_total_mark
     total = get_subtotal + get_total_extra_points
     # added_percentage
-    percentage = get_total_extra_percentage   
+    percentage = get_total_extra_percentage
     total = total + (percentage * submission.assignment.total_mark / 100)
     self.total_mark = total
     self.save
@@ -40,34 +48,34 @@ class Result < ActiveRecord::Base
   def get_positive_extra_points
     return extra_marks.positive.points.sum('extra_mark')
   end
-  
+
   # Returns the sum of all the negative bonus marks
   def get_negative_extra_points
     return extra_marks.negative.points.sum('extra_mark')
   end
-  
+
   def get_total_extra_points
     return 0.0 if extra_marks.empty?
     return get_positive_extra_points + get_negative_extra_points
   end
-  
+
   def get_positive_extra_percentage
     return extra_marks.positive.percentage.sum('extra_mark')
   end
-  
+
   def get_negative_extra_percentage
     return extra_marks.negative.percentage.sum('extra_mark')
   end
 
   def get_total_extra_percentage
-    return 0.0 if extra_marks.empty?    
+    return 0.0 if extra_marks.empty?
     return get_positive_extra_percentage + get_negative_extra_percentage
   end
-  
+
   def get_total_extra_percentage_as_points
     return (get_total_extra_percentage * submission.assignment.total_mark / 100)
   end
-  
+
   # un-releases the result
   def unrelease_results
     self.released_to_students = false
@@ -79,7 +87,7 @@ class Result < ActiveRecord::Base
     self.marking_state = Result::MARKING_STATES[:partial]
     self.save
   end
-  
+
   private
   # If this record is marked as "partial", ensure that its
   # "released_to_students" value is set to false.
@@ -89,6 +97,6 @@ class Result < ActiveRecord::Base
     end
     return true
   end
-  
-  
+
+
 end
