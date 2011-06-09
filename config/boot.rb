edcf54ddce0ccad85e6fcdd5e5f9cb2a0d8d3f38
Benjamin Vialle <benjaminvialle@gmail.com>
2011-06-09 11:57:27 +0200

    Added YAML engine to avoid crash at startup

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,7 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path(File.join('..', '..', 'Gemfile'), __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Required to avoid Psych::SyntaxError
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
