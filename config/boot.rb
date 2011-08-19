93ef5d428472fc99583ea250395dcd51a66e3ebb
Benjamin Vialle <benjaminvialle@gmail.com>
2011-08-19 07:26:11 +0200

    Added Ruby_version test before changing settings

--- config/boot.rb
+++ config/boot.rb
@@ -6,5 +6,8 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path(File.join('..', '..', 'Gemfile'), __F
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Required to avoid Psych::SyntaxError
-require 'yaml'
-YAML::ENGINE.yamler= 'syck'
+# on Ruby 1.9
+if RUBY_VERSION > "1.9"
+  require 'yaml'
+  YAML::ENGINE.yamler= 'syck'
+end
