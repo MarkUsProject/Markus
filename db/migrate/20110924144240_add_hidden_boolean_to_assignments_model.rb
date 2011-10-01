e44671e8034d53d08241ebc52f8289b962cfe7e7
Severin Gehwolf <Severin.Gehwolf@utoronto.ca>
2011-10-01 15:27:56 -0400

    Fix french translations and migration boolean.
    This commit together with the previous 3 closes #495.

--- db/migrate/20110924144240_add_hidden_boolean_to_assignments_model.rb
+++ db/migrate/20110924144240_add_hidden_boolean_to_assignments_model.rb
@@ -2,7 +2,7 @@ class AddHiddenBooleanToAssignmentsModel < ActiveRecord::Migration
   def self.up
     # Boolean flag in order to be able to hide assignments from students.
     # Default to visible assignments
-    add_column :assignments, :is_hidden, :boolean, :default => 0
+    add_column :assignments, :is_hidden, :boolean, :default => false
   end
 
   def self.down
