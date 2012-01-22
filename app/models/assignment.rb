c2bb67b7bf4035c0ff31e92cac153c8ae30b6c39
Aaron Lee <aaron.lee@alumni.ubc.ca>
2012-01-21 17:05:25 -0800

    Modified all occurrences that used the old field name: instructor_form_groups to invalid_override

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -206,7 +206,7 @@ class Assignment < ActiveRecord::Base
 
   # Return true if this is a group assignment; false otherwise
   def group_assignment?
-    instructor_form_groups || group_min != 1 || group_max > 1
+    invalid_override || group_min != 1 || group_max > 1
   end
 
   # Returns the group by the user for this assignment. If pending=true,
