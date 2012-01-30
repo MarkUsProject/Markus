4b913662afefec5b95beec53327aadd251258de0
Christine Yu <christine.tc.yu@gmail.com>
2012-01-29 21:36:07 -0800

    Closes #695. Fix for cookie detection.
    Need to set a parameter and redirect page in order to test cookies and
    distinguish between an expired session.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -50,6 +50,11 @@ class MainController < ApplicationController
     if !cookies_enabled
       flash[:login_notice] = I18n.t(:cookies_off)
       return
+    else
+      if !params[:cookieTest].nil?
+      # remove the :cookieTest => "currentlyTesting" parameter after testing for cookies by redirecting
+      redirect_to :controller => "main", :action => "login"
+      end
     end
 
     @current_user = current_user
