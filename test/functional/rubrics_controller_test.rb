201353e9e3733525827394784ed53624808b80f6
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2012-02-14 08:43:54 +0100

    closes #257 - closes #660 - restructured completely rubrics criteria controller test.
    - deleted redundant tests
    - reorganised them
    - properly used contexts and should - we have a speed up of 300% on this file
    
    They know are of the form:
    
      + rubrics_controller_test.rb
        |
        +--- An unauthenticated and unauthorized user
        |    |
        |    +--- with an assignment
        |         |   - should be redirected on index
        |         |   - should be redirected on new
        |         |   - should be redirected on :download_csv
        |         |   - should be redirected on :download_yml
        |         |   - should be redirected on :csv_upload
        |         |   - should be redirected on :update_positions
        |         |   - should be redirected on :move_criterion
        |         |
        |         +--- and a submission
        |              |   - should be redirect on edit
        |              |   - should be redirected on update
        |              |   - should be redirect on delete
        |
        +--- An admin, with an assignment
             |   - should upload successfully properly formatted csv file
             |   - should deal properly with ill formatted CSV files
             |   - should upload successfully well formatted yml criteria
             |   - should deal with bad weight on yaml file
             |   - should deal properly with yml syntax error
             |   - should deal properly with empty yml file
             |
             +--- with a criterion
                  |   - should see index
                  |   - should be able to get on :edit
                  |   - should be able to save with errors
                  |   - should be able to  save without errors
                  |   - should be able to get the form for new rubric
                  |   - should be able to save with error
                  |   - should save without error on an assignment as the first
    criterion
                  |   - should save without errors
                  |   - should delete criterion
                  |   - should download rubrics as CSV
                  |   - should upload yml file without deleting preexisting
    criteria
                  |
                  +--- with another criterion
                       |   - should be able to update_positions
                       |   - should be able to move_criterion up
                       |   - should be able to move_criterion down
                       |
                       +--- And yet another
                            |   - should be able to move up top criteria
                            |   - should be able to move down top criteria

--- test/functional/rubrics_controller_test.rb
+++ test/functional/rubrics_controller_test.rb
@@ -97,6 +97,7 @@ Correctness,2.0,Horrible,Poor,Satisfactory,Good,Excellent,,,,,\n"
       @admin = Admin.make
       @assignment = Assignment.make
 
+
     end
 
     should "upload successfully properly formatted csv file" do
@@ -142,39 +143,6 @@ Correctness,2.0,Horrible,Poor,Satisfactory,Good,Excellent,,,,,\n"
       assert_response :redirect
     end
 
-    should "have valid values in database after an upload of a UTF-8 encoded file parsed as UTF-8" do
-      post_as @admin,
-              :csv_upload,
-              :assignment_id => @assignment.id,
-              :csv_upload => {:rubric => fixture_file_upload('../files/test_rubric_criteria_UTF-8.csv')},
-              :encoding => "UTF-8"
-      assert_response :redirect
-      test_criterion = RubricCriterion.find_by_assignment_id_and_rubric_criterion_name(@assignment.id, "RubricCriteriaÈrÉØrr")
-      assert_not_nil test_criterion # rubric criterion should exist
-    end
-
-    should "have valid values in database after an upload of a ISO-8859-1 encoded file parsed as ISO-8859-1" do
-      post_as @admin,
-              :csv_upload,
-              :assignment_id => @assignment.id,
-              :csv_upload => {:rubric => fixture_file_upload('../files/test_rubric_criteria_ISO-8859-1.csv')},
-              :encoding => "ISO-8859-1"
-      assert_response :redirect
-      test_criterion = RubricCriterion.find_by_assignment_id_and_rubric_criterion_name(@assignment.id, "RubricCriteriaÈrÉØrr")
-      assert_not_nil test_criterion # rubric criterion should exist
-    end
-
-    should "have valid values in database after an upload of a UTF-8 encoded file parsed as ISO-8859-1" do
-      post_as @admin,
-              :csv_upload,
-              :assignment_id => @assignment.id,
-              :csv_upload => {:rubric => fixture_file_upload('../files/test_rubric_criteria_UTF-8.csv')},
-              :encoding => "ISO-8859-1"
-      assert_response :redirect
-      test_criterion = RubricCriterion.find_by_assignment_id_and_rubric_criterion_name(@assignment.id, "RubricCriteriaÈrÉØrr")
-      assert_nil test_criterion # rubric criterion should not exist, despite being in file
-    end
-
     should "upload successfully well formatted yml criteria" do
       post_as @admin,
               :yml_upload,
@@ -206,6 +174,7 @@ Correctness,2.0,Horrible,Poor,Satisfactory,Good,Excellent,,,,,\n"
               :yml_upload,
               :assignment_id => @assignment.id,
               :yml_upload => {:rubric =>
+
                 "cr1:\n  weight: monstrously heavy\n"}
 
       assert_response  :redirect
