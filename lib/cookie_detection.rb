b740b6def8dd64e9f291f88b4e580eb512fc8ce1
Christine Yu <christine.tc.yu@gmail.com>
2012-03-05 15:57:03 -0800

    attempt at fixing cookie detection

--- lib/cookie_detection.rb
+++ lib/cookie_detection.rb
@@ -5,13 +5,32 @@ module CookieDetection
   protected
 
   # true if cookies are enabled, false otherwise.
-  def cookies_enabled
-    return true unless cookies["cookieTest"].blank?
-    cookies["cookieTest"] = Time.now
-    session[:return_to] = request.fullpath
-    if cookies["cookie_test"].blank?
-      return false
-    end
-    return true
+  def cookies_enabled(*wasEmpty)
+    debugger
+    if cookies[:cookieTest].blank?
+      if wasEmpty.empty?
+        cookies[:cookieTest] = Time.now
+        cookies_enabled("testing")
+      else
+        return false
+      end
+   else
+     return true
+   end 
+   #   return true
+   # else  
+   #   cookies_enabled(true)
+   # end if
+   # if(wasEmpty[0])
+   #   return false
+   # else
+   #   return true
+   # end if
+   # cookies[:cookieTest] = Time.now
+   # session[:return_to] = request.fullpath
+   # if cookies[:cookieTest].blank?
+   #   return false
+   # end
+   # return true
   end
 end
