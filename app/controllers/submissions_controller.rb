884d073e0204f8c27cbf3931cdd1fb95e59d0230
Kristian Lejao <kristianlejao@gmail.com>
2012-06-19 13:24:16 -0400

    testing use of cookies

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -88,9 +88,6 @@ class SubmissionsController < ApplicationController
     }
   }
   
-  if (defined? params[:per_page])
-   cookies[:kristian] = 15
- 
   def repo_browser
     @assignment = Assignment.find(params[:assignment_id])
     @grouping = Grouping.find(params[:id])
