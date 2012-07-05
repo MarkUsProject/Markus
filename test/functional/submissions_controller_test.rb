369c2b7d727fb986843ad669022da3ae9c8a1b88
Kristian Lejao <kristianlejao@gmail.com>
2012-07-04 21:33:45 -0400

    redirected assert

--- test/functional/submissions_controller_test.rb
+++ test/functional/submissions_controller_test.rb
@@ -342,7 +342,7 @@ class SubmissionsControllerTest < AuthenticatedControllerTest
           @submission_collector.expects(:push_groupings_to_queue).once
           post_as @grader, :browse, :assignment_id => 1, :id => 1, :per_page => 30 
 
-          assert_equal params[:per_page], 30 
+          assert_response :redirect 
          end 
       end 
     end
