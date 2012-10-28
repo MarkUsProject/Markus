571275f63157b8557578eb1cff228e11a3bdcc13
Mina Almasry <almasry.mina@hotmail.com>
2012-10-28 18:57:14 -0400

    add presence (not Null) constraints

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -1,7 +1,16 @@
 class TestResult < ActiveRecord::Base
   belongs_to :submission
+
   validates_presence_of :submission # we require an associated submission
   validates_associated :submission # submission need to be valid
+  validate :completion_status, :presence => true
+  validates :assignment, :presence => true
+  validates :test_script, :presence => true
+  validates :marks_earned, :presence => true
+  validates :input_description, :presence => true
+  validates :actual_output, :presence => true
+  validates :expected_output, :presence => true
+
 
   #=== Description
   # Updates the file_content attribute of an TestResult object
--- app/models/test_script.rb
+++ app/models/test_script.rb
@@ -1,5 +1,15 @@
 class TestScript < ActiveRecord::Base
   validates :seq_num, :presence => true
   validates :script_name, :presence => true
-
+  validates :max_marks, :presence => true
+  validates :run_on_submission, :presence => true
+  validates :run_on_request, :presence => true
+  validates :uses_token, :presence => true
+  validates :halts_testing, :presence => true
+  validates :display_description, :presence => true
+  validates :display_run_status, :presence => true
+  validates :display_input, :presence => true
+  validates :display_marks_earned, :presence => true
+  validates :display_expected_output, :presence => true
+  validates :display_actual_output, :presence => true
 end
