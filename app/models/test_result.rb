bdc6fa97bda143bb3b689c94f5ea7569f272e6f0
Brian Bo <ybo@uwaterloo.ca>
2012-10-29 16:44:46 -0400

    Added model documentation and model association

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -1,6 +1,29 @@
+##############################################################
+# This is the model for the database table test_results,
+# which each instance of this model represents the test result
+# of a test script. It contains all the information of a test
+# run, but not all the information is shown to the student.
+# (Configurable for each test script) Also, the admin decides
+# whether or not and when to show the result to the student.
+#
+# The columns of test_support_files are:
+#   submission_id:      id of the submission
+#   test_script_id:     id of the corresponding test script
+#   completion_status:  one of {pass, fail, error}
+#   marks_earned:       number of points earned for this test
+#                       run. A non-negative integer.
+#   input_description:  a string describing the input. It can
+#                       be empty, the input data, or just
+#                       a description.
+#   actual_output:      actual output from running the test
+#   expected_output:    expected output from running the test  
+#############################################################
+
 class TestResult < ActiveRecord::Base
   belongs_to :submission
+  has_one :test_script
 
   validates_presence_of :submission # we require an associated submission
   validates_associated :submission # submission need to be valid
 end
+
