f01975c18b1e92829312ab760694597fab7f161a
Mina Almasry <almasry.mina@hotmail.com>
2012-10-29 16:05:16 -0400

    added null false constaints in the database

--- app/models/test_result.rb
+++ app/models/test_result.rb
@@ -3,22 +3,4 @@ class TestResult < ActiveRecord::Base
 
   validates_presence_of :submission # we require an associated submission
   validates_associated :submission # submission need to be valid
-  validates :completion_status, :presence => true
-  validates :assignment, :presence => true
-  validates :test_script, :presence => true
-  validates :marks_earned, :presence => true
-  validates :input_description, :presence => true
-  validates :actual_output, :presence => true
-  validates :expected_output, :presence => true
-
-
-  #=== Description
-  # Updates the file_content attribute of an TestResult object
-  #=== Returns
-  # True if saving with the new content succeeds, false otherwise
-  def update_file_content(new_content)
-    return false if new_content.nil?
-    self.file_content = new_content
-    return self.save
-  end
 end
