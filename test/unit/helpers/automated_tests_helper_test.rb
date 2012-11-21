6db467b681c7768833c7ff8dca1c46c03ea32001
Brian Bo <ybo@uwaterloo.ca>
2012-11-21 09:36:18 -0500

    Modified some test messages in automated test helper's tests

--- test/unit/helpers/automated_tests_helper_test.rb
+++ test/unit/helpers/automated_tests_helper_test.rb
@@ -61,24 +61,24 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       
     end
     
-    should "be able to return all scripts when test is requested at collection" do
+    should "be able to return all test scripts when automated test is requested at collection" do
       @call_on = "collection"
       scripts = scripts_to_run(@asst, @call_on)
       assert_equal scripts.size, 3
-      assert_equal scripts[0], @scriptfile2, "The scripts should be sorted in seq_num order"
-      assert_equal scripts[1], @scriptfile, "The scripts should be sorted in seq_num order"
-      assert_equal scripts[2], @scriptfile3, "The scripts should be sorted in seq_num order"
+      assert_equal scripts[0], @scriptfile2, "The test scripts should be sorted in seq_num order"
+      assert_equal scripts[1], @scriptfile, "The test scripts should be sorted in seq_num order"
+      assert_equal scripts[2], @scriptfile3, "The test scripts should be sorted in seq_num order"
     end
     
-    should "be able to return a list of scripts to run when test is requested" do
+    should "be able to return a list of test scripts to run when automated test is requested" do
       @call_on = "request"
       scripts = scripts_to_run(@asst, @call_on)
       assert_equal scripts.size, 2
-      assert_equal scripts[0], @scriptfile2, "The scripts should be sorted in seq_num order"
-      assert_equal scripts[1], @scriptfile, "The scripts should be sorted in seq_num order"
+      assert_equal scripts[0], @scriptfile2, "The test scripts should be sorted in seq_num order"
+      assert_equal scripts[1], @scriptfile, "The test scripts should be sorted in seq_num order"
     end
     
-    should "be able to return a list of scripts to run when submission received" do
+    should "be able to return a list of test scripts to run when submission received" do
       @call_on = "submission"
       scripts = scripts_to_run(@asst, @call_on)
       assert_equal scripts.size, 1
@@ -108,7 +108,7 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       @admin = Admin.make
       @current_user = @admin
     end
-    should "be allowed to do test" do
+    should "be allowed to do automated test" do
       assert has_permission?
     end
   end
@@ -118,7 +118,7 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       @ta = Ta.make
       @current_user = @ta
     end
-    should "be allowed to do test" do
+    should "be allowed to do automated test" do
       assert has_permission?
     end
   end
@@ -134,7 +134,7 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       @grouping.add_member(@student)
       @current_user = @student
     end
-    should "be allowed to do test" do
+    should "be allowed to do automated test" do
       assert has_permission?
     end
   end
@@ -150,12 +150,12 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       @grouping.add_member(@student)
       @current_user = @student
     end
-    should "not be allowed to do test" do
+    should "not be allowed to do automated test" do
       assert !has_permission?
     end
   end
 
-  context "A student allowed to do test but without a token object" do
+  context "A student allowed to do automated test but without a token object" do
     setup do
       @student = Student.make
       @token = Token.make
@@ -166,7 +166,7 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       @grouping.add_member(@student)
       @current_user = @student
     end
-    should "not be allowed to do test (no tokens are found for this student)" do
+    should "not be allowed to do automated test (no tokens are found for this student)" do
       assert_raise(RuntimeError) do
         has_permission? # raises exception
       end
@@ -182,7 +182,7 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       @token.save
       @current_user = @student
     end
-    should "not be allowed to run tests on a group they do not belong to" do
+    should "not be allowed to run automated tests on a group they do not belong to" do
       assert !has_permission?
     end
   end
@@ -197,17 +197,17 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
       @test_dir = File.join(MarkusConfigurator.markus_config_automated_tests_repository, @assignment.short_identifier)
     end
     
-    should "not be allowed to run tests if no script file is available" do
+    should "not be allowed to run automated tests if no test script file is available" do
       delete_test_repo(@test_dir)
       assert !files_available?
     end
     
-    should "not be allowed to run tests if no source file is available" do
+    should "not be allowed to run automated tests if no source file is available" do
       delete_test_repo(@repo_dir)
       assert !files_available?
     end
     
-    should "be able to run tests if script files and source files are presented" do
+    should "be able to run automated tests if test script files and source files are presented" do
       @aScript = TestScript.make
       @aScript.assignment_id = @assignment.id
       @aScript.save
@@ -218,24 +218,24 @@ class AutomatedTestsHelperTest < ActiveSupport::TestCase
   end
   
   context "MarkUs" do
-    should "return 0, the index of the first available server." do
+    should "return 0, the index of the first available test server." do
       # @last_server is not yet defined here
       assert_equal choose_test_server, 0
     end
     
-    should "return the index of an available server, if there is at least one available" do
+    should "return the index of an available test server, if there is at least one available" do
       @last_server = 0
       server_id = choose_test_server
       assert server_id >= 0
       assert server_id < MarkusConfigurator.markus_ate_num_test_servers
     end
     
-    should "return 0, if last_server is the last server in the list" do
+    should "return 0, if last_server is the last test server in the list" do
       @last_server = MarkusConfigurator.markus_ate_num_test_servers - 1
       assert_equal choose_test_server, 0
     end
     
-    should "return -1 if there is no available server" do
+    should "return -1 if there is no available test server" do
       #TODO: this fails because no implementation checking the max num of tests running on a server yet
       assert_equal choose_test_server, -1
     end
