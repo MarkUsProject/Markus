bfec0ffb7e466d5018d56cdbdb295f954b558028
Brian Bo <ybo@uwaterloo.ca>
2012-12-18 10:12:03 -0500

    Updated locales

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -26,4 +26,9 @@ module ResultsHelper
     return (!student_can_edit_remark_request(submission))
   end
 
+  # ATE_SIMPLE_UI: this is temporary
+  def test_result_available(submission)
+    test_results = TestResult.find_all_by_submission_id(submission.id)
+    return !test_results.empty?
+  end
 end
