e21f36966a5277d02b4cb2ea32f2bf8d45ac5ef2
Marc Bodmer <marc.bodmer@gmail.com>
2013-03-01 21:17:33 -0500

    Make students controller return JSON and add helper function to return table formatted JSON

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,12 +17,19 @@ class StudentsController < ApplicationController
   end
 
   def populate
+    # Process:
+    # 1. AJAX call made by jQuery to populate the student table
+    # 2. Find students from the database
+    # 3. Pass in the student data into the helper function (construct_table_rows defined in UsersHelper)
+    # 4. Return a collection of JSON for the jQuery DataTable plugin to render
     @students_data = Student.find(:all,
                                   :order => "user_name",
                                   :include => [:section,
                                                :grace_period_deductions])
-    # construct_table_rows defined in UsersHelper
-    @students = construct_table_rows(@students_data)
+    @students = construct_students_table_data(@students_data)
+    respond_to do |format|
+      format.json { render :json => @students} 
+    end
   end
 
   def edit
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -31,4 +31,28 @@ module UsersHelper
     return result
   end
 
+  # Construct a nicely formatted JSON response in the form of an array for our student table
+  def construct_students_table_data(users)
+    student_list = []
+    users.each do |user|
+      result = []
+      result[0] = CGI.escapeHTML(user.user_name)
+      result[1] = CGI.escapeHTML(user.last_name)
+      result[2] = CGI.escapeHTML(user.first_name)
+      student_list.push(result)
+      @render_note_link = false
+      if user.student?
+        result[3] = user.remaining_grace_credits.to_s + '/' + user.grace_credits.to_s
+        if user.has_section?
+          result[4] = user.section.name
+        else
+          result[4] = '-'
+        end
+        @render_note_link = true
+      end
+      result[5] = user.hidden
+    end
+    return student_list
+  end
+
 end
