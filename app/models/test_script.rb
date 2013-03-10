f2d961745837170e0c404202588ae6f965ae3708
Ian Smith <umsmit32@gmail.com>
2013-03-10 15:32:00 -0500

    FIX for issue 1015 Deleting a test script should delete all of the associated results because the information about the test script is no longer around and if the test script is deleted, then we don't need the results from it.

--- app/models/test_script.rb
+++ app/models/test_script.rb
@@ -43,8 +43,8 @@
 
 class TestScript < ActiveRecord::Base
   belongs_to :assignment
-  has_many :test_results
-  has_many :test_script_results
+  has_many :test_results, :dependent => :delete_all
+  has_many :test_script_results, :dependent => :delete_all
   
   # Run sanitize_filename before saving to the database
   before_save :sanitize_filename
