732d6e9f28457839dc6336e0295b7639a6c93155
Daniel St. Jules <danielst.jules@gmail.com>
2013-06-23 18:55:43 -0400

    Add user creation to the API wrapper

--- lib/tools/api_wrapper/api_wrapper.rb
+++ lib/tools/api_wrapper/api_wrapper.rb
@@ -2,75 +2,100 @@ require 'httparty'
 require 'singleton'
 
 class MarkusRESTfulAPI
-  include HTTParty
   attr_accessor :config
 
   # Stores the api_url and auth_key for later use
   def MarkusRESTfulAPI.configure(api_url, auth_key)
-    @@api_url = api_url
     @@auth_key = auth_key
+    api_url = "#{api_url}/" if api_url[-1, 1] != '/'
+    @@api_url = api_url
   end
 
-  # A singleton that allows us to retrieve user(s)
+  # Makes a GET request to provided URL while supplying the authorization 
+  # header, and raising an exception on failure
+  def MarkusRESTfulAPI.get(url)
+    response = HTTParty.get(@@api_url + url, :headers => 
+      { 'Authorization' => "MarkUsAuth #{@@auth_key}" })
+    raise response['rsp']['__content__'] unless response.success?
+
+    response
+  end
+
+  # Makes a POST request to the provided URL, along with the supplied POST data.
+  # Also uses the authorization header, and raises an exception on failure
+  def MarkusRESTfulAPI.post(url, query)
+    options = { :headers => { 'Authorization' => "MarkUsAuth #{@@auth_key}" },
+                :body => query }
+    response = HTTParty.post(@@api_url + url, options)
+    raise response['rsp']['__content__'] unless response.success?
+
+    response
+  end    
+
+  # A singleton that allows us to retrieve user(s). It enables us to retrieve 
+  # users by user_name, id, first_name, last_name, as well as type
   class Users < MarkusRESTfulAPI
     include Singleton
 
-    def self.get_by_username(username)
-      response = self.get("#{@@api_url}/users.json?filter=user_name:#{username}",
-        :headers => {'Authorization' => "MarkUsAuth #{@@auth_key}"})
-      raise response.response unless response.success?
+    def self.get_by_user_name(user_name)
+      response = self.get("users.json?filter=user_name:#{user_name}")
       User.new(response['users']['user'])
     end
 
     def self.get_by_id(id)
-      response = self.get("#{@@api_url}/users/#{id}.json",
-        :headers => {'Authorization' => "MarkUsAuth #{@@auth_key}"})
-      raise response.response unless response.success?
+      response = self.get("users/#{id}.json")
       User.new(response['users'])
     end
 
     def self.get_all_by_first_name(first_name)
-      response = self.get("#{@@api_url}/users.json?filter=first_name:#{first_name}",
-        :headers => {'Authorization' => "MarkUsAuth #{@@auth_key}"})
-      raise response.response unless response.success?
+      response = self.get("users.json?filter=first_name:#{first_name}")
 
       users = []
       response['users']['user'].each do |user|
         users << User.new(user)
       end
+
+      users
     end
 
     def self.get_all_admins()
-      response = self.get("#{@@api_url}/users.json?filter=type:admin",
-        :headers => {'Authorization' => "MarkUsAuth #{@@auth_key}"})
-      raise response.response unless response.success?
+      response = self.get('users.json?filter=type:admin')
 
       users = []
       response['users']['user'].each do |user|
         users << User.new(user)
       end
+
+      users
     end
 
     def self.get_all_tas()
-      response = self.get("#{@@api_url}/users.json?filter=type:ta",
-        :headers => {'Authorization' => "MarkUsAuth #{@@auth_key}"})
-      raise response.response unless response.success?
+      response = self.get('users.json?filter=type:ta')
 
       users = []
       response['users']['user'].each do |user|
         users << User.new(user)
       end
+
+      users
     end
 
     def self.get_all_students()
-      response = self.get("#{@@api_url}/users.json?filter=type:student",
-        :headers => {'Authorization' => "MarkUsAuth #{@@auth_key}"})
-      raise response.response unless response.success?
+      response = self.get('users.json?filter=type:student')
 
       users = []
       response['users']['user'].each do |user|
         users << User.new(user)
       end
+
+      users
+    end
+
+    def self.create(attributes)
+      url = 'users.json'
+      response = self.post(url, attributes)
+      # Now that the user's been created, return that user
+      user = self.get_by_user_name(attributes['user_name'])
     end
   end # Users
 
--- lib/tools/api_wrapper/example.rb
+++ lib/tools/api_wrapper/example.rb
@@ -1,13 +1,50 @@
 require './api_wrapper.rb'
  
+# CONFIGURATION
+
 # Set the API Url and your auth/api key
-MarkusRESTfulAPI.configure('http://localhost:3000/api/',
+MarkusRESTfulAPI.configure('http://localhost:3000/api',
   'MzNjMDcwMDhjZjMzY2E0NjdhODM2YWRkZmFhZWVjOGE=')
 
-user = MarkusRESTfulAPI::Users.get_by_username('c5anthei')
+# USERS
+
+# Get a user by their username
+puts 'Get a user by their username'
+user = MarkusRESTfulAPI::Users.get_by_user_name('c5anthei')
 puts user.user_name
+puts "\n"
 
-user2 = MarkusRESTfulAPI::Users.get_by_id(84)
-puts user2.user_name
+# Getting a user by their id
+puts 'Get a user by their id'
+user = MarkusRESTfulAPI::Users.get_by_id(84)
+puts user.user_name
+puts "\n"
 
-puts MarkusRESTfulAPI::Users.get_all_admins()
+# Getting all admin users. Returns an array of objects of class User
+puts 'Getting all admins'
+admins = MarkusRESTfulAPI::Users.get_all_admins()
+admins.each do |admin|
+  puts "user_name: #{admin.user_name}"
+  puts "first_name: #{admin.first_name}"
+  puts "last_name: #{admin.last_name}"
+end
+puts "\n"
+
+# Example of getting a user that doesn't exist
+begin
+  user = MarkusRESTfulAPI::Users.get_by_id(84999999)
+rescue Exception => e 
+  # Outputs 'No user exists with that id'
+  puts e.message
+end
+puts "\n"
+
+# Creating a new ta with a generated user_name
+puts 'Creating a new user'
+attributes = { 'user_name' => "User#{rand(10000)}", 'first_name' => 'Dan', 
+               'last_name' => 'Test', 'type' => 'ta' }
+user = MarkusRESTfulAPI::Users.create(attributes)
+puts "user_name: #{user.user_name}"
+puts "type: #{user.type}"
+puts "first_name: #{user.first_name}"
+puts "last_name: #{user.last_name}"
