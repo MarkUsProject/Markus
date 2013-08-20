ab5a17e42eb498f5189d3456e9cace250582252f
Ghislain Gt <gt.ghislain@gmail.com>
2013-08-20 12:07:38 +0200

    fix application.js
    - moved application.js inside assets/javascripts
    - removed jquery.js, prototype.js and rails.js to use the gems.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,12 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require prototype
+//= require prototype_ujs
+//= require jquery
 
 var ModalMarkus = function (elem) {
   this.elem = elem;
