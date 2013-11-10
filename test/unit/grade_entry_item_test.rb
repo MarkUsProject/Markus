a1ccdcb78df6fc7c91f56e0593ee520c8fcc3838
ariannedee <ariannedee@gmail.com>
2013-11-09 18:37:28 -0800

    Fixed failing unit tests and added tests for updating invalid out_of and duplicate names

--- test/unit/grade_entry_item_test.rb
+++ test/unit/grade_entry_item_test.rb
@@ -9,7 +9,6 @@ class GradeEntryItemTest < ActiveSupport::TestCase
   should have_many :grades
 
   should validate_presence_of :name
-  should validate_presence_of :out_of
   should validate_presence_of :position
 
   should validate_numericality_of(
@@ -32,41 +31,54 @@ class GradeEntryItemTest < ActiveSupport::TestCase
   should_not allow_value(-1).for(:position)
   should_not allow_value(-100).for(:position)
 
-  context 'A good Grade Entry Item model' do
+  context 'A good Grade Entry Form ' do
     setup do
-      GradeEntryItem.make(:name => 'Q1', :out_of => 10, :position => 1)
+      @grade_entry_form = GradeEntryForm.make
+
     end
 
-    should validate_uniqueness_of(:name).scoped_to(
-            :grade_entry_form_id).with_message(
-                I18n.t('grade_entry_forms.invalid_name'))
-  end
+    should 'update name if duplicate' do
+      item1 = @grade_entry_form.grade_entry_items.make(:name => 'Q1', :out_of => 10, :position => 1)
+      item2 = @grade_entry_form.grade_entry_items.make(:name => 'Q1', :out_of => 10, :position => 2)
 
-  # Make sure different grade entry forms can have grade entry items
-  # with the same name
-  should 'allow same column name for different grade entry forms' do
-    grade_entry_form_1 = GradeEntryForm.make
-    grade_entry_form_2 = GradeEntryForm.make
-    column = grade_entry_form_1.grade_entry_items.make(:name => 'Q1', :out_of => 10, :position => 1)
-
-    # Re-use the column name for a different grade entry form
-    dup_column = GradeEntryItem.new
-    dup_column.name = column.name
-    dup_column.out_of = column.out_of
-    dup_column.position = column.position
-    dup_column.grade_entry_form = grade_entry_form_2
-
-    assert dup_column.valid?
-  end
-
-  context 'A GradeEntryItem' do
-    setup do
-      GradeEntryItem.make(:name => 'Q1', :out_of => 10, :position => 1)
+      assert item1.valid?
+      assert item2.valid?
+      assert_not_equal item1.name, item2.name
     end
 
-    # Need at least one GradeEntryItem created for this test to
-    # pass.
-    should validate_uniqueness_of(:name).scoped_to(:grade_entry_form_id).with_message(I18n.t('grade_entry_forms.invalid_name'))
+
+    # Make sure different grade entry forms can have grade entry items
+    # with the same name
+    should 'allow same column name as a different grade entry form' do
+      grade_entry_form_2 = GradeEntryForm.make
+      column = @grade_entry_form.grade_entry_items.make(:name => 'Q1', :out_of => 10, :position => 1)
+
+      # Re-use the column name for a different grade entry form
+      dup_column = GradeEntryItem.new
+      dup_column.name = column.name
+      dup_column.out_of = column.out_of
+      dup_column.position = column.position
+      dup_column.grade_entry_form = grade_entry_form_2
+
+      assert dup_column.valid?
+    end
   end
 
+
+  # Saving should cause invalid inputs to be updated to default
+  context 'A Grade Entry Item model with invalid inputs' do
+
+    should 'allow negative out_of values' do
+      item = GradeEntryItem.make(:name => 'Q1', :out_of => -10, :position => 1)
+      assert item.valid?
+      assert_equal 1, item.out_of
+    end
+
+    should 'allow empty out_of values' do
+      item = GradeEntryItem.make(:name => 'Q1', :out_of => nil, :position => 1)
+      assert item.valid?
+      assert_equal 1, item.out_of
+    end
+
+  end
 end
