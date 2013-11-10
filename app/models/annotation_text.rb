946e30651a346caee1aa07e46685be5b36fe381d
Benjamin Vialle <github@vialle.io>
2013-11-10 04:23:35 -0800

    Merge pull request #1209 from drunkel/issue-999
    Closes #999. Added created_by and last_edited_by fields to annotation texts.

--- app/models/annotation_text.rb
+++ app/models/annotation_text.rb
@@ -1,10 +1,20 @@
 class AnnotationText < ActiveRecord::Base
 
   belongs_to :annotation_category
+  belongs_to :user, :foreign_key => :creator_id
   # An AnnotationText has many Annotations that are destroyed when an
   # AnnotationText is destroyed.
   has_many :annotations, :dependent => :destroy
   validates_associated   :annotation_category,
                          :message => 'annotation_category associations failed'
+  
+  #Find creator, return nil if not found
+  def get_creator
+    user =  User.find_by_id(creator_id)
+  end
 
+  #Find last user to update this text, nil if not found
+  def get_last_editor
+    editor = User.find_by_id(last_editor_id)
+  end
 end
