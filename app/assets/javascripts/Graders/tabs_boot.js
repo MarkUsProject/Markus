5013b2d6a489e25ed78db62bab4b43599ec155c2
Gusht <augustin.doury@eleves.ec-nantes.fr>
2013-11-21 15:47:54 +0100

    MovetojQuery7

--- app/assets/javascripts/Graders/tabs_boot.js
+++ app/assets/javascripts/Graders/tabs_boot.js
@@ -1,16 +1,17 @@
 var groups_tab_menu = null;
 var graders_tab_menu = null;
 
-document.observe('dom:loaded', function() {
+jQuery(document).ready(function() {
   groups_tab_menu = new Control.Tabs('groups_tabs');
   groups_tab_menu.setActiveTab('groups_table');
   graders_tab_menu = new Control.Tabs('graders_tabs');
  graders_tab_menu.setActiveTab('all');
-  groups_tab_menu.observe('afterChange',function(new_container){
-    $('current_table').value = new_container.id;
+
+  groups_tab_menu.bind('change',function(new_container){
+    jQuery('#current_table').val(new_container.id);
     clear_all();
   });
-  graders_tab_menu.observe('afterChange',function(new_container){
+  graders_tab_menu.bind('change',function(new_container){
    filter(new_container.id);
   });
 });
