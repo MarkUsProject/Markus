aea3a0f47c7d1dca8efc937e99648192d2f5cdd2
selyanhb <h.benmouffok@yahoo.fr>
2013-11-30 17:25:48 +0100

    Move to jQuery

--- app/assets/javascripts/Grader/file_selector.js
+++ app/assets/javascripts/Grader/file_selector.js
@@ -1,6 +1,8 @@
+// Hamid est passé par là .......
+
 function bump_select(select_node, bump_amount, back_button, next_button) {
-  var node_num = $(select_node).descendants().length;
-  var selected_index = $(select_node).selectedIndex; //Work for IE?
+  var node_num = jQuery('#select_node').descendants().length;
+  var selected_index = jQuery('#select_node').selectedIndex; //Work for IE?
   var result = selected_index + bump_amount;
   
   if (result == node_num -1){
@@ -23,6 +25,8 @@ function bump_select(select_node, bump_amount, back_button, next_button) {
     return false;
   }
   
-  $(select_node).selectedIndex = result;
-  load_submitted_file($F(select_node));
+  jQuery('#select_node').selectedIndex = result;
+  load_submitted_file(jQuery('#select_node').val();
+
+
 }
