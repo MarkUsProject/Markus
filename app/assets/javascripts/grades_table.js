407ba1faf64c18f9b465fd9a6043f3bae51659ef
davidrd <david.das@utoronto.ca>
2014-03-17 10:04:14 -0400

    Fixed bug where user could not modify grades after moving through pages

--- app/assets/javascripts/grades_table.js
+++ app/assets/javascripts/grades_table.js
@@ -1,38 +1,74 @@
-document.observe('dom:loaded', function() {
+document.observe('dom:loaded', function () {
 
-  /**
-   * get all of the grade input fields, attach an observer that updates
-   * the grade when it is changed
-   */
-  $$('.grade-input').each(function(item) {
-    new Form.Element.EventObserver(item, function(element, value) {
+    // Bind the 'send data to db' event to all grade id boxes
+    bindEventToGradeEntry();
 
-      var url = element.readAttribute('data-action');
-      var params = {
-        'updated_grade': value,
-        'student_id': element.readAttribute('data-student-id'),
-        'grade_entry_item_id': element.readAttribute('data-grade-entry-item-id'),
-        'authenticity_token': AUTH_TOKEN
-      }
+    /*
+     Because the page has loaded and 'dom:loaded' has fired, we can't
+     rely on this event to detect changes to the subtree since when we click
+     'Next >' we replace elements on the page instead of reloading it- so
+     we need to detect this change and call bindEventToGradeEntry(); above
+    */
 
-      new Ajax.Request(url, {
-        asynchronous: true,
-        evalScripts: true,
-        parameters: params
-      });
+    // The following from https://developer.mozilla.org/en/docs/Web/API/MutationObserver
+
+    // DOM mutation observers for Firefox and Chrome respectively.
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    // select the target node, which is 'content' in our case. This is the highest level
+    // DOM with an ID that contains the grades table
+    var target = document.querySelector('#content');
+
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+        // For any mutation observed, call bindEventToGradeEntry();
+        mutations.forEach(function(mutation) {
+            bindEventToGradeEntry();
+        });
     });
-  });
+
+    // configuration of the observer: we want to detect changes to the subtree (when the
+    // grade elements are replaced)
+    var config = { childList: true, subtree: true };
+
+    // start observing
+    observer.observe(target, config);
+
 });
+
+/**
+ * get all of the grade input fields, attach an observer that updates
+ * the grade when it is changed
+ */
+function bindEventToGradeEntry() {
+    $$('.grade-input').each(function (item) {
+        new Form.Element.EventObserver(item, function (element, value) {
+
+            var url = element.readAttribute('data-action');
+            var params = {
+                'updated_grade': value,
+                'student_id': element.readAttribute('data-student-id'),
+                'grade_entry_item_id': element.readAttribute('data-grade-entry-item-id'),
+                'authenticity_token': AUTH_TOKEN
+            }
+
+            new Ajax.Request(url, {
+                asynchronous: true,
+                evalScripts: true,
+                parameters: params
+            });
+        });
+    });
+}
 
 function toggleTotalColVisibility() {
     var allElements = document.getElementsByClassName("total_value");
 
-    for(var i=0; i < allElements.length; i++)
-    {
-	if(allElements [i].style.display == 'inline-block')
-	  allElements [i].style.display = 'none';
-	else
-	  allElements [i].style.display = 'inline-block';
+    for (var i = 0; i < allElements.length; i++) {
+        if (allElements [i].style.display == 'inline-block')
+            allElements [i].style.display = 'none';
+        else
+            allElements [i].style.display = 'inline-block';
     }
 
 }
