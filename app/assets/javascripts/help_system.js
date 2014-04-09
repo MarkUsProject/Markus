2e1d43fd0a0e33d2ba39ba84c455b27e2db504e6
cj343 <cj343@cornell.edu>
2014-04-09 18:27:45 -0400

    Help System for Assignment Form

--- app/assets/javascripts/help_system.js
+++ app/assets/javascripts/help_system.js
@@ -4,9 +4,11 @@
 //= require jquery_ujs
 
 jQuery(document).ready(function() {
-    jQuery(".help-message").hide();
+    jQuery(".help-message-box").hide();
+    jQuery(".help-message-tooltip").hide();
     jQuery(".help").click(function(){
         var help_section = (jQuery(this).attr('class').split(' ')[1]);
-        jQuery(".help-message").filter("."+help_section).toggle();
+        jQuery(".help-message-box").filter("."+help_section).toggle();
+        jQuery(".help-message-tooltip").filter("."+help_section).toggle();
     });
 });
