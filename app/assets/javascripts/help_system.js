05c3c92e2d0fb62a2b156076a8ef5d4d6dbd856d
Ope Akanji <beepted@gmail.com>
2014-04-11 12:33:29 -0400

    Help system for Users/Manage Students page

--- app/assets/javascripts/help_system.js
+++ app/assets/javascripts/help_system.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function() {
     jQuery(".help-message-box").hide();
     jQuery(".help-message-tooltip").hide();
     jQuery(".help-break").hide();
-    jQuery(".help").click(function(){
+    jQuery(".help, .title-help").click(function(){
         var help_section = (jQuery(this).attr('class').split(' ')[1]);
         jQuery(".help-message-box").filter("."+help_section).toggle();
         jQuery(".help-message-tooltip").filter("."+help_section).toggle();
