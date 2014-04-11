e7bcb8404f0c45f233563dad854ecc92af5710dc
cj343 <cj343@cornell.edu>
2014-04-11 11:52:04 -0400

    Submission pages

--- app/assets/javascripts/help_system.js
+++ app/assets/javascripts/help_system.js
@@ -6,9 +6,11 @@
 jQuery(document).ready(function() {
     jQuery(".help-message-box").hide();
     jQuery(".help-message-tooltip").hide();
+    jQuery(".help-break").hide();
     jQuery(".help").click(function(){
         var help_section = (jQuery(this).attr('class').split(' ')[1]);
         jQuery(".help-message-box").filter("."+help_section).toggle();
         jQuery(".help-message-tooltip").filter("."+help_section).toggle();
+        jQuery(".help-break").filter("."+help_section).toggle();
     });
 });
