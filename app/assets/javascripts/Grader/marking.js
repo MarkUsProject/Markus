74f55fd77808fdc9bdca34a8dda7d1e2779e939d
Eugene Cheung <ey.cheung@outlook.com>
2014-05-12 17:46:31 -0400

    More funky alternating list colours simplified. Error message on login page looks proper.

--- app/assets/javascripts/Grader/marking.js
+++ app/assets/javascripts/Grader/marking.js
@@ -1,10 +1,9 @@
+// $(document).ready(function() {
 document.observe('dom:loaded', function() {
-
   // changing the marking status
   new Form.Element.EventObserver('marking_state', update_status);
 
   function update_status(element, value) {
-
     var url = element.readAttribute('data-action');
 
     var params = {
@@ -20,11 +19,9 @@ document.observe('dom:loaded', function() {
   }
 
   // releasing the grades, only available on the admin page
-  var release = $('released')
-  if (release)
-  {
+  var release = document.getElementById('released');
+  if (release) {
     new Form.Element.EventObserver(release, function(element, value) {
-
       var url = element.readAttribute('data-action');
 
       var params = {
@@ -52,7 +49,6 @@ document.observe('dom:loaded', function() {
     });
 
     new Form.Element.EventObserver(item, function(element, value) {
-
       var url = element.readAttribute('data-action');
 
       var params = {
@@ -75,7 +71,7 @@ document.observe('dom:loaded', function() {
 });
 
 function focus_mark_criterion(id) {
-  if($('mark_criterion_title_' + id + '_expand').hasClassName('expanded')) {
+  if (jQuery('#mark_criterion_title_' + id + '_expand').hasClass('expanded')) {
     hide_criterion(id);
   } else {
     show_criterion(id);
@@ -83,41 +79,43 @@ function focus_mark_criterion(id) {
 }
 
 function hide_criterion(id) {
-    $('mark_criterion_inputs_' + id).hide();
-    $('mark_criterion_title_' + id).show();
-    $('mark_criterion_title_' + id + "_expand").innerHTML = "+ &nbsp;"
-    $('mark_criterion_title_' + id + "_expand").removeClassName('expanded');
+  document.getElementById("mark_criterion_inputs_" + id).style.display = "none";
+  document.getElementById("mark_criterion_title_" + id).style.display = "";
+  document.getElementById("mark_criterion_title_" + id + "_expand").innerHTML = "+ &nbsp;";
+  jQuery('#mark_criterion_title_' + id + "_expand").removeClass('expanded');
 }
 
 function show_criterion(id) {
-    $('mark_criterion_title_'+id+"_expand").innerHTML = "- &nbsp;"
-    $('mark_criterion_inputs_' + id).show();
-    $('mark_criterion_title_' + id + "_expand").addClassName('expanded');
+  document.getElementById("mark_criterion_title_" + id + "_expand").innerHTML = "- &nbsp;";
+  document.getElementById("mark_criterion_inputs_" + id).style.display = "";
+  jQuery('#mark_criterion_title_' + id + "_expand").addClass('expanded');
 }
 
 function select_mark(mark_id, mark) {
-  original_mark = $$('#mark_' + mark_id + '_table .rubric_criterion_level_selected').first()
+  original_mark = $$('#mark_' + mark_id + '_table .rubric_criterion_level_selected').first();
+
   if (typeof(original_mark) != "undefined") {
     original_mark.removeClassName('rubric_criterion_level_selected');
   }
-  if (mark != null){
-	$('mark_' + mark_id + '_' + mark).addClassName('rubric_criterion_level_selected');
+
+  if (mark != null) {
+	  $('mark_' + mark_id + '_' + mark).addClassName('rubric_criterion_level_selected');
   }
 }
 
 function update_total_mark(total_mark) {
-  $('current_mark_div').update(total_mark);
-  $('current_total_mark_div').update(total_mark);
+  document.getElementById("current_mark_div").innerHTML = total_mark;
+  document.getElementById("current_total_mark_div").innerHTML = total_mark;
 }
 
-function update_marking_state_selected(current_marking_state, new_marking_state){
-    $('marking_state').value = new_marking_state;
+function update_marking_state_selected(current_marking_state, new_marking_state) {
+  document.getElementById("marking_state").value = new_marking_state;
 
-    var error_message = document.getElementById('criterion_incomplete_error');
+  var error_message = document.getElementById('criterion_incomplete_error');
 
-    /* Update server state if error displayed or new state is different from server state */
-    if(error_message.style.display != 'none' || current_marking_state != new_marking_state){
-       error_message.style.display = 'none';
-       Event.fire($('marking_state'), "dom:loaded", {reason: 'Update Server'});
-    }
+  /* Update server state if error displayed or new state is different from server state */
+  if (error_message.style.display != 'none' || current_marking_state != new_marking_state) {
+    error_message.style.display = 'none';
+    Event.fire(document.getElementById("marking_state"), "dom:loaded", {reason: 'Update Server'});
+  }
 }
