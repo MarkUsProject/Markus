a88082f190d33fdfb9640689e35b04c7240bddbd
Eugene Cheung <ey.cheung@outlook.com>
2014-05-27 10:41:55 -0400

    Switched to  with appropriate locales.

--- app/assets/javascripts/PeriodDeltaChain/PeriodDeltaChain.js
+++ app/assets/javascripts/PeriodDeltaChain/PeriodDeltaChain.js
@@ -8,11 +8,11 @@ var PeriodDeltaChain = Class.create({
 
   initialize: function(params) {
     if (Date.parseFormattedString == null || Date.parseFormattedString == undefined) {
-      throw("Expected Date.prototype.parseFormattedString implemented.  See CalendarDateSelect plugin for Rails");
+      throw('Expected Date.prototype.parseFormattedString implemented.  See CalendarDateSelect plugin for Rails');
     }
     this.hour = 3600000; // 1 hour is 3600000 milliseconds
     this.period_root_id = params.period_root_id;
-    this.date_format = this.set_or_default(params.date_format, "");
+    this.date_format = this.set_or_default(params.date_format, '');
     this.set_due_date(params.due_date);
     this.period_class = this.set_or_default(params.period_class, 'period');
   },
@@ -27,19 +27,19 @@ var PeriodDeltaChain = Class.create({
       var to_time = new Date(current_time);
       to_time.setTime(to_time.getTime() + (me.hour * hours_value));
 
-      var language = document.getElementById("locale").value;
+      var language = document.getElementById('locale').value;
       if (language.indexOf('fr') >= 0) {
         /* French locale */
-        from_time_node.update(from_time.toLocale('fr'));
-        to_time_node.update(to_time.toLocale('fr'));
+        from_time_node.update(from_time.toLocaleString('fr-FR'));
+        to_time_node.update(to_time.toLocaleString('fr-FR'));
       } else if (language.indexOf('pt') >= 0) {
         /* Portuguese locale */
-        from_time_node.update(from_time.toLocale('pt'));
-        to_time_node.update(to_time.toLocale('pt'));
+        from_time_node.update(from_time.toLocaleString('pt-BR'));
+        to_time_node.update(to_time.toLocaleString('pt-BR'));
       } else {
-        /* English locale, or something else: no need to change */
-        from_time_node.update(from_time.toLocaleString());
-        to_time_node.update(to_time.toLocaleString());
+        /* English locale, or something else */
+        from_time_node.update(from_time.toLocaleString('en-CA'));
+        to_time_node.update(to_time.toLocaleString('en-CA'));
       }
 
       current_time = to_time;
@@ -57,19 +57,3 @@ var PeriodDeltaChain = Class.create({
     return value;
   }
 });
-
-Date.prototype.toLocale = function(locale) {
-  if (locale == 'fr') {
-    var months = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
-    return pad(this.getDate()) + " " + months[this.getMonth()] + " " + this.getFullYear() + ", " +
-           pad(this.getHours()) + ":" + pad(this.getMinutes()) + ":" + pad(this.getSeconds());
-  } else if (locale == 'pt') {
-    var months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
-    return pad(this.getDate()) + " de " + months[this.getMonth()] + " de " + this.getFullYear() + ", " +
-           pad(this.getHours()) + ":" + pad(this.getMinutes()) + ":" + pad(this.getSeconds());
-  }
-}
-
-function pad(number) {
-  return (number < 10) ? "0" + number : number;
-}
