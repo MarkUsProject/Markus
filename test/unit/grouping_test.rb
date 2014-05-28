c097cfbe55eb36c8989fe4ec0af9dc4adb527a9c
Su Zhang <me@zhang.su>
2014-05-28 06:03:59 -0400

    Add tests for new class methods of Grouping

--- test/unit/grouping_test.rb
+++ test/unit/grouping_test.rb
@@ -4,8 +4,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'blueprints', '
 require 'shoulda'
 require 'machinist'
 require 'mocha/setup'
-
-
+require 'set'
 
 class GroupingTest < ActiveSupport::TestCase
 
@@ -60,6 +59,10 @@ class GroupingTest < ActiveSupport::TestCase
       assert_equal 'Empty Group', @grouping.get_all_students_in_group
     end
 
+    should 'have 0 criteria coverage count after updating the counts' do
+      verify_update_criteria_coverage_counts(@grouping, 0)
+    end
+
     context 'and two unassigned tas' do
       setup do
         @ta2 = Ta.make
@@ -76,6 +79,10 @@ class GroupingTest < ActiveSupport::TestCase
         assert_equal 0, @grouping.ta_memberships.count
         @grouping.add_tas_by_user_name_array(user_name_array)
         assert_equal 2, @grouping.ta_memberships.count
+      end
+
+      should 'have 0 criteria coverage count after updating the counts' do
+        verify_update_criteria_coverage_counts(@grouping, 0)
       end
     end
 
@@ -163,6 +170,9 @@ class GroupingTest < ActiveSupport::TestCase
         assert(!@grouping.deletable_by?(non_inviter))
       end
 
+      should 'have 0 criteria coverage count after updating the counts' do
+        verify_update_criteria_coverage_counts(@grouping, 0)
+      end
     end
 
     context 'with a pending membership' do
@@ -204,11 +214,12 @@ class GroupingTest < ActiveSupport::TestCase
       end
     end
 
-    context 'with a ta assigned to grade' do
+    context 'with TAs assigned' do
+      ta_count = 3
+
       setup do
-        @membership = TaMembership.make(
-          :grouping => @grouping)
-        @ta = @membership.user
+        @tas = Array.new(ta_count) { Ta.make }
+        @grouping.add_tas(@tas)
       end
 
       should 'have a ta for marking' do
@@ -216,22 +227,93 @@ class GroupingTest < ActiveSupport::TestCase
       end
 
       should 'get ta names' do
-        assert_equal @ta.user_name,
+        assert_equal @tas[0].user_name,
                      @grouping.get_ta_names[0]
       end
 
-      should 'not be able to assign same ta twice' do
-        assert_equal 1, @grouping.ta_memberships.count
-        @grouping.add_tas(@ta)
-        assert_equal 1, @grouping.ta_memberships.count
+      should 'not be able to assign same TAs twice' do
+        @grouping.reload
+        assert_equal 3, @grouping.ta_memberships.count
+        @grouping.add_tas(@tas)
+        assert_equal 3, @grouping.ta_memberships.count
       end
 
       should 'be able to remove ta' do
-        @grouping.remove_tas(@ta)
+        @grouping.remove_tas(@tas)
         assert_equal 0, @grouping.ta_memberships.count
 
       end
 
+      should 'have 0 criteria coverage count after updating the counts' do
+        verify_update_criteria_coverage_counts(@grouping, 0)
+      end
+
+      context 'and with criteria' do
+        # Create more criteria than TAs to verify that irrelevant criteria are
+        # not counted. Only ta_count number of criteria are assigned TAs.
+        criterion_count = ta_count + 2
+
+        setup do
+          @criteria = Array.new(criterion_count) do
+            RubricCriterion.make(assignment: @grouping.assignment)
+          end
+        end
+
+        should 'have 0 criteria coverage count after updating the counts' do
+          verify_update_criteria_coverage_counts(@grouping, 0)
+        end
+
+        context 'of which only one is assigned a TA' do
+          setup { @criteria[0].add_tas(@tas[0]) }
+
+          should 'have 1 criteria coverage count after updating the counts' do
+            verify_update_criteria_coverage_counts(@grouping, 1)
+          end
+        end
+
+        context 'of which only one is assigned multiple TAs' do
+          setup { @criteria[0].add_tas(@tas) }
+
+          should 'have 1 criteria coverage count after updating the counts' do
+            verify_update_criteria_coverage_counts(@grouping, 1)
+          end
+        end
+
+        context "of which #{ta_count} are assigned unique TAs" do
+          setup { ta_count.times { |i| @criteria[i].add_tas(@tas[i]) } }
+
+          should "have #{ta_count} criteria coverage count after " +
+                 'updating the counts' do
+            verify_update_criteria_coverage_counts(@grouping, ta_count)
+          end
+        end
+
+        context "of which #{ta_count} are assigned non-unique TAs" do
+          setup do
+            ta_count.times { |i| @criteria[i].add_tas(@tas) }
+          end
+
+          should "have #{ta_count} criteria coverage count after " +
+                 'updating the counts' do
+            verify_update_criteria_coverage_counts(@grouping, ta_count)
+          end
+
+          context 'who are also assigned to groups of another assignment' do
+            setup do
+              # Making a new criterion also makes a new assignment.
+              criterion = RubricCriterion.make
+              grouping = Grouping.make(assignment: criterion.assignment)
+              criterion.add_tas(@tas)
+              grouping.add_tas(@tas)
+            end
+
+            should "have #{ta_count} criteria coverage count after " +
+                   'updating the counts' do
+              verify_update_criteria_coverage_counts(@grouping, ta_count)
+            end
+          end
+        end
+      end
     end
 
 
@@ -720,6 +802,150 @@ class GroupingTest < ActiveSupport::TestCase
     end
   end
 
+  context 'with multiple groupings of the same assignment and multiple TAs' do
+    grouping_count = 3
+    ta_count = 3
+
+    setup do
+      @assignment = Assignment.make
+      @groupings = Array.new(grouping_count) do
+        Grouping.make(assignment: @assignment)
+      end
+      @tas = Array.new(ta_count) { Ta.make }
+
+      @grouping_ids = @groupings.map(&:id)
+      @ta_ids = @tas.map(&:id)
+    end
+
+    should 'be OK to randomly bulk assign no TAs to no groupings' do
+      Grouping.randomly_assign_tas([], [], @assignment)
+    end
+
+    should 'be OK to randomly bulk assign TAs to no groupings' do
+      Grouping.randomly_assign_tas([], @ta_ids, @assignment)
+    end
+
+    should 'be OK to randomly bulk assign no TAs to all groupings' do
+      Grouping.randomly_assign_tas(@grouping_ids, [], @assignment)
+    end
+
+    should 'be able to randomly bulk assign TAs to all groupings' do
+      Grouping.randomly_assign_tas(@grouping_ids, @ta_ids, @assignment)
+
+      ta_id_set = @ta_ids.to_set
+      @groupings.each do |grouping|
+        grouping.reload
+        assert_equal 1, grouping.tas.size
+        assert ta_id_set.include?(grouping.tas.first.id)
+      end
+    end
+
+    should 'be OK to randomly bulk assign duplicated TAs to groupings' do
+      # The probability of assigning no duplicated TAs after 4 trials is 0.
+      4.times do
+        Grouping.randomly_assign_tas(@grouping_ids, @ta_ids, @assignment)
+      end
+
+      ta_id_set = @ta_ids.to_set
+      @groupings.each do |grouping|
+        grouping.reload
+        assert grouping.tas.size.between?(1, 3)
+        assert grouping.tas.pluck(:id).to_set.subset?(ta_id_set)
+      end
+    end
+
+    should 'update criteria coverage counts after randomly bulk assign TAs' do
+      Grouping.expects(:update_criteria_coverage_counts).with do |grouping_ids|
+        grouping_ids.to_set == @grouping_ids.to_set
+      end
+      Grouping.randomly_assign_tas(@grouping_ids, @ta_ids, @assignment)
+    end
+
+    should 'update assigned groups counts after randomly bulk assign TAs' do
+      RubricCriterion.expects(:update_assigned_groups_counts)
+        .with(@assignment.id)
+      Grouping.randomly_assign_tas(@grouping_ids, @ta_ids, @assignment)
+    end
+    should 'be OK to bulk assign no TAs to no groupings' do
+      Grouping.assign_all_tas([], [], @assignment)
+    end
+
+    should 'be OK to bulk assign all TAs to no groupings' do
+      Grouping.assign_all_tas([], @ta_ids, @assignment)
+    end
+
+    should 'be OK to bulk assign no TAs to all groupings' do
+      Grouping.assign_all_tas(@grouping_ids, [], @assignment)
+    end
+
+    should 'be able to bulk assign all TAs to all groupings' do
+      Grouping.assign_all_tas(@grouping_ids, @ta_ids, @assignment)
+
+      ta_id_set = @ta_ids.to_set
+      @groupings.each do |grouping|
+        grouping.reload
+        assert_equal ta_id_set, grouping.tas.pluck(:id).to_set
+      end
+    end
+
+    should 'be OK to bulk assign duplicated TAs to groupings' do
+      Grouping.assign_all_tas(@grouping_ids.first, @ta_ids, @assignment)
+      Grouping.assign_all_tas(@grouping_ids, @ta_ids.first, @assignment)
+
+      # First grouping gets all the TAs.
+      grouping = @groupings.shift
+      grouping.reload
+      assert_equal @ta_ids.to_set, grouping.tas.pluck(:id).to_set
+
+      # The rest of the groupings gets only the first TA.
+      @groupings.each do |grouping|
+        grouping.reload
+        assert_equal @ta_ids.first, grouping.tas.first.id
+      end
+    end
+
+    should 'update criteria coverage counts after bulk assign all TAs' do
+      Grouping.expects(:update_criteria_coverage_counts).with do |grouping_ids|
+        grouping_ids.to_set == @grouping_ids.to_set
+      end
+      Grouping.assign_all_tas(@grouping_ids, @ta_ids, @assignment)
+    end
+
+    should 'update assigned groups counts after bulk assign all TAs' do
+      RubricCriterion.expects(:update_assigned_groups_counts)
+        .with(@assignment.id)
+      Grouping.assign_all_tas(@grouping_ids, @ta_ids, @assignment)
+    end
+
+    should 'be OK to bulk unassign no TAs' do
+      Grouping.unassign_tas([], [], @assignment)
+    end
+
+    should 'be able to bulk unassign TAs' do
+      Grouping.assign_all_tas(@grouping_ids, @ta_ids, @assignment)
+      ta_membership_ids = @groupings
+        .map { |grouping| grouping.memberships.pluck(:id) }
+        .reduce(:+)
+      Grouping.unassign_tas(ta_membership_ids, @grouping_ids, @assignment)
+
+      @groupings.each do |grouping|
+        grouping.reload
+        assert_equal 0, grouping.tas.size
+      end
+    end
+
+    should 'update criteria coverage counts after bulk unassign TAs' do
+      Grouping.expects(:update_criteria_coverage_counts).with(@grouping_ids)
+      Grouping.unassign_tas([], @grouping_ids, @assignment)
+    end
+
+    should 'update assigned groups counts after bulk unassign TAs' do
+      RubricCriterion.expects(:update_assigned_groups_counts)
+        .with(@assignment.id)
+      Grouping.unassign_tas([], @grouping_ids, @assignment)
+    end
+  end
+
   def submit_files_after_due_date(time, filename, text)
     pretend_now_is(Time.parse(time)) do
       assert Time.now > @assignment.due_date
@@ -738,4 +964,11 @@ class GroupingTest < ActiveSupport::TestCase
     txn
   end
 
+  # Verifies the criteria coverage count of +grouping+ is equal to
+  # +expected_count+ after updating it.
+  def verify_update_criteria_coverage_counts(grouping, expected_count)
+    Grouping.update_criteria_coverage_counts(grouping.id)
+    grouping.reload
+    assert_equal expected_count, grouping.criteria_coverage_count
+  end
 end
