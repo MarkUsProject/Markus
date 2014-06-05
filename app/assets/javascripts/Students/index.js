d8ce8a3b8e4d6f86d83599b5ca79b57086872bc8
Lawrence <lawrence@lawrencewu.me>
2014-06-05 19:19:52 +0000

    initial commit for react.js tables

--- app/assets/javascripts/Students/index.js
+++ app/assets/javascripts/Students/index.js
@@ -1,5 +1,6 @@
 
 jQuery(document).ready(function () {
+  console.log("getting called...");
   window.modal_upload = new ModalMarkus('#upload_dialog');
   window.modal_download = new ModalMarkus('#download_dialog');
 });
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,23 +12,56 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all(:order => 'user_name')
-    @sections = Section.all(:order => 'name')
-  end
-
-  def populate
-    # Process:
-    # 1. AJAX call made by jQuery to populate the student table
-    # 2. Find students from the database
-    # 3. Pass in the student data into the helper function (construct_table_rows defined in UsersHelper)
-    @students_data = Student.all(:order => 'user_name',
-                                 :include => [:section,
-                                              :grace_period_deductions])
-
-    # Function below contained within helpers/users_helper.rb
-    @students = construct_table_rows(@students_data)
     respond_to do |format|
-      format.json { render :json => @students }
+      format.html do # Renders students/index.html.erb
+        @columns = { 'user_name' => I18n.t('user.user_name'),
+                     'last_name' => I18n.t('user.last_name'),
+                     'first_name' => I18n.t('user.first_name'),
+                     'section' => I18n.t('user.section'),
+                     'grace_credits' => I18n.t('user.grace_credits'),
+                     'notes' => I18n.t('user.notes_count'),
+                     'edit' => I18n.t('edit') }
+        @rows = { 'edit' => I18n.t('edit'),
+                  'notes' => I18n.t('user.notes_count') }
+        @search = I18n.t('students.search_students')
+        @filters = { 'all' => I18n.t('all'),
+                     'active' => I18n.t('active'),
+                     'not_active' => I18n.t('not_active') }
+        @actions = { 'give_grace_credits' => I18n.t('give_grace_credits'),
+                     'add_section' => I18n.t('add_section'),
+                     'no_sections' => I18n.t('section.no_sections'),
+                     'hide_students' => I18n.t('hide_students'),
+                     'unhide_students' => I18n.t('unhide_students') }
+      end
+      format.json do
+        @sections = Section.all
+        @students = Student.includes(:grace_period_deductions, :section)
+                           .all(order: 'user_name')
+        # Gets extra info needed to table, such as grace credits remaining,
+        # links to edit, notes, etc.
+        @students_table_info = @students.map do |student|
+          s = student.attributes
+          s['edit_link'] = url_for(controller: 'students',
+                                   action: 'edit',
+                                   id: student.id)
+          s['grace_credits_remaining'] = student.remaining_grace_credits
+          s['section_name'] = student.has_section? ? student.section.name : nil
+          s['notes_link'] = url_for(controller: 'notes',
+                                    action: 'notes_dialog',
+                                    id: student.id,
+                                    noteable_id: student.id,
+                                    noteable_type: 'Student',
+                                    action_to: 'note_message',
+                                    controller_to: 'students',
+                                    number_of_notes_field:
+                                      "num_notes_#{student.id}",
+                                    highlight_field:
+                                      "notes_highlight_#{student.id}")
+          s['num_notes'] = student.notes.size
+          s
+        end
+        render json: [@students_table_info]
+      end
     end
   end
 
@@ -59,20 +92,17 @@ class StudentsController < ApplicationController
         raise I18n.t('students.no_students_selected')
       end
       case params[:bulk_action]
-        when 'hide'
-          Student.hide_students(student_ids)
-          @students = construct_table_rows(Student.find(student_ids))
-        when 'unhide'
-          Student.unhide_students(student_ids)
-          @students = construct_table_rows(Student.find(student_ids))
-        when 'give_grace_credits'
-          Student.give_grace_credits(student_ids, params[:number_of_grace_credits])
-          @students = construct_table_rows(Student.find(student_ids))
-        when 'add_section'
-          Student.update_section(student_ids, params[:section])
-          @students = construct_table_rows(Student.find(student_ids))
+      when 'hide'
+        Student.hide_students(student_ids)
+      when 'unhide'
+        Student.unhide_students(student_ids)
+      when 'give_grace_credits'
+        Student.give_grace_credits(student_ids,
+                                   params[:number_of_grace_credits])
+      when 'update_section'
+        Student.update_section(student_ids, params[:section])
       end
-      render :bulk_modify, :formats => [:js]
+      head :ok
     rescue RuntimeError => e
       @error = e.message
       render :display_error
