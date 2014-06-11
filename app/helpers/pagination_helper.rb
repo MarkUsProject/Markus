870b6e56a1754a9be2bf05de63dd2a7142a2024f
Su Zhang <me@zhang.su>
2014-06-10 22:47:53 -0400

    Replace helpers, lib, config, Gemfile hash rockets
    This is a global search-and-replace that replaces the Ruby 1.8 symbol
    key hash rocket syntax to Ruby 1.9 syntax in
    
            {lib,config,app/helpers,Gemfile}
    
    Since this is a big change list, the style issues in the changed lines
    are not being dealt with so that errors are minimized and code review
    is easier. There is one exception though:
    
    If the lines before the change have a good alignment of hash keys that
    span multiple lines and the lines after the change do not (because the
    change might alters the number of spaces needed for the alignment), then
    the alignment is fixed, such that no new style issues are introduced. If
    the alignment was not correct to begin with, then it is not fixed.
    
    As a result, please ignore linter comments for this change.
    
    Tested:
    - rake test:units
    - rake test:functionals
    - rails s

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -48,7 +48,7 @@ module PaginationHelper
       params[:page] = AP_DEFAULT_PAGE
     end
 
-    return items.paginate(:per_page => params[:per_page], :page => params[:page]).clone, items.size
+    return items.paginate(per_page: params[:per_page], page: params[:page]).clone, items.size
 
   end
 
@@ -69,8 +69,8 @@ module PaginationHelper
       when 'repo_name' then to_include = [:group]
       when 'section' then to_include = [:group]
       when 'revision_timestamp' then to_include = [:current_submission_used]
-      when 'marking_state' then to_include = [{:current_submission_used => :results}]
-      when 'total_mark' then to_include = [{:current_submission_used => :results}]
+      when 'marking_state' then to_include = [{current_submission_used: :results}]
+      when 'total_mark' then to_include = [{current_submission_used: :results}]
       when 'grace_credits_used' then to_include = [:grace_period_deductions]
     end
     items = hash[:filters][filter][:proc].call(object_hash, to_include)
