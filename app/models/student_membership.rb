a54a9e615c18e71ff3c43ae8c2e0d6fcd4e2150d
Su Zhang <me@zhang.su>
2014-06-10 20:51:41 -0400

    Replace model symbol hash rockets with 1.9 syntax
    This is a global search-and-replace that replaces the Ruby 1.8 symbol
    key hash rocket syntax to Ruby 1.9 syntax in model code.
    
    Since this is a big change list, the style issues in the changed lines
    are not being dealt with so that errors are minimized and code review
    is easier. There is one exception though:
    
    If the lines before the change have a good alignment of hash keys that
    span multiple lines and the lines after the change do not (because the
    change might alters the number of spaces needed for the alignment), then
    the alignment is fixed, such that no new style issues are introduced. If
    the alignment was not correct to begin with, then it is not fixed.
    
    As a result, please ignore linter comments for this change.
    
    Tested:
    - rake test:units
    - rake test:functionals
    - rails s

--- app/models/student_membership.rb
+++ app/models/student_membership.rb
@@ -1,28 +1,28 @@
 class StudentMembership < Membership
 
   STATUSES = {
-    :accepted => 'accepted',
-    :inviter => 'inviter',
-    :pending => 'pending',
-    :rejected => 'rejected'
+    accepted: 'accepted',
+    inviter: 'inviter',
+    pending: 'pending',
+    rejected: 'rejected'
   }
 
   scope :accepted,
-              :conditions => {:membership_status => STATUSES[:accepted]}
+              conditions: {membership_status: STATUSES[:accepted]}
   scope :inviter,
-              :conditions => {:membership_status => STATUSES[:inviter]}
+              conditions: {membership_status: STATUSES[:inviter]}
   scope :pending,
-              :conditions => {:membership_status => STATUSES[:pending]}
+              conditions: {membership_status: STATUSES[:pending]}
   scope :rejected,
-              :conditions => {:membership_status => STATUSES[:rejected]}
+              conditions: {membership_status: STATUSES[:rejected]}
   scope :accepted_or_inviter,
-              :conditions => {:membership_status => [STATUSES[:accepted],
+              conditions: {membership_status: [STATUSES[:accepted],
                                                      STATUSES[:inviter]]}
   validate :must_be_valid_student
 
   validates_presence_of :membership_status
   validates_format_of :membership_status,
-                      :with => /inviter|pending|accepted|rejected/
+                      with: /inviter|pending|accepted|rejected/
 
   def must_be_valid_student
     if user && !user.is_a?(Student)
