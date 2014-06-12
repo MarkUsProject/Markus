4adc7256b3c7ffb86c9e60d9e8d4a42e55d5b16a
Su Zhang <me@zhang.su>
2014-06-11 20:19:41 -0400

    Replace forgotten symbol key hash rockets
    The symbol key hash rockets in the controller and routes.rb are somehow
    missed by the `hash_rocket` gem. They got replaced, however, when
    rerunning `hash_rocket`.
    
    The Rake task files have a different suffix, which are missed when
    running `hash_rocket`.

--- app/controllers/graders_controller.rb
+++ app/controllers/graders_controller.rb
@@ -45,7 +45,7 @@ class GradersController < ApplicationController
     @assignment = Assignment.find(params[:assignment_id])
     groupings = groupings_with_assoc(@assignment)
     @table_rows = construct_table_rows(groupings, @assignment)
-    render :populate, :formats => [:js] 
+    render :populate, formats: [:js]
   end
 
   def populate_graders
@@ -58,7 +58,7 @@ class GradersController < ApplicationController
     @assignment = Assignment.find(params[:assignment_id])
     criteria = criteria_with_assoc(@assignment)
     @table_rows = construct_criterion_table_rows(criteria, @assignment)
-    render :populate_criteria, :formats => [:js] 
+    render :populate_criteria, formats: [:js]
   end
 
   def set_assign_criteria
