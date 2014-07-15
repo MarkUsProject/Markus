0e7dfb29ff42fe052115cfe733108c75806ab871
david-yz-liu <liudavid@cs.toronto.edu>
2014-07-15 11:54:28 -0400

    arkon's comments

--- app/assets/javascripts/help_system.js
+++ app/assets/javascripts/help_system.js
@@ -1,15 +1,10 @@
-//= require prototype
-//= require jquery
-//= require jquery-ui
-//= require jquery_ujs
-
 jQuery(document).ready(function() {
     jQuery(".help-message-box").hide();
     jQuery(".help-message-tooltip").hide();
     jQuery(".help-message-title").hide();
     jQuery(".help-break").hide();
-    jQuery(".help, .title-help").click(function(){
-        var help_section = (jQuery(this).attr('class').split(' ')[1]);
+    jQuery(".help, .title-help").click(function() {
+        var help_section = jQuery(this).attr('class').get(1);
         jQuery(".help-message-box").filter("."+help_section).toggle();
         jQuery(".help-message-tooltip").filter("."+help_section).toggle();
         jQuery(".help-message-title").filter("."+help_section).toggle();
