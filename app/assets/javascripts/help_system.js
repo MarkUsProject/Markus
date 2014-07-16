3489414bc12b5e0bdf48de80d0d34e28f4d2a933
david-yz-liu <liudavid@cs.toronto.edu>
2014-07-16 13:16:50 -0400

    More comments

--- app/assets/javascripts/help_system.js
+++ app/assets/javascripts/help_system.js
@@ -5,9 +5,9 @@ jQuery(document).ready(function() {
     jQuery(".help-break").hide();
     jQuery(".help, .title-help").click(function() {
         var help_section = jQuery(this).attr('class').split(' ')[1];
-        jQuery(".help-message-box").filter("."+help_section).toggle();
-        jQuery(".help-message-tooltip").filter("."+help_section).toggle();
-        jQuery(".help-message-title").filter("."+help_section).toggle();
-        jQuery(".help-break").filter("."+help_section).toggle();
+        jQuery(".help-message-box").filter("." + help_section).toggle();
+        jQuery(".help-message-tooltip").filter("." + help_section).toggle();
+        jQuery(".help-message-title").filter("." + help_section).toggle();
+        jQuery(".help-break").filter("." + help_section).toggle();
     });
 });
