36ab06d20c31032efb2751693a7155d7c62c77b9
Eugene Cheung <ey.cheung@outlook.com>
2014-07-17 12:34:18 -0400

    About dialog

--- app/assets/javascripts/layouts.js
+++ app/assets/javascripts/layouts.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
   window.role_switch_modal = new ModalMarkus('#role_switch_dialog');
   window.redirect_modal    = new ModalMarkus('#redirect_dialog');
-  window.dialog_modal      = new ModalMarkus('#about_dialog');
+  window.about_modal       = new ModalMarkus('#about_dialog');
 });
