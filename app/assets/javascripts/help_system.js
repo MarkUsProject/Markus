c8ea2d240a5e6e8f74e7ec0d2d60f38445e5e74b
Eugene Cheung <ey.cheung@outlook.com>
2014-07-18 17:19:04 -0400

    Help text styling/formatting.

--- app/assets/javascripts/help_system.js
+++ app/assets/javascripts/help_system.js
@@ -1,12 +1,10 @@
 jQuery(document).ready(function() {
     jQuery(".help-message-box").hide();
-    jQuery(".help-message-tooltip").hide();
     jQuery(".help-message-title").hide();
     jQuery(".help-break").hide();
     jQuery(".help, .title-help").click(function() {
         var help_section = jQuery(this).attr('class').split(' ')[1];
         jQuery(".help-message-box").filter("." + help_section).toggle();
-        jQuery(".help-message-tooltip").filter("." + help_section).toggle();
         jQuery(".help-message-title").filter("." + help_section).toggle();
         jQuery(".help-break").filter("." + help_section).toggle();
     });
