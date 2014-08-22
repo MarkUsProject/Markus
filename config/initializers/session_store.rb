c94f3bfa1967918a4ca1cbddbe037890a86d90ee
Lawrence <lawrence@lawrencewu.me>
2014-08-22 14:51:21 +0000

    make session store point to config settings

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Markus::Application.config.session_store :cookie_store, key: '_markus_session'
+Markus::Application.config.session_store :cookie_store, key: MarkusConfigurator.markus_config_session_cookie_name
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
