ba4374d0751917934d012831ec5138b657109997
Bryan J Muscedere <bmuscede@uwo.ca>
2014-09-21 11:49:45 -0400

    Fixed houndci suggestions for new rspec files

--- spec/models/annotation_spec.rb
+++ spec/models/annotation_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
 
 describe Annotation do
-  context "checks relationships" do
+  context 'checks relationships' do
     it { is_expected.to belong_to(:submission_file) }
     it { is_expected.to belong_to(:annotation_text) }
   end
 
-  context "requires items to be set" do
+  context 'requires items to be set' do
     it { is_expected.to validate_presence_of(:submission_file) }
     it { is_expected.to validate_presence_of(:annotation_text) }
     it { is_expected.to validate_presence_of(:annotation_number) }
   end
 
-  context "validates certain values" do
+  context 'validates certain values' do
     it { is_expected.to validate_numericality_of(:annotation_text_id) }
     it { is_expected.to validate_numericality_of(:submission_file_id) }
     it { is_expected.to validate_numericality_of(:annotation_number) }
   end
 
-  context "ensures invalid values cannot be added" do
+  context 'ensures invalid values cannot be added' do
     it { should_not allow_value(-1).for(:annotation_text_id) }
     it { should_not allow_value(-1).for(:submission_file_id) }
     it { should_not allow_value(-1).for(:annotation_number) }
