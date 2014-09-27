461194088b153a07d845ae48733f30cfaa0635a8
Bryan J Muscedere <bmuscede@uwo.ca>
2014-09-27 14:21:04 -0400

    Fix for #1726. Buttons only enabled on file select.

--- app/assets/javascripts/upload_button_control.js
+++ app/assets/javascripts/upload_button_control.js
@@ -1,23 +1,29 @@
 function _get(param_name){
-    // Gets the correct attribute from the script.
-    var scriptElement = document.getElementById('upload_script');
-    return scriptElement.getAttribute(param_name);
+  // Gets the correct attribute from the script.
+  var scriptElement = document.getElementById('upload_script');
+
+  // Now creates an array with each element.
+  return scriptElement.getAttribute(param_name).split(" ");
 }
 
 jQuery(document).ready(function(){
-    // First, gets the upload id and user id.
-    var upload_id = _get('upload_id');
-    var button_id = _get('button_id');
+  // First, gets the upload id and user id.
+  var upload_id = _get('upload_id');
+  var button_id = _get('button_id');
 
-    // Checks to see if the file upload id changed.
-    jQuery("input#" + upload_id).change(function () {
-        var filePath = jQuery(this).val();
+  // Checks to see if the file upload id changed.
+  jQuery("input#" + upload_id).change(function () {
+    var filePath = jQuery(this).val();
 
-        // Either disables or enables upload buttons.
-        if (filePath === "") {
-            document.getElementById(button_id).disabled = true;
-        } else {
-            document.getElementById(button_id).disabled = false;
-        }
+    var i;
+    // Enables/Disables all buttons.
+    for (i = 0; i < button_id.size(); i++) {
+      // Either disables or enables upload buttons.
+      if (filePath === "") {
+        document.getElementById(button_id[i]).disabled = true;
+      } else {
+        document.getElementById(button_id[i]).disabled = false;
+      }
+    }
 })});
 
