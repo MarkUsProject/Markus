c0877741bc913d225ff44d90aba868a64c957967
vagrant <vagrant@packer-debian-7.4-amd64>
2014-09-28 20:24:59 -0400

    updating rspec file

--- spec/models/image_annotation_spec.rb
+++ spec/models/image_annotation_spec.rb
@@ -25,24 +25,26 @@ describe ImageAnnotation do
       end
 
       it 'creates coords specified above for basic annot' do
-        expect (basic_annot.extract_coords ==
-            { id: basic_annot.annotation_text_id,
-              x_range: { start: 0, end: 10 },
-              y_range: { start: 0, end: 10 } })
-      end
-      it 'creates negative coords as specified above' do
-        expect (negative_annot.extract_coords ==
-            { id: negative_annot.annotation_text_id,
-              x_range: { start: -1, end: 3 },
-              y_range: { start: -2, end: 5 } })
-      end
-      it 'creates coords as specified above for spaces_annot' do
-        expect (spaces_annot.extract_coords ==
-            { id: spaces_annot.annotation_text_id,
-              x_range: { start: -1, end: 3 },
-              y_range: { start: 5, end: 123 } })
+        expected_basic_hash = { id: basic_annot.annotation_text_id,
+                                x_range: { start: 0, end: 10 },
+                                y_range: { start: 0, end: 10 } }
+
+        expect(basic_annot.extract_coords).to eq(expected_basic_hash)
       end
 
+      it 'creates negative coords as specified above' do
+        expected_negative_hash = { id: negative_annot.annotation_text_id,
+                                   x_range: { start: -1, end: 3 },
+                                   y_range: { start: -2, end: 5 } }
+        expect(negative_annot.extract_coords).to eq(expected_negative_hash)
+      end
+
+      it 'creates coords as specified above for spaces_annot' do
+        expected_spaces_hash = { id: spaces_annot.annotation_text_id,
+                        x_range: { start: -1, end: 3 },
+                        y_range: { start: 5, end: 123 } };
+        expect(spaces_annot.extract_coords).to eq(expected_spaces_hash)
+      end
     end
   end
 
@@ -53,8 +55,8 @@ describe ImageAnnotation do
       end
 
       it 'renders partial' do
-        expect (annotation.annotation_list_link_partial ==
-            '/annotations/image_annotation_list_link')
+        expected = "/annotations/image_annotation_list_link"
+        expect(annotation.annotation_list_link_partial).to eq(expected)
       end
     end
   end
