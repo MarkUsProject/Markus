2a6983230f6fbb8e49ace36366b10e54e031c132
Bryan J Muscedere <bmuscede@uwo.ca>
2014-10-04 14:56:03 -0400

    Fixed javascript spacing.

--- app/assets/javascripts/upload_button_control.js
+++ app/assets/javascripts/upload_button_control.js
@@ -7,37 +7,37 @@ var upload_id = _get('upload_id');
 var button_id = _get('button_id');
 
 var Function_List = {
-    onReady: function(u_id, b_id){
-        // Ensures only one upload field was entered.
-        if (u_id.size() != 1) return;
+  onReady: function(u_id, b_id){
+    // Ensures only one upload field was entered.
+    if (u_id.size() != 1) return;
 
-        // Checks to see if the file upload id changed.
-        jQuery('body').on('change','#' + u_id, function(){
-            if (document.getElementById(b_id[0]) == null) return;
+    // Checks to see if the file upload id changed.
+    jQuery('body').on('change','#' + u_id, function(){
+      if (document.getElementById(b_id[0]) == null) return;
 
-            var filePath = this.value;
+      var filePath = this.value;
+      var i;
 
-            var i;
-            // Enables/Disables all buttons.
-            for (i = 0; i < b_id.size(); i++) {
-                // Either disables or enables upload buttons.
-                if (filePath === "") {
-                    document.getElementById(b_id[i]).disabled = true;
-                } else {
-                    document.getElementById(b_id[i]).disabled = false;
-                }
-            }
-        });
-    }
+      // Enables/Disables all buttons.
+      for (i = 0; i < b_id.size(); i++) {
+        // Either disables or enables upload buttons.
+        if (filePath === "") {
+          document.getElementById(b_id[i]).disabled = true;
+        } else {
+          document.getElementById(b_id[i]).disabled = false;
+        }
+      }
+    });
+  }
 };
 
 // Finally, executes the jQuery ready command.
 jQuery(document).ready(Function_List.onReady(upload_id, button_id));
 
 function _get(param_name){
-    // Gets the correct attribute from the script.
-    var scriptElement = document.getElementById(script_id);
+  // Gets the correct attribute from the script.
+  var scriptElement = document.getElementById(script_id);
 
-    // Now creates an array with each element.
-    return scriptElement.getAttribute(param_name).split(" ");
+  // Now creates an array with each element.
+  return scriptElement.getAttribute(param_name).split(" ");
 };
