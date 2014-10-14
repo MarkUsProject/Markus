c4f1d808ec5278812b7fed0b0241edf296bb17df
Yusi Fan <yfan094@gmail.com>
2014-10-14 17:27:12 -0400

    remove tab

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -43,9 +43,9 @@ class GroupsController < ApplicationController
     @errors = []
     @removed_groupings = []
     students_to_remove = grouping.students.to_a
-		grouping.student_memberships.each do |member|
-			grouping.remove_member(member.id)
-		end
+    grouping.student_memberships.each do |member|
+      grouping.remove_member(member.id)
+    end
     # TODO: return errors through request
     if grouping.has_submission?
         @errors.push(grouping.group.group_name)
