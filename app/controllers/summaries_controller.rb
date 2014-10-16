101ded58593c1efc225724daf37e71ca9ba74c3b
david-yz-liu <liudavid@cs.toronto.edu>
2014-10-15 21:51:37 -0400

    Make flexible criteria visible

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -3,7 +3,11 @@ class SummariesController < ApplicationController
 
   def index
     @assignment = Assignment.find(params[:assignment_id])
-    @criteria = @assignment.rubric_criteria
+    if @assignment.marking_scheme_type == 'rubric'
+      @criteria = @assignment.rubric_criteria
+    else
+      @criteria = @assignment.flexible_criteria
+    end
     respond_to do |format|
       format.html
       format.json { render json: get_summaries_table_info(@assignment) }
