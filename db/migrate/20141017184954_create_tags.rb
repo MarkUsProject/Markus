27b47a306e0f083b956853f06d8e374603969334
vagrant <vagrant@packer-debian-7.4-amd64>
2014-10-26 18:35:10 -0400

    few changes to the migration files

--- db/migrate/20141017184954_create_tags.rb
+++ db/migrate/20141017184954_create_tags.rb
@@ -1,9 +1,8 @@
 class CreateTags < ActiveRecord::Migration
   def up
     create_table :tags do |t|
-      t.integer :tag_id,           :null => false
-      t.string :content,           :null => false
-      t.belongs_to :assignment_id, :null => false
+      t.string :content,           null: false
+      t.belongs_to :assignment,    null: false
     end
   end
 
