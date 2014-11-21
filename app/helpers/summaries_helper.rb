17194f1ded38e256ee12e248b1a34ebdea5517d5
David Liu <liudavid@cs.toronto.edu>
2014-11-21 16:31:07 -0500

    Fix whitespace

--- app/helpers/summaries_helper.rb
+++ app/helpers/summaries_helper.rb
@@ -42,7 +42,7 @@ module SummariesHelper
   def get_grouping_criteria(assignment, grouping)
     # put all criteria in a hash for retrieval
     criteria_hash = Hash.new
-    if (assignment.marking_scheme_type == 
+    if (assignment.marking_scheme_type ==
         Assignment::MARKING_SCHEME_TYPE[:flexible])
       criteria = assignment.flexible_criteria
     else
