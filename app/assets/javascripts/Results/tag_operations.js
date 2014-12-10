4f6b02995c22756490f26bc82fe3ea3d8ab52f59
Bryan Muscedere <bmuscede@uwo.ca>
2014-12-10 18:00:04 -0500

    Improvements to Ajax Script

--- app/assets/javascripts/Results/tag_operations.js
+++ app/assets/javascripts/Results/tag_operations.js
@@ -1,6 +1,12 @@
 function formatTag(type, tag_id, grouping_id) {
-    //Notifies that the system is working.
+    //Gets the tag element
+    var tag = jQuery('span#' + tag_id);
+    // Notifies that the system is working.
     document.getElementById('working').style.display = '';
+
+    // Disables hyperlink.
+    tag.removeClass('tag_element');
+    tag.addClass('tag_element_disabled');
 
     // Performs an AJAX call to add the tag.
     jQuery.ajax({
@@ -18,6 +24,10 @@ function formatTag(type, tag_id, grouping_id) {
         // Remove working notice.
         document.getElementById('working').style.display = 'none';
 
+        // Re-enable the tag.
+        tag.removeClass('tag_element_disabled');
+        tag.addClass('tag_element');
+
         // Handle tag movement.
         if (type === "add")
           addTagToCurrent(tag_id);
@@ -31,24 +41,24 @@ function addTagToCurrent(tag_id) {
   // We get the old tag text.
   var spanElement = jQuery('span#' + tag_id).html();
 
+  // Finally, deletes the current element.
+  jQuery('span#' + tag_id).remove();
+
   // Replace 'add' with 'remove'
-  spanElement.replace('add', 'remove');
-  alert('<span id="' + tag_id + '" class="tag_element">' + spanElement + '</span>');
+  spanElement = spanElement.replace(/add/g, 'remove');
+
   // Now goes to insert it.
   jQuery("#active_tags").append(
       '<span id="' + tag_id + '" class="tag_element">' + spanElement + '</span>');
 
-  // Finally, deletes the current element.
-  jQuery('span#' + tag_id).remove();
-
   // Checks and sees if there are no tags in the available tags.
-  if (jQuery('div#available_tags').length == 1) {
+  if (jQuery('#available_tags span').length == 1) {
     jQuery('span#available-none').removeClass('no_tags_hidden');
     jQuery('span#available-none').addClass('no_tags');
   }
 
   // Checks and sees if there are no tags in the active tags.
-  if (jQuery('div#active_tags').length == 1) {
+  if (jQuery('#active_tags span').length == 1) {
       jQuery('span#active-none').removeClass('no_tags');
       jQuery('span#active-none').addClass('no_tags_hidden');
   }
@@ -58,24 +68,24 @@ function addTagToAvailable(tag_id) {
   // We get the old tag text.
   var spanElement = jQuery('span#' + tag_id).html();
 
-  // Replace 'add' with 'remove'
-  spanElement.replace('remove', 'add');
+  // Finally, deletes the current element.
+  jQuery('span#' + tag_id).remove();
+
+   // Replace 'add' with 'remove'
+  spanElement = spanElement.replace(/remove/g, 'add');
 
   // Now goes to insert it.
   jQuery("#available_tags").append(
           '<span id="' + tag_id + '" class="tag_element">' + spanElement + '</span>');
 
-  // Finally, deletes the current element.
-  jQuery('span#' + tag_id).remove();
-
   // Checks and sees if there are no tags in the active tags.
-  if (jQuery('div#active_tags').length == 1) {
+  if (jQuery('#active_tags span').length == 1) {
       jQuery('span#active-none').removeClass('no_tags_hidden');
       jQuery('span#active-none').addClass('no_tags');
   }
 
   // Checks and sees if there are no tags in the available tags.
-  if (jQuery('div#available_tags').length == 1) {
+  if (jQuery('#available_tags span').length == 1) {
       jQuery('span#available-none').removeClass('no_tags');
       jQuery('span#available-none').addClass('no_tags_hidden');
   }
