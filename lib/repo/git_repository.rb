c6218094501520934d0a572e1404c08934e56d2a
Alex Grenier <alex.grenier@gmail.com>
2015-01-16 18:00:51 -0500

    Hound

--- lib/repo/git_repository.rb
+++ lib/repo/git_repository.rb
@@ -78,10 +78,12 @@ module Repository
       index = repo.index
       index.add(path: 'README.md', oid: oid, mode: 0100644)
       index.write
-      Rugged::Commit.create(repo, 
-			    commit_options(repo,
-					   'Markus',
-                                           'Initial readme commit.'))
+      Rugged::Commit.create(
+	repo,
+	commit_options(
+		repo,
+		'Markus',
+	        'Initial readme commit.'))
       return true
     end
 
