141e735dedcdf25ea7704292844143b060d103e9
Paymahn Moghadasian <paymahn1@gmail.com>
2015-01-28 13:30:10 -0600

    Fix test and address more hound comments

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -1,5 +1,4 @@
 module AuthenticationHelper
-
   def user_params(attrs = {})
     args = { user_name:  'test',
              first_name: 'Mark',
@@ -7,26 +6,25 @@ module AuthenticationHelper
     ActionController::Parameters.new(user: args)
   end
 
-  def get_as(user, action, params=nil, flash=nil)
+  def get_as(user, action, params = nil, flash = nil)
     session_vars = { 'uid' => user.id, 'timeout' => 3.days.from_now }
     get action, params, session_vars, flash
   end
 
   # Performs POST request as the supplied user for authentication
-  def post_as(user, action, params=nil, flash=nil)
+  def post_as(user, action, params = nil, flash = nil)
     session_vars = { 'uid' => user.id, 'timeout' => 3.days.from_now }
     post action, params, session_vars, flash
   end
 
   # Performs PUT request as the supplied user for authentication
-  def put_as(user, action, params=nil, flash=nil)
+  def put_as(user, action, params = nil, flash = nil)
     session_vars = { 'uid' => user.id, 'timeout' => 3.days.from_now }
     put action, params, session_vars, flash
   end
 
-
   # Performs DELETE request as the supplied user for authentication
-  def delete_as(user, action, params=nil, flash=nil)
+  def delete_as(user, action, params = nil, flash = nil)
     session_vars = { 'uid' => user.id, 'timeout' => 3.days.from_now }
     delete action, params, session_vars, flash
   end
