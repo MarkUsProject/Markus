2bc7d32b892c892223a82830705155142d907c97
Ryan S <spring.ryanc@gmail.com>
2015-01-31 11:12:07 -0500

    Saving progress made on iteration2

--- app/assets/javascripts/SourceCodeGlower/SourceCodeLine.js
+++ app/assets/javascripts/SourceCodeGlower/SourceCodeLine.js
@@ -36,9 +36,9 @@ SourceCodeLine.prototype.glow = function(start_column, end_column) {
     (end_column == -1) ? this.getLineNode().textContent.length : end_column);
 
   // If hilitecolor doesnt work try backcolor
-  if (!document.execCommand("HiliteColor", false, this.getGlowDepth() == 1 ? '#fffcc4' : '#ff9')) {
-    document.execCommand("BackColor", false, this.getGlowDepth() == 1 ? '#fffcc4' : '#ff9');
-  }
+  //if (!document.execCommand("HiliteColor", false, this.getGlowDepth() == 1 ? '#fffcc4' : '#ff9')) {
+  //  document.execCommand("BackColor", false, this.getGlowDepth() == 1 ? '#fffcc4' : '#ff9');
+  //}
 
   // Turn off design mode and remove fake mouse selection
   document.designMode = "off";
@@ -53,7 +53,21 @@ SourceCodeLine.prototype.selectGlowRange = function(node, start, end) {
   var foundStart = false;
   var currentCharCount = 0; // The amount of characters we've passed
 
+  //init swaps outside of loop
+  var start_span_plain = null;
+  var start_span_glow = null;
+  var end_span_plain = null;
+  var end_span_glow = null;
+
   // Iterate over text nodes, add length of passed nodes, save start and break at end
+
+  // TODO:
+  // Case 1: highlight one entire node
+  // Case 2: highlight a section of one node
+  // Case 3: highlight two separate nodes + everything in between
+
+  // IDEA: use for loop to find start and end nodes + save offset.
+  // Then after loop check for the case and handle there ie (start_node == end_node)
   for (var i = 0; i < textNodes.length; i++) {
     var endCharCount = currentCharCount + textNodes[i].length;
 
@@ -61,18 +75,70 @@ SourceCodeLine.prototype.selectGlowRange = function(node, start, end) {
     if (!foundStart && start >= currentCharCount && (start < endCharCount || (start == endCharCount && i < textNodes.length))) {
       range.setStart(textNodes[i], start - currentCharCount); // Set start as offset in current text node
       foundStart = true;
-    }
-    // Some code to start replacing the highlight call, adds a class to the nodes between the start and stop
-    //else if (foundStart){
-    //  textNodes[i].parentNode.addClass('source_code_glowing_' + this.getGlowDepth());
-    //}
 
+      // Split the node if the glow starts inside of it
+      if (start != endCharCount) {
+        // Save reference to span
+        var start_node = textNodes[i].parentNode;
+
+        // Create two separate spans, one with the highlight the other without
+        // Save and replace after loop to avoid changing indexes/offsets
+        start_span_plain = document.createElement("span");
+        start_span_glow = document.createElement("span");
+        start_span_plain.innerHTML = start_node.textContent.substr(0, start - currentCharCount);
+        start_span_glow.innerHTML = start_node.textContent.substr(start - currentCharCount);
+        if(start_node.classList.length > 0) {
+          start_span_plain.classList.add(start_node.classList);
+          start_span_glow.classList.add(start_node.classList);
+        }
+        start_span_glow.addClass('source_code_glowing_' + this.getGlowDepth());
+
+
+        // TODO check if end is in the same node and handle that special case, then break! BRAH
+      }
+    }
+    // Adds a class to the nodes between the start and stop
+    else if (foundStart && end >= endCharCount){
+      textNodes[i].parentNode.addClass('source_code_glowing_' + this.getGlowDepth());
+    }
     // If we have the start and the current node contains the end, save it
-    if (foundStart && end <= endCharCount) {
-      range.setEnd(textNodes[i], end - currentCharCount);
+    else if (foundStart && end < endCharCount) {
+      //range.setEnd(textNodes[i], end - currentCharCount);
+
+      // Save reference to span
+      var end_node = textNodes[i].parentNode;
+
+      // Create two separate spans, one with the highlight the other without
+      // Save and replace after loop to avoid changing indexes/offsets
+      var end_span_plain = document.createElement("span");
+      var end_span_glow = document.createElement("span");
+      end_span_plain.innerHTML = end_node.textContent.substr(end - currentCharCount);
+      end_span_glow.innerHTML = end_node.textContent.substr(0, end - currentCharCount);
+      if ( end_node.classList.length > 0) {
+        end_span_plain.classList.add(end_node.classList);
+        end_span_glow.classList.add(end_node.classList);
+      }
+      end_span_glow.addClass('source_code_glowing_' + this.getGlowDepth());
       break;
     }
+
     currentCharCount = endCharCount;
+  }
+
+  // Insert the start nodes and remove the old one
+  if( start_span_glow != null && start_span_plain != null)
+  {
+    start_node.parentNode.insertBefore(start_span_plain, start_node.nextSibling);
+    start_span_plain.parentNode.insertBefore(start_span_glow, start_span_plain.nextSibling);
+    start_node.parentNode.removeChild(start_node);
+  }
+
+  // Insert the end nodes and remove the old one
+  if( end_span_glow != null && end_span_plain != null)
+  {
+    end_node.parentNode.insertBefore(end_span_glow, end_node.nextSibling);
+    end_span_glow.parentNode.insertBefore(end_span_plain, end_span_glow.nextSibling);
+    end_node.parentNode.removeChild(end_node);
   }
 
   // Grab the window selection, clear old range, and set custom range
