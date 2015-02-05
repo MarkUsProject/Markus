331f24b3ec1177e46d39287fa31e6305be43f431
MatakuT <alzu@i.ua>
2015-02-05 16:20:40 -0600

    fixing hounci comments v3

--- spec/models/submission_rule_spec.rb
+++ spec/models/submission_rule_spec.rb
@@ -7,26 +7,32 @@ describe SubmissionRule do
     let(:assignment) { create(:assignment) }
 
     it 'returns something other than nil' do
-      expect(assignment.submission_rule.calculate_collection_time).to_not be_nil
+      expect(assignment.submission_rule.calculate_collection_time).
+            to_not be_nil
     end
     
     it 'returns date value' do
-      expect(assignment.submission_rule.calculate_collection_time.to_date).to be_kind_of(Date)
+      expect(assignment.submission_rule.
+             calculate_collection_time.to_date).
+             to be_kind_of(Date)
     end
   end
   
-  
   context '#calculate_grouping_collection_time' do
-      let(:assignment) { create(:assignment) }
-      let(:grouping) { create(:grouping) }
+    let(:assignment) { create(:assignment) }
+    let(:grouping) { create(:grouping) }
       
-      it 'returns something other than nil' do
-          expect(assignment.submission_rule.calculate_grouping_collection_time(grouping)).to_not be_nil
-      end
+    it 'returns something other than nil' do
+      expect(assignment.submission_rule.
+             calculate_grouping_collection_time(grouping)).
+             to_not be_nil
+    end
       
-      it 'returns date value' do
-          expect(assignment.submission_rule.calculate_grouping_collection_time(grouping).to_date).to be_kind_of(Date)
-      end
+    it 'returns date value' do
+      expect(assignment.submission_rule.
+             calculate_grouping_collection_time(grouping).
+             to_date).to be_kind_of(Date)
+    end
       
       # create a new test that is triggered when grouping exists
   end
