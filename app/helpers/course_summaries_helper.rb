06402d3ed3525e9c009afdd1cae7e77c1671f9e0
Ishan Thukral <dev@ishan.me>
2015-02-05 02:25:42 -0500

    marks now show up in table

--- app/helpers/course_summaries_helper.rb
+++ app/helpers/course_summaries_helper.rb
@@ -9,7 +9,8 @@ module CourseSummariesHelper
 				:id => student.id,
 				:user_name => student.user_name,
 				:first_name => student.first_name,
-				:last_name => student.last_name
+				:last_name => student.last_name,
+				:marks => get_marks_for_student_for_assignment(student, all_assignments)
 			}
 		end
 
@@ -24,7 +25,45 @@ module CourseSummariesHelper
 
 	end
 
-	def get_marks_for_student_for_assignment(student_id, assignment_id)
+	def get_marks_for_student_for_assignment(student, all_assignments)
+		marks = []
+		all_assignments.each do |assignment|
+			marks[assignment.id-1] = get_mark_for_assignment_and_student(assignment, student)
+		end
+		marks
+	end
+
+	def get_mark_for_assignment_and_student(assignment, student)
+		grouping = get_grouping_for_user_for_assignment(student, assignment)
+		if (grouping)
+			submission = Submission.where(grouping_id: grouping.id).first
+			if (submission)
+				result = Result.where(submission_id: submission.id).first
+				if (result)
+					return result.total_mark
+				end
+			end
+		end
+		return nil
+	end
+
+	def get_grouping_for_user_for_assignment(student, assignment)
+		memberships = Membership.where(user_id: student.id)
+		if (memberships.count == 0)
+			return nil
+		end
+		grouping = get_grouping_for_assignment(memberships, assignment)
+		return grouping
+	end
+
+	def get_grouping_for_assignment(memberships, assignment)
+		memberships.each do |membership|
+			grouping = Grouping.where(id: membership.grouping_id).first
+			if (grouping.assignment_id == assignment.id)
+				return grouping
+			end
+		end
+		return nil
 	end
 
 end
