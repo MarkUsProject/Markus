487d1cd2406d3b565035edddb4dbffd730f7f9bb
Ishan Thukral <dev@ishan.me>
2015-02-17 00:59:09 -0500

    added react table outline for marking schemes

--- app/controllers/marking_schemes_controller.rb
+++ app/controllers/marking_schemes_controller.rb
@@ -1,8 +1,10 @@
 class MarkingSchemesController < ApplicationController
   def index
+    @assignments = Assignment.all
   end
   
   def populate
+    render json: get_table_json_data
   end
   
   def new
