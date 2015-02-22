033c4a5b9d443afbd875f062d9d3f4a942456baf
MatakuT <alzu@i.ua>
2015-02-22 12:38:20 -0600

    fixed hounci comments v1

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -5,9 +5,9 @@ require 'spec_helper'
 describe 'Routing to main page', :type => :routing do
   context 'Locale-less root' do
     it 'routes / to login' do
-      expect(:get => "/").to route_to(
-        :controller => 'main',
-        :action => 'login'
+      expect(get: '/').to route_to(
+        controller: 'main',
+        action: 'login'
       )
     end
   end
@@ -15,7 +15,7 @@ describe 'Routing to main page', :type => :routing do
   
   context 'Root with locale' do
     it 'routes /en/ to error' do
-      expect(:get => "/en/").not_to be_routable
+      expect(get: "/en/").not_to be_routable
     end
   end
 end
@@ -28,40 +28,40 @@ context 'Admin resource' do
 
   
   it 'routes GET index correctly' do
-    expect(:get => path).to route_to(
-      :controller => ctrl,
-      :action => 'index',
-      :locale => 'en')
+    expect(get: path).to route_to(
+      controller: ctrl,
+      action: 'index',
+      locale: 'en')
   end
   
   it 'routes GET new correctly' do
-    expect(:get => path + '/new').to route_to(
-      :controller => ctrl,
-      :action => 'new',
-      :locale => 'en')
+    expect(get: path + '/new').to route_to(
+      controller: ctrl,
+      action: 'new',
+      locale: 'en')
   end
   
   it 'routes POST create correctly' do
-    expect(:post => path).to route_to(
-      :controller => ctrl,
-      :action => 'create',
-      :locale => 'en')
+    expect(post: path).to route_to(
+      controller: ctrl,
+      action: 'create',
+      locale: 'en')
   end
   
   it 'routes GET show correctly' do
-    expect(:get => path + '/' + admin.id.to_s).to route_to(
-      :controller => ctrl,
-      :action => 'show',
-      :id => admin.id.to_s,
-      :locale => 'en')
+    expect(get: path + '/' + admin.id.to_s).to route_to(
+      controller: ctrl,
+      action: 'show',
+      id: admin.id.to_s,
+      locale: 'en')
   end
   
   it 'routes GET edit correctly' do
-    expect(:get => path + '/' + admin.id.to_s + '/edit').to route_to(
-      :controller => ctrl,
-      :action => 'edit',
-      :id => admin.id.to_s,
-      :locale => 'en')
+    expect(get: path + '/' + admin.id.to_s + '/edit').to route_to(
+      controller: ctrl,
+      action: 'edit',
+      id: admin.id.to_s,
+      locale: 'en')
   end
   
 end
