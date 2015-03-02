48a89dee601c41d2076184e45253f7490cef2133
MatakuT <alzu@i.ua>
2015-03-01 19:54:37 -0600

    fixed hounci comments v4

--- spec/factories/groupings.rb
+++ spec/factories/groupings.rb
@@ -1,14 +1,12 @@
 FactoryGirl.define do
-
   factory :grouping do
     association :group
     association :assignment
   end
 
-factory :grouping2, class: Grouping do
+  factory :grouping2, class: Grouping do
     association :group
     association :assignment
     inviter { Student.new(section: Section.new) }
   end
-
 end
