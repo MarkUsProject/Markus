15b87523207ec63859a7dd23f99be8b6106d28f2
lifejuggler <jtchoiboy@gmail.com>
2015-03-19 15:34:00 -0400

    Revert "Revert "master""
    This reverts commit b0f5ca701f5d97d72f70ebedeee2e99034f796ca, reversing
    changes made to 2bb24fe992d0ee0e5e5858d473dea448b8df8767.

--- spec/factories/groupings.rb
+++ spec/factories/groupings.rb
@@ -3,4 +3,10 @@ FactoryGirl.define do
     association :group
     association :assignment
   end
+
+  factory :grouping_with_inviter, class: Grouping do
+    association :group
+    association :assignment
+    inviter { Student.new(section: Section.new) }
+  end
 end
