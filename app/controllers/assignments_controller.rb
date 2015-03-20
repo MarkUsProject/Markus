70d548963315238ce9e116d065603db4e2a43ca6
Stephen Tsimicalis <stephen.tsimicalis@gmail.com>
2015-03-20 18:38:19 -0400

    Revert "Fixed sorting issue with enum_penalty"
    This reverts commit 9ec55797c3fc8ecbecaae5c814c223e6d72ec773.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -78,7 +78,9 @@ class AssignmentsController < ApplicationController
     @student = current_user
     @grouping = @student.accepted_grouping_for(@assignment.id)
     @penalty = SubmissionRule.find_by_assignment_id(@assignment.id)
-    @enum_penalty = Period.find_all_by_submission_rule_id(@penalty.id).sort
+    # sort if it's penaltydecay or penalty period
+    @enum_penalty = Period.find_all_by_submission_rule_id(@penalty.id)
+    @enum_penalty.sort_by { |obj| obj.id }
 
     if @student.section &&
        !@student.section.section_due_date_for(@assignment.id).nil?
