24b2f788b9a3fac78375fe9c869f7a8c65146c13
Paymahn Moghadasian <paymahn1@gmail.com>
2015-03-21 16:27:51 -0500

    Preview new annotation with math
    When a marker creates a new annotation with math in it,
    a preview of the annotation will be shown to the marker
    in the create-an-annotation popup.

--- app/assets/javascripts/MathJax/mathjax_helpers.js
+++ app/assets/javascripts/MathJax/mathjax_helpers.js
@@ -3,3 +3,25 @@
 function reloadDOM() {
     MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 }
+
+function updateAnnotationPreview(){
+    var updatedAnnotation = document.getElementById("new_annotation_content").value;
+
+    var previewParagraph = document.getElementById("annotation_preview");
+    var title = document.getElementById("annotation_preview_title");
+
+    var firstIndex = updatedAnnotation.indexOf("$$");
+    var secondIndex = updatedAnnotation.indexOf("$$", firstIndex + 1);
+
+    if(firstIndex != -1 && secondIndex != -1 && firstIndex != secondIndex){
+        title.show();
+        previewParagraph.show();
+        previewParagraph.innerHTML = updatedAnnotation;
+
+        // typeset the preview
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, previewParagraph]);
+    }else{
+        title.hide();
+        previewParagraph.hide()
+    }
+}
