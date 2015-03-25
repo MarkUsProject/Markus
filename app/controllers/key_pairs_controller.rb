35cc14f65a077e2212c1f5869019612977f91034
Peter Zhao <peterzhao333@gmail.com>
2015-03-25 19:19:09 -0400

    Revert "Key Upload and Key Location Constant"
    This reverts commit 4ef52624df30e5ef013e24052484e06cece10a41.

--- app/controllers/key_pairs_controller.rb
+++ app/controllers/key_pairs_controller.rb
@@ -37,42 +37,18 @@ class KeyPairsController < ApplicationController
     @key_pair = KeyPair.find(params[:id])
   end
 
-  # Given a File object to upload, save it on the file system with
-  # association to the user_name given.
-  # Admins are able to save keys under different user names then
-  # their own.
-  # Creates the KEY_STORAGE directory if it does not yet exist
-  def upload_key_file(file, user_name)
-    Dir.mkdir(KEY_STORAGE) unless File.exists?(KEY_STORAGE)
-    uploaded_io = key_pair_params[:file]
-
-    # todo: check current user to see if they have permission to
-    # use a different user_name then their current one (admins)
-
-    File.open(Rails.root.join(KEY_STORAGE, user_name + '.pub'), 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
-
-  end
-
   # POST /key_pairs
   # POST /key_pairs.json
   def create
-    upload_key_file(key_pair_params[:file], key_pair_params[:user_name])
     @key_pair = KeyPair.new(key_pair_params)
 
-    # Set the file_name to key's new name
-    @key_pair[:file_name] = key_pair_params[:user_name] + '.pub'
     respond_to do |format|
       if @key_pair.save
-        format.html { redirect_to @key_pair,
-                                  notice: 'Key pair was successfully created.'}
-        format.json { render json: @key_pair,
-            status: :created, location: @key_pair }
+        format.html { redirect_to @key_pair, notice: 'Key pair was successfully created.' }
+        format.json { render json: @key_pair, status: :created, location: @key_pair }
       else
         format.html { render action: "new" }
-        format.json { render json: @key_pair.errors,
-            status: :unprocessable_entity }
+        format.json { render json: @key_pair.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -111,6 +87,6 @@ class KeyPairsController < ApplicationController
     # params.require(:person).permit(:name, :age)
     # Also, you can specialize this method with per-user checking of permissible attributes.
     def key_pair_params
-      params.require(:key_pair).permit(:file, :user_id, :user_name)
+      params.require(:key_pair).permit(:file_name, :user_id, :user_name)
     end
 end
