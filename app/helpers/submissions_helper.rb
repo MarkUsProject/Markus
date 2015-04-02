a58fd94f5ca5d701c9156b90882fe5028531b2ca
Christian Millar <christian_millar@msn.com>
2015-04-02 12:53:12 -0700

    Removed the method, now we just don't show members at all. Also, removed from the submission screen.

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -74,18 +74,18 @@ module SubmissionsHelper
     group_name = ''
       if !grouping.has_submission?
         if assignment.submission_rule.can_collect_grouping_now?(grouping)
-          group_name = view_context.link_to(grouping.get_group_name,
+          group_name = view_context.link_to(grouping.group.group_name,
             collect_and_begin_grading_assignment_submission_path(
               assignment.id, grouping.id))
         else
-          group_name = grouping.get_group_name
+          group_name = grouping.group.group_name
         end
       elsif !grouping.is_collected
-        group_name = view_context.link_to(grouping.get_group_name,
+        group_name = view_context.link_to(grouping.group.group_name,
           collect_and_begin_grading_assignment_submission_path(
             assignment.id, grouping.id))
       else
-        group_name = view_context.link_to(grouping.get_group_name,
+        group_name = view_context.link_to(grouping.group.group_name,
           edit_assignment_submission_result_path(
             assignment.id, grouping.current_submission_used.id,
             grouping.current_submission_used.get_latest_result))
