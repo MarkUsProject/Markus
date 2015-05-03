2e1e5cb65ebb9cb9ef73cd47ebd7de7c11844f7d
Eugene Cheung <ey.cheung@outlook.com>
2015-05-02 23:10:54 -0400

    Tweak new annotation dialog

--- app/assets/javascripts/MathJax/mathjax_helpers.js
+++ app/assets/javascripts/MathJax/mathjax_helpers.js
@@ -4,29 +4,16 @@ function reloadDOM() {
   MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
 }
 
-function recenterDialog() {
-  var dialog = jQuery('#create_annotation_dialog');
-  dialog.css('margin-left', -1 * dialog.width() / 2);
-}
-
-function hideAnnotationPreview() {
-  document.getElementById('annotation-preview').style.display = 'none';
-  recenterDialog();
-}
-
-function showAnnotationPreview() {
-  document.getElementById('annotation-preview').style.display = '';
-  recenterDialog();
-}
-
 function updateAnnotationPreview() {
   var newAnnotation = document.getElementById('new_annotation_content').value;
 
   var preview = document.getElementById('annotation-preview-text');
   preview.innerHTML = newAnnotation;
 
-  showAnnotationPreview();
-
   // Typeset the preview
   MathJax.Hub.Queue(['Typeset', MathJax.Hub, preview]);
+
+  // Recenter dialog
+  var dialog = jQuery('#create_annotation_dialog');
+  dialog.css('margin-left', -1 * dialog.width() / 2);
 }
