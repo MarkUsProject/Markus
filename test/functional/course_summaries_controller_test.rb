f1c64e400f2455e105d4a7f1f5567a713c5fa218
David Liu <liudavid@cs.toronto.edu>
2015-05-05 12:19:02 -0400

    remove scaffolded files

--- test/functional/course_summaries_controller_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CourseSummariesControllerTest < ActionController::TestCase
-end
--- test/unit/helpers/course_summaries_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CourseSummariesHelperTest < ActionView::TestCase
-end
