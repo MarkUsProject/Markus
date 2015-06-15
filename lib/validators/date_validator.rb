7a89978267645bd1622fa642963f56a51fb1b805
Lysette <isjochan@gmail.com>
2015-06-15 18:39:03 +0000

    fixed date validator and added it back in grade entry form

--- lib/validators/date_validator.rb
+++ lib/validators/date_validator.rb
@@ -2,7 +2,7 @@
 class DateValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    if Time.zone.parse(value.to_s).nil? && !value.nil?
+    if Time.zone.parse(value.to_s).nil? && !record.date_before_type_cast.blank?
       record.errors.add attribute, I18n.t('date_validator.invalid_date')
       false
     end
