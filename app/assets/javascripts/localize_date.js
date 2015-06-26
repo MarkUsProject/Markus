bb9ae375bc852c9b76d6e571015d3ad97ed484fc
David Liu <liudavid@cs.toronto.edu>
2015-06-26 16:37:38 -0400

    due-dates: fix time zone in penalty period preview times

--- app/assets/javascripts/localize_date.js
+++ app/assets/javascripts/localize_date.js
@@ -1,7 +1,7 @@
 /** Convert a date/time to a nice Date object.
     Example: 2014-08-21 14:38:00 UTC -> [Date object] */
 function convert_date_time(date) {
-  var arr_date = date.split(' ');
+  var arr_date = date.split(/[ T]/);
   var iso_date = arr_date[0] + ' ' + arr_date[1] + arr_date[2];
   return moment(iso_date);
 }
