950191436323996b2d84c14ca19f2702e83c6696
Alexander Grenier <alex.grenier@gmail.com>
2015-06-29 11:05:47 -0400

    Hound

--- spec/lib/repo/git_revision_spec.rb
+++ spec/lib/repo/git_revision_spec.rb
@@ -45,7 +45,7 @@ describe Repository::GitRevision do
       # Repo is created by gitolite, proceed to clone it in
       # the repository storage location
       Git.clone('git@localhost:' + "test_repo",
-       "#{::Rails.root}/data/test/workdir")
+                "#{::Rails.root}/data/test/workdir")
     end
 
     let!(:repo) { build(:git_repository) }
