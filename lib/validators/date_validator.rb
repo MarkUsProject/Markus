3bc0d9b21db053ffedf7404eba8db39e6b47e434
Lysette <isjochan@gmail.com>
2015-07-06 14:58:27 +0000

    changed date_before_type_cast

--- lib/validators/date_validator.rb
+++ lib/validators/date_validator.rb
@@ -2,8 +2,7 @@
 class DateValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    temp = {attribute => value}
-    if Time.zone.parse(value.to_s).nil? && !temp.read_attribute_before_type_cast(attribute).blank?
+    if Time.zone.parse(value.to_s).nil? && !record.read_attribute_before_type_cast(attribute).blank?
       record.errors.add attribute, I18n.t('date_validator.invalid_date')
       false
     end
