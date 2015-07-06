584184258bd825f468e8e25991831b92113334cd
Lysette <isjochan@gmail.com>
2015-07-06 14:10:18 +0000

    remove date label now localized to different languages

--- lib/validators/date_validator.rb
+++ lib/validators/date_validator.rb
@@ -2,7 +2,8 @@
 class DateValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    if Time.zone.parse(value.to_s).nil? && !record.date_before_type_cast.blank?
+    temp = {attribute => value}
+    if Time.zone.parse(value.to_s).nil? && !temp.read_attribute_before_type_cast(attribute).blank?
       record.errors.add attribute, I18n.t('date_validator.invalid_date')
       false
     end
