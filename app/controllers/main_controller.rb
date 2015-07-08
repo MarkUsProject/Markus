813d032776121660b8f5e1d0e8870e1ab3a971fa
David Liu <liudavid@cs.toronto.edu>
2015-07-08 11:24:42 -0400

    fix order calls for Rails 4

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -157,7 +157,7 @@ class MainController < ApplicationController
     ]).order('due_date ASC')
     @grade_entry_forms = GradeEntryForm.unscoped.includes([
       :grade_entry_items
-    ]).all(order: 'id ASC')
+    ]).order('id ASC')
 
     @current_assignment = Assignment.get_current_assignment
 
