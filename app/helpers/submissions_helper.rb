4e5d895dc1049e753e9177c20f924e0af1c61241
hootiehoo <jesse.riemann@gmail.com>
2015-07-09 12:20:18 -0400

    fixed n+1 query on submissions page as Ta

--- app/helpers/submissions_helper.rb
+++ app/helpers/submissions_helper.rb
@@ -254,7 +254,8 @@ module SubmissionsHelper
 
   # Checks if all the assignments for the current submission are marked.
   def all_assignments_marked?
-    Assignment.find(params[:assignment_id]).groupings.all? do |grouping|
+    Assignment.includes(:groupings => :current_submission_used).find(
+      params[:assignment_id]).groupings.all? do |grouping|
       grouping.marking_completed?
     end
   end
