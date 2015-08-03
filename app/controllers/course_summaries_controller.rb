95a472b6ba6882b5c62f3d89c62e0d1fd0122599
PatGuan <vagrant@vagrant-ubuntu-precise-32.(none)>
2015-08-03 21:03:28 +0000

    Fixed styling errors

--- app/controllers/course_summaries_controller.rb
+++ app/controllers/course_summaries_controller.rb
@@ -25,13 +25,13 @@ class CourseSummariesController < ApplicationController
     course_name = "#{COURSE_NAME}"
     course_name_underscore = course_name.squish.downcase.tr(" ", "_")
     send_data csv_string, disposition: 'attachment',
-                          filename: "#{course_name_underscore}_grades_report.csv"
+                         filename: "#{course_name_underscore}_grades_report.csv"
   end
 end
 
 # ---- HELPER FUNCTIONS FOR DOWNLOAD_CSV_GRADES_REPORT BELOW-----#
 
-# Helper function for download_csv_grades_report. Populates the headers in the CSV
+# Populates the headers in the CSV
 def populate_header_titles(assignments)
   header = ['Username']
   assignments.each do |assignment|
@@ -40,17 +40,18 @@ def populate_header_titles(assignments)
   header.push('Total marks')
 end
 
-# Helper function for populate_students_and_marks. Checks grouping, submission and it's mark for nil.
+# Checks grouping, submission and it's mark for nil.
 def fill_row_with_marks(grouping, row)
   if check_for_nil_fields(grouping)
     row.push('')
   else
-    row.push(grouping.current_submission_used.get_latest_result.total_mark/grouping.assignment.total_mark * 100)
+    row.push(grouping.current_submission_used.get_latest_result.total_mark /
+                                       grouping.assignment.total_mark * 100)
   end
   row
 end
 
-# Helper function for download_csv_grades_report. Loads up the CSV with headers, students and marks.
+# Loads up the CSV with headers, students and marks.
 def populate_students_and_marks(csv)
   students = Student.all
   assignments = Assignment.order(:id)
@@ -79,7 +80,8 @@ def get_total_marks_for_student(grouping)
     # Create Hashmap with Criteria ID as keys and it's weight as values
     criteria_id_to_weight = return_criteria_to_weight_for_grouping(grouping)
     get_assignment_marks(grouping).each do |mark|
-      current_total_marks += mark.mark * criteria_id_to_weight[mark.markable_id].to_f
+      current_total_marks += mark.mark *
+                              criteria_id_to_weight[mark.markable_id].to_f
     end
   end
   current_total_marks
@@ -88,9 +90,10 @@ end
 # Function that is called to check for nill grouping, submissions and marks
 def check_for_nil_fields(grouping)
   if grouping.nil? || grouping.current_submission_used.nil? ||
-    (grouping.current_submission_used.get_latest_result.total_mark/grouping.assignment.total_mark * 100).nan?
+    (grouping.current_submission_used.get_latest_result.total_mark /
+                          grouping.assignment.total_mark * 100).nan?
     return true
-  else 
+  else
     return false
   end
 end
@@ -99,21 +102,21 @@ end
 def return_criteria_to_weight_for_grouping(grouping)
   criteria_id_to_weight = {}
   if grouping.assignment.marking_scheme_type == 'rubric'
-    criteria_id_to_weight = map_criteria_id_to_weight('rubric', 
-      grouping.assignment.rubric_criteria)
+    criteria_id_to_weight = map_criteria_id_to_weight('rubric',
+                                                      grouping.assignment.rubric_criteria)
   elsif grouping.assignment.marking_scheme_type == 'flexible'
-    criteria_id_to_weight = map_criteria_id_to_weight('flexible', 
-      grouping.assignment.flexible_criteria)
+    criteria_id_to_weight = map_criteria_id_to_weight('flexible',
+                                                      grouping.assignment.flexible_criteria)
   end
   criteria_id_to_weight
 end
- 
+
 # Returns the marks that the group got on the assignment
 def get_assignment_marks(grouping)
   grouping.current_submission_used.results.first.marks
 end
 
-# Load a hashmap with the criteria id key to its value weight according to its criterion
+# Load a hashmap with the criteria id to its weight according to its criterion
 def map_criteria_id_to_weight(type, criterias)
   criteria_id_to_weight = {}
   if type == 'rubric'
