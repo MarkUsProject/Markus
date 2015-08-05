0eb3179b127b496402075c4673d95f660b0d8524
Danny Reis <dannyreisdmr@gmail.com>
2015-08-05 00:04:03 -0400

    Created CSV with Spreadsheets and Weights

--- app/controllers/course_summaries_controller.rb
+++ app/controllers/course_summaries_controller.rb
@@ -17,39 +17,50 @@ class CourseSummariesController < ApplicationController
   end
 
   def download_csv_grades_report
-    assignments = Assignment.order(:id)
-    students = Student.all
     csv_string = CSV.generate do |csv|
-      header = ['Username']
-      assignments.each do |assignment|
-        header.push(assignment.short_identifier)
-      end
-      csv << header
-      students.each do |student|
-        row = []
-        row.push(student.user_name)
-        assignments.each do |assignment|
-          out_of = assignment.total_mark
-          grouping = student.accepted_grouping_for(assignment.id)
-          if grouping.nil?
-            row.push('')
-          else
-            submission = grouping.current_submission_used
-            if submission.nil?
-              row.push('')
-            else
-              total_mark_percentage = submission.get_latest_result.total_mark / out_of * 100
-              if total_mark_percentage.nan?
-                row.push('')
-              else
-                row.push(total_mark_percentage)
-              end
-            end
-          end
-        end
-        csv << row
-      end
+      generate_csv_header(csv)
+      insert_student_marks(csv)
     end
+    name_grades_report_file(csv_string)
+  end
+
+  def generate_csv_header(csv)
+    assignments = Assignment.order(:id)
+    grade_entry_forms = GradeEntryForm.order(:id)
+    marking_schemes = MarkingScheme.order(:id)
+
+    header = ['Username']
+    assignments.each do |assignment|
+      header.push(assignment.short_identifier)
+    end
+    grade_entry_forms.each do |grade_entry_form|
+      header.push(grade_entry_form.short_identifier)
+    end
+    marking_schemes.each do |marking_scheme|
+      header.push(marking_scheme.name)
+    end
+    csv << header
+  end
+
+  def insert_student_marks(csv)
+    JSON.parse(get_table_json_data).each do |student|
+      puts (student)
+      row = []
+      row.push(student['user_name'])
+      student['assignment_marks'].values.each do |assignment_mark|
+        row.push(assignment_mark)
+      end
+      student['grade_entry_form_marks'].values.each do |grade_entry_form_mark|
+        row.push(grade_entry_form_mark)
+      end
+      student['weighted_marks'].values.each do |scheme_grade|
+        row.push(scheme_grade)
+      end
+      csv << row
+    end
+  end
+
+  def name_grades_report_file(csv_string)
     course_name = "#{COURSE_NAME}"
     course_name_underscore = course_name.squish.downcase.tr(" ", "_")
     send_data csv_string, disposition: 'attachment',
