c5b7629cd0a22be3cfebf93c99d72ced509f3a18
David Liu <liudavid@cs.toronto.edu>
2015-08-12 12:56:49 -0400

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -315,7 +315,6 @@ Markus::Application.routes.draw do
         get 'populate'
         get 'get_marking_scheme_details'
         get 'download_csv_grades_report'
-        get 'student_course_summary'
       end
     end
 
