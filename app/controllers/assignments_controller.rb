69c36aeeb4b1f4fb2a1c5c5ecf38eb163f450ebd
shaughnfinnerty <finnergizer@gmail.com>
2015-09-19 13:00:14 -0400

    Made hidden grade_entry_forms not appear in assignment index for students.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -140,9 +140,10 @@ class AssignmentsController < ApplicationController
   # assignment information
   def index
     @marking_schemes = MarkingScheme.all
-    @grade_entry_forms = GradeEntryForm.order(:id)
+
     @default_fields = DEFAULT_FIELDS
     if current_user.student?
+      @grade_entry_forms = GradeEntryForm.where(is_hidden:false).order(:id)
       @assignments = Assignment.where(is_hidden: false).order(:id)
       #get the section of current user
       @section = current_user.section
@@ -175,9 +176,11 @@ class AssignmentsController < ApplicationController
 
       render :student_assignment_list
     elsif current_user.ta?
+      @grade_entry_forms = GradeEntryForm.order(:id)
       @assignments = Assignment.includes(:submission_rule).order(:id)
       render :grader_index
     else
+      @grade_entry_forms = GradeEntryForm.order(:id)
       @assignments = Assignment.includes(:submission_rule).order(:id)
       render :index
     end
