8f4c35b719d32f60e5e6896aa9a9d1ae171fe627
Melissa Neubert <mneubert@mail.uoguelph.ca>
2015-09-20 13:52:25 -0400

    Fixed 404 and 500 error for remark request

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -197,8 +197,8 @@ class Submission < ActiveRecord::Base
     end
 
     original_result.marks.each do |mark|
-      remark_result.marks.create(mark.attributes.merge(
-                                   result: remark, created_at: Time.zone.now))
+     remark_result.marks.create(result: remark, created_at: Time.zone.now, markable_id: extra_mark.markable_id,
+                                  mark: extra_mark.mark, markable_type: extra_mark.markable_type)
     end
   end
 
