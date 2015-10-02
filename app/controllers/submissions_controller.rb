0fce6a686140ec09c6cae7824c0733951389c21c
Karen Reid <reid@cs.toronto.edu>
2015-10-02 19:09:26 -0400

    fixing hound complaints

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -117,8 +117,10 @@ class SubmissionsController < ApplicationController
     last_rev = @grouping.submissions
     @last_submission = nil
     if !last_rev.empty?
-      selected = @revisions_history.select { |rev| rev[:num] == 
-                                             last_rev.last.revision_number }
+      selected = @revisions_history.select do |rev| 
+        rev[:num] == last_rev.last.revision_number 
+      end
+
       @last_submission = selected.empty? ? nil : last_rev.last
     end
 
