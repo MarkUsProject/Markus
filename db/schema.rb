33ea4830dbc1951eb8a3c1ac51cf7a6895ed6a3c
david-yz-liu <liudavid@cs.toronto.edu>
2015-11-10 11:39:26 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20151005010958) do
     t.boolean  "is_hidden",                        default: false
     t.boolean  "only_required_files"
     t.boolean  "vcs_submit"
+    t.boolean  "unlimited_tokens",                 default: false
   end
 
   add_index "assignments", ["short_identifier"], name: "index_assignments_on_short_identifier", unique: true, using: :btree
@@ -415,29 +416,58 @@ ActiveRecord::Schema.define(version: 20151005010958) do
 
   add_index "tags", ["user_id"], name: "index_tags_on_user_id", using: :btree
 
-  create_table "test_files", force: :cascade do |t|
-    t.string   "filename"
-    t.integer  "assignment_id"
-    t.string   "filetype"
-    t.boolean  "is_private"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "test_files", ["assignment_id", "filename"], name: "index_test_files_on_assignment_id_and_filename", unique: true, using: :btree
-
   create_table "test_results", force: :cascade do |t|
-    t.string   "filename"
-    t.text     "file_content"
-    t.integer  "submission_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "status"
-    t.integer  "user_id"
+    t.integer "grouping_id"
+    t.integer "test_script_id"
+    t.integer "test_script_result_id"
+    t.string  "name"
+    t.string  "completion_status",     null: false
+    t.integer "marks_earned",          null: false
+    t.integer "repo_revision"
+    t.text    "input_description",     null: false
+    t.text    "actual_output",         null: false
+    t.text    "expected_output",       null: false
+    t.integer "submission_id"
   end
 
-  add_index "test_results", ["filename"], name: "index_test_results_on_filename", using: :btree
-  add_index "test_results", ["submission_id"], name: "index_test_results_on_submission_id", using: :btree
+  add_index "test_results", ["grouping_id", "test_script_id"], name: "grouping_id_and_test_script_id", using: :btree
+
+  create_table "test_script_results", force: :cascade do |t|
+    t.integer  "grouping_id"
+    t.integer  "test_script_id"
+    t.integer  "marks_earned"
+    t.integer  "repo_revision"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "submission_id"
+  end
+
+  create_table "test_scripts", force: :cascade do |t|
+    t.integer "assignment_id",           null: false
+    t.float   "seq_num",                 null: false
+    t.string  "script_name",             null: false
+    t.text    "description",             null: false
+    t.integer "max_marks",               null: false
+    t.boolean "run_on_submission"
+    t.boolean "run_on_request"
+    t.boolean "halts_testing"
+    t.string  "display_description",     null: false
+    t.string  "display_run_status",      null: false
+    t.string  "display_marks_earned",    null: false
+    t.string  "display_input",           null: false
+    t.string  "display_expected_output", null: false
+    t.string  "display_actual_output",   null: false
+  end
+
+  add_index "test_scripts", ["assignment_id", "seq_num"], name: "index_test_scripts_on_assignment_id_and_seq_num", using: :btree
+
+  create_table "test_support_files", force: :cascade do |t|
+    t.string  "file_name",     null: false
+    t.integer "assignment_id", null: false
+    t.text    "description",   null: false
+  end
+
+  add_index "test_support_files", ["assignment_id"], name: "index_test_files_on_assignment_id", using: :btree
 
   create_table "tokens", force: :cascade do |t|
     t.integer "grouping_id"
