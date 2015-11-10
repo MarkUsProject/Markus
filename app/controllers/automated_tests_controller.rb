508746a6c97d2f5e293bcb05f0b439665ec9b7f0
Jeremy Goh <jeremy.goh@alumni.ubc.ca>
2015-11-10 01:39:44 -0500

    fixed interface to display test results

--- app/controllers/automated_tests_controller.rb
+++ app/controllers/automated_tests_controller.rb
@@ -36,15 +36,15 @@ class AutomatedTestsController < ApplicationController
     # Perform transaction, if errors, none of new config saved
     @assignment.transaction do
       @assignment = process_test_form(@assignment, assignment_params)
-      # begin
-      #   # Process testing framework form for validation
-      #   @assignment = process_test_form(@assignment, params)
-      # rescue Exception, RuntimeError => e
-      #   @assignment.errors.add(:base, I18n.t("assignment.error",
-      #                                        :message => e.message))
-      #   render :manage
-      #   return
-      # end
+      begin
+        # Process testing framework form for validation
+        @assignment = process_test_form(@assignment, params)
+      rescue Exception, RuntimeError => e
+        @assignment.errors.add(:base, I18n.t("assignment.error",
+                                             :message => e.message))
+        render :manage
+        return
+      end
 
       # Save assignment and associated test files
       if @assignment.save
