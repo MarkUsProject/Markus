fa5c6c12b2c700dcb15819d4a9d622b687e0c93e
shaughnfinnerty <finnergizer@gmail.com>
2015-11-30 02:48:53 -0500

    Fixed mathjax rendering issue with redcarpet markdown in annotation summary list. Removed unnecessary whitespace.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,11 +30,10 @@ module ApplicationHelper
   end
 
   def markdown(text)
-    options = { filter_html:     false, hard_wrap:       true,
+    options = { filter_html: false, hard_wrap: true,
                 link_attributes: { rel: 'nofollow', target: '_blank' },
                 space_after_headers: true, fenced_code_blocks: true }
-    extensions = { autolink: true, superscript: true,
-                   disable_indented_code_blocks: true }
+    extensions = { autolink: true }
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(text).html_safe
