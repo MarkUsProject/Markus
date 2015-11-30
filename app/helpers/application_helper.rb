181e5b90f5d6eeb83888f4e639896c2a5a93ac04
shaughnfinnerty <finnergizer@gmail.com>
2015-11-30 01:54:31 -0500

    Adding redcarpet for server-side rendering. Added marked for client-side changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,15 @@ module ApplicationHelper
       flash.now[type].push(text)
     end
   end
+
+  def markdown(text)
+    options = { filter_html:     false, hard_wrap:       true,
+                link_attributes: { rel: 'nofollow', target: '_blank' },
+                space_after_headers: true, fenced_code_blocks: true }
+    extensions = { autolink: true, superscript: true,
+                   disable_indented_code_blocks: true }
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
+  end
 end
