cbff532666d6be1776f5d90cb6a55688d2e16ace
William Kwan <william@voidupdate.com>
2015-12-07 00:37:16 -0500

    make a select_file_id so that previous code depending on the flat file list still works

--- app/assets/javascripts/Grader/file_selector.js
+++ app/assets/javascripts/Grader/file_selector.js
@@ -1,28 +1,7 @@
-function bump_select(select_node_id, bump_amount, back_button_id, next_button_id) {
-  // var select_node = document.getElementById(select_node_id);
-  // var back_button = document.getElementById(back_button_id);
-  // var next_button = document.getElementById(next_button_id);
-
-  // var node_num       = select_node.length;
-  // var selected_index = select_node.selectedIndex;
-  // var result         = selected_index + bump_amount;
-
-  // next_button.disabled = (result == node_num - 1);
-  // back_button.disabled = (result == 0);
-
-  // if (result >= node_num || result < 0) {
-  //   back_button.disabled = true;
-  //   next_button.disabled = true;
-  //   return false;
-  // }
-
-  // select_node.selectedIndex = result;
-  // load_submitted_file(select_node.value);
-}
-
-function open_file(select_node_id, filename) {
-  load_submitted_file(select_node_id);
-  document.getElementById("file_selector_dropdown_text").innerHTML = filename;
+function open_file(id, name) {
+  load_submitted_file(id);
+  document.getElementById("select_file_id").value = id;
+  document.getElementById("file_selector_dropdown_text").innerHTML = name;
 }
 
 function open_submenu(dir_element) {
@@ -53,9 +32,9 @@ function close_submenu_recursive(dir_element, orig_dir_element) {
 }
 
 jQuery(document).ready(function() {
-  if (first_file_to_load_id != null && first_file_to_load_name != null)
+  if (first_file_id != null && first_file_name != null)
   {
-    open_file(first_file_to_load_id, first_file_to_load_name);
+    open_file(first_file_id, first_file_name);
   }
 });
 
