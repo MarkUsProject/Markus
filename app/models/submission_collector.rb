33f47d0eaaba1f2d39cdc36314d2ea92ae770288
William Kwan <william@voidupdate.com>
2015-12-12 22:52:49 -0500

    fix hound

--- app/models/submission_collector.rb
+++ app/models/submission_collector.rb
@@ -229,8 +229,8 @@ class SubmissionCollector < ActiveRecord::Base
     grouping.save
 
     new_submission = Submission.create_by_revision_number(grouping, rev_num)
-    apply_penalty_or_add_grace_credits(grouping, 
-                                       apply_late_penalty, 
+    apply_penalty_or_add_grace_credits(grouping,
+                                       apply_late_penalty,
                                        new_submission)
 
     #This is to help determine the progress of the method.
