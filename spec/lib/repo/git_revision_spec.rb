7f6848087b965d0968ea8fe872a85e9f5ecad5e8
Alex Clarke <aclar48@uwo.ca>
2016-01-21 14:19:26 -0500

    fix indent for hound

--- spec/lib/repo/git_revision_spec.rb
+++ spec/lib/repo/git_revision_spec.rb
@@ -15,11 +15,11 @@ describe Repository::GitRevision do
 
       if repo.nil?
         Repository.conf =
-            {
-              REPOSITORY_STORAGE: REPOSITORY_STORAGE,
-              REPOSITORY_PERMISSION_FILE: REPOSITORY_PERMISSION_FILE,
-              IS_REPOSITORY_ADMIN: IS_REPOSITORY_ADMIN
-            }
+          {
+            REPOSITORY_STORAGE: REPOSITORY_STORAGE,
+            REPOSITORY_PERMISSION_FILE: REPOSITORY_PERMISSION_FILE,
+            IS_REPOSITORY_ADMIN: IS_REPOSITORY_ADMIN
+          }
 
         Repository::GitRepository.create('test_repo_workdir')
       end
