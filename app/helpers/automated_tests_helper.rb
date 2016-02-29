b598055617cc12d5d0f3c24a63b3bc573b53ed24
dmitry khabarov <dmitry.khabarov@mail.utoronto.ca>
2016-02-29 16:43:28 -0500

    style-changes

--- app/helpers/automated_tests_helper.rb
+++ app/helpers/automated_tests_helper.rb
@@ -69,16 +69,16 @@ module AutomatedTestsHelper
 
           # Uploading new script
           assignment_tests_path = File.join(
-                                       MarkusConfigurator.markus_config_automated_tests_repository,
-                                       @assignment.repository_folder,
-                                       new_update_script.original_filename)
+                    MarkusConfigurator.markus_config_automated_tests_repository,
+                    @assignment.repository_folder,
+                    new_update_script.original_filename)
           File.open(assignment_tests_path, 'w') { |f| f.write new_update_script.read }
 
           # Deleting old script
           old_script_path = File.join(
-                                 MarkusConfigurator.markus_config_automated_tests_repository,
-                                 @assignment.repository_folder,
-                                 old_scrip_name)
+                    MarkusConfigurator.markus_config_automated_tests_repository,
+                    @assignment.repository_folder,
+                    old_scrip_name)
           if File.exist?(old_script_path)
             File.delete(old_script_path)
           end
@@ -118,16 +118,16 @@ module AutomatedTestsHelper
 
           # Uploading new file
           assignment_tests_path = File.join(
-                                       MarkusConfigurator.markus_config_automated_tests_repository,
-                                       @assignment.repository_folder,
-                                       new_update_file.original_filename)
+                    MarkusConfigurator.markus_config_automated_tests_repository,
+                    @assignment.repository_folder,
+                    new_update_file.original_filename)
           File.open(assignment_tests_path, 'w') { |f| f.write new_update_file.read }
 
           # Deleting old file
           old_file_path = File.join(
-                               MarkusConfigurator.markus_config_automated_tests_repository,
-                               @assignment.repository_folder,
-                               old_file_name)
+                    MarkusConfigurator.markus_config_automated_tests_repository,
+                    @assignment.repository_folder,
+                    old_file_name)
           if File.exist?(old_file_path)
             File.delete(old_file_path)
           end
