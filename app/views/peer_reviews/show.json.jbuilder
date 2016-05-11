79c225cda9628895400be7b3af6d00e73a2055f0
Chris Koehler <christopher.koehler@mail.utoronto.ca>
2016-05-11 12:03:50 -0700

    Removed files that are not used

--- app/views/peer_reviews/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @peer_review, :id, :reviewer_id, :reviewee_id, :result_id, :created_at, :updated_at
--- spec/models/peer_review_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe PeerReview, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
