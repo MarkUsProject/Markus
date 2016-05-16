7609d60e489bcae12e1707c5316524d6d3856750
Chris Koehler <christopher.koehler@mail.utoronto.ca>
2016-05-16 14:14:50 -0700

    Added fixes and added a new validate method for making a test pass

--- app/models/peer_review.rb
+++ app/models/peer_review.rb
@@ -1,6 +1,6 @@
 class PeerReview < ActiveRecord::Base
-  belongs_to :reviewer, class_name: "Student", foreign_key: "reviewer_id"
-  belongs_to :reviewee, class_name: "Student", foreign_key: "reviewee_id"
+  belongs_to :reviewer, class_name: "Grouping", foreign_key: "reviewer_id"
+  belongs_to :reviewee, class_name: "Grouping", foreign_key: "reviewee_id"
   belongs_to :result
 
   validates_associated :reviewer
@@ -12,10 +12,17 @@ class PeerReview < ActiveRecord::Base
   validates_numericality_of :reviewer_id, only_integer: true,  greater_than: 0
   validates_numericality_of :reviewee_id, only_integer: true,  greater_than: 0
   validates_numericality_of :result_id, only_integer: true,  greater_than: 0
-  validate :prevent_reviewer_from_being_reviewee
+  validate :prevent_reviewer_from_being_reviewee, :do_not_allow_one_nil_review
+
+  def do_not_allow_one_nil_review
+    if reviewer.nil? ^ reviewee.nil?
+      errors.add(:reviewer, 'cannot have one of either the reviewer or reviewee being nil')
+    end
+  end
 
   def prevent_reviewer_from_being_reviewee
-    errors.add(:reviewer, 'cannot be the reviewer and reviewee') unless
-        reviewer != reviewee
+    if not reviewer.nil? and not reviewee.nil? and reviewer == reviewee
+      errors.add(:reviewer, 'cannot be the reviewer and reviewee')
+    end
   end
 end
