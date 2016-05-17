4a9ac5547cb13a3af1c32e85d3fac0fd8cbdedee
Chris Koehler <christopher.koehler@mail.utoronto.ca>
2016-05-17 15:29:54 -0700

    Schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -115,8 +115,6 @@ ActiveRecord::Schema.define(version: 20160511224541) do
     t.boolean  "vcs_submit",                       default: false
     t.datetime "token_start_date"
     t.float    "token_period"
-    t.integer  "parent_assignment_id"
-    t.integer  "pr_assignment_id"
   end
 
   add_index "assignments", ["short_identifier"], name: "index_assignments_on_short_identifier", unique: true, using: :btree
