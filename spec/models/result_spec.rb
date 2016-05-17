240be3bbd336a6f71e32e9d0df39742516976806
Christian Garcia <christian.garciasalguero@mail.utoronto.ca>
2016-05-17 15:14:43 -0400

    Added rspec tests for validations in the result model

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,6 +1,14 @@
 require 'spec_helper'
 
 describe Result do
+  it { is_expected.to belong_to(:submission) }
+  it { is_expected.to have_many(:marks) }
+  it { is_expected.to have_many(:extra_marks) }
+  it { is_expected.to have_many(:annotations) }
+  it { is_expected.to validate_presence_of(:marking_state) }
+  it { is_expected.to validate_inclusion_of(:marking_state).in_array(['complete', 'incomplete']) }
+  it { is_expected.to validate_numericality_of(:total_mark).is_greater_than_or_equal_to(0) }
+
   describe '.student_marks_by_assignment' do
     let(:assignment) { create(:assignment) }
     let!(:criteria) { Array.new(2) { create(:rubric_criterion, assignment: assignment) } }
