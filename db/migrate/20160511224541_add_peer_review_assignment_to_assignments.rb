7bb9c70acaa0a26144e01fa19ceb92b58ad1a574
Chris Koehler <christopher.koehler@mail.utoronto.ca>
2016-05-18 10:17:26 -0700

    Removed column that is not needed

--- db/migrate/20160511224541_add_peer_review_assignment_to_assignments.rb
+++ db/migrate/20160511224541_add_peer_review_assignment_to_assignments.rb
@@ -1,6 +1,5 @@
 class AddPeerReviewAssignmentToAssignments < ActiveRecord::Migration
   def change
     add_reference :assignments, :parent_assignment
-    add_reference :assignments, :pr_assignment
   end
 end
