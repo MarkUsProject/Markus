ef62e19435024d12f91b0ac0bb8bd8a7778eb7aa
Christine Murad <christine.murad@mail.utoronto.ca>
2016-05-30 15:20:38 -0400

    download-files-modal: Fixed whitespace

--- app/assets/javascripts/Results/index.js
+++ app/assets/javascripts/Results/index.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function () {
-    window.modal_download_files   = new ModalMarkus('#download_files_dialog');
+    window.modal_download_files = new ModalMarkus('#download_files_dialog');
 });
