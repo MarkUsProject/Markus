864acd23a16fee9ad058ee9bd7d8e0f9995ca6b3
Alessio Di Sandro <alessio.disandro@gmail.com>
2016-06-01 17:28:30 -0400

    automarking: Properly invoke uam wip

--- automated-tests-files/test_runner/sample_files/uam/pam.rb
+++ automated-tests-files/test_runner/sample_files/uam/pam.rb
@@ -1,37 +1,55 @@
 #!/usr/bin/env ruby
 
+require 'open3'
+
 if __FILE__ == $0 then
 
-  path_to_uam = '../../../../uam'
+  path_to_virtualenv = '/home/adisandro/Code/uam-virtualenv'
+  path_to_uam = '/home/adisandro/Desktop/uam'
   assignment = 'asst'
-  output, status = Open3.capture2e("ln -s #{path_to_uam} uam")
+  output, status = Open3.capture2e("ln -s #{path_to_uam} uam; cd uam/")
   unless status.success?
-    print "Can't find automarking framework: #{output}"
-    return
+    abort "Can't find the automarking framework: #{output}"
   end
   # TODO It should be enough to use a standard config.py
-  output, status = Open3.capture2e('cd uam/; python3 test_runner.py')
+  output, status = Open3.capture2e("
+    cd uam/;
+    source #{path_to_virtualenv}/bin/activate;
+    python3 test_runner.py
+  ")
+  print output
   unless status.success?
-    print "Test suite failed: #{output}"
-    return
+    # TODO The test runner returns success even in case of errors
+    abort "Test suite failed: #{output}"
   end
   # TODO Only one submission at a time runs: modify test framework or run aggregator/templator separately?
   # TODO Need to generate all the necessary files too
   output, status = Open3.capture2e("
     cd uam/;
-    python3 aggregator.py #{assignment} examples/pam/dirs_and_names.txt examples/pam/students.csv examples/pam/groups.txt result.json #{assignment}_aggregated.json
+    source #{path_to_virtualenv}/bin/activate;
+    python3 aggregator.py #{assignment} examples/pam/dirs_and_names.txt examples/pam/students.csv examples/pam/groups.txt result.json aggregated.json
   ")
+  print output
   unless status.success?
-    print "Aggregator failed: #{output}"
-    return
+    abort "Aggregator failed: #{output}"
   end
   output, status = Open3.capture2e("
     cd uam/;
-    python3 templator.py #{assignment}_aggregated.json txt
+    source #{path_to_virtualenv}/bin/activate;
+    python3 templator.py aggregated.json txt
   ")
+  print output
   unless status.success?
-    print "Templator failed: #{output}"
-    return
+    abort "Templator failed: #{output}"
   end
+
+  print "<test>\n" \
+          "<name>uam</name>\n" \
+          "<input>test</input>\n" \
+          "<expected>ok</expected>\n" \
+          "<actual>ok</actual>\n" \
+          "<marks_earned>1</marks_earned>\n" \
+          "<status>pass</status>\n" \
+        "</test>"
 
 end
