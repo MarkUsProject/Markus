3de7b11fd86f13213f585c853640b716296a1d88
Chris Koehler <christopher.koehler@mail.utoronto.ca>
2016-06-03 01:20:34 -0400

    Removing something that should be in another branch

--- app/models/peer_review.rb
+++ app/models/peer_review.rb
@@ -10,8 +10,7 @@ class PeerReview < ActiveRecord::Base
   validates_numericality_of :result_id, only_integer: true, greater_than: 0
 
   def reviewee
-    # TODO - Is this what we want? Is this better?
-    #Grouping.joins({ :submissions => { :results => :peer_reviews }}).where('peer_reviews.id = ?', self.id).first
-    result.submission.grouping
+    # TODO - Research optimizing or see if rails can do better
+    Grouping.joins({ :submissions => { :results => :peer_reviews }}).where('peer_reviews.id = ?', self.id).first
   end
 end
