98272d805d2c4642967858cc03187d5836c707a8
Alessio Di Sandro <alessio.disandro@gmail.com>
2016-06-02 22:29:03 -0400

    automarking: Properly invoke uam done, examples only (aka hard-coded)

--- automated-tests-files/test_runner/sample_files/uam/pam.rb
+++ automated-tests-files/test_runner/sample_files/uam/pam.rb
@@ -6,43 +6,42 @@ if __FILE__ == $0 then
 
   path_to_virtualenv = '/home/adisandro/Code/uam-virtualenv'
   path_to_uam = '/home/adisandro/Desktop/uam'
-  assignment = 'asst'
+  # TODO Get the assignment name from MarkUs
+  assignment = 'A1'
   output, status = Open3.capture2e("ln -s #{path_to_uam} uam; cd uam/")
   unless status.success?
     abort "Can't find the automarking framework: #{output}"
   end
-  # TODO It should be enough to use a standard config.py
+  # TODO Generate a config.py to target the real assignment
+  # TODO Launch pam.py directly?
   output, status = Open3.capture2e("
     cd uam/;
-    source #{path_to_virtualenv}/bin/activate;
+    . #{path_to_virtualenv}/bin/activate;
     python3 test_runner.py
   ")
-  print output
   unless status.success?
     # TODO The test runner returns success even in case of errors
     abort "Test suite failed: #{output}"
   end
-  # TODO Only one submission at a time runs: modify test framework or run aggregator/templator separately?
-  # TODO Need to generate all the necessary files too
+  # TODO Cut the aggregator and the templator, they're not needed
   output, status = Open3.capture2e("
     cd uam/;
-    source #{path_to_virtualenv}/bin/activate;
-    python3 aggregator.py #{assignment} examples/pam/dirs_and_names.txt examples/pam/students.csv examples/pam/groups.txt result.json aggregated.json
+    . #{path_to_virtualenv}/bin/activate;
+    python3 aggregator.py #{assignment} pam/examples/dirs_and_names.txt pam/examples/students.csv pam/examples/groups.txt result.json aggregated.json
   ")
-  print output
   unless status.success?
     abort "Aggregator failed: #{output}"
   end
   output, status = Open3.capture2e("
     cd uam/;
-    source #{path_to_virtualenv}/bin/activate;
+    . #{path_to_virtualenv}/bin/activate;
     python3 templator.py aggregated.json txt
   ")
-  print output
   unless status.success?
     abort "Templator failed: #{output}"
   end
 
+  # TODO Read result.json and produce this
   print "<test>\n" \
           "<name>uam</name>\n" \
           "<input>test</input>\n" \
@@ -50,6 +49,14 @@ if __FILE__ == $0 then
           "<actual>ok</actual>\n" \
           "<marks_earned>1</marks_earned>\n" \
           "<status>pass</status>\n" \
+        "</test>\n" \
+        "<test>\n" \
+          "<name>uam</name>\n" \
+          "<input>test</input>\n" \
+          "<expected>ok</expected>\n" \
+          "<actual>ok</actual>\n" \
+          "<marks_earned>0</marks_earned>\n" \
+          "<status>fail</status>\n" \
         "</test>"
 
 end
