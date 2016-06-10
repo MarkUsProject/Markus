fa55e198047e0d3ff649d975266702f92f59fe16
Alessio Di Sandro <alessio.disandro@gmail.com>
2016-06-10 12:22:12 -0400

    automarking: Fix reading pam results with some json keys absent

--- automated-tests-files/test_runner/sample_files/uam/pam.rb
+++ automated-tests-files/test_runner/sample_files/uam/pam.rb
@@ -4,6 +4,7 @@ require 'open3'
 require 'json'
 
 def print_result(name, input, expected, actual, marks, status)
+
   print <<-EOF
     <test>
       <name>#{name}</name>
@@ -21,15 +22,21 @@ def print_results(result_filename, timeout_filename)
   begin
     File.open(result_filename, 'r') do |result_file|
       result = JSON.parse(result_file.read)
-      result['results'].each do |test_class|
-        test_class['passes'].each do |test_name, test_desc|
-          print_result(test_name, test_desc, '', '', 1, 'pass')
+      result['results'].values.each do |test_class|
+        if test_class.key?('passes')
+          test_class['passes'].each do |test_name, test_desc|
+            print_result(test_name, test_desc, '', '', 1, 'pass')
+          end
         end
-        test_class['failures'].each do |test_name, test_stack|
-          print_result(test_name, test_stack['description'], '', test_stack['message'], 0, 'fail')
+        if test_class.key?('failures')
+          test_class['failures'].each do |test_name, test_stack|
+            print_result(test_name, test_stack['description'], '', test_stack['message'], 0, 'fail')
+          end
         end
-        test_class['errors'].each do |test_name, test_stack|
-          print_result(test_name, test_stack['description'], '', test_stack['message'], 0, 'fail')
+        if test_class.key?('errors')
+          test_class['errors'].each do |test_name, test_stack|
+            print_result(test_name, test_stack['description'], '', test_stack['message'], 0, 'fail')
+          end
         end
       end
     end
@@ -42,7 +49,7 @@ def print_results(result_filename, timeout_filename)
   end
 end
 
-if __FILE__ == $0 then
+if __FILE__ == $0
 
   path_to_virtualenv = '/home/adisandro/Code/uam-virtualenv'
   path_to_uam = '/home/adisandro/Desktop/uam'
@@ -58,41 +65,4 @@ if __FILE__ == $0 then
   end
   print_results(result_filename, timeout_filename)
   # TODO Get test.py filename from markus
-
-  # # TODO Get the assignment name from MarkUs
-  # assignment = 'A1'
-  # output, status = Open3.capture2e("ln -s #{path_to_uam} uam; cd uam/")
-  # unless status.success?
-  #   abort "Can't find the automarking framework: #{output}"
-  # end
-  # # TODO Generate a config.py to target the real assignment..
-  # # TODO ..or launch pam.py directly?
-  # # TODO Think about the actual
-  # output, status = Open3.capture2e("
-  #   cd uam/;
-  #   . #{path_to_virtualenv}/bin/activate;
-  #   python3 test_runner.py
-  # ")
-  # unless status.success?
-  #   # TODO The test runner returns success even in case of errors
-  #   abort "Test suite failed: #{output}"
-  # end
-  # # TODO Cut the aggregator and the templator, they're not needed
-  # output, status = Open3.capture2e("
-  #   cd uam/;
-  #   . #{path_to_virtualenv}/bin/activate;
-  #   python3 aggregator.py #{assignment} pam/examples/dirs_and_names.txt pam/examples/students.csv pam/examples/groups.txt result.json aggregated.json
-  # ")
-  # unless status.success?
-  #   abort "Aggregator failed: #{output}"
-  # end
-  # output, status = Open3.capture2e("
-  #   cd uam/;
-  #   . #{path_to_virtualenv}/bin/activate;
-  #   python3 templator.py aggregated.json txt
-  # ")
-  # unless status.success?
-  #   abort "Templator failed: #{output}"
-  # end
-
 end
