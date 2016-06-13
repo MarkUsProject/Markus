7c726bacb41fc5ad6ff7f92386cdfa9c464644b3
Chris Koehler <christopher.koehler@mail.utoronto.ca>
2016-06-13 16:33:46 -0400

    Undid timestamp deletion

--- db/migrate/20160610203549_remove_timestamps_from_peer_reviews.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTimestampsFromPeerReviews < ActiveRecord::Migration
-  def change
-    remove_column :peer_reviews, :created_at, :string
-    remove_column :peer_reviews, :updated_at, :string
-  end
-end
