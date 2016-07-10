f65744f2e9bc0ecda8415cf2751d40bc48980160
Christian Garcia <christian.garciasalguero@mail.utoronto.ca>
2016-07-10 18:00:33 -0400

    update-positions-transaction: Deleted position variable

--- app/controllers/criteria_controller.rb
+++ app/controllers/criteria_controller.rb
@@ -4,11 +4,10 @@ class CriteriaController < ApplicationController
   def update_positions
     @assignment = Assignment.find(params[:assignment_id])
     @criteria = @assignment.get_criteria
-    position = 1
 
     ActiveRecord::Base.transaction do
       params[:criterion].
-        each_with_index { |id, index| @assignment.criterion_class.update(id, position: position + index) if id != '' }
+        each_with_index { |id, index| @assignment.criterion_class.update(id, position: index + 1) if id != '' }
     end
   end
 end
