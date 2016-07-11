11c9119530710b6f616363126dd54c6b0b9f26e7
Chris Koehler <christopher.koehler@mail.utoronto.ca>
2016-07-11 11:09:38 -0400

    Moved test that should be in a controller test to that location

--- spec/models/peer_review_spec.rb
+++ spec/models/peer_review_spec.rb
@@ -21,29 +21,4 @@ describe PeerReview do
       expect(peer_review.reviewer.peer_reviews_to_others.first.id).to eq peer_review.id
     end
   end
-
-  describe 'peer review assignment' do
-    it 'random assigns properly' do
-      expect(PeerReview.all.size).to eq 0
-
-      assignment_with_pr = create(:assignment_with_peer_review_and_groupings_results)
-      selected_reviewer_group_ids = assignment_with_pr.pr_assignment.groupings.map { |g| g.id }
-      selected_reviewee_group_ids = assignment_with_pr.groupings.map(&:id)
-      PeerReviewsController.new.perform_random_assignment(assignment_with_pr.pr_assignment, 2,
-                                                          selected_reviewer_group_ids, selected_reviewee_group_ids)
-
-      expect(Grouping.all.size).to eq 6
-      expect(PeerReview.all.size).to eq 6
-
-      pr_groupings_list = assignment_with_pr.pr_assignment.groupings.to_a
-      results_list = assignment_with_pr.groupings.map { |g| g.current_submission_used.get_latest_result }
-
-      # Due to previous expect() verifications, along with schema restraints,
-      # and how the factory for the pr assignment is set up, this will hold in
-      # testing both proper assignment and forward/backward assignments.
-      results_list.each do |result|
-        expect(PeerReview.where(result: result).size).to eq 2
-      end
-    end
-  end
 end
