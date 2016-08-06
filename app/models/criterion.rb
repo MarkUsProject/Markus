90dd50c1b8a93bc9479537b0765a7c783dea30cb
david-yz-liu <david@cs.toronto.edu>
2016-08-05 20:45:41 -0400

    Some improvements to #2679

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -115,14 +115,6 @@ class Criterion < ActiveRecord::Base
       checkbox_criterion_ids_str = ''
     else
       checkbox_criterion_ids_str = Array(criterion_ids_by_type['CheckboxCriterion'])
-          .map { |criterion_id| connection.quote(criterion_id) }
-          .join(',')
-    end
-
-    if criterion_ids_by_type.nil?  or criterion_ids_by_type['CheckboxCriterion'].nil?
-      checkbox_criterion_ids_str = ''
-    else
-      checkbox_criterion_ids_str = Array(criterion_ids_by_type['CheckboxCriterion'])
         .map { |criterion_id| connection.quote(criterion_id) }
         .join(',')
     end
