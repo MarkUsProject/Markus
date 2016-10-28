3379854544706a61acf4b2ca727b36e978ffbac3
Alessio Di Sandro <alessio.disandro@gmail.com>
2016-10-27 20:10:56 -0400

    automarking: Reorganize testers part 3

--- lib/automated_tests/testers/uam/server/add_venv.sh
+++ lib/automated_tests/testers/uam/server/add_venv.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [ $# -ne 4 ]; then
-    echo usage: $0 server_user server_host server_basedir course_name
+    echo usage: $0 server_user server_host server_install_dir course_name
     exit 1
 fi
 USER=$1
 SERVER=$2
-BASEDIR=$3
+INSTALLDIR=$3
 COURSE=$4
 VENVDIR=venvs
 ssh ${USER}@${SERVER} bash -c "'
-    cd ${BASEDIR}
+    cd ${INSTALLDIR}
     mkdir -p ${VENVDIR}
     pyvenv ${VENVDIR}/${COURSE}
     source ${VENVDIR}/${COURSE}/bin/activate
--- lib/automated_tests/testers/uam/server/install.sh
+++ lib/automated_tests/testers/uam/server/install.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 3 ]; then
-    echo usage: $0 server_user server_host server_basedir
+if [ $# -ne 4 ]; then
+    echo usage: $0 server_user server_host server_install_dir server_pythonpath_dir
     exit 1
 fi
 USER=$1
 SERVER=$2
-BASEDIR=$3
+INSTALLDIR=$3
+PYTHONPATHDIR=$4
 UAMDIR=uam
-PATHDIR=markus_uam
 ssh ${USER}@${SERVER} bash -c "'
-    cd ${BASEDIR}
+    cd ${INSTALLDIR}
     if cd ${UAMDIR}; then
         git pull
         cd ..
     else
         git clone https://github.com/ProjectAT/uam.git ${UAMDIR}
     fi
-    mkdir -p ${PATHDIR}
 '"
-scp pam_wrapper.py markus_pam_wrapper.py ../../../markusapi/markusapi.py ${USER}@${SERVER}:${BASEDIR}/${PATHDIR}
-# remember to add ${BASEDIR}/${PATHDIR} to PYTHONPATH on the server
+scp pam_wrapper.py markus_pam_wrapper.py ../../../markusapi/markusapi.py ${USER}@${SERVER}:${PYTHONPATHDIR}
