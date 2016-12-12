32177d5534a8f1a368084a59946f906b30ca5076
Brent Simmons <brent.simmons@unb.ca>
2016-12-12 11:13:10 -0400

    Adding grade_entry_forms and graders views - only changing the upload and download modals

--- app/assets/javascripts/Graders/index.js
+++ app/assets/javascripts/Graders/index.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
-  window.modal_upload   = new ModalMarkus('#upload_dialog');
-  window.modal_download = new ModalMarkus('#download_dialog');
+  window.modal_upload   = jQuery('#upload_dialog').easyModal();
+  window.modal_download = jQuery('#download_dialog').easyModal();
   window.modal_coverage = new ModalMarkus('#groups_coverage_dialog');
   window.modal_criteria = new ModalMarkus('#grader_criteria_dialog');
 });
