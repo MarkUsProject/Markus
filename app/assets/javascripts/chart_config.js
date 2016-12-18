a62ee2b656c80da858ed7c75b0be524da8628d1e
david-yz-liu <david@cs.toronto.edu>
2016-12-17 21:24:39 -0500

    Update to chartjs v2.4.0, and introduce global chart config

---
+++ app/assets/javascripts/chart_config.js
@@ -0,0 +1,22 @@
+// Requires Chart.js to have been loaded.
+Chart.defaults.global.defaultColor = 'rgba(151,187,205,0.5)';
+Chart.defaults.global.elements.rectangle.backgroundColor = 'rgba(151,187,205,0.5)';
+Chart.defaults.global.elements.rectangle.borderColor = 'rgba(151,187,205,0.5)';
+
+Chart.defaults.bar.scales.xAxes = [{
+  type: 'category',
+
+  categoryPercentage: 0.8,
+  barPercentage: 0.9,
+
+  // grid line settings
+  gridLines: {
+    offsetGridLines: false,
+  },
+  ticks: {
+    maxRotation: 0,
+    autoSkip: false
+  }
+}];
+
+Chart.defaults.global.legend.display = false;
