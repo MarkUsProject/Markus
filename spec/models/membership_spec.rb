83584e5bacfc06630374b7cb652fd68cf7c597dd
Rathu Kugan <rathu.kugan@gmail.com>
2017-05-02 17:15:46 -0400

    Fixed assert and removed blanks

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -4,32 +4,22 @@ describe Membership do
   it { should belong_to :grouping }
   it { should have_many :grace_period_deductions }
 
-  it do
-    should validate_presence_of(:user_id)
-    .with_message('needs a user id')
-  end
-
-  it do
-    should validate_presence_of(:grouping_id)
-    .with_message('needs a grouping id')
-  end
+  it { should validate_presence_of(:user_id).with_message('needs a user id') }
+  it { should validate_presence_of(:grouping_id).with_message('needs a grouping id') }
 
   context 'StudentMembership is inviter' do
     before { @membership = StudentMembership.create(membership_status: StudentMembership::STATUSES[:inviter]) }
 
     it 'be inviter' do
-      assert @membership.inviter?
+      expect(@membership.inviter?).to be true
     end
-
   end
 
   context 'StudentMembership is not inviter' do
     before { @membership = StudentMembership.create(membership_status: StudentMembership::STATUSES[:accepted]) }
 
     it 'not be inviter' do
-      assert !@membership.inviter?
+      expect(!@membership.inviter?).to be true
     end
-
   end
-
 end
