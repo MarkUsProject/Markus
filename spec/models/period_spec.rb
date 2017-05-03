9efb753ec044135c22c69a0b92071f12669dec72
Tyler Han <tylerhan97@gmail.com>
2017-05-03 10:10:39 -0400

    added new spec and deleted old test

---
+++ spec/models/period_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe Period do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:hours) }
+    it { is_expected.to have_db_column(:interval) }
+    it { is_expected.to belong_to(:submission_rule) }
+
+    it { is_expected.to allow_value(0).for(:hours) }
+    it { is_expected.to allow_value(1).for(:hours) }
+    it { is_expected.to allow_value(2).for(:hours) }
+    it { is_expected.to allow_value(100).for(:hours) }
+    it { is_expected.not_to allow_value(-1).for(:hours) }
+    it { is_expected.not_to allow_value(-100).for(:hours) }
+  end
+
+  it 'A valid grace period' do
+      period = Period.create(hours: 1)
+      expect(period.valid?).to be true
+  end
+
+  it 'invalid grace period with nil hours' do
+    period = Period.create(hours: nil)
+    expect(period.valid?).to be false
+  end
+
+  context 'A penalty decay period' do
+    before {@period = Period.create(submission_rule_type: 'PenaltyDecayPeriodSubmissionRule')}
+
+    it 'validate presence of deduction' do
+      #no deduction is set
+      expect(@period.valid?).to be false
+    end
+
+    it 'validate numericality of deduction' do
+      @period.deduction = 'string'
+      expect(@period.valid?).to be false
+    end
+
+    it 'validate presence of interval' do
+      #no interval is set
+      expect(@period.valid?).to be false
+    end
+
+    it 'validate numericality of interval' do
+      @period.interval = 'string'
+      expect(@period.valid?).to be false
+    end
+  end
+
+  context 'A penalty period' do
+    before {@period = Period.create(submission_rule_type: 'PenaltyPeriodSubmissionRule')}
+
+    it 'validate presence of deduction' do
+      #no deduction is set
+      expect(@period.valid?).to be false
+    end
+
+    it 'validate numericality of deduction' do
+      @period.deduction = 'string'
+      expect(@period.valid?).to be false
+    end
+
+  end
+
+end
