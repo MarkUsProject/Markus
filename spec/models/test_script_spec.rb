64312c4cfdf1f9fd4d2498240859bd436c550335
jenachoi <jena.choi1106@gmail.com>
2017-05-04 16:35:25 -0400

    modified test_script_spec.rb as requested

--- spec/models/test_script_spec.rb
+++ spec/models/test_script_spec.rb
@@ -38,7 +38,7 @@ describe TestScript do
                      section_due_dates_type: false,
                      due_date: 2.days.from_now)
 
-      @scriptfile = TestScript.create(assignment_id:             @asst.id,
+      @script_file = TestScript.create(assignment_id:             @asst.id,
                                       seq_num:                    1,
                                       script_name:                'script.sh',
                                       description:                'This is a bash script file',
@@ -55,20 +55,19 @@ describe TestScript do
     end
 
     it 'return true when a valid file is created' do
-      expect(@scriptfile).to be_valid
+      expect(@script_file).to be_valid
     end
 
     it 'return true when a valid file is created even if the description is empty' do
-      @scriptfile.description = ''
-      expect(@scriptfile).to be_valid
+      @script_file.description = ''
+      expect(@script_file).to be_valid
     end
 
     it 'return true when a valid file is created even if the max_marks is zero' do
-      @scriptfile.max_marks = 0
-      expect(@scriptfile).to be_valid
+      @script_file.max_marks = 0
+      expect(@script_file).to be_valid
     end
   end
-
 
   # update
   context 'An invalid script file' do
@@ -78,7 +77,7 @@ describe TestScript do
                      due_date: 2.days.from_now)
       display_option = %w(do_not_display display_after_submission display_after_collection)
 
-      @validscriptfile = TestScript.create(assignment_id:               @asst.id,
+      @valid_script_file = TestScript.create(assignment_id:               @asst.id,
                                            seq_num:                     1,
                                            script_name:                 'validscript.sh',
                                            description:                 'This is a bash script file',
@@ -93,7 +92,7 @@ describe TestScript do
                                            display_expected_output:     display_option[1],
                                            display_actual_output:       display_option[2])
 
-      @invalidscriptfile = TestScript.create(assignment_id:             @asst.id,
+      @invalid_script_file = TestScript.create(assignment_id:             @asst.id,
                                              seq_num:                   2,
                                              script_name:               'invalidscript.sh',
                                              description:               'This is a bash script file',
@@ -110,86 +109,86 @@ describe TestScript do
     end
 
     context 'script file expected to be invalid when assignment is nil' do
-      it 'return false when assignment is nii' do
-        @invalidscriptfile.assignment_id = nil
-        expect(@invalidscriptfile).not_to be_valid
+      it 'return false when assignment is nil' do
+        @invalid_script_file.assignment_id = nil
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the description is nil' do
       it 'return false when the description is nil' do
-        @invalidscriptfile.description = nil
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.description = nil
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the max_marks is negative' do
       it 'return false when the max_marks is negative' do
-        @invalidscriptfile.max_marks = -1
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.max_marks = -1
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the max_marks is not integer' do
       it 'return false when the max_marks is not integer' do
-        @invalidscriptfile.max_marks = 0.5
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.max_marks = 0.5
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the script_name already exists in the same assignment' do
       it 'return false when the script_name already exists' do
-        @invalidscriptfile.script_name = 'validscript.sh'
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.script_name = 'validscript.sh'
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the seq_num already exists in the same assignment' do
       it 'return true when the seq_num already exists' do
-        @invalidscriptfile.seq_num = 2
-        expect(@invalidscriptfile).to be_valid
+        @invalid_script_file.seq_num = 2
+        expect(@invalid_script_file).to be_valid
       end
     end
 
     context 'script file expected to be invalid when the display_description option has an invalid option' do
       it 'return false when the display_description option has an invalid option' do
-        @invalidscriptfile.display_description = 'display_after_due_date'
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.display_description = 'display_after_due_date'
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the display_run_status option has an invalid option' do
       it 'return false when the display_run_status option has an invalid option' do
-        @invalidscriptfile.display_run_status = 'display_after_submit'
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.display_run_status = 'display_after_submit'
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the display_marks_earned option has an invalid option' do
       it 'return false when the display_marks_earned option has an invalid option' do
-        @invalidscriptfile.display_marks_earned = 'display_before_due_date'
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.display_marks_earned = 'display_before_due_date'
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the display_input option has an invalid option' do
       it 'return false when the display_input option has an invalid option' do
-        @invalidscriptfile.display_input = 'display_before_collection'
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.display_input = 'display_before_collection'
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the display_expected_output option has an invalid option' do
       it 'return false when the display_expected_output option has an invalid option' do
-        @invalidscriptfile.display_expected_output = 'display_at_submission'
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.display_expected_output = 'display_at_submission'
+        expect(@invalid_script_file).not_to be_valid
       end
     end
 
     context 'script file expected to be invalid when the display_actual_output option has an invalid option' do
       it 'return false when the display_actual_output option has an invalid option' do
-        @invalidscriptfile.display_actual_output = 'display_at_collection'
-        expect(@invalidscriptfile).not_to be_valid
+        @invalid_script_file.display_actual_output = 'display_at_collection'
+        expect(@invalid_script_file).not_to be_valid
       end
     end
   end
