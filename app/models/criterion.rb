03b20a5b9dbb918d2822896e8e46a1930e0bae01
jenachoi <jena.choi1106@gmail.com>
2017-05-10 16:51:35 -0400

    fixed it so that marks are created with correct result_id

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -164,21 +164,30 @@ class Criterion < ActiveRecord::Base
 
   def create_marks
     # results with specific assignment
-    byebug
     results = Result
                 .joins(submission: :grouping)
                 .where(groupings: {assignment_id: self.assignment_id})
-    marks = []
+
     # creates mark object and updates total mark
-    results.each do |r|
-      # if criteria is ta visible
+    marks = []
+    if self.visible?
+      # if criteria is ta_visible
       if self.ta_visible
-        marks << self.marks.new(result_id: r.submission_id) # create mark object for ta review result
-      # if criteria is peer visible
+        results.each do |r|
+          if !r.is_a_review? # filter results that are not peer reviews
+            marks << self.marks.new(result_id: r.id)  # create mark object for ta review result
+            r.update_total_mark
+          end
+        end
+      # if criteria is peer_visible
       elsif self.peer_visible
-        marks << self.marks.new(result_id: r.peer_review_id) # create mark object for peer review result
+        results.each do |r|
+          if r.is_a_review? # filter results that are peer reviews
+            marks << self.marks.new(result_id: r.id) # create mark object for peer review result
+            r.update_total_mark
+          end
+        end
       end
-      r.update_total_mark
     end
     Mark.import marks
   end
