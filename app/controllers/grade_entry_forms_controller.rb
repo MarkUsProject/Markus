8a12febb73d1b92f9aac787549e430afb184b492
Rathu Kugan <rathu.kugan@gmail.com>
2017-05-12 10:04:10 -0400

    Changed if not to unless

--- app/controllers/grade_entry_forms_controller.rb
+++ app/controllers/grade_entry_forms_controller.rb
@@ -373,7 +373,7 @@ class GradeEntryFormsController < ApplicationController
 
       # Parse the grades
       result = MarkusCSV.parse(grades_file.read, encoding: encoding) do |row|
-        next if !row.any?
+        next unless row.any?
         # grab names and totals from the first two rows
         if names.empty?
           names = row
