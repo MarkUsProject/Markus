dff46633a3216656a7468ba0d43143859d2b13ef
jenachoi <jena.choi1106@gmail.com>
2017-05-19 16:45:06 -0400

    accessing filename, num_pages, etc for seeded exam template

--- app/controllers/exam_templates_controller.rb
+++ app/controllers/exam_templates_controller.rb
@@ -9,15 +9,12 @@ class ExamTemplatesController < ApplicationController
 
   def index
     @assignment = Assignment.find(params[:assignment_id])
-    @assignment.test_scripts.build(
-      # TODO: make these default values
-      run_by_instructors: true,
-      run_by_students: false,
-      display_input: :do_not_display,
-      display_expected_output: :do_not_display,
-      display_actual_output: :do_not_display
-    )
-    @assignment.test_support_files.build
-    @student_tests_on = MarkusConfigurator.markus_ate_experimental_student_tests_on?
+    @exam_templates = ExamTemplate.all
+
+    # exam template with specific assignment
+    @exam_template = ExamTemplate.find_by(assignment: @assignment)
+    @filename = @exam_template.filename
+    @num_pages = @exam_template.num_pages
+    @template_divisions = @exam_template.template_divisions
   end
 end
