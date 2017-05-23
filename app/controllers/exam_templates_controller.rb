efdd0b061ad88022e1fab8b83e0b3e77704570cb
jenachoi <jena.choi1106@gmail.com>
2017-05-23 11:49:01 -0400

    removed copied-and-pasted lines

--- app/controllers/exam_templates_controller.rb
+++ app/controllers/exam_templates_controller.rb
@@ -1,6 +1,3 @@
-# The actions necessary for managing the Testing Framework form
-require 'helpers/ensure_config_helper.rb'
-
 class ExamTemplatesController < ApplicationController
 
   before_filter      :authorize_only_for_admin
