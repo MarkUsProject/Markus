7137b246d3cf3a01fcb1d2b30e585127471214ec
Brenda Huang <bh.huang@mail.utoronto.ca>
2017-05-22 21:15:00 -0400

    Added getter for weight of a marking weight

--- app/models/marking_weight.rb
+++ app/models/marking_weight.rb
@@ -1,3 +1,16 @@
 class MarkingWeight < ActiveRecord::Base
   belongs_to :marking_scheme
+
+  def get_gradable_item
+    if self.is_assignment
+      gradable_item = Assignment.where(id: gradable_item_id).first
+    else
+      gradable_item = GradeEntryForm.where(id: gradable_item_id).first
+    end
+    return gradable_item
+  end
+
+  def get_weight
+    return weight
+  end
 end
