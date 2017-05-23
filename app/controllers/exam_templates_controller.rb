e2f9479eb07f3b6d9656ab18d83fbd0ed687debf
jenachoi <jena.choi1106@gmail.com>
2017-05-23 10:49:52 -0400

    added download method to controller

--- app/controllers/exam_templates_controller.rb
+++ app/controllers/exam_templates_controller.rb
@@ -17,4 +17,14 @@ class ExamTemplatesController < ApplicationController
     @num_pages = @exam_template.num_pages
     @template_divisions = @exam_template.template_divisions
   end
+
+  def download
+    @assignment = Assignment.find(params[:assignment_id])
+    @exam_template = ExamTemplate.find_by(assignment: @assignment)
+    @filename = @exam_template.filename
+    @basename = File.basename(@filename, ".pdf")
+    send_file("#{Rails.root}/data/dev/exam_templates/#{@basename}/#{@filename}",
+              filename: "#{@filename}",
+              type: "application/pdf")
+  end
 end
