329a105ca038114b3a5627b11372fda5e0fd54d9
jenachoi <jena.choi1106@gmail.com>
2017-05-23 15:11:21 -0400

    look up a specific exam template to download based on params[:id]

--- app/controllers/exam_templates_controller.rb
+++ app/controllers/exam_templates_controller.rb
@@ -11,8 +11,8 @@ class ExamTemplatesController < ApplicationController
 
   def download
     assignment = Assignment.find(params[:assignment_id])
-    exam_templates = ExamTemplate.find_by(assignment: assignment)
-    filename = exam_templates.filename
+    exam_template = ExamTemplate.find_by(assignment: assignment, id: params[:id]) # look up a specific exam template based on the params[:id]
+    filename = exam_template.filename
     basename = File.basename(filename, ".pdf")
     send_file("#{EXAM_TEMPLATE_DIR}/#{basename}/#{filename}",
               filename: "#{filename}",
