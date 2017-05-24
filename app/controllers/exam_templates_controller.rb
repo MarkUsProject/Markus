3c84d196e278df3a5fc00414724d4e7b1c2dce96
jenachoi <jena.choi1106@gmail.com>
2017-05-24 11:08:25 -0400

    correctly uploading Exam Template model

--- app/controllers/exam_templates_controller.rb
+++ app/controllers/exam_templates_controller.rb
@@ -20,17 +20,10 @@ class ExamTemplatesController < ApplicationController
   end
 
   def update
+    new_uploaded_io = params[:new_template]
     assignment = Assignment.find(params[:assignment_id])
-    old_exam_template = ExamTemplate.find_by(assignment: assignment, id: params[:id]) # look up a specific exam template based on the params[:id]
-    filename = old_exam_template.filename
-    # Get new exam template from upload form
-    new_exam_template = File.open(params[:upload])
-    template = ExamTemplate.create_with_file(new_exam_template.read, assignment_id: assignment.id, filename: filename)
-    #template.template_divisions.create_with_associations(assignment.id, label: 'Q1', start: 2, end: 2)
-    #template.template_divisions.create_with_associations(assignment.id, label: 'Q2', start: 3, end: 3)
-    #template.template_divisions.create_with_associations(assignment.id, label: 'Q3', start: 3, end: 3)
-    #template.template_divisions.create_with_associations(assignment.id, label: 'Q4', start: 4, end: 5)
-    #flash_message(:success, I18n.t('update success'))
-    #redirect_to action: 'index'
+    old_exam_template = ExamTemplate.find_by(assignment: assignment, id: params[:id])
+    old_template_filename = old_exam_template.filename
+    ExamTemplate.create_with_file(new_uploaded_io.read, assignment_id: assignment.id, filename: old_template_filename)
   end
 end
