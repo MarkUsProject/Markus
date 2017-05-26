a2ab741d8fdcd6f279e9b727a1b1a3eab3064979
jenachoi <jena.choi1106@gmail.com>
2017-05-26 12:40:28 -0400

    better to use associations: assignment.exam_templates.find_by(...)

--- app/controllers/exam_templates_controller.rb
+++ app/controllers/exam_templates_controller.rb
@@ -11,7 +11,7 @@ class ExamTemplatesController < ApplicationController
 
   def download
     assignment = Assignment.find(params[:assignment_id])
-    exam_template = ExamTemplate.find_by(assignment: assignment, id: params[:id]) # look up a specific exam template based on the params[:id]
+    exam_template = assignment.exam_templates.find_by(id: params[:id]) # look up a specific exam template based on the params[:id]
     filename = exam_template.filename
     basename = File.basename(filename, ".pdf")
     send_file("#{EXAM_TEMPLATE_DIR}/#{basename}/#{filename}",
