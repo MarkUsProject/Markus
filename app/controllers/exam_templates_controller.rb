e5698c4059752feb853b46b8118a248f413d0a3d
jenachoi <jena.choi1106@gmail.com>
2017-05-31 13:09:01 -0400

    fixed two flash messages showing up and errors

--- app/controllers/exam_templates_controller.rb
+++ app/controllers/exam_templates_controller.rb
@@ -34,14 +34,17 @@ class ExamTemplatesController < ApplicationController
       else
         old_template_filename = old_exam_template.filename
         old_exam_template.replace_with_file(new_uploaded_io.read, assignment_id: assignment.id, filename: old_template_filename)
-        respond_with(old_exam_template)
+        old_exam_template.update(exam_template_params)
+        respond_with(old_exam_template, location: assignment_exam_templates_url)
+        return
       end
-    end
-    # updating template division
-    if old_exam_template.update(exam_template_params)
-      flash_message(:success, t('exam_templates.update.success'))
     else
-      flash_message(:error, t('exam_templates.update.failure'))
+      # updating template division
+      if old_exam_template.update(exam_template_params)
+        flash_message(:success, t('exam_templates.update.success'))
+      else
+        flash_message(:error, t('exam_templates.update.failure'))
+      end
     end
     redirect_to action: 'index'
   end
