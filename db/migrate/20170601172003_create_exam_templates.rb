6ae3406558f7abd6234ea74784ea3bfcd36ce025
jenachoi <jena.choi1106@gmail.com>
2017-06-01 14:01:23 -0400

    updated migration file to have same condition as previous ones

--- db/migrate/20170601172003_create_exam_templates.rb
+++ db/migrate/20170601172003_create_exam_templates.rb
@@ -2,9 +2,9 @@ class CreateExamTemplates < ActiveRecord::Migration
   def change
     create_table :exam_templates do |t|
       t.references :assignment, index: true, foreign_key: true
-      t.string :filename
-      t.string :name
-      t.integer :num_pages
+      t.string :filename, null: false
+      t.string :name, null: false
+      t.integer :num_pages, null: false
 
       t.timestamps null: false
     end
