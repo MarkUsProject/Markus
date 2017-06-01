9d3bf16e38af7f78df9093b87bfdfbfdfbb9f2c2
jenachoi <jena.choi1106@gmail.com>
2017-06-01 14:14:48 -0400

    name field should be unique attribute

--- db/migrate/20170601172003_create_exam_templates.rb
+++ db/migrate/20170601172003_create_exam_templates.rb
@@ -3,7 +3,7 @@ class CreateExamTemplates < ActiveRecord::Migration
     create_table :exam_templates do |t|
       t.references :assignment, index: true, foreign_key: true
       t.string :filename, null: false
-      t.string :name, null: false
+      t.string :name, unique: true, null: false
       t.integer :num_pages, null: false
 
       t.timestamps null: false
