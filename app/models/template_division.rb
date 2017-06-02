1dd6e595e3eb219ab4b6f87e57d7c7520fa7dded
jenachoi <jena.choi1106@gmail.com>
2017-06-02 10:51:35 -0400

    Add a new validation for :end to be less than :num_pages

--- app/models/template_division.rb
+++ app/models/template_division.rb
@@ -7,7 +7,7 @@ class TemplateDivision < ActiveRecord::Base
   validates :start, numericality: { greater_than_or_equal_to: 1,
                                     less_than_or_equal_to: :end,
                                     only_integer: true }
-  validates :end, numericality: { greater_than_or_equal_to: :start,
+  validates :end, numericality: { less_than_or_equal_to: TemplateDivision.first.exam_template.num_pages,
                                   only_integer: true }
   validates :label, uniqueness: true, allow_blank: false
 
