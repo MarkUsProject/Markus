95ab408709a7b14959bcfa861b32a872d877bad8
Tyler Han <tylerhan97@gmail.com>
2017-06-05 14:22:55 -0400

    fixed query

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -128,7 +128,7 @@ class GroupsController < ApplicationController
   def get_names
     names = Student
               .select(:id, "CONCAT(first_name,' ',last_name) AS label, CONCAT(first_name,' ',last_name) AS value")
-              .where("(lower(first_name) like ? OR lower(last_name) like ? OR id_number like ?) AND users.id NOT IN (?)",
+              .where("(lower(first_name) like ? OR lower(last_name) like ? OR CAST(id_number AS TEXT) like ?) AND users.id NOT IN (?)",
                      "#{params[:term].downcase}%", "#{params[:term].downcase}%", "#{params[:term]}%",
                      Membership.select(:user_id).joins(:grouping).where("groupings.assignment_id = ?", params[:assignment]));
     render json: names
