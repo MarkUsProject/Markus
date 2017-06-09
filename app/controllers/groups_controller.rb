bde7a467423ffedeec14825813f10f06f7bd77f1
Tyler Han <tylerhan97@gmail.com>
2017-06-09 14:26:33 -0400

    Updated autocomplete to also search/display username

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -127,9 +127,9 @@ class GroupsController < ApplicationController
 
   def get_names
     names = Student
-              .select(:id, :id_number, "CONCAT(first_name,' ',last_name) AS label, CONCAT(first_name,' ',last_name) AS value")
-              .where("(lower(first_name) like ? OR lower(last_name) like ? OR id_number like ?) AND users.id NOT IN (?)",
-                     "#{params[:term].downcase}%", "#{params[:term].downcase}%", "#{params[:term]}%",
+              .select(:id, :id_number, :user_name, "CONCAT(first_name,' ',last_name) AS label, CONCAT(first_name,' ',last_name) AS value")
+              .where("(lower(first_name) like ? OR lower(last_name) like ? OR lower(user_name) like ? OR id_number like ?) AND users.id NOT IN (?)",
+                     "#{params[:term].downcase}%", "#{params[:term].downcase}%", "#{params[:term].downcase}%", "#{params[:term]}%",
                      Membership.select(:user_id).joins(:grouping).where("groupings.assignment_id = ?", params[:assignment]));
     render json: names
   end
