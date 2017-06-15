b5db79596d7e5a22619085e1ea8bddb4ad7feb9e
jenachoi <jena.choi1106@gmail.com>
2017-06-15 16:05:28 -0400

    set up model for splitpdflog

--- app/models/split_pdf_log.rb
+++ app/models/split_pdf_log.rb
@@ -1,3 +1,13 @@
 class SplitPdfLog < ActiveRecord::Base
+  after_initialize :set_defaults_for_uploaded_when, unless: :persisted? # will only work if the object is new
   belongs_to :user
+  validates :filename,
+            :num_groups_in_complete, :num_groups_in_incomplete, :num_pages_qr_scan_error,
+            :original_num_pages, presence: true
+
+  private
+  def set_defaults_for_uploaded_when
+    # Attribute 'uploaded_when' of split_pdf_log is by default set to the time the object gets created.
+    self.uploaded_when = DateTime.now
+  end
 end
