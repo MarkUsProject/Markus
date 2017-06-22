e036de23e4cd8ace51c348312dffc98fee72e051
Tyler Han <tylerhan97@gmail.com>
2017-06-22 14:23:36 -0400

    added keyboard shortcuts

--- app/assets/javascripts/MathJax/mathjax_helpers.js
+++ app/assets/javascripts/MathJax/mathjax_helpers.js
@@ -13,25 +13,31 @@ function hideAnnotationPreview() {
     dialog.css('margin-left', -1 * dialog.width() / 2);
 }
 
-function showAnnotationPreview() {
-    $('#annotation_preview').show();
-    $('#annotation_preview_title').show();
-
-    // recenter dialog
-    var dialog = $('#annotation_dialog');
-    dialog.css('margin-left', -1 * dialog.width() / 2);
-}
-
 function updateAnnotationPreview() {
     var newAnnotation = document.getElementById('new_annotation_content').value;
 
     var preview = document.getElementById('annotation_preview');
     preview.innerHTML = marked(newAnnotation);
 
-    showAnnotationPreview();
-
     // typeset the preview
     MathJax.Hub.Queue(['Typeset', MathJax.Hub, preview]);
 }
 
-$(document).on("keyup", "#new_annotation_content", updateAnnotationPreview);
+// Allow inline single dollar sign notation
+MathJax.Hub.Config({
+  tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}
+});
+
+var delay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
+$(document).on("keyup", "#new_annotation_content", function () {
+  delay(function(){
+    updateAnnotationPreview();
+  }, 300 );
+});
