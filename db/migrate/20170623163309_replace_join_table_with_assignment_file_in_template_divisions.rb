148eba9b5e8870c72c7d0e37d2134b4bda06fbc9
jenachoi <jena.choi1106@gmail.com>
2017-06-30 10:24:22 -0400

    restore foreign keys in migration file

--- db/migrate/20170623163309_replace_join_table_with_assignment_file_in_template_divisions.rb
+++ db/migrate/20170623163309_replace_join_table_with_assignment_file_in_template_divisions.rb
@@ -1,8 +1,8 @@
 class ReplaceJoinTableWithAssignmentFileInTemplateDivisions < ActiveRecord::Migration
   def change
     # Remove criteria_assignment_files_join column from TemplateDivision
-    remove_reference :template_divisions, :criteria_assignment_files_join
+    remove_reference :template_divisions, :criteria_assignment_files_join, foreign_key: true
     # Add assignment_file column from TemplateDivision
-    add_reference :template_divisions, :assignment_file
+    add_reference :template_divisions, :assignment_file, foreign_key: true
   end
 end
