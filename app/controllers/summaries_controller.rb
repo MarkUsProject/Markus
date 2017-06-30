6e048ea0bc79c2738ddc1881408c1c26fedbad2b
jenachoi <jena.choi1106@gmail.com>
2017-06-30 12:25:39 -0400

    check whether to splice the column in javascript side

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,6 +1,5 @@
 class SummariesController < ApplicationController
   include SummariesHelper
-  include SubmissionsHelper
 
   before_filter  :authorize_for_ta_and_admin
 
@@ -22,35 +21,12 @@ class SummariesController < ApplicationController
 
   def populate
     @assignment = Assignment.find(params[:assignment_id])
-    if @current_user.ta?
-      ta = Ta.find(grader_id)
-      groupings = @assignment.groupings
-                    .includes(:assignment,
-                              :group,
-                              :grace_period_deductions,
-                              current_result: :marks,
-                              accepted_student_memberships: :user)
-                    .select do |g|
-        g.non_rejected_student_memberships.size > 0 and
-          ta.is_assigned_to_grouping?(g.id)
-      end
-    else
-      groupings = @assignment.groupings
-                    .includes(:assignment,
-                              :group,
-                              :grace_period_deductions,
-                              current_result: :marks,
-                              accepted_student_memberships: :user)
-                    .select { |g| g.non_rejected_student_memberships.size > 0 }
-    end
 
-    @render_bonus_deductions_column = false
-    groupings.map do |grouping|
-      result = grouping.current_result
-      if grouping.total_extra_points(result) > 0
-        @render_bonus_deductions_column = true
-      end
+    if @current_user.ta?
+      render json: get_summaries_table_info(@assignment,
+                                            @current_user.id)
+    else
+      render json: get_summaries_table_info(@assignment)
     end
-    render json: get_summaries_table_info(@assignment, groupings, @render_bonus_deductions_column)
   end
 end
--- app/helpers/summaries_helper.rb
+++ app/helpers/summaries_helper.rb
@@ -1,6 +1,28 @@
 module SummariesHelper
   include SubmissionsHelper
-  def get_summaries_table_info(assignment, groupings, render_bonus_deductions_column)
+  def get_summaries_table_info(assignment, grader_id=nil)
+    if grader_id.nil?
+      groupings = assignment.groupings
+        .includes(:assignment,
+                  :group,
+                  :grace_period_deductions,
+                  current_result: :marks,
+                  accepted_student_memberships: :user)
+        .select { |g| g.non_rejected_student_memberships.size > 0 }
+    else
+      ta = Ta.find(grader_id)
+      groupings = assignment.groupings
+        .includes(:assignment,
+                  :group,
+                  :grace_period_deductions,
+                  current_result: :marks,
+                  accepted_student_memberships: :user)
+        .select do |g|
+          g.non_rejected_student_memberships.size > 0 and
+          ta.is_assigned_to_grouping?(g.id)
+        end
+    end
+
     groupings.map do |grouping|
       result = grouping.current_result
       final_due_date = assignment.submission_rule.get_collection_time(grouping.inviter.section)
@@ -18,9 +40,7 @@ module SummariesHelper
       g[:grace_credits_used] = grouping.grace_period_deduction_single
       g[:final_grade] = grouping.final_grade(result)
       g[:criteria] = get_grouping_criteria(assignment, grouping)
-      if render_bonus_deductions_column # send extra points only if we are rendering bonus/deductions column
-        g[:total_extra_points] = grouping.total_extra_points(result)
-      end
+      g[:total_extra_points] = grouping.total_extra_points(result)
       g[:tas] = grouping.tas.pluck(:user_name)
       g
     end
