6127212059804bc29e2537ce34a826279b3fd357
Rathu Kugan <rathu.kugan@gmail.com>
2017-07-06 12:59:25 -0400

    Added on complete js func to split

--- app/jobs/split_pdf_job.rb
+++ app/jobs/split_pdf_job.rb
@@ -3,6 +3,10 @@ class SplitPDFJob < ActiveJob::Base
 
   queue_as MarkusConfigurator.markus_job_split_pdf_queue_name
 
+  def self.on_complete_js
+    puts "function() {window.location.reload.bind(window.location);}"
+  end
+
   def perform(exam_template, path, original_filename=nil, current_user=nil)
     m_logger = MarkusLogger.instance
     begin
