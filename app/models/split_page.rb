fa982e253c0cc536e5f91d165c024c6d7fbfd563
jenachoi <jena.choi1106@gmail.com>
2017-07-13 13:05:01 -0400

    allow blank for exam_page_number in case QR code is not found

--- app/models/split_page.rb
+++ app/models/split_page.rb
@@ -2,7 +2,10 @@ class SplitPage < ActiveRecord::Base
   belongs_to :split_pdf_log
   belongs_to :group, required: false
   validates :split_pdf_log, :filename, :raw_page_number, presence: true
-  validates :raw_page_number, :exam_page_number,
+  validates :raw_page_number,
+            numericality: { greater_than_or_equal_to: 1,
+                            only_integer: true }
+  validates :exam_page_number,
             numericality: { greater_than_or_equal_to: 1,
                             only_integer: true,
                             allow_blank: true }
