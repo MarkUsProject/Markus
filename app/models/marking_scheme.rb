9fec5d2d7cf1328fa7ed5215536c94dc1e29d031
Brenda Huang <bh.huang@mail.utoronto.ca>
2017-07-19 17:22:17 +0800

    Eager load multiple associations

--- app/models/marking_scheme.rb
+++ app/models/marking_scheme.rb
@@ -15,11 +15,13 @@ class MarkingScheme < ActiveRecord::Base
   def students_weighted_grades_array
     student_marks = Hash.new(0)
 
-    Student.find_each do |student|
-      marking_weights.each do |mw|
-        gradable_item = mw.get_gradable_item
+    students = Student.includes({accepted_groupings: [:assignment, :current_result]}, :grade_entry_students)
+
+    marking_weights.each do |mw|
+      gradable_item = mw.get_gradable_item
+      students.each do |student|
         if mw.is_assignment && !gradable_item.max_mark.nil? && gradable_item.max_mark != 0
-          grouping = student.accepted_grouping_for(gradable_item.id)
+          grouping = student.accepted_grouping_for(gradable_item.id) #########################################################
           unless grouping.nil?
             result = grouping.current_result
             unless result.nil? || result.total_mark.nil? || result.marking_state != Result::MARKING_STATES[:complete]
@@ -28,7 +30,7 @@ class MarkingScheme < ActiveRecord::Base
             end
           end
         elsif !mw.is_assignment && !gradable_item.out_of_total.nil? && gradable_item.out_of_total != 0
-          grade_entry_student = GradeEntryStudent.find_by(user_id: student.id, grade_entry_form_id: gradable_item.id)
+          grade_entry_student = GradeEntryStudent.find_by(user: student, grade_entry_form: gradable_item) #######
           unless grade_entry_student.nil?
             result = grade_entry_student.total_grade
             unless result.nil?
