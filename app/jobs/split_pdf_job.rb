66c0cd3e8c4cd69a7068b8dc75b33a1e3170942e
Rathu Kugan <rathu.kugan@gmail.com>
2017-07-19 16:14:42 -0400

    Added brackets so split pdf on complete js executes with anon function

--- app/jobs/split_pdf_job.rb
+++ app/jobs/split_pdf_job.rb
@@ -4,7 +4,7 @@ class SplitPDFJob < ActiveJob::Base
   queue_as MarkusConfigurator.markus_job_split_pdf_queue_name
 
   def self.on_complete_js(status)
-    "window.location.reload.bind(window.location)"
+    "window.location.reload.bind(window.location)()"
   end
 
   def self.show_status(status)
