f28a4e1d0af81da003007dc964395a601ecba0db
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-07-20 19:41:40 -0400

    git: Modify top level hook to deal with file changes too

--- lib/repo/git_hooks/update.d/01-block_forced_push_master.py
+++ lib/repo/git_hooks/update.d/01-block_forced_push_master.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     ref_name = sys.argv[1]
     old_commit = sys.argv[2]
     new_commit = sys.argv[3]
+    # no need to check if old_commit or new_commit are 0, master can't be deleted or created
     if ref_name == 'refs/heads/master':
-        # no need to check if old_commit or new_commit are 0, master can't be deleted or created
+        # check if there are commits reachable from old_commit but not from new_commit, i.e. the old tree was replaced
         rev_list = subprocess.run(['git', 'rev-list', old_commit, '^{}'.format(new_commit)], stdout=subprocess.PIPE,
                                   universal_newlines=True)
         if rev_list.stdout:
-            # there are commits reachable from old_commit but not from new_commit, i.e. the old tree was replaced
-            print('[MARKUS] Forced push is not allowed on master!')
+            print('[MARKUS] Error: forced push is not allowed on master!')
             exit(1)
