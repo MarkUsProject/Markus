129cb7423bb74ff35c92484d2803e6e83a287dbd
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-08-03 20:00:44 +0200

    autotest: Fix failing tests + add new tests for time

--- spec/models/test_script_result_spec.rb
+++ spec/models/test_script_result_spec.rb
@@ -34,7 +34,8 @@ describe TestScriptResult do
                                                   grouping: @sub.grouping,
                                                   test_script: @test_script,
                                                   marks_earned: 5,
-                                                  repo_revision: 0)
+                                                  repo_revision: 0,
+                                                  time: 0)
     end
 
     # create
@@ -70,6 +71,20 @@ describe TestScriptResult do
       context 'test script result expected to be invalid when the marks_earned is not an integer' do
         it 'return false when the marks_earned is not an integer' do
           @test_script_result.marks_earned = 0.5
+          expect(@test_script_result).not_to be_valid
+        end
+      end
+
+      context 'test script result expected to be invalid when the time is negative' do
+        it 'return false when the time is negative' do
+          @test_script_result.time = -1
+          expect(@test_script_result).not_to be_valid
+        end
+      end
+
+      context 'test script result expected to be invalid when the time is not an integer' do
+        it 'return false when the time is not an integer' do
+          @test_script_result.time = 0.5
           expect(@test_script_result).not_to be_valid
         end
       end
