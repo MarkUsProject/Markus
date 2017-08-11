169db3d0836e6edae20c0ca66e0adba9cbd0f7c9
jenachoi <jena.choi1106@gmail.com>
2017-08-11 14:59:30 -0400

    convert submission_helper_test to spec

--- spec/helpers/submission_helper_spec.rb
+++ spec/helpers/submission_helper_spec.rb
@@ -1,15 +1,12 @@
-# encoding: utf-8
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper'))
-require 'shoulda'
+require 'spec_helper'
 
-class SubmissionHelperTest < ActiveSupport::TestCase
-
+describe SubmissionsHelper do
   include SubmissionsHelper
 
   # Put some confidence in our submission filename sanitization
   context 'A new file when submitted' do
     context "containing characters outside what's allowed in a filename" do
-      setup do
+      before :each do
         @filenames_to_be_sanitized = [ { expected: 'llll_', orig: 'llllé' },
                                        { expected: '________', orig: 'öä*?`ßÜÄ' },
                                        { expected: '', orig: nil },
@@ -19,15 +16,15 @@ class SubmissionHelperTest < ActiveSupport::TestCase
                                        { expected: 'garbage-__.txt', orig: 'garbage-éæ.txt' } ]
       end
 
-      should 'have sanitized them properly' do
+      it 'have sanitized them properly' do
         @filenames_to_be_sanitized.each do |item|
-          assert_equal item[:expected], sanitize_file_name(item[:orig])
+          expect(sanitize_file_name(item[:orig])).to eq item[:expected]
         end
       end
     end
 
     context 'containing only valid characters in a filename' do
-      setup do
+      before :each do
         @filenames_not_to_be_sanitized = %w(valid_file.sh
                                             valid_001.file.ext
                                             valid-master.png
@@ -35,9 +32,9 @@ class SubmissionHelperTest < ActiveSupport::TestCase
                                             001.txt)
       end
 
-      should 'NOT have sanitized away any of their characters' do
+      it 'NOT have sanitized away any of their characters' do
         @filenames_not_to_be_sanitized.each do |orig|
-          assert_equal orig, sanitize_file_name(orig)
+          expect(sanitize_file_name(orig)).to eq orig
         end
       end
     end
