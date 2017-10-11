41d5cf01364f4550ffe0738d3dbde6539b91ed05
David Liu <david@cs.toronto.edu>
2017-10-11 19:13:48 -0400

    Fix bug with test support file

--- app/controllers/automated_tests_controller.rb
+++ app/controllers/automated_tests_controller.rb
@@ -45,8 +45,7 @@ class AutomatedTestsController < ApplicationController
               MarkusConfigurator.markus_ate_client_dir,
               @assignment.repository_folder,
               new_support_file.original_filename)
-          # Replace bad line endings from windows
-          contents = new_support_file.read.tr("\r", '')
+          contents = new_support_file.read
           File.open(
               assignment_tests_path, 'w') { |f| f.write contents }
         end
