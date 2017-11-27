e690d9eeb854f6c666fd78f2bed2430b0c6e8dba
Alessio Di Sandro <alessio.disandro@gmail.com>
2017-11-27 17:38:48 -0500

    csrf: Use protect_from_forgery with exception
     * Closes #3237

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   layout 'content'
 
