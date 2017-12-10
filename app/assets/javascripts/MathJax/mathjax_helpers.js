ce75745e7a4194103886f260ba818e45c975ed9f
David Liu <david@cs.toronto.edu>
2017-12-10 17:42:39 -0500

    Javascript improvements for MathJAX (should be faster) and checkmark-style annotations for FIrefox and OSX

--- app/assets/javascripts/MathJax/mathjax_helpers.js
+++ app/assets/javascripts/MathJax/mathjax_helpers.js
@@ -5,33 +5,26 @@ function reloadDOM() {
 }
 
 function updatePreview(source, des) {
-  delay(function () {
-    var newAnnotation = document.getElementById(source);
+  var newAnnotation = document.getElementById(source);
+  var preview = document.getElementById(des);
 
-    var preview = document.getElementById(des);
-
-    if (preview !== null && newAnnotation !== null) {
-      preview.innerHTML = marked(newAnnotation.value);
-      // typeset the preview
-      MathJax.Hub.Queue(['Typeset', MathJax.Hub, preview]);
-    }
-  }, 300);
+  if (preview !== null && newAnnotation !== null) {
+    preview.innerHTML = marked(newAnnotation.value);
+    // typeset the preview
+    MathJax.Hub.Queue(['Typeset', MathJax.Hub, preview]);
+  }
 }
 
-// Allow inline single dollar sign notation
 $(document).ready(function () {
+  // Allow inline single dollar sign notation
   MathJax.Hub.Config({
     tex2jax: {inlineMath: [['$', '$'], ['\\(', '\\)']]}
   });
+  // Update when the document loads so preview is available for existing comments/annotations
+  updatePreview('new_annotation_content', 'annotation_preview');
+  updatePreview('overall_comment_text_area', 'overall_comment_preview');
+  updatePreview('overall_remark_comment_text_area', 'overall_remark_comment_preview');
 });
-
-var delay = (function () {
-  var timer = 0;
-  return function (callback, ms) {
-    clearTimeout(timer);
-    timer = setTimeout(callback, ms);
-  };
-})();
 
 $(document).on("keyup", "#new_annotation_content", function () {
   updatePreview('new_annotation_content', 'annotation_preview');
@@ -42,13 +35,3 @@ $(document).on("keyup", "#overall_comment_text_area", function () {
 $(document).on("keyup", "#overall_remark_comment_text_area", function () {
   updatePreview('overall_remark_comment_text_area', 'overall_remark_comment_preview');
 });
-
-// Update when the document loads so preview is available for existing comments/annotations
-$(document).ready(function () {
-  updatePreview('new_annotation_content', 'annotation_preview');
-  updatePreview('overall_comment_text_area', 'overall_comment_preview');
-  updatePreview('overall_remark_comment_text_area', 'overall_remark_comment_preview');
-});
-
-
-
