d714283520c75134c6f1545fccb0d95b2aca09f3
David Liu <david@cs.toronto.edu>
2017-12-19 23:13:11 -0500

    Update redirect (login) javascript code

--- app/assets/javascripts/layouts.js
+++ app/assets/javascripts/layouts.js
@@ -1,5 +1,4 @@
 $(document).ready(() => {
   window.role_switch_modal = new ModalMarkus('#role_switch_dialog');
-  window.redirect_modal    = new ModalMarkus('#redirect_dialog');
   window.about_modal       = new ModalMarkus('#about_dialog');
 });
--- app/assets/javascripts/redirect.js
+++ app/assets/javascripts/redirect.js
@@ -1,9 +1,18 @@
-function redirectToLogin() {
-  window.location = Routes.root_url();
-}
+$(document).ready(() => {
+  let redirect_modal = new ModalMarkus('#redirect_dialog');
+  $(document).ajaxError((event, xhr) => {
+    if (xhr.status === 403) {
+      redirect_modal.open();
+    }
+  });
 
-$(document).ajaxError((event, xhr) => {
-  if (xhr.status === 403) {
-    redirect_modal.open();
-  }
+  let $redirect_dialog = $('#redirect_dialog');
+
+  $redirect_dialog.find('button.cancel').click(() => {
+    redirect_modal.close();
+  });
+
+  $redirect_dialog.find('button.submit').click(() => {
+    window.location = Routes.root_url();
+  });
 });
