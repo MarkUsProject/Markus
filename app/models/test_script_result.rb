6e73e534fe23974a204f5902da7d66072deff02e
Alessio Di Sandro <alessio.disandro@gmail.com>
2018-01-22 17:17:14 -0500

    autotest: Improve handling of stderr from autotesting server

--- app/models/test_script_result.rb
+++ app/models/test_script_result.rb
@@ -53,18 +53,18 @@ class TestScriptResult < ActiveRecord::Base
     create_test_result(name, '', message, '', 0.0, 0.0, 'error')
   end
 
-  def create_test_result_from_xml(xml)
-    test_name = xml['name']
+  def create_test_result_from_xml(xml_test)
+    test_name = xml_test['name']
     if test_name.nil?
       create_test_error_result(I18n.t('automated_tests.test_result.unknown_test'),
-                               I18n.t('automated_tests.test_result.bad_results', {xml: xml}))
+                               I18n.t('automated_tests.test_result.bad_results', {xml: xml_test}))
       raise 'Malformed xml'
     end
 
-    input = xml['input'].nil? ? '' : xml['input']
-    expected = xml['expected'].nil? ? '' : xml['expected']
-    actual = xml['actual'].nil? ? '' : xml['actual']
-    status = xml['status']
+    input = xml_test['input'].nil? ? '' : xml_test['input']
+    expected = xml_test['expected'].nil? ? '' : xml_test['expected']
+    actual = xml_test['actual'].nil? ? '' : xml_test['actual']
+    status = xml_test['status']
     # check first if we have to stop
     if !status.nil? && status == 'error_all'
       status = 'error'
@@ -73,20 +73,20 @@ class TestScriptResult < ActiveRecord::Base
       stop_processing = false
     end
     # look for all status and marks errors (but only the last message will be shown)
-    if xml['marks_earned'].nil?
+    if xml_test['marks_earned'].nil?
       actual = I18n.t('automated_tests.test_result.bad_marks_earned') unless stop_processing
       status = 'error'
       marks_earned = 0.0
     else
-      marks_earned = xml['marks_earned'].to_f
+      marks_earned = xml_test['marks_earned'].to_f
     end
-    if xml['marks_total'].nil?
+    if xml_test['marks_total'].nil?
       actual = I18n.t('automated_tests.test_result.bad_marks_total') unless stop_processing
       status = 'error'
       marks_earned = 0.0
       marks_total = 0.0
     else
-      marks_total = xml['marks_total'].to_f
+      marks_total = xml_test['marks_total'].to_f
     end
     if status.nil? || !status.in?(%w(pass partial fail error))
       actual = I18n.t('automated_tests.test_result.bad_status', {status: status}) unless stop_processing
