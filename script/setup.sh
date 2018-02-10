5dfbe9dc3000c9964ceddca56151c205997c7910
PkHutch <pk-hutch@hotmail.com>
2018-02-09 20:01:04 -0500

    Update the Setup Script to fix the database.yml error
    It should be noted that this is only "part one" on fixing this error and
    doesn't completely resolve the issue with the database. The changes up
    until now will result in an error related to SVN.

--- script/setup.sh
+++ script/setup.sh
@@ -52,5 +52,31 @@ yarn add @rails/webpacker
 # TODO: Is this really necessary?
 cp node_modules/@rails/webpacker/lib/install/config/webpacker.yml config
 
+# Setup the postgres database.
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+sudo -u postgres psql -U postgres -d postgres -c "create role markus createdb login password 'markus';"
+
+# Editing postgres configuration file needed to setup the database.
+cd ../../../etc/postgresql/9.3/main
+sudo sed -i 's/local   all             all                                     peer/local   all             all         md5/g' pg_hba.conf
+
+# Restarting the postgres server after changing the database.
+cd ../../../
+init.d/postgresql restart
+
+# Switching back to the Markus folder.
+cd ../home/vagrant/Markus/
+
+# Copy the new database file.
+cp config/database.yml.postgresql config/database.yml
+
+# Set the permissions so that the log file is writeable.
+chmod 0664 log/development.log
+
+cd Markus
+
+# Setup the database.
+rake db:seed
+
 # TODO: change to `rails webpacker:install` when rails5 upgrade is done.
-bundle exec rake webpacker:install
+# bundle exec rake webpacker:install
