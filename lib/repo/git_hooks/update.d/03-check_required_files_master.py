77e0301ea33a36e2fa02bf4b3bc0d15477e18465
Alessio Di Sandro <alessio.disandro@gmail.com>
2018-03-22 16:49:35 -0400

    assignment: Add support for required files in subdirs (part 4)
     * Update git hooks to support subdirs

--- lib/repo/git_hooks/update.d/03-check_required_files_master.py
+++ lib/repo/git_hooks/update.d/03-check_required_files_master.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
         # M non-required is warned
         for change in changes.stdout.splitlines():
             status, path = change.split(maxsplit=1)
-            assignment, file_path = path.split(os.sep, maxsplit=1)  # it can never be a top level file/dir
+            assignment, file_path = path.split('/', maxsplit=1)  # it can never be a top level file/dir
             req = required_files.get(assignment)
             if req is None:
                 # this can happen only if an assignment becomes hidden after being visible for a while
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             if status == 'A':
                 if file_path not in req['required']:
                     msg = "you are adding '{}' to assignment '{}', but it only requires '{}'".format(
-                        file_path, assignment, ', '.join(req['required']))
+                          file_path, assignment, ', '.join(req['required']))
                     if req['required_only']:
                         print('[MARKUS] Error: {}!'.format(msg))
                         exit(1)
@@ -45,16 +45,16 @@ if __name__ == '__main__':
                         print('[MARKUS] Warning: {}.'.format(msg))
             elif status == 'D' and file_path in req['required']:
                 print("[MARKUS] Warning: you are deleting '{}' from assignment '{}', but it requires it.".format(
-                    file_path, assignment))
+                      file_path, assignment))
             elif status == 'M' and file_path not in req['required']:
                 print("[MARKUS] Warning: you are modifying '{}' in assignment '{}', but it only requires '{}'.".format(
-                    file_path, assignment, ', '.join(req['required'])))
+                      file_path, assignment, ', '.join(req['required'])))
         # warn about missing files
         new_ls = subprocess.run(['git', 'ls-tree', '-r', '--name-only', new_commit], stdout=subprocess.PIPE,
                                 universal_newlines=True)
-        files = [path.split('/') for path in new_ls.stdout.splitlines()]
+        files = {tuple(path.split('/', maxsplit=1)) for path in new_ls.stdout.splitlines()}
         for assignment in required_files.keys():
             for file_path in required_files[assignment]['required']:
-                if [assignment, file_path] not in files:
+                if (assignment, file_path) not in files:
                     print("[MARKUS] Warning: required file '{}' is missing in assignment '{}'.".format(
-                        file_path, assignment))
+                          file_path, assignment))
