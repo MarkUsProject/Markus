b55c2ab9b5db5c15b45274ec571c907049bdcbc8
Alessio Di Sandro <alessio.disandro@gmail.com>
2018-03-22 16:59:05 -0400

    assignment: Add support for required files in subdirs (part 5)
     * Simplify autotest repo extractor with a one-liner

--- app/helpers/automated_tests_client_helper.rb
+++ app/helpers/automated_tests_client_helper.rb
@@ -137,11 +137,7 @@ module AutomatedTestsClientHelper
       end
       submission.submission_files.each do |file|
         dir = file.path.partition(File::SEPARATOR)[2] # cut the top-level assignment dir
-        if dir == ''
-          file_path = file.filename
-        else
-          file_path = File.join(dir, file.filename)
-        end
+        file_path = if dir == '' then file.filename else File.join(dir, file.filename) end
         unless required_files.nil? || required_files.include?(file_path) # TODO use &. with ruby > 2.3
           # do not export non-required files, if only required files are allowed
           # (a non-required file may end up in a repo if a hook to prevent it does not exist or is not enforced)
