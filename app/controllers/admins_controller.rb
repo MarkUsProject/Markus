2cf5e05ce6d5d0c2b86ded971105dfc0a55fb715
Parto Vafaeikia <Parto@Partos-MacBook-Pro.local>
2018-05-15 21:16:10 -0400

    adding email and id_number for new users and storing them in db

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -55,6 +55,6 @@ class AdminsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :first_name, :last_name)
+    params.require(:user).permit(:user_name, :first_name, :last_name, :email)
   end
 end
