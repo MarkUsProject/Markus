d089a5f72113034dc6c9da5670b398a7a64dd862
David Liu <david@cs.toronto.edu>
2018-06-19 13:27:24 -0400

    i18n: refactor user localization strings

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,6 +3,8 @@ class AdminsController < ApplicationController
 
   layout 'assignment_content'
 
+  responders :flash, :collection
+
   def index
     respond_to do |format|
       format.html
@@ -22,39 +24,22 @@ class AdminsController < ApplicationController
 
   def update
     @user = Admin.find(params[:id])
-    # update_attributes supplied by ActiveRecords
-    if @user.update_attributes(user_params).nil?
-      flash_message(:error, I18n.t('admins.update.error'))
-      render :edit
-    else
-      flash_message(:success, I18n.t('admins.update.success',
-                                     user_name: @user.user_name))
-      redirect_to action: 'index'
-    end
+    @user.update(user_params)
+    respond_with(@user)
   end
 
-  # Create a new Admin
   def create
-    # Default attributes: role = TA or role = STUDENT
-    # params[:user] is a hash of values passed to the controller
-    # by the HTML form with the help of ActiveView::Helper::
-    @user = Admin.new(user_params)
-    # Return unless the save is successful; save inherted from
-    # active records--creates a new record if the model is new, otherwise
-    # updates the existing record
-    if @user.save
-      flash_message(:success, I18n.t('admins.create.success',
-                                     user_name: @user.user_name))
-      redirect_to action: 'index'
-    else
-      flash_message(:error, I18n.t('admins.create.error'))
-      render 'new'
-    end
+    @user = Admin.create(user_params)
+    respond_with(@user)
   end
 
   private
 
   def user_params
     params.require(:user).permit(:user_name, :first_name, :last_name, :email)
+  end
+
+  def flash_interpolation_options
+    { resource_name: @user.user_name }
   end
 end
