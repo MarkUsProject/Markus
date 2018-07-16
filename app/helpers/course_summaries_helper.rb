0a172d85c4d1d5669529d86e4bd1113ad64efa9b
partoo-vk <p.vafaeikia@gmail.com>
2018-07-16 19:37:13 -0400

    removed redundant return from helper function

--- app/helpers/course_summaries_helper.rb
+++ app/helpers/course_summaries_helper.rb
@@ -14,7 +14,7 @@ module CourseSummariesHelper
 
   def get_student_row_information
     course_information
-    return [get_student_information(@current_user, false)]
+    [get_student_information(@current_user, false)]
   end
 
   def course_information
