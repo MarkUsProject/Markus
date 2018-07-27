542c7c1f28ef1bee755d718f004cc33a842359e6
Misha Schwartz <misha.schwartz@gmail.com>
2018-07-27 11:40:50 -0400

    timeout: re-added modal to indicate a session has expired if an ajax request fails (with 403)

--- app/assets/javascripts/redirect.js
+++ app/assets/javascripts/redirect.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   $(document).ajaxError((event, xhr) => {
     if (xhr.status === 403) {
-      window.location.reload()
+      session_expired_modal.open()
     }
   });
 });
