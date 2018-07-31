314d67485bd5c159c975a6c78f6664034633b092
Amr Sharaf <amrmagdy.sharaf@gmail.com>
2018-07-31 01:16:53 -0400

    Added statuses dict which contains info about the number of in_progress and completed runs

--- app/assets/javascripts/Components/batch_test_run_table.jsx
+++ app/assets/javascripts/Components/batch_test_run_table.jsx
@@ -6,8 +6,10 @@ import {SubmissionFileManager} from "./submission_file_manager";
 
 const makeDefaultState = () => ({
   data: [],
-  newData: []
+  newData: [],
 });
+
+var statuses = {};
 
 class BatchTestRunTable extends React.Component {
 
@@ -22,6 +24,8 @@ class BatchTestRunTable extends React.Component {
     this.fetchData();
   }
 
+
+
   fetchData() {
     console.log("fetch data start");
     $.ajax({
@@ -35,24 +39,33 @@ class BatchTestRunTable extends React.Component {
   }
 
   addButtons(){
+    var status = {}
     var newData = this.state.data;
     for(let i = 0; i < this.state.data.length; i++){
+      // Check if key exists in dictionary
+      if(!(newData[i].test_batch_id in status)){
+        // add key to dictionairy
+        status[newData[i].test_batch_id] = {total: 0, in_progress: 0};
+      }
       // Change this to in_progress
       if(newData[i].status === "complete"){
         const stop_tests_url = Routes.stop_tests_assignment_path(this.props.assignment_id);
         newData[i].action = <a href={stop_tests_url}>Stop test {newData[i].id}</a>;
         const result_url = Routes.edit_assignment_submission_result_path(this.props.assignment_id,newData[i].result_id,newData[i].result_id);
         newData[i].group_name = <a href={result_url}>{newData[i].group_name}</a>;
+        // increment in_progress number for this batch_id
+        status[newData[i].test_batch_id].total += 1;
+        status[newData[i].test_batch_id].in_progress += 1;
       } else {
         newData[i].time_to_service_estimate = "";
+        status[newData[i].test_batch_id].total += 1;
       }
     }
+    statuses = status;
     return newData;
   }
 
   render() {
-    console.log("render");
-    var {data} = this.state;
     // Set the row map to expand the latest test run when the webpage is loaded
 
     return(
