c447cca087aff07693380434cfda4e7a5b2fb3e0
David Liu <david@cs.toronto.edu>
2018-08-03 14:49:07 -0400

    react: Move old React version to only the pages that need it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,9 +8,6 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require jquery.easyModal
-// TODO: bundle ReactComponents with the actual React files.
-// (No need to load React on pages that do not use it.)
-//= require_tree ./ReactComponents
 //= require js-routes
 //= require job_poller
 //= require i18n
