cc9cdf3c1595222aea1258ef99120cecc2448d82
Alessio Di Sandro <alessio.disandro@gmail.com>
2018-09-20 11:42:01 -0400

    script: Add script to reset markus+autotesting part 2

--- script/reset.sh
+++ script/reset.sh
@@ -5,34 +5,37 @@ THIS_SCRIPT_DIR=$(dirname "${THIS_SCRIPT}")
 MARKUS_ROOT=$(readlink -f "${THIS_SCRIPT_DIR}/..")
 AUTOTEST_ROOT=$(readlink -f "${MARKUS_ROOT}/../markus-autotesting")
 
-echo "Shutting down existing MarkUs processes"
+echo "[MARKUS] Shutting down existing MarkUs processes"
 pkill -f rails
 pkill -f resque
+pkill -KILL -f webpack-dev-server # shuts down only with a -KILL
 
-echo "Shutting down existing autotest processes"
+echo "[MARKUS] Shutting down existing autotest processes"
 pkill -f supervisord
 
-echo "Removing old MarkUs files"
-rm -rf ${MARKUS_ROOT}/data/dev/autotest/*
-rm -rf ${MARKUS_ROOT}/data/dev/exam_templates/*
-rm -rf ${MARKUS_ROOT}/data/dev/repos/*
-rm -rf ${MARKUS_ROOT}/log/*
-rm -rf ${MARKUS_ROOT}/public/javascripts
-rm -rf ${MARKUS_ROOT}/public/packs
-rm -rf ${MARKUS_ROOT}/tmp/cache
+echo "[MARKUS] Removing old MarkUs files"
+rm -rf "${MARKUS_ROOT}"/data/dev/autotest/*
+rm -rf "${MARKUS_ROOT}"/data/dev/exam_templates/*
+rm -rf "${MARKUS_ROOT}"/data/dev/repos/*
+rm -rf "${MARKUS_ROOT}"/log/*
+rm -rf "${MARKUS_ROOT}"/public/javascripts
+rm -rf "${MARKUS_ROOT}"/public/packs
+rm -rf "${MARKUS_ROOT}"/tmp/cache
 
-echo "Removing old autotest files"
-rm -rf ${AUTOTEST_ROOT}/server/workspace/scripts/*
-rm -rf ${AUTOTEST_ROOT}/server/workspace/results/*
+echo "[MARKUS] Removing old autotest files"
+rm -rf "${AUTOTEST_ROOT}"/server/workspace/scripts/*
+rm -rf "${AUTOTEST_ROOT}"/server/workspace/results/*
 
-echo "Removing old redis entries"
+echo "[MARKUS] Removing old redis entries"
 redis-cli --raw DUMP autotest:workers | head -c-1 >| /tmp/.redis.tmp # make the dump play nice with shell commands
 redis-cli FLUSHDB
 cat /tmp/.redis.tmp | redis-cli -x RESTORE autotest:workers 0
 
-echo "Resetting MarkUs database"
+echo "[MARKUS] Resetting MarkUs database"
+pushd "${MARKUS_ROOT}" > /dev/null
 bundle exec rake db:reset
+popd > /dev/null
 
-echo "Restarting MarkUs"
-${MARKUS_ROOT}/script/start-autotest-workers.sh
-${MARKUS_ROOT}/bin/markus
+echo "[MARKUS] Restarting MarkUs"
+"${MARKUS_ROOT}"/script/start-autotest-workers.sh
+"${MARKUS_ROOT}"/bin/markus
