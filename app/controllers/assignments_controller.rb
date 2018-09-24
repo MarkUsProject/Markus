92bc44fdf9d79810d25d9028a28effbd033f5ec3
Misha Schwartz <misha.schwartz@gmail.com>
2018-09-24 10:10:47 -0400

    Revert "batch-tests: only show results from thecollected submission"
    This reverts commit b0092dd16d5ad62c098861bf1091a65ee4d404d3.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -393,10 +393,9 @@ class AssignmentsController < ApplicationController
       format.html
       format.json do
         user_ids = current_user.admin? ? Admin.pluck(:id) : current_user.id
-        test_runs = TestRun.left_outer_joins(:test_batch, :submission, grouping: [:group, :current_result])
-                           .where(test_runs: { user_id: user_ids },
-                                  'groupings.assignment_id': @assignment.id,
-                                  'submissions.submission_version_used': true)
+        test_runs = TestRun.left_outer_joins(:test_batch, grouping: [:group, :current_result])
+                           .where(test_runs: {user_id: user_ids},
+                                  'groupings.assignment_id': @assignment.id)
                            .pluck_to_hash(:id,
                                           :test_batch_id,
                                           :time_to_service,
