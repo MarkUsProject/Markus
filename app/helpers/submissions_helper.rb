MarkUsProject/Markus

commit be22a084ec04c3f2ed0a08528a283f1a45122bb2

Add ability to upload starter code in subdirectories through version control (#3640)
starter-code: add ability to upload starter code in subdirectories through version control

app/helpers/submissions_helper.rb
@@ -179,30 +179,28 @@ module SubmissionsHelper
     end
   end
 
-  def get_files_info(files, assignment_id, revision_identifier, path, grouping_id)
-    files.map do |file_name, file|
-      next if Repository.get_class.internal_file_names.include? file_name
-      f = {}
-      f[:id] = file.object_id
-      f[:url] = download_assignment_submissions_url(
-        id: assignment_id,
-        revision_identifier: revision_identifier,
-        file_name: file_name,
-        path: path,
-        grouping_id: grouping_id
-      )
-      f[:filename] = view_context.image_tag('icons/page_white_text.png') +
-          view_context.link_to(" #{file_name}", action: 'download',
-                               id: assignment_id,
-                               revision_identifier: revision_identifier,
-                               file_name: file_name,
-                               path: path, grouping_id: grouping_id)
-      f[:raw_name] = file_name
-      f[:last_revised_date] = I18n.l(file.last_modified_date)
-      f[:last_modified_revision] = revision_identifier
-      f[:revision_by] = file.user_id
-      f
-    end.compact
+  def get_file_info(file_name, file, assignment_id, revision_identifier, path, grouping_id)
+    return if Repository.get_class.internal_file_names.include? file_name
+    f = {}
+    f[:id] = file.object_id
+    f[:url] = download_assignment_submissions_url(
+      id: assignment_id,
+      revision_identifier: revision_identifier,
+      file_name: file_name,
+      path: path,
+      grouping_id: grouping_id
+    )
+    f[:filename] = view_context.image_tag('icons/page_white_text.png') +
+    f[:filename] += view_context.link_to(" #{file_name}", action: 'download',
+                                                          id: assignment_id,
+                                                          revision_identifier: revision_identifier,
+                                                          file_name: file_name,
+                                                          path: path, grouping_id: grouping_id)
+    f[:raw_name] = file_name
+    f[:last_revised_date] = I18n.l(file.last_modified_date)
+    f[:last_modified_revision] = revision_identifier
+    f[:revision_by] = file.user_id
+    f
   end
 
   def sanitize_file_name(file_name)

app/models/grouping.rb
@@ -530,9 +530,13 @@ class Grouping < ApplicationRecord
           starter_revision = starter_repo.get_latest_revision
           if starter_revision.path_exists?(assignment_folder)
             txn = repo.get_transaction('Markus')
-            starter_revision.files_at_path(assignment_folder).each do |starter_file_name, starter_file|
+            starter_revision.tree_at_path(assignment_folder).each do |starter_file_name, starter_obj|
               starter_file_path = File.join(assignment_folder, starter_file_name)
-              txn.add(starter_file_path, starter_repo.download_as_string(starter_file))
+              if starter_obj.is_a? Repository::RevisionDirectory
+                txn.add_path(starter_file_path)
+              else
+                txn.add(starter_file_path, starter_repo.download_as_string(starter_obj))
+              end
             end
             result = repo.commit(txn)
             revision = repo.get_latest_revision

app/controllers/assignments_controller.rb
@@ -853,46 +853,36 @@ class AssignmentsController < ApplicationController
     full_path = File.join(assignment.repository_folder, path)
     return [] unless revision.path_exists?(full_path)
 
-    files = revision.files_at_path(full_path)
-    entries = get_files_info(files, assignment.id, revision.revision_identifier, path)
-
-    entries.each do |data|
+    entries = revision.tree_at_path(full_path)
+                      .select { |_, obj| obj.is_a? Repository::RevisionFile }.map do |file_name, file_obj|
+      data = get_file_info(file_name, file_obj, assignment.id, path)
       data[:key] = path.blank? ? data[:raw_name] : File.join(path, data[:raw_name])
       data[:modified] = data[:last_revised_date]
       data[:size] = 1 # Dummy value
+      data
     end
-
-    revision.directories_at_path(full_path).each do |directory_name, _|
-      entries.concat(get_all_file_data(
-                       revision,
-                       path.blank? ? directory_name : File.join(path, directory_name)
-                     ))
-    end
-
     entries
   end
 
-  def get_files_info(files, assignment_id, revision_identifier, path)
-    files.map do |file_name, file|
-      {
-        id: file.object_id,
-        url: download_starter_code_assignment_url(
-          id: assignment_id,
-          file_name: file_name,
-          path: path,
-        ),
-        filename: view_context.image_tag('icons/page_white_text.png') +
-          view_context.link_to(file_name,
-                               action: 'download_starter_code',
-                               id: assignment_id,
-                               file_name: file_name,
-                               path: path),
-        raw_name: file_name,
-        last_revised_date: l(file.last_modified_date),
-        last_modified_revision: file.last_modified_revision,
-        revision_by: file.user_id
-      }
-    end
+  def get_file_info(file_name, file, assignment_id, path)
+    {
+      id: file.object_id,
+      url: download_starter_code_assignment_url(
+        id: assignment_id,
+        file_name: file_name,
+        path: path,
+      ),
+      filename: view_context.image_tag('icons/page_white_text.png') +
+        view_context.link_to(file_name,
+                             action: 'download_starter_code',
+                             id: assignment_id,
+                             file_name: file_name,
+                             path: path),
+      raw_name: file_name,
+      last_revised_date: l(file.last_modified_date),
+      last_modified_revision: file.last_modified_revision,
+      revision_by: file.user_id
+    }
   end
 
     def update_assignment!(map)

app/controllers/submissions_controller.rb
@@ -764,23 +764,15 @@ class SubmissionsController < ApplicationController
     full_path = File.join(grouping.assignment.repository_folder, path)
     return [] unless revision.path_exists?(full_path)
 
-    files = revision.files_at_path(full_path)
-    entries = get_files_info(files, grouping.assignment.id, revision.revision_identifier, path,
-                             grouping.id)
-
-    entries.each do |data|
+    entries = revision.tree_at_path(full_path)
+                      .select { |_, obj| obj.is_a? Repository::RevisionFile }.map do |file_name, file_obj|
+      data = get_file_info(file_name, file_obj, grouping.assignment.id, revision.revision_identifier, path, grouping.id)
+      next if data.nil?
       data[:key] = path.blank? ? data[:raw_name] : File.join(path, data[:raw_name])
       data[:modified] = data[:last_revised_date]
       data[:size] = 1 # Dummy value
-    end
-
-    revision.directories_at_path(full_path).each do |directory_name, _|
-      entries.concat(get_all_file_data(
-        revision, grouping,
-        path.blank? ? directory_name : File.join(path, directory_name)
-      ))
-    end
-
+      data
+    end.compact
     entries
   end
 end

app/lib/git_repository.rb
@@ -638,6 +638,21 @@ class GitRevision < Repository::AbstractRevision
     entries_at_path(path, :tree)
   end
 
+  # Walks all files and subdirectories starting at +path+ and
+  # returns an array of tuples containing [path, revision_object]
+  # for every file and directory discovered in this way
+  #
+  # It returns an array to ensure ordering, so that a directory
+  # will always appear before any of the files or subdirectories
+  # contained within it
+  def tree_at_path(path)
+    result = entries_at_path(path).to_a
+    result.select { |_, obj| obj.is_a? Repository::RevisionDirectory }.each do |dir_path, _|
+      result.push(*(tree_at_path(File.join(path, dir_path)).map { |sub_pth, obj| [File.join(dir_path, sub_pth), obj] }))
+    end
+    result
+  end
+
   def last_modified_date
     self.timestamp
   end

app/lib/memory_repository.rb
@@ -449,6 +449,23 @@ class MemoryRevision < Repository::AbstractRevision
     return files_at_path_helper(path, false, Repository::RevisionDirectory)
   end
 
+  # Walks all files and subdirectories starting at +path+ and
+  # returns an array of tuples containing [path, revision_object]
+  # for every file and directory discovered in this way
+  #
+  # It returns an array to ensure ordering, so that a directory
+  # will always appear before any of the files or subdirectories
+  # contained within it
+  def tree_at_path(path)
+    result = files_at_path(path).to_a
+    dirs = directories_at_path(path)
+    result.push(*dirs.to_a)
+    dirs.each do |dir_path, _|
+      result.push(*(tree_at_path(File.join(path, dir_path)).map { |sub_pth, obj| [File.join(dir_path, sub_pth), obj] }))
+    end
+    result
+  end
+
   def changes_at_path?(path)
     !files_at_path_helper(path, true).empty?
   end

app/lib/subversion_repository.rb
@@ -780,6 +780,23 @@ class SubversionRevision < Repository::AbstractRevision
     # p.start_with?(path) only would be wrong, there can be two assignments named like 'aX' and 'aXsuffix'
   end
 
+  # Walks all files and subdirectories starting at +path+ and
+  # returns an array of tuples containing [path, revision_object]
+  # for every file and directory discovered in this way
+  #
+  # It returns an array to ensure ordering, so that a directory
+  # will always appear before any of the files or subdirectories
+  # contained within it
+  def tree_at_path(path)
+    result = files_at_path(path).to_a
+    dirs = directories_at_path(path)
+    result.push(*dirs.to_a)
+    dirs.each do |dir_path, _|
+      result.push(*(tree_at_path(File.join(path, dir_path)).map { |sub_pth, obj| [File.join(dir_path, sub_pth), obj] }))
+    end
+    result
+  end
+
   private
 
   def files_at_path_helper(path = '/', only_changed = false)

commit e0dbfe4282d4110dac8b7aca8029d5dfdbb989d3

starter-code:  bug fixes that were introduced in PR #3640

app/helpers/submissions_helper.rb
@@ -191,11 +191,11 @@ module SubmissionsHelper
       grouping_id: grouping_id
     )
     f[:filename] = view_context.image_tag('icons/page_white_text.png') +
-    f[:filename] += view_context.link_to(" #{file_name}", action: 'download',
-                                                          id: assignment_id,
-                                                          revision_identifier: revision_identifier,
-                                                          file_name: file_name,
-                                                          path: path, grouping_id: grouping_id)
+                   view_context.link_to(" #{file_name}", action: 'download',
+                                                         id: assignment_id,
+                                                         revision_identifier: revision_identifier,
+                                                         file_name: file_name,
+                                                         path: path, grouping_id: grouping_id)
     f[:raw_name] = file_name
     f[:last_revised_date] = I18n.l(file.last_modified_date)
     f[:last_modified_revision] = revision_identifier

app/models/assignment.rb
@@ -1095,6 +1095,11 @@ class Assignment < ApplicationRecord
                      "Error message: '#{e.message}'",
                    MarkusLogger::ERROR)
     end
+    access_repo do |repo|
+      txn = repo.get_transaction('Markus')
+      txn.add_path(repository_folder)
+      repo.commit(txn)
+    end
     true
   end
 

app/models/grouping.rb
@@ -534,8 +534,10 @@ class Grouping < ApplicationRecord
               starter_file_path = File.join(assignment_folder, starter_file_name)
               if starter_obj.is_a? Repository::RevisionDirectory
                 txn.add_path(starter_file_path)
+              elsif revision.path_exists? starter_file_path
+                txn.replace(starter_file_path, starter_repo.download_as_string(starter_obj), starter_obj.mime_type, revision.revision_identifier)
               else
-                txn.add(starter_file_path, starter_repo.download_as_string(starter_obj))
+                txn.add(starter_file_path, starter_repo.download_as_string(starter_obj), starter_obj.mime_type)
               end
             end
             result = repo.commit(txn)