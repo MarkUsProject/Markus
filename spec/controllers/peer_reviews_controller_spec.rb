bbe2355ac0f0eeaf721e184cc0f095be036fca14
Hannah Li <hannah.li@mail.utoronto.ca>
2018-11-27 16:51:47 -0500

    added more controller tests

--- spec/controllers/peer_reviews_controller_spec.rb
+++ spec/controllers/peer_reviews_controller_spec.rb
@@ -16,6 +16,95 @@ describe PeerReviewsController do
 
   end
 
+  context 'download CSV' do
+    before :each do
+      post :assign_groups,
+           params: { actionString: 'random_assign',
+                     selectedReviewerGroupIds: @selected_reviewer_group_ids,
+                     selectedRevieweeGroupIds: @selected_reviewee_group_ids,
+                     assignment_id: @pr_id,
+                     numGroupsToAssign: 1
+           }
+      @num_peer_reviews = PeerReview.all.count
+
+      # Remember who was assigned to who before comparing
+      @pr_expected_lines = Set.new
+      @assignment_with_pr.peer_reviews.each do |pr|
+        @pr_expected_lines.add("#{pr.reviewer.group.group_name},#{pr.reviewee.group.group_name}")
+      end
+
+      # Perform downloading via GET
+      get :download_reviewer_reviewee_mapping, params: { assignment_id: @pr_id }
+      @downloaded_text = response.body
+      @found_filename = response.header['Content-Disposition'].include?('filename="peer_review_group_to_group_mapping.csv"')
+      @lines = @downloaded_text[0...-1].split("\n")
+    end
+
+    it 'has valid header' do
+      expect(@found_filename).to be_truthy
+    end
+
+    it 'ends with \n' do
+      expect(@downloaded_text[-1, 1]).to eql("\n")
+    end
+
+    it 'has the correct number of lines' do
+      # Since we compress all the reviewees onto one line with the reviewer,
+      # there should only be 'num reviewers' amount of lines
+      expect(@lines.count).to be @selected_reviewer_group_ids.count
+    end
+
+    it 'all lines match with no duplicates' do
+      # Make sure that they all match, and that we don't have any duplicates
+      # by accident (ex: having 'group1,group2,group3' repeated two times)
+      uniqueness_test_set = Set.new
+      @lines.each do |line|
+        uniqueness_test_set.add(line)
+        expect(@pr_expected_lines.member?(line)).to be_truthy
+      end
+      expect(uniqueness_test_set.count).to be @num_peer_reviews
+    end
+  end
+
+  context 'upload CSV' do
+    before :each do
+      post :assign_groups,
+           params: { actionString: 'random_assign',
+                     selectedReviewerGroupIds: @selected_reviewer_group_ids,
+                     selectedRevieweeGroupIds: @selected_reviewee_group_ids,
+                     assignment_id: @pr_id,
+                     numGroupsToAssign: 1
+           }
+      get :download_reviewer_reviewee_mapping, params: { assignment_id: @pr_id }
+      @downloaded_text = response.body
+      PeerReview.all.destroy_all
+      @path = File.join(self.class.fixture_path, TEMP_CSV_FILE_PATH)
+      # Now allow uploading by placing the data in a temporary file and reading
+      # the data back through 'uploading' (requires a clean database)
+      File.open(@path, 'w') do |f|
+        f.write(@downloaded_text)
+      end
+      csv_upload = fixture_file_upload(TEMP_CSV_FILE_PATH, 'text/csv')
+      fixture_upload = fixture_file_upload(TEMP_CSV_FILE_PATH, 'text/csv')
+      allow(csv_upload).to receive(:read).and_return(File.read(fixture_upload))
+
+      post :csv_upload_handler, params: { assignment_id: @pr_id, peer_review_mapping: csv_upload, encoding: 'UTF-8' }
+    end
+
+    it 'has the correct number of groups' do
+      expect(Grouping.all.size).to eq 6
+    end
+
+    it 'has the correct number of peer reviews' do
+      expect(PeerReview.all.size).to eq 3
+    end
+
+    it 'temporary file is deleted' do
+      File.delete(@path)
+      expect(File.exist?(@path)).to be_falsey
+    end
+  end
+
   context 'random assign' do
     before :each do
       post :assign_groups,
@@ -43,87 +132,8 @@ describe PeerReviewsController do
       end
     end
 
-    # it 'random assigns properly' dp
-    #   expect(PeerReview.where(result: Result.all).size).to eq 3
-    # end
-
     it 'assigns all selected reviewer groups' do
       expect(@assignment_with_pr.peer_reviews.count).to be @selected_reviewer_group_ids.count
-    end
-
-    context 'download CSV' do
-      before :each do
-        @num_peer_reviews = PeerReview.all.count
-
-        # Remember who was assigned to who before comparing
-        @pr_expected_lines = Set.new
-        @assignment_with_pr.peer_reviews.each do |pr|
-          @pr_expected_lines.add("#{pr.reviewer.group.group_name},#{pr.reviewee.group.group_name}")
-        end
-
-        # Perform downloading via GET
-        get :download_reviewer_reviewee_mapping, params: { assignment_id: @pr_id }
-        @downloaded_text = response.body
-        @found_filename = response.header['Content-Disposition'].include?('filename="peer_review_group_to_group_mapping.csv"')
-        @lines = @downloaded_text[0...-1].split("\n")
-      end
-
-      it 'has valid header' do
-        expect(@found_filename).to be_truthy
-      end
-
-      it 'ends with \n' do
-        expect(@downloaded_text[-1, 1]).to eql("\n")
-      end
-
-      it 'has the correct number of lines' do
-        # Since we compress all the reviewees onto one line with the reviewer,
-        # there should only be 'num reviewers' amount of lines
-        expect(@lines.count).to be @selected_reviewer_group_ids.count
-      end
-
-      it 'all lines match with no duplicates' do
-        # Make sure that they all match, and that we don't have any duplicates
-        # by accident (ex: having 'group1,group2,group3' repeated two times)
-        uniqueness_test_set = Set.new
-        @lines.each do |line|
-          uniqueness_test_set.add(line)
-          expect(@pr_expected_lines.member?(line)).to be_truthy
-        end
-        expect(uniqueness_test_set.count).to be @num_peer_reviews
-      end
-    end
-
-    context 'upload CSV' do
-      before :each do
-        get :download_reviewer_reviewee_mapping, params: { assignment_id: @pr_id }
-        @downloaded_text = response.body
-        PeerReview.all.destroy_all
-        @path = File.join(self.class.fixture_path, TEMP_CSV_FILE_PATH)
-        # Now allow uploading by placing the data in a temporary file and reading
-        # the data back through 'uploading' (requires a clean database)
-        File.open(@path, 'w') do |f|
-          f.write(@downloaded_text)
-        end
-        csv_upload = fixture_file_upload(TEMP_CSV_FILE_PATH, 'text/csv')
-        fixture_upload = fixture_file_upload(TEMP_CSV_FILE_PATH, 'text/csv')
-        allow(csv_upload).to receive(:read).and_return(File.read(fixture_upload))
-
-        post :csv_upload_handler, params: { assignment_id: @pr_id, peer_review_mapping: csv_upload, encoding: 'UTF-8' }
-      end
-
-      it 'has the correct number of groups' do
-        expect(Grouping.all.size).to eq 6
-      end
-
-      it 'has the correct number of peer reviews' do
-        expect(PeerReview.all.size).to eq 3
-      end
-
-      it 'temporary file is deleted' do
-        File.delete(@path)
-        expect(File.exist?(@path)).to be_falsey
-      end
     end
   end
 
@@ -180,19 +190,25 @@ describe PeerReviewsController do
 
     context 'selected reviewer for selected reviewees' do
       before :each do
+        @reviewer = Grouping.where(id: @selected_reviewer_group_ids[0]).first
+        @reviewee = Grouping.where(id: @selected_reviewee_group_ids[1]).first
         selected_group = {}
-        selected_group[@selected_reviewer_group_ids[0]] = true
+        selected_group[@reviewer.id] = true
         selected = {}
-        selected[@selected_reviewee_group_ids[1]] = selected_group
+        selected[@reviewee.id] = selected_group
         post :assign_groups,
              params: { actionString: 'unassign',
-                       electedReviewerInRevieweeGroups: selected,
+                       selectedReviewerInRevieweeGroups: selected,
                        assignment_id: @pr_id,
              }
       end
 
       it 'deletes the correct number of peer reviews' do
         expect(PeerReview.all.count).to eq @num_peer_reviews - 1
+      end
+
+      it 'removes selected reviewer as reviewer for selected reviewee' do
+        expect(PeerReview.review_exists_between?(@reviewer, @reviewee)).to be false
       end
     end
   end
