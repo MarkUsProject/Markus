cc526d7694f446b75d067f65bd45601194efae74
Misha Schwartz <misha.schwartz@gmail.com>
2018-11-28 16:06:20 -0500

    react-table: documentation for table helper functions

--- app/assets/javascripts/Components/ta_table.jsx
+++ app/assets/javascripts/Components/ta_table.jsx
@@ -3,7 +3,6 @@ import {render} from 'react-dom';
 
 import ReactTable from 'react-table';
 import {stringFilter} from './Helpers/table_helpers';
-import {CheckboxTable} from "./markus_with_selection_hoc";
 
 
 class TATable extends React.Component {
