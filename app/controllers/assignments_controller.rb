7387db3d2a67894b28b1e3fac79e7d5b989ba65e
Hannah Li <hannah.li@mail.utoronto.ca>
2018-12-06 18:27:16 -0500

    removed fixes for student-side of peer review

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -126,11 +126,8 @@ class AssignmentsController < ApplicationController
     @penalty = @assignment.submission_rule
     @enum_penalty = Period.where(submission_rule_id: @penalty.id).sort
 
-    @prs = @student.grouping_for(@assignment.parent_assignment.id)&.
-        peer_reviews&.where(results: { released_to_students: true })
-    if @prs.nil?
-      @prs = []
-    end
+    @prs = @student.grouping_for(@assignment.parent_assignment.id).
+        peer_reviews.where(results: { released_to_students: true })
 
     if @student.section &&
         !@student.section.section_due_date_for(@assignment.id).nil?
