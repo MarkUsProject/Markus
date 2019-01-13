0b792256c830fa88b08b5817a312fbced4bb3555
David Liu <david@cs.toronto.edu>
2019-01-13 18:24:08 -0500

    Cleanup old JavaScript files.

--- app/assets/javascripts/Graders/tabs_boot.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#graders-tabs').tabs();
-  $('#groups-tabs').tabs({
-    activate: function(event, ui) {
-      var new_container_id =  ui.newPanel.get(0).id;
-      document.getElementById('current_table').value = new_container_id;
-    }
-  });
-});
