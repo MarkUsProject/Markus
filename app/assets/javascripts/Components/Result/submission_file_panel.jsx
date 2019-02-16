144b628b4257dd4f386eea313a8230f3aee20202
David Liu <david@cs.toronto.edu>
2019-02-16 13:31:03 -0500

    results: Move file selector into its own component

--- app/assets/javascripts/Components/Result/submission_file_panel.jsx
+++ app/assets/javascripts/Components/Result/submission_file_panel.jsx
@@ -12,7 +12,7 @@ export class SubmissionFilePanel extends React.Component {
       fileContents: null,
       fileType: null,
       fileData: {files: [], directories: {}, name: ''},
-      expanded: null
+      visibleAnnotations: [],
     };
     this.submissionFileViewer = React.createRef();
   }
@@ -79,59 +79,6 @@ export class SubmissionFilePanel extends React.Component {
     return null;
   };
 
-  // Convert a nested hash into a nested <ul>.
-  hashToHTMLList = (hash, expanded) => {
-    let dirs = [];
-    let newExpanded, displayStyle;
-    if (expanded === null) {
-      newExpanded = null;
-      displayStyle = 'none';
-    } else if (hash['name'] === '') {
-      newExpanded = expanded;
-      displayStyle = 'block'
-    } else {
-      newExpanded = expanded.slice(1);
-      displayStyle = hash['name'] === expanded[0] ? 'block' : 'none';
-    }
-
-    for (let d in hash['directories']) {
-      if (hash['directories'].hasOwnProperty(d)) {
-        let dir = hash['directories'][d];
-        dirs.push(
-          <li className='nested-submenu' key={dir.path.join('/')}>
-            <a onClick={(e) => {
-              e.stopPropagation();
-              this.expandFileSelector(dir.path);
-            }}><strong>{dir.name}</strong></a>
-            {this.hashToHTMLList(dir, newExpanded)}
-          </li>
-        );
-      }
-    }
-
-    return (
-      <ul className='nested-folder' style={{display: displayStyle}}>
-        {dirs}
-        {hash['files'].map(f => {
-          const [name, id] = f;
-          const fullPath = hash.path.concat([name]).join('/');
-          return (<li className='file_item' key={fullPath}>
-            <a onClick={(e) => {
-              e.stopPropagation();
-              this.selectFile(fullPath, id);
-            }}>
-              {f[0]}
-            </a>
-          </li>)
-        })}
-      </ul>
-    );
-  };
-
-  expandFileSelector = (path) => {
-    this.setState({expanded: path});
-  };
-
   selectFile = (file, id, focus_line) => {
     this.setState({selectedFile: [file, id]});
     localStorage.setItem('file', file);
@@ -173,47 +120,17 @@ export class SubmissionFilePanel extends React.Component {
   };
 
   render() {
-    const fileSelector = this.hashToHTMLList(this.state.fileData, this.state.expanded);
-    let arrow, expand;
-    if (this.state.expanded !== null) {
-      arrow = <span className='arrow_up' />;
-      expand = null;
-    } else {
-      arrow = <span className='arrow_down' />;
-      expand = [];
-    }
-    let selectorLabel;
-    if (this.state.fileData.files.length === 0 && this.state.fileData.directories.length === 0) {
-      selectorLabel = I18n.t('submissions.no_files_available');
-    } else if (this.state.selectedFile !== null) {
-      selectorLabel = this.state.selectedFile[0];
-    } else {
-      selectorLabel = '';
-    }
-
     const submission_file_id = this.state.selectedFile === null ? null : this.state.selectedFile[1];
 
     return (
       <div>
         <div id='sel_box'/>
         <div id='annotation_menu'>
-          <div className='file_selector'>
-            <div
-              className='dropdown'
-              onClick={(e) => {
-                e.stopPropagation();
-                this.expandFileSelector(expand);
-              }}
-              onMouseLeave={() => this.expandFileSelector(null)}
-            >
-              <a>{selectorLabel}</a>
-              {arrow}
-              {this.state.expanded &&
-               <div>
-                 {fileSelector}
-               </div>}
-            </div>
-          </div>
+          <FileSelector
+            fileData={this.props.fileData}
+            onSelectFile={this.selectFile}
+            selectedFile={this.state.selectedFile}
+          />
           {this.props.canDownload &&
             <button onClick={() => this.modalDownload.open()}>
               {I18n.t('download')}
@@ -237,6 +154,117 @@ export class SubmissionFilePanel extends React.Component {
             result_id={this.props.result_id}
             selectedFile={submission_file_id}
           />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// Component for the file selector.
+class FileSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: null
+    }
+  }
+
+  // Convert a nested hash into a nested <ul>.
+  hashToHTMLList = (hash, expanded) => {
+    let dirs = [];
+    let newExpanded, displayStyle;
+    if (expanded === null) {
+      newExpanded = null;
+      displayStyle = 'none';
+    } else if (hash['name'] === '') {
+      newExpanded = expanded;
+      displayStyle = 'block'
+    } else {
+      newExpanded = expanded.slice(1);
+      displayStyle = hash['name'] === expanded[0] ? 'block' : 'none';
+    }
+
+    for (let d in hash['directories']) {
+      if (hash['directories'].hasOwnProperty(d)) {
+        let dir = hash['directories'][d];
+        dirs.push(
+          <li className='nested-submenu' key={dir.path.join('/')}>
+            <a onClick={(e) => this.selectDirectory(e, dir.path)}>
+              <strong>{dir.name}</strong>
+            </a>
+            {this.hashToHTMLList(dir, newExpanded)}
+          </li>
+        );
+      }
+    }
+
+    return (
+      <ul className='nested-folder' style={{display: displayStyle}}>
+        {dirs}
+        {hash['files'].map(f => {
+          const [name, id] = f;
+          const fullPath = hash.path.concat([name]).join('/');
+          return (<li className='file_item' key={fullPath}>
+            <a onClick={(e) => this.selectFile(e, fullPath, id)}>
+              {f[0]}
+            </a>
+          </li>)
+        })}
+      </ul>
+    );
+  };
+
+  selectFile = (e, fullPath, id) => {
+    e.stopPropagation();
+    this.props.onSelectFile(fullPath, id);
+    this.setState({expanded: null});
+  };
+
+  selectDirectory = (e, path) => {
+    e.stopPropagation();
+    this.setState({expanded: path});
+  };
+
+  expandFileSelector = (path) => {
+    this.setState({expanded: path});
+  };
+
+  render() {
+    const fileSelector = this.hashToHTMLList(this.props.fileData, this.state.expanded);
+    let arrow, expand;
+    if (this.state.expanded !== null) {
+      arrow = <span className='arrow_up' />;
+      expand = null;
+    } else {
+      arrow = <span className='arrow_down' />;
+      expand = [];
+    }
+    let selectorLabel;
+    if (this.props.fileData.files.length === 0 && this.props.fileData.directories.length === 0) {
+      selectorLabel = I18n.t('submissions.no_files_available');
+    } else if (this.props.selectedFile !== null) {
+      selectorLabel = this.props.selectedFile[0];
+    } else {
+      selectorLabel = '';
+    }
+
+    return (
+      <div className='file_selector'>
+        <div
+          className='dropdown'
+          onClick={(e) => {
+            e.stopPropagation();
+            this.expandFileSelector(expand);
+          }}
+          onMouseLeave={() => this.expandFileSelector(null)}
+        >
+          <a>{selectorLabel}</a>
+          {arrow}
+          {this.state.expanded &&
+           <div>
+             {fileSelector}
+           </div>}
         </div>
       </div>
     );
