e9b302901811de683aba43888ad0c72d5a0fc498
YolandaZIK <zyq666@yandex.com>
2019-02-18 12:16:55 -0500

    Modify the test for annotation_category controller, especially update testcases for #yml_upload.

--- spec/controllers/annotation_categories_controller_spec.rb
+++ spec/controllers/annotation_categories_controller_spec.rb
@@ -10,40 +10,21 @@ describe AnnotationCategoriesController do
   let(:assignment) { FactoryBot.create(:assignment) }
 
   context 'csv_upload' do
-    before :each do
-      # We need to mock the rack file to return its content when
-      # the '.read' method is called to simulate the behaviour of
-      # the http uploaded file
+    # We need to mock the rack file to return its content when
+    # the '.read' method is called to simulate the behaviour of the http uploaded file
+    it 'accepts a valid file' do
       @file_good = fixture_file_upload(
-        'files/annotation_categories/form_good.csv', 'text/csv')
+        'files/annotation_categories/form_good.csv', 'text/csv'
+      )
       allow(@file_good).to receive(:read).and_return(
         File.read(fixture_file_upload(
                     'files/annotation_categories/form_good.csv',
-                    'text/csv')))
-
-      @file_invalid_column = fixture_file_upload(
-        'files/annotation_categories/form_invalid_column.csv', 'text/csv')
-      allow(@file_invalid_column).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/annotation_categories/form_invalid_column.csv',
-                    'text/csv')))
-
-      @file_bad_csv = fixture_file_upload(
-        'files/bad_csv.csv', 'text/xls')
-      allow(@file_bad_csv).to receive(:read).and_return(
-        File.read(fixture_file_upload('files/bad_csv.csv', 'text/csv')))
-
-      @file_wrong_format = fixture_file_upload(
-        'files/wrong_csv_format.xls', 'text/xls')
-      allow(@file_wrong_format).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/wrong_csv_format.xls', 'text/csv')))
-      # This must line up with the second entry in the file_good
+                    'text/csv'
+                  ))
+      )
       @test_category_name = 'test_category'
       @test_content = 'c6conley'
-    end
 
-    it 'accepts a valid file' do
       post :csv_upload, params: { assignment_id: assignment.id, annotation_category_list_csv: @file_good }
 
       expect(response.status).to eq(302)
@@ -59,59 +40,203 @@ describe AnnotationCategoriesController do
       found_cat = false
       AnnotationCategory.all.each do |ac|
         next unless ac['annotation_category_name'] == @test_category_name
+
         found_cat = true
-        expect(AnnotationText.where(annotation_category: ac).take['content'])
-          .to eq(@test_content)
+        expect(AnnotationText.where(annotation_category: ac).take['content']).to eq(@test_content)
       end
       expect(found_cat).to eq(true)
     end
 
+    # this test case is to test a file with an annotation under an annotation category that has no name
     it 'does not accept files with invalid columns' do
+      @file_invalid_column = fixture_file_upload(
+        'files/annotation_categories/form_invalid_column.csv', 'text/csv'
+      )
+      allow(@file_invalid_column).to receive(:read).and_return(
+        File.read(fixture_file_upload(
+                    'files/annotation_categories/form_invalid_column.csv',
+                    'text/csv'
+                  ))
+      )
+
       post :csv_upload, params: { assignment_id: assignment.id, annotation_category_list_csv: @file_invalid_column }
 
       expect(response.status).to eq(302)
-      expect(flash[:error]).to_not be_empty
-      expect(response).to redirect_to(action: 'index',
-                                      id: assignment.id)
+      expect(flash[:error].size).to eq(1)
+      expect(AnnotationCategory.all.size).to eq(0)
+      # print(flash[:error].first) => <p>The following CSV rows were invalid: c6conley</p>
+      expect(response).to redirect_to(action: 'index', id: assignment.id)
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
     end
 
     it 'does not accept fileless submission' do
       post :csv_upload, params: { assignment_id: assignment.id }
 
       expect(response.status).to eq(302)
-      expect(flash[:error]).to_not be_empty
-      expect(response).to redirect_to(action: 'index',
-                                      id: assignment.id)
+      expect(flash[:error].size).to eq(1)
+      # print(flash[:error].first) => <p>You must select a file to upload.</p>
+      expect(response).to redirect_to(action: 'index', id: assignment.id)
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
     end
 
     it 'does not accept a non-csv file with .csv extension' do
+      @file_bad_csv = fixture_file_upload(
+        'files/bad_csv.csv', 'text/xls'
+      )
+      allow(@file_bad_csv).to receive(:read).and_return(
+        File.read(fixture_file_upload('files/bad_csv.csv', 'text/csv'))
+      )
       post :csv_upload, params: { assignment_id: assignment.id, annotation_category_list_csv: @file_bad_csv }
 
       expect(response.status).to eq(302)
       expect(flash[:error]).to_not be_empty
-      expect(response).to redirect_to(action: 'index',
-                                      id: assignment.id)
+      # print (flash[:error].first) =>
+      # <p>The selected file is not a CSV file (even though it may have a .csv extension).</p>
+      expect(response).to redirect_to(action: 'index', id: assignment.id)
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
     end
 
     it 'does not accept a .xls file' do
+      @file_wrong_format = fixture_file_upload(
+        'files/wrong_csv_format.xls', 'text/xls'
+      )
+      allow(@file_wrong_format).to receive(:read).and_return(
+        File.read(fixture_file_upload(
+                    'files/wrong_csv_format.xls', 'text/csv'
+                  ))
+      )
+
       post :csv_upload, params: { assignment_id: assignment.id, annotation_category_list_csv: @file_wrong_format }
 
       expect(response.status).to eq(302)
       expect(flash[:error]).to_not be_empty
+      # print(flash[:error].first) =>
+      # <p>The selected file is not a CSV file (even though it may have a .csv extension).</p>
+      expect(AnnotationCategory.all.size).to eq(0)
       expect(flash[:error].map { |f| extract_text f })
         .to eq([I18n.t('upload_errors.unparseable_csv')].map { |f| extract_text f })
-      expect(response).to redirect_to(action: 'index',
-                                      id: assignment.id)
+      expect(response).to redirect_to(action: 'index', id: assignment.id)
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
+    end
+
+    it 'does not accept any file without .csv extension' do
+      @yml_file_not_for_csv_upload = fixture_file_upload(
+        'files/annotation_categories/yml_file_should_not_be_saved_by_csv_file.yml', 'text/yml'
+      )
+      allow(@yml_file_not_for_csv_upload).to receive(:read).and_return(
+        File.read(
+          fixture_file_upload('files/annotation_categories/yml_file_should_not_be_saved_by_csv_file.yml', 'text/yml')
+        )
+      )
+      post :csv_upload, params: { assignment_id: assignment.id,
+                                  annotation_category_list_csv: @yml_file_not_for_csv_upload }
+      expect(response.status).to eq(302)
+      expect(flash[:error]).to_not be_empty
+      # print(flash[:error].first) => <p>The following CSV rows were invalid:  -  -  -  -  -  -  -  -  - </p>
+      expect(AnnotationCategory.all.size).to eq(0)
+      expect(response).to redirect_to(action: 'index', id: assignment.id)
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
+    end
+  end
+
+  context 'yml_upload' do
+    it 'accept a valid file' do
+      @valid_yml_file = fixture_file_upload('files/annotation_categories/valid_yml.yml', 'text/yml')
+      allow(@valid_yml_file).to receive(:read).and_return(
+        File.read(fixture_file_upload('spec/fixtures/files/annotation_categories/valid_yml.yml'))
+      )
+
+      post :yml_upload, params: { assignment_id: assignment.id, annotation_category_list_yml: @valid_yml_file }
+
+      expect(flash[:success].size).to eq(1)
+      expect(response.status).to eq(302)
+
+      annotation_category_list = AnnotationCategory.all
+      index = 0
+      while index < annotation_category_list.size
+        curr_cat = annotation_category_list[index]
+        expect(curr_cat.annotation_category_name).to be_eql(('Problem ' + (index + 1).to_s))
+        expect(curr_cat.annotation_texts_count).to eq(1)
+        expect(curr_cat.annotation_texts.all[0].content).to be_eql(('Test on question ' + (index + 1).to_s))
+        index += 1
+      end
+      expect(annotation_category_list.size).to eq(4)
+    end
+
+    it 'does not accept files with empty annotation category name' do
+      @yml_with_invalid_category = fixture_file_upload('files/annotation_categories/yml_with_invalid_category.yml')
+      allow(@yml_with_invalid_category).to receive(:read).and_return(
+        File.read(fixture_file_upload('files/annotation_categories/yml_with_invalid_category.yml'))
+      )
+
+      post :yml_upload, params: { assignment_id: assignment.id,
+                                  annotation_category_list_yml: @yml_with_invalid_category }
+      expect(response.status).to eq(302)
+      expect(flash[:error].size).to eq(1)
+      expect(AnnotationCategory.all.size).to eq(0)
+      # print(flash[:error].first) =>
+      # <p>There is an error in the file you uploaded: (<unknown>):
+      # did not find expected '-' indicator while parsing a block collection at line 7 column 3</p>
+      expect(response).to redirect_to action: 'index', assignment_id: assignment.id
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
+    end
+
+    it 'does not accept fileless submission' do
+      post :yml_upload, params: { assignment_id: assignment.id }
+
+      expect(response.status).to eq(302)
+      expect(flash[:message]).not_to be_nil
+      expect(response).to redirect_to action: 'index', assignment_id: assignment.id
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
+    end
+
+    it 'does not accept a non-yml file with .yml extension' do
+      @non_yml_file = fixture_file_upload('files/annotation_categories/non_yml_file.yml', 'image/png')
+      allow(@non_yml_file).to receive(:read).and_return(
+        File.read(fixture_file_upload('files/annotation_categories/non_yml_file.yml', 'text/yml'))
+      )
+      post :yml_upload, params: { assignment_id: assignment.id, annotation_category_list_yml: @non_yml_file }
+
+      expect(response.status).to eq(302)
+      expect(flash[:error].size).to eq(1)
+      # print(flash[:error].first) =>
+      # <p>There is an error in the file you uploaded: (<unknown>):
+      # control characters are not allowed at line 1 column 1</p>
+      expect(response).to redirect_to action: 'index', assignment_id: assignment.id
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
+    end
+
+    it 'does not accept a .xls file' do
+      @xls_file = fixture_file_upload('files/annotation_categories/xls_annotation_cat.xls', 'text/xls')
+      allow(@xls_file).to receive(:read).and_return(
+        File.read(fixture_file_upload('files/annotation_categories/xls_annotation_cat.xls', 'text/xls'))
+      )
+      post :yml_upload, params: { assignment_id: assignment.id, annotation_category_list_yml: @xls_file }
+
+      expect(response.status).to eq(302)
+      expect(flash[:error].size).to eq(1)
+      expect(AnnotationCategory.all.size).to eq(0)
+      # print(flash[:error].first) =>
+      # <p>There is an error in the file you uploaded: (<unknown>):
+      # control characters are not allowed at line 1 column 1</p>
+      expect(response).to redirect_to action: 'index', assignment_id: assignment.id
+      expect(response.location).to eq('http://test.host/en/assignments/1/annotation_categories?id=1')
     end
   end
 
   context 'CSV_Downloads' do
-    let(:annotation_category) { create(:annotation_category,
-                                       assignment: assignment) }
-    let(:annotation_text) { create(:annotation_text,
-                                   annotation_category: annotation_category) }
-    let(:csv_data) { "#{annotation_category.annotation_category_name}," +
-      "#{annotation_text.content}\n" }
+    let(:annotation_category) do
+      create(:annotation_category,
+             assignment: assignment)
+    end
+    let(:annotation_text) do
+      create(:annotation_text,
+             annotation_category: annotation_category)
+    end
+    let(:csv_data) do
+      "#{annotation_category.annotation_category_name}," \
+      "#{annotation_text.content}\n"
+    end
     let(:csv_options) do
       {
         filename: "#{assignment.short_identifier}_annotations.csv",
@@ -149,7 +274,7 @@ describe AnnotationCategoriesController do
     it 'filename passes naming conventions' do
       get :download, params: { assignment_id: assignment.id }, format: 'csv'
       filename = response.header['Content-Disposition']
-        .split.last.split('"').second
+                         .split.last.split('"').second
       expect(filename).to eq "#{assignment.short_identifier}_annotations.csv"
     end
   end
@@ -158,32 +283,31 @@ describe AnnotationCategoriesController do
     before(:each) do
       @annotation_text_one = create(:annotation_text,
                                     annotation_category: annotation_category,
-                                    content: "This is an annotation text.")
-
+                                    content: 'This is an annotation text.')
     end
 
     it 'should render an annotation context, where first part of its content matches given string' do
-      string = "This is an"
+      string = 'This is an'
 
       get :find_annotation_text, params: { assignment_id: annotation_category.assignment_id, string: string }
       expect(response.body).to eq(@annotation_text_one.content)
     end
 
     it 'should render an empty string if string does not match first part of any annotation text' do
-      string = "Hello"
+      string = 'Hello'
 
       get :find_annotation_text, params: { assignment_id: assignment.id, string: string }
-      expect(response.body).to eq("")
+      expect(response.body).to eq('')
     end
 
     it 'should render an empty string if string matches first part of more than one annotation text' do
       annotation_text_two = create(:annotation_text,
                                    annotation_category: annotation_category,
-                                   content: "This is another annotation text.")
-      string = "This is an"
+                                   content: 'This is another annotation text.')
+      string = 'This is an'
 
       get :find_annotation_text, params: { assignment_id: assignment.id, string: string }
-      expect(response.body).to eq("")
+      expect(response.body).to eq('')
     end
   end
 end
