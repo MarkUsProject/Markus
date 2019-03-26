eaba89465be14d570549ab31d202fd326a9d7e9e
Alex Shih <alexander.shih@mail.utoronto.ca>
2019-03-26 14:30:47 -0400

    added new test cases

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -8,7 +8,7 @@ describe TagsController do
 
   let(:assignment) { FactoryBot.create(:assignment) }
 
-  context 'download_tag_list' do
+  context 'CSV Downloads' do
     let(:csv_options) do
       {
         type: 'text/csv',
@@ -48,6 +48,60 @@ describe TagsController do
       csv_data =
         "#{@tag1.name},#{@tag1.description},#{@user.first_name} #{@user.last_name}\n" +
         "#{@tag2.name},#{@tag2.description},#{@user.first_name} #{@user.last_name}\n"
+      expect(@controller).to receive(:send_data).with(csv_data, csv_options) {
+        # to prevent a 'missing template' error
+        @controller.head :ok
+      }
+      get :download_tag_list, params: { assignment_id: assignment.id }, format: 'csv'
+    end
+
+    # parse header object to check for the right content type
+    it 'returns text/csv type' do
+      get :download_tag_list, params: { assignment_id: assignment.id }, format: 'csv'
+      expect(response.content_type).to eq 'text/csv'
+    end
+  end
+
+  context 'YML Downloads' do
+    let(:csv_options) do
+      {
+        type: 'text/csv',
+        disposition: 'attachment',
+        filename: 'tag_list.csv'
+      }
+    end
+
+    before :each do
+      @user = create(:student)
+      @tag1 = Tag.find_or_create_by(name: 'tag1')
+      @tag1.name = 'tag1'
+      @tag1.description = 'tag1_description'
+      @tag1.user = @user
+      @tag1.save
+
+      @tag2 = Tag.find_or_create_by(name: 'tag2')
+      @tag2.name = 'tag2'
+      @tag2.description = 'tag2_description'
+      @tag2.user = @user
+      @tag2.save
+    end
+
+    it 'responds with appropriate status' do
+      get :download_tag_list, params: { assignment_id: assignment.id }, format: 'csv'
+      expect(response.status).to eq(200)
+    end
+
+    # parse header object to check for the right disposition
+    it 'sets disposition as attachment' do
+      get :download_tag_list, params: { assignment_id: assignment.id }, format: 'csv'
+      d = response.header['Content-Disposition'].split.first
+      expect(d).to eq 'attachment;'
+    end
+
+    it 'expects a call to send_data' do
+      csv_data =
+        "#{@tag1.name},#{@tag1.description},#{@user.first_name} #{@user.last_name}\n" +
+          "#{@tag2.name},#{@tag2.description},#{@user.first_name} #{@user.last_name}\n"
       expect(@controller).to receive(:send_data).with(csv_data, csv_options) {
         # to prevent a 'missing template' error
         @controller.head :ok
