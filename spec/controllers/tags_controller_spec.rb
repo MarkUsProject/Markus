5ca802dd7873193fa58e5dcb120572ca66c68f65
Alex Shih <alexander.shih@mail.utoronto.ca>
2019-03-26 14:48:23 -0400

    changed styling

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -8,6 +8,89 @@ describe TagsController do
 
   let(:assignment) { FactoryBot.create(:assignment) }
 
+  context 'CSV_Uploads' do
+    before :each do
+      # We need to mock the rack file to return its content when
+      # the '.read' method is called to simulate the behaviour of
+      # the http uploaded file
+      @file_good = fixture_file_upload(
+        'files/tags/form_good.csv', 'text/csv')
+      allow(@file_good).to receive(:read).and_return(
+        File.read(fixture_file_upload(
+                    'files/tags/form_good.csv',
+                    'text/csv')))
+
+      @file_invalid_column = fixture_file_upload(
+        'files/tags/form_invalid_column.csv', 'text/csv')
+      allow(@file_invalid_column).to receive(:read).and_return(
+        File.read(fixture_file_upload(
+                    'files/tags/form_invalid_column.csv',
+                    'text/csv')))
+
+      @file_bad_csv = fixture_file_upload(
+        'files/bad_csv.csv', 'text/xls')
+      allow(@file_bad_csv).to receive(:read).and_return(
+        File.read(fixture_file_upload('files/bad_csv.csv', 'text/csv')))
+
+      @file_wrong_format = fixture_file_upload(
+        'files/wrong_csv_format.xls', 'text/xls')
+      allow(@file_wrong_format).to receive(:read).and_return(
+        File.read(fixture_file_upload(
+                    'files/wrong_csv_format.xls', 'text/csv')))
+
+      #set the :back redirect
+      @redirect = 'index'
+      request.env['HTTP_REFERER'] = @redirect
+    end
+
+    it 'accepts a valid file' do
+      post :csv_upload, params: { csv_tags: @file_good, assignment_id: assignment.id }
+
+      expect(response.status).to eq(302)
+      expect(flash[:error]).to be_nil
+      expect(flash[:success].map { |f| extract_text f }).to eq([I18n.t('upload_success',
+                                                                       count: 2)].map { |f| extract_text f })
+      expect(response).to redirect_to @redirect
+
+      expect(Tag.where(name: 'tag').take['description']).to eq('desc')
+      expect(Tag.where(name: 'tag1').take['description']).to eq('desc1')
+    end
+
+    it 'does not accept files with invalid columns' do
+      post :csv_upload, params: { assignment_id: assignment.id, csv_tags: @file_invalid_column }
+
+      expect(response.status).to eq(302)
+      expect(flash[:error]).to_not be_empty
+      expect(response).to redirect_to @redirect
+    end
+
+    it 'does not accept fileless submission' do
+      post :csv_upload, params: { assignment_id: assignment.id }
+
+      expect(response.status).to eq(302)
+      expect(response).to redirect_to @redirect
+    end
+
+    it 'does not accept a non-csv file with .csv extension' do
+      post :csv_upload, params: { assignment_id: assignment.id, csv_tags: @file_bad_csv }
+
+      expect(response.status).to eq(302)
+      expect(flash[:error]).to_not be_empty
+      expect(response).to redirect_to @redirect
+    end
+
+    it 'does not accept a .xls file' do
+      post :csv_upload, params: { assignment_id: assignment.id, csv_tags: @file_wrong_format }
+
+
+      expect(response.status).to eq(302)
+      expect(flash[:error]).to_not be_empty
+      expect(extract_text flash[:error][0])
+        .to eq(extract_text(I18n.t('upload_errors.unparseable_csv')))
+      expect(response).to redirect_to @redirect
+    end
+  end
+
   context 'download_ta_list' do
     context 'csv' do
       let(:csv_options) do
@@ -115,92 +198,6 @@ describe TagsController do
         get :download_tag_list, params: { assignment_id: assignment.id }, format: 'csv'
         expect(response.content_type).to eq 'text/csv'
       end
-    end
-
-    end
-
-
-
-  context 'CSV_Uploads' do
-    before :each do
-      # We need to mock the rack file to return its content when
-      # the '.read' method is called to simulate the behaviour of
-      # the http uploaded file
-      @file_good = fixture_file_upload(
-        'files/tags/form_good.csv', 'text/csv')
-      allow(@file_good).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/tags/form_good.csv',
-                    'text/csv')))
-
-      @file_invalid_column = fixture_file_upload(
-        'files/tags/form_invalid_column.csv', 'text/csv')
-      allow(@file_invalid_column).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/tags/form_invalid_column.csv',
-                    'text/csv')))
-
-      @file_bad_csv = fixture_file_upload(
-        'files/bad_csv.csv', 'text/xls')
-      allow(@file_bad_csv).to receive(:read).and_return(
-        File.read(fixture_file_upload('files/bad_csv.csv', 'text/csv')))
-
-      @file_wrong_format = fixture_file_upload(
-        'files/wrong_csv_format.xls', 'text/xls')
-      allow(@file_wrong_format).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/wrong_csv_format.xls', 'text/csv')))
-
-      #set the :back redirect
-      @redirect = 'index'
-      request.env['HTTP_REFERER'] = @redirect
-    end
-
-    it 'accepts a valid file' do
-      post :csv_upload, params: { csv_tags: @file_good, assignment_id: assignment.id }
-
-      expect(response.status).to eq(302)
-      expect(flash[:error]).to be_nil
-      expect(flash[:success].map { |f| extract_text f }).to eq([I18n.t('upload_success',
-                                                                       count: 2)].map { |f| extract_text f })
-      expect(response).to redirect_to @redirect
-
-      expect(Tag.where(name: 'tag').take['description']).to eq('desc')
-      expect(Tag.where(name: 'tag1').take['description']).to eq('desc1')
-    end
-
-    it 'does not accept files with invalid columns' do
-      post :csv_upload, params: { assignment_id: assignment.id, csv_tags: @file_invalid_column }
-
-      expect(response.status).to eq(302)
-      expect(flash[:error]).to_not be_empty
-      expect(response).to redirect_to @redirect
-    end
-
-    it 'does not accept fileless submission' do
-      post :csv_upload, params: { assignment_id: assignment.id }
-
-      expect(response.status).to eq(302)
-      expect(response).to redirect_to @redirect
-    end
-
-    it 'does not accept a non-csv file with .csv extension' do
-      post :csv_upload, params: { assignment_id: assignment.id, csv_tags: @file_bad_csv }
-
-      expect(response.status).to eq(302)
-      expect(flash[:error]).to_not be_empty
-      expect(response).to redirect_to @redirect
-    end
-
-    it 'does not accept a .xls file' do
-      post :csv_upload, params: { assignment_id: assignment.id, csv_tags: @file_wrong_format }
-
-
-      expect(response.status).to eq(302)
-      expect(flash[:error]).to_not be_empty
-      expect(extract_text flash[:error][0])
-        .to eq(extract_text(I18n.t('upload_errors.unparseable_csv')))
-      expect(response).to redirect_to @redirect
     end
   end
 end
