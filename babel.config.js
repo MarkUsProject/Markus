517eb361958d2dde1cfa8c880e3e8820ed1688d7
Alessio Di Sandro <alessio.disandro@gmail.com>
2019-04-03 18:19:41 +0200

    js: Be explicit about using core-js2 with @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -30,6 +30,7 @@ module.exports = function(api) {
         {
           forceAllTransforms: true,
           useBuiltIns: 'entry',
+          corejs: 2,
           modules: false,
           exclude: ['transform-typeof-symbol']
         }
