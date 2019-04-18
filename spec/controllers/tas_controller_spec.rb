6a6ef7582e4056b2744a921ce08f61b95097d3e9
YolandaBeCool <zyq666@yandex.com>
2019-04-17 23:26:37 -0400

    Add 2 testcases for 'CSV_Uploads' under TasController, and import a sample file for uploading a list of graders.

--- spec/controllers/tas_controller_spec.rb
+++ spec/controllers/tas_controller_spec.rb
@@ -7,37 +7,9 @@ describe TasController do
   end
 
   context 'CSV_Uploads' do
-    before :each do
-      # We need to mock the rack file to return its content when
-      # the '.read' method is called to simulate the behaviour of
-      # the http uploaded file
-      @file_good = fixture_file_upload(
-        'files/tas/form_good.csv', 'text/csv')
-      allow(@file_good).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/tas/form_good.csv',
-                    'text/csv')))
-
-      @file_invalid_column = fixture_file_upload(
-        'files/tas/form_invalid_column.csv', 'text/csv')
-      allow(@file_invalid_column).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/tas/form_invalid_column.csv',
-                    'text/csv')))
-
-      @file_bad_csv = fixture_file_upload(
-        'files/bad_csv.csv', 'text/xls')
-      allow(@file_bad_csv).to receive(:read).and_return(
-        File.read(fixture_file_upload('files/bad_csv.csv', 'text/csv')))
-
-      @file_wrong_format = fixture_file_upload(
-        'files/wrong_csv_format.xls', 'text/xls')
-      allow(@file_wrong_format).to receive(:read).and_return(
-        File.read(fixture_file_upload(
-                    'files/wrong_csv_format.xls', 'text/csv')))
-    end
 
     it 'accepts a valid file' do
+      @file_good = fixture_file_upload('files/tas/form_good.csv', 'text/csv')
       post :upload_ta_list, params: { userlist: @file_good }
 
       expect(response.status).to eq(302)
@@ -55,12 +27,13 @@ describe TasController do
     end
 
     it 'does not accept files with invalid columns' do
+      @file_invalid_column = fixture_file_upload('files/tas/form_invalid_column.csv', 'text/csv')
       post :upload_ta_list, params: { userlist: @file_invalid_column }
 
       expect(response.status).to eq(302)
       expect(flash[:error]).to_not be_empty
       expect(response).to redirect_to action: 'index'
-
+      expect(Ta.all.size).to be == 1
       expect(Ta.where(first_name: 'Mike')).to be_empty
     end
 
@@ -71,7 +44,40 @@ describe TasController do
       expect(response).to redirect_to action: 'index'
     end
 
+    it 'does not check the validity of an username' do
+      @invalid_user_name = fixture_file_upload('files/tas/grader_with_invalid_username.csv', 'text/csv')
+      post :upload_ta_list, params: { userlist: @invalid_user_name}
+
+      expect(response.status).to eq(302)
+      expect(flash[:error]).to be_nil
+      expect(Ta.all.size).to be == 2
+      valid_ta = Ta.where(user_name: 'user123')
+      expect(valid_ta.take['first_name']).to eq('Jack')
+      expect(valid_ta.take['last_name']).to eq('Wood')
+      # users should have the same username length
+      # valid_ta has username of 6 characters
+      # invalid_ta has username of 5 characters
+      invalid_ta = Ta.where(user_name: 'user12')
+      expect(invalid_ta.take['first_name']).to eq('James')
+      expect(invalid_ta.take['last_name']).to eq('Wood')
+    end
+
+    it "does not upload grader's email address" do
+      @with_emails = fixture_file_upload('files/tas/grader_with_email.csv', 'text/csv')
+      post :upload_ta_list, params: { userlist: @with_emails}
+
+      expect(response.status).to eq(302)
+      expect(flash[:error]).to be_nil
+      expect(Ta.all.size).to be == 2
+      ta1 = Ta.where(user_name: 'user123')
+      expect(ta1.take['email']).to be_nil
+      ta2 = Ta.where(user_name: 'user124')
+      expect(ta2.take['email']).to be_nil
+
+    end
+
     it 'does not accept a non-csv file with .csv extension' do
+      @file_bad_csv = fixture_file_upload('files/bad_csv.csv', 'text/xls')
       post :upload_ta_list, params: { userlist: @file_bad_csv }
 
       expect(response.status).to eq(302)
@@ -80,6 +86,7 @@ describe TasController do
     end
 
     it 'does not accept a .xls file' do
+      @file_wrong_format = fixture_file_upload('files/wrong_csv_format.xls', 'text/xls')
       post :upload_ta_list, params: { userlist: @file_wrong_format }
 
       expect(response.status).to eq(302)
