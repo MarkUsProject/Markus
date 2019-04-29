ceba93c98819514f2c1158b2589219bf221bc423
YolandaBeCool <zyq666@yandex.com>
2019-04-28 21:25:59 -0400

    Fix styling issue

--- app/controllers/tas_controller.rb
+++ app/controllers/tas_controller.rb
@@ -57,8 +57,10 @@ class TasController < ApplicationController
       count = 0
       tas.all.each do |ta|
         count += 1
-        output.push([{"TA_#{count}" => [[{"Username" => [ta.user_name]}], [{"Last Name" => [ta.last_name]}],
-                                        [{"First Name" => ta.first_name}], [{"Email" => ta.email}]]}])
+        output.push([{ "TA_#{count}" => [[{ 'Username' => [ta.user_name] }],
+                                         [{ 'Last Name' => [ta.last_name] }],
+                                         [{ 'First Name' => ta.first_name }],
+                                         [{ 'Email' => ta.email }]] }])
       end
       output = output.to_yaml
       format = 'text/yaml'
