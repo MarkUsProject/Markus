fb323b8bf5e9128f5be2081b60d3269691e7cf35
Misha Schwartz <misha.schwartz@gmail.com>
2019-05-17 13:27:40 -0400

    durations: get around rails bug that creates invalid iso8601 strings

--- config/initializers/types.rb
+++ config/initializers/types.rb
@@ -1,12 +1,30 @@
 class DurationType < ActiveRecord::Type::String
+
   def cast(value)
     return value if value.blank? || value.is_a?(ActiveSupport::Duration)
 
-    ActiveSupport::Duration.parse(value)
+    normalize_duration(ActiveSupport::Duration.parse(value))
   end
 
   def serialize(duration)
-    duration ? duration.iso8601 : nil
+    duration ? remove_weeks(duration).iso8601 : nil
+  end
+
+  private
+
+  # TODO: remove this after https://github.com/rails/rails/pull/34683 is pulled in
+  def remove_weeks(duration)
+    days_per_week = 7
+    if duration.parts[:weeks] > 0
+      duration - duration.parts[:weeks].weeks + (duration.parts[:weeks] * days_per_week).days
+    else
+      duration
+    end
+  end
+
+  # TODO: remove this after https://github.com/rails/rails/pull/34683 is pulled in
+  def normalize_duration(duration)
+    ActiveSupport::Duration.build(duration)
   end
 end
 
