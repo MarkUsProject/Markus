e633ae7ad261c15a3536a208ad4dd40dc2501548
Misha Schwartz <misha.schwartz@gmail.com>
2019-05-21 14:41:51 -0400

    flash message after un/successful creation/deletion of an extension

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -3,15 +3,21 @@ class ExtensionsController < ApplicationController
 
   def create_or_update
     time_delta = params[:weeks].to_i.weeks + params[:days].to_i.days + params[:hours].to_i.hours
-    Extension.find_or_initialize_by(grouping_id: params[:grouping_id])
-             .update_attributes(time_delta: time_delta,
-                                apply_penalty: params[:penalty],
-                                note: params[:note])
+    extension = Extension.find_or_initialize_by(grouping_id: params[:grouping_id])
+    if extension.update_attributes(time_delta: time_delta, apply_penalty: params[:penalty], note: params[:note])
+      flash_now(:success, I18n.t('extensions.create.success'))
+    else
+      flash_now(:error, I18n.t('extensions.create.error'))
+    end
     head :ok
   end
 
   def delete_by_grouping
-    Extension.find_by_grouping_id(params[:grouping_id]).destroy
+    if Extension.find_by_grouping_id(params[:grouping_id]).destroy
+      flash_now(:success, I18n.t('extensions.delete.success'))
+    else
+      flash_now(:error, I18n.t('extensions.delete.error'))
+    end
     head :ok
   end
 end
