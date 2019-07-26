be621fc1c30eb98b976d61161e32ba1cc80ba77d
David Liu <david@cs.toronto.edu>
2019-07-26 11:43:02 -0400

    svn: Fix vagrant provisioning script for svn installation.

--- script/install-svn.sh
+++ script/install-svn.sh
@@ -4,18 +4,13 @@
 # svn on travis in case we want to test interactions with svn
 # in the future.
 
-SVN_BASE=subversion-1.9.7
-MIRROR_PRIMARY=http://www-us.apache.org/dist/subversion
+SVN_BASE=subversion-1.9.3  # Pinned to version in libsvn1.
+MIRROR_PRIMARY=http://archive.apache.org/dist/subversion
 MIRROR_SECONDARY=http://www-eu.apache.org/dist/subversion
-
-echo "- - - Detecting Ruby version - - -"
-source /etc/profile.d/rvm.sh
-rvm use
-RUBY_DIR=$(dirname $(dirname $(which ruby)))
-echo $RUBY_DIR
+SQLITE_BASE=sqlite-amalgamation-3290000
 
 echo "- - - Installing subversion dependencies - - -"
-sudo apt-get install -y libaprutil1-dev swig
+sudo apt-get install -y libaprutil1-dev libutf8proc-dev liblz4-dev swig subversion libsvn1 unzip
 
 echo "- - - Downloading subversion and sqlite source - - -"
 if ! wget --tries 5 ${MIRROR_PRIMARY}/${SVN_BASE}.tar.gz; then
@@ -24,22 +19,21 @@ if ! wget --tries 5 ${MIRROR_PRIMARY}/${SVN_BASE}.tar.gz; then
   fi
 fi
 tar xzf ${SVN_BASE}.tar.gz
-wget https://www.sqlite.org/sqlite-amalgamation-3071501.zip
-unzip sqlite-amalgamation-3071501.zip
-mv sqlite-amalgamation-3071501 ${SVN_BASE}/sqlite-amalgamation
+wget https://www.sqlite.org/2019/${SQLITE_BASE}.zip
+unzip ${SQLITE_BASE}.zip
+mv ${SQLITE_BASE} ${SVN_BASE}/sqlite-amalgamation
 
 cd $SVN_BASE
 
 echo "- - - ./configure - - -"
-LDFLAGS="-L$RUBY_DIR/lib/" ./configure --prefix=$RUBY_DIR
-
-echo "- - - make - - -"
-make
+./configure
 echo "- - - make swig-rb - - -"
 make swig-rb
-echo "- - - make install - - -"
-make install
 echo "- - - make install-swig-rb - - -"
-make install-swig-rb
+sudo make install-swig-rb
 
+# Fix the installation.
+# TODO: figure out how to fix this properly in ./configure.
+sudo cp -r /usr/local/lib/site_ruby/usr/local/lib/x86_64-linux-gnu/site_ruby/svn /usr/lib/ruby/vendor_ruby/2.5.0
+sudo cp /usr/local/lib/libsvn* /usr/lib/x86_64-linux-gnu
 cd ..
