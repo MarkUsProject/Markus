542aa1b1132be221a8d93bfa4a67470c6487a2ed
Misha Schwartz <misha.schwartz@gmail.com>
2019-07-26 16:47:31 -0400

    autotest: clarify display output strings

--- app/helpers/automated_tests_helper.rb
+++ app/helpers/automated_tests_helper.rb
@@ -15,7 +15,7 @@ module AutomatedTestsHelper
           enum: TestGroup.display_outputs.keys,
           enumNames: TestGroup.display_outputs.keys.map { |k| I18n.t("automated_tests.display_output.#{k}") },
           default: TestGroup.display_outputs.keys.first,
-          title: TestGroup.human_attribute_name(:display_output)
+          title: I18n.t('automated_tests.display_output_title')
         },
         criterion: {
           type: :string,
