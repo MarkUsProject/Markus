20123b5c3d62386f83dba384fa4837cf08bb82aa
mishaschwartz <misha.schwartz@gmail.com>
2019-08-06 22:37:53 -0400

    Convert existing API tests to rspec (#4102)

---
+++ spec/controllers/api/assignments_controller_spec.rb
@@ -0,0 +1,266 @@
+describe Api::AssignmentsController do
+  context 'An unauthenticated request' do
+    before :each do
+      request.env['HTTP_AUTHORIZATION'] = 'garbage http_header'
+      request.env['HTTP_ACCEPT'] = 'application/xml'
+    end
+
+    it 'should fail to authenticate a GET index request' do
+      get :index
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a GET show request' do
+      get :show, params: { id: 1 }
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a POST create request' do
+      post :create
+
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a PUT update request' do
+      put :create, params: { id: 1 }
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a DELETE destroy request' do
+      delete :destroy, params: { id: 1 }
+      expect(response.status).to eq(403)
+    end
+  end
+  context 'An authenticated request requesting' do
+    let(:assignment) { create :assignment }
+    before :each do
+      admin = create :admin
+      admin.reset_api_key
+      request.env['HTTP_AUTHORIZATION'] = "MarkUsAuth #{admin.api_key.strip}"
+    end
+
+    context 'GET index' do
+      context 'expecting an xml response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/xml'
+        end
+        context 'with a single assignment' do
+          before :each do
+            assignment
+            get :index
+          end
+          it 'should be successful' do
+            expect(response.status).to eq(200)
+          end
+          it 'should return xml content' do
+            expect(Hash.from_xml(response.body).dig('assignments', 'assignment', 'id')).to eq(assignment.id.to_s)
+          end
+          it 'should return all default fields' do
+            keys = Hash.from_xml(response.body).dig('assignments', 'assignment').keys.map(&:to_sym).sort
+            expect(keys).to eq(Api::AssignmentsController::DEFAULT_FIELDS.sort)
+          end
+        end
+        context 'with multiple assignments' do
+          before :each do
+            5.times { create :assignment }
+            get :index
+          end
+          it 'should return xml content about all assignments' do
+            expect(Hash.from_xml(response.body).dig('assignments', 'assignment').length).to eq(5)
+          end
+          it 'should return all default fields for all assignments' do
+            keys = Hash.from_xml(response.body).dig('assignments', 'assignment').map { |h| h.keys.map(&:to_sym).sort }
+            expect(keys).to all(eq(Api::AssignmentsController::DEFAULT_FIELDS.sort))
+          end
+        end
+      end
+      context 'expecting a json response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/json'
+        end
+        context 'with a single assignment' do
+          before :each do
+            assignment
+            get :index
+          end
+          it 'should be successful' do
+            expect(response.status).to eq(200)
+          end
+          it 'should return json content' do
+            expect(JSON.parse(response.body)&.first&.dig('id')).to eq(assignment.id)
+          end
+          it 'should return all default fields' do
+            keys = JSON.parse(response.body)&.first&.keys&.map(&:to_sym)&.sort
+            expect(keys).to eq(Api::AssignmentsController::DEFAULT_FIELDS.sort)
+          end
+        end
+        context 'with multiple assignments' do
+          before :each do
+            5.times { create :assignment }
+            get :index
+          end
+          it 'should return xml content about all assignments' do
+            expect(JSON.parse(response.body).length).to eq(5)
+          end
+          it 'should return all default fields for all assignments' do
+            keys = JSON.parse(response.body).map { |h| h.keys.map(&:to_sym).sort }
+            expect(keys).to all(eq(Api::AssignmentsController::DEFAULT_FIELDS.sort))
+          end
+        end
+      end
+    end
+    context 'GET show' do
+      context 'expecting an xml response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/xml'
+          get :show, params: { id: assignment.id }
+        end
+        it 'should be successful' do
+          expect(response.status).to eq(200)
+        end
+        it 'should return xml content' do
+          expect(Hash.from_xml(response.body).dig('assignment', 'id')).to eq(assignment.id.to_s)
+        end
+        it 'should return all default fields' do
+          keys = Hash.from_xml(response.body).dig('assignment').keys.map(&:to_sym).sort
+          expect(keys).to eq(Api::AssignmentsController::DEFAULT_FIELDS.sort)
+        end
+      end
+      context 'expecting a json response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/json'
+          get :show, params: { id: assignment.id }
+        end
+        it 'should be successful' do
+          expect(response.status).to eq(200)
+        end
+        it 'should return json content' do
+          expect(JSON.parse(response.body)&.dig('id')).to eq(assignment.id)
+        end
+        it 'should return all default fields' do
+          keys = JSON.parse(response.body)&.keys&.map(&:to_sym)&.sort
+          expect(keys).to eq(Api::AssignmentsController::DEFAULT_FIELDS.sort)
+        end
+      end
+      context 'requesting a non-existant assignment' do
+        it 'should respond with 404' do
+          get :show, params: { id: 9999 }
+          expect(response.status).to eq(404)
+        end
+      end
+    end
+    context 'POST create' do
+      let(:params) { { short_identifier: 'A0', description: 'something', due_date: Time.now } }
+      let(:full_params) do
+        { short_identifier: 'A0', message: 'Test Message',
+          description: 'Test', due_date: '2012-03-26 18:04:39',
+          tokens_per_period: 13, repository_folder: 'Folder',
+          allow_web_submits: false,
+          display_grader_names_to_students: true, enable_test: true,
+          assign_graders_to_criteria: true, student_form_groups: true,
+          group_name_autogenerated: false, submission_rule_deduction: 10,
+          submission_rule_hours: 11, submission_rule_interval: 12,
+          remark_due_date: '2012-03-26 18:04:39', group_max: 3,
+          submission_rule_type: 'PenaltyDecayPeriod', group_min: 2,
+          remark_message: 'Remark', allow_remarks: false,
+          non_regenerating_tokens: false, unlimited_tokens: false,
+          token_period: 1.0, token_start_date: '2012-03-25 18:04:39' }
+      end
+      context 'with minimal required params' do
+        it 'should respond with 201' do
+          post :create, params: params
+          expect(response.status).to eq(201)
+        end
+        it 'should create an assignment' do
+          expect(Assignment.find_by_short_identifier(params[:short_identifier])).to be_nil
+          post :create, params: params
+          expect(Assignment.find_by_short_identifier(params[:short_identifier])).not_to be_nil
+        end
+      end
+      context 'with all params' do
+        it 'should respond with 201' do
+          post :create, params: params
+          expect(response.status).to eq(201)
+        end
+        it 'should create an assignment' do
+          expect(Assignment.find_by_short_identifier(params[:short_identifier])).to be_nil
+          post :create, params: params
+          expect(Assignment.find_by_short_identifier(params[:short_identifier])).not_to be_nil
+        end
+      end
+      context 'with missing params' do
+        context 'missing short_id' do
+          it 'should respond with 404' do
+            post :create, params: params.slice(:description, :due_date)
+            expect(response.status).to eq(422)
+          end
+          it 'should not create an assignment' do
+            post :create, params: params.slice(:description, :due_date)
+            expect(Assignment.find_by_description(params[:description])).to be_nil
+          end
+        end
+        context 'missing description' do
+          it 'should respond with 404' do
+            post :create, params: params.slice(:short_identifier, :due_date)
+            expect(response.status).to eq(422)
+          end
+          it 'should not create an assignment' do
+            post :create, params: params.slice(:short_identifier, :due_date)
+            expect(Assignment.find_by_short_identifier(params[:short_identifier])).to be_nil
+          end
+        end
+        context 'missing due_date' do
+          it 'should respond with 404' do
+            post :create, params: params.slice(:short_identifier, :description)
+            expect(response.status).to eq(422)
+          end
+          it 'should not create an assignment' do
+            post :create, params: params.slice(:short_identifier, :description)
+            expect(Assignment.find_by_short_identifier(params[:short_identifier])).to be_nil
+          end
+        end
+      end
+      context 'where short_identifier is already taken' do
+        it 'should respond with 409' do
+          post :create, params: { **params, short_identifier: (create :assignment).short_identifier }
+          expect(response.status).to eq(409)
+        end
+      end
+      context 'where due_date is invalid' do
+        it 'should respond with 500' do
+          post :create, params: { **params, due_date: 'not a real date' }
+          expect(response.status).to eq(500)
+        end
+      end
+      context 'where submission rule is invalid' do
+        it 'should respond with 500' do
+          post :create, params: { **full_params, submission_rule_interval: 'not a real interval' }
+          expect(response.status).to eq(500)
+        end
+      end
+    end
+    context 'PUT update' do
+      it 'should update an existing assignment' do
+        new_desc = assignment.description + 'more!'
+        put :update, params: { id: assignment.id, description: new_desc }
+        expect(response.status).to eq(200)
+      end
+      it 'should not update a short identifier' do
+        new_short_id = assignment.short_identifier + 'more!'
+        put :update, params: { id: assignment.id, short_identifier: new_short_id }
+        expect(response.status).to eq(500)
+      end
+      it 'should not update an assignment that does not exist' do
+        new_desc = assignment.description + 'more!'
+        put :update, params: { id: 1, description: new_desc }
+        expect(response.status).to eq(404)
+      end
+    end
+    context 'DELETE destroy' do
+      it 'should not be able to delete an assignment' do
+        delete :destroy, params: { id: assignment.id }
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+end
---
+++ spec/controllers/api/groups_controller_spec.rb
@@ -0,0 +1,334 @@
+describe Api::GroupsController do
+  context 'An unauthenticated request' do
+    before :each do
+      request.env['HTTP_AUTHORIZATION'] = 'garbage http_header'
+      request.env['HTTP_ACCEPT'] = 'application/xml'
+    end
+
+    it 'should fail to authenticate a GET index request' do
+      get :index, params: { assignment_id: 1 }
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a GET show request' do
+      get :show, params: { id: 1, assignment_id: 1 }
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a POST create request' do
+      post :create, params: { assignment_id: 1 }
+
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a PUT update request' do
+      put :create, params: { id: 1, assignment_id: 1 }
+      expect(response.status).to eq(403)
+    end
+
+    it 'should fail to authenticate a DELETE destroy request' do
+      delete :destroy, params: { id: 1, assignment_id: 1 }
+      expect(response.status).to eq(403)
+    end
+  end
+  context 'An authenticated request requesting' do
+    let(:assignment) { create :assignment }
+    let(:grouping) { create :grouping_with_inviter, assignment: assignment }
+    before :each do
+      admin = create :admin
+      admin.reset_api_key
+      request.env['HTTP_AUTHORIZATION'] = "MarkUsAuth #{admin.api_key.strip}"
+    end
+    context 'GET index' do
+      context 'expecting an xml response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/xml'
+        end
+        context 'with a single grouping' do
+          before :each do
+            get :index, params: { assignment_id: grouping.assignment.id }
+          end
+          it 'should be successful' do
+            expect(response.status).to eq(200)
+          end
+          it 'should return xml content' do
+            expect(Hash.from_xml(response.body).dig('groups', 'group', 'id')).to eq(grouping.group.id.to_s)
+          end
+        end
+        context 'with multiple assignments' do
+          before :each do
+            5.times { create :grouping_with_inviter, assignment: assignment }
+            get :index, params: { assignment_id: assignment.id }
+          end
+          it 'should return xml content about all assignments' do
+            expect(Hash.from_xml(response.body).dig('groups', 'group').length).to eq(5)
+          end
+        end
+      end
+      context 'expecting a json response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/json'
+        end
+        context 'with a single assignment' do
+          before :each do
+            get :index, params: { assignment_id: grouping.assignment.id }
+          end
+          it 'should be successful' do
+            expect(response.status).to eq(200)
+          end
+          it 'should return json content' do
+            expect(JSON.parse(response.body)&.first&.dig('id')).to eq(grouping.group.id)
+          end
+        end
+        context 'with multiple groupings' do
+          let(:groupings) { Array.new(5) { create :grouping_with_inviter, assignment: assignment } }
+          it 'should return content about all groupings' do
+            groupings
+            get :index, params: { assignment_id: assignment.id }
+            expect(JSON.parse(response.body).length).to eq(5)
+          end
+          it 'should return only filtered content' do
+            gr = groupings.first
+            get :index, params: { assignment_id: gr.assignment.id, filter: { group_name: gr.group.group_name } }
+            expect(JSON.parse(response.body)&.first&.dig('id')).to eq(gr.group.id)
+          end
+          it 'should not return groups that match the filter from another assignment' do
+            get :index, params: { assignment_id: create(:assignment).id,
+                                  filter: { group_name: groupings.last.group.group_name } }
+            expect(JSON.parse(response.body)).to be_empty
+          end
+          it 'should reject invalid filters' do
+            get :index, params: { assignment_id: groupings.first.assignment.id, filter: { bad_filter: 'something' } }
+            expect(response.status).to eq(422)
+          end
+        end
+      end
+    end
+    context 'GET show' do
+      context 'expecting an xml response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/xml'
+        end
+        context 'with a single grouping' do
+          before :each do
+            get :show, params: { id: grouping.group.id, assignment_id: grouping.assignment.id }
+          end
+          it 'should be successful' do
+            expect(response.status).to eq(200)
+          end
+          it 'should return xml content' do
+            expect(Hash.from_xml(response.body).dig('groups', 'group', 'id')).to eq(grouping.group.id.to_s)
+          end
+        end
+      end
+      context 'expecting a json response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/json'
+        end
+        context 'with a single assignment' do
+          before :each do
+            get :show, params: { id: grouping.group.id, assignment_id: grouping.assignment.id }
+          end
+          it 'should be successful' do
+            expect(response.status).to eq(200)
+          end
+          it 'should return json content' do
+            expect(JSON.parse(response.body)&.first&.dig('id')).to eq(grouping.group.id)
+          end
+        end
+      end
+      context 'requesting a non-existant assignment' do
+        it 'should respond with 404' do
+          get :show, params: { id: 9999, assignment_id: assignment.id }
+          expect(response.status).to eq(404)
+        end
+      end
+    end
+    context 'POST add_new_members' do
+      context 'when adding a student to an existing group with a member already' do
+        let(:student) { create :student }
+        before :each do
+          post :add_members, params: { id: grouping.group.id,
+                                       assignment_id: grouping.assignment.id,
+                                       members: [student.user_name] }
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should add the student with accepted status' do
+          expect(grouping.accepted_students).to include(student)
+          status = grouping.accepted_student_memberships.find_by(user_id: student.id).membership_status
+          expect(status).to eq(StudentMembership::STATUSES[:accepted])
+        end
+      end
+      context 'when adding a student to an existing group without a member already' do
+        let(:grouping) { create :grouping, assignment: assignment }
+        let(:student) { create :student }
+        before :each do
+          post :add_members, params: { id: grouping.group.id,
+                                       assignment_id: grouping.assignment.id,
+                                       members: [student.user_name] }
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should add the student with inviter status' do
+          expect(grouping.accepted_students).to include(student)
+          status = grouping.accepted_student_memberships.find_by(user_id: student.id).membership_status
+          expect(status).to eq(StudentMembership::STATUSES[:inviter])
+        end
+      end
+      context 'when adding a student to a group without a grouping for this assignment' do
+        let(:grouping) { create :grouping }
+        let(:student) { create :student }
+        before :each do
+          post :add_members, params: { id: grouping.group.id,
+                                       assignment_id: assignment.id,
+                                       members: [student.user_name] }
+        end
+        it 'should respond with 422' do
+          expect(response.status).to eq(422)
+        end
+        it 'should not add the student to the group' do
+          expect(grouping.memberships).to be_empty
+        end
+      end
+      context 'add multiple group members' do
+        let(:students) { create_list(:student, 3) }
+        before :each do
+          post :add_members, params: { id: grouping.group.id,
+                                       assignment_id: grouping.assignment.id,
+                                       members: students.map(&:user_name) }
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should add the students with accepted status' do
+          statuses = grouping.accepted_student_memberships.where(user_id: students.map(&:id)).pluck(:membership_status)
+          expect(statuses).to all(be == StudentMembership::STATUSES[:accepted])
+        end
+      end
+    end
+    context 'POST update_marks' do
+      let(:criterion) { create(:flexible_criterion, assignment: assignment, max_mark: 10) }
+      let(:submission) { create(:version_used_submission, grouping: grouping) }
+      context 'when a grouping does not yet have a mark' do
+        before :each do
+          submission
+          post :update_marks, params: { id: grouping.group.id,
+                                        assignment_id: grouping.assignment.id,
+                                        criterion.name => 4 }
+          grouping.reload
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should add a mark for a grouping' do
+          result = submission.current_result
+          expect(result.get_total_mark).to eq(4)
+        end
+      end
+      context 'when a grouping does have a mark already' do
+        before :each do
+          mark = submission.current_result.marks.find_or_initialize_by(markable_id: criterion.id,
+                                                                       markable_type: criterion.class.name)
+          mark.mark = 10
+          mark.save!
+          post :update_marks, params: { id: grouping.group.id,
+                                        assignment_id: grouping.assignment.id,
+                                        criterion.name => 4 }
+          grouping.reload
+          submission.reload
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should add a mark for a grouping' do
+          result = submission.current_result
+          expect(result.get_total_mark).to eq(4)
+        end
+      end
+      context 'when a result is complete' do
+        before :each do
+          mark = submission.current_result.marks.find_or_initialize_by(markable_id: criterion.id,
+                                                                       markable_type: criterion.class.name)
+          mark.mark = 10
+          mark.save!
+          submission.current_result.update_attributes(marking_state: Result::MARKING_STATES[:complete])
+          post :update_marks, params: { id: grouping.group.id,
+                                        assignment_id: grouping.assignment.id,
+                                        criterion.name => 4 }
+          grouping.reload
+          submission.reload
+        end
+        it 'should respond with 404' do
+          expect(response.status).to eq(404)
+        end
+        it 'should add a mark for a grouping' do
+          result = submission.current_result
+          expect(result.get_total_mark).to eq(10)
+        end
+      end
+    end
+    context 'GET group_ids_by_name' do
+      context 'expecting a json response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/json'
+          get :group_ids_by_name, params: { assignment_id: grouping.assignment.id }
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should return a mapping from group names to ids' do
+          expect(JSON.parse(response.body)).to eq(grouping.group.group_name => grouping.group.id)
+        end
+      end
+      context 'expecting a xml response' do
+        before :each do
+          request.env['HTTP_ACCEPT'] = 'application/xml'
+          get :group_ids_by_name, params: { assignment_id: grouping.assignment.id }
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should return a mapping from group names to ids' do
+          expect(Hash.from_xml(response.body)['groups']).to eq(grouping.group.group_name => grouping.group.id.to_s)
+        end
+      end
+    end
+    context 'POST update_marking_state' do
+      let(:criterion) { create(:flexible_criterion, assignment: assignment, max_mark: 10) }
+      let(:submission) { create(:version_used_submission, grouping: grouping) }
+      context 'should complete a result' do
+        before :each do
+          submission.current_result.update_attributes(marking_state: Result::MARKING_STATES[:incomplete])
+          post :update_marking_state, params: { id: grouping.group.id,
+                                                assignment_id: grouping.assignment.id,
+                                                marking_state: Result::MARKING_STATES[:complete] }
+          submission.reload
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should set the marking state to complete' do
+          expect(submission.current_result.marking_state).to eq(Result::MARKING_STATES[:complete])
+        end
+      end
+      context 'should un-complete a result' do
+        before :each do
+          submission.current_result.update_attributes(marking_state: Result::MARKING_STATES[:complete])
+          post :update_marking_state, params: { id: grouping.group.id,
+                                                assignment_id: grouping.assignment.id,
+                                                marking_state: Result::MARKING_STATES[:incomplete] }
+          submission.reload
+        end
+        it 'should respond with 200' do
+          expect(response.status).to eq(200)
+        end
+        it 'should set the marking state to complete' do
+          expect(submission.current_result.marking_state).to eq(Result::MARKING_STATES[:incomplete])
+        end
+      end
+    end
+  end
+end
