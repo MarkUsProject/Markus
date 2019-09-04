594d5c7e0077d0029b96a05090bce0df7ff1f784
mishaschwartz <misha.schwartz@gmail.com>
2019-09-04 10:48:35 -0400

    New api routes for grade entry form controller (#4117)
    * api: new api routes for grade entry controller (create and update)
    
    * api: allow creation of columns on update as well as on create

--- app/controllers/api/grade_entry_forms_controller.rb
+++ app/controllers/api/grade_entry_forms_controller.rb
@@ -2,7 +2,7 @@ module Api
 
   class GradeEntryFormsController < MainApiController
 
-    DEFAULT_FIELDS = [:id, :short_identifier, :description, :date, :is_hidden].freeze
+    DEFAULT_FIELDS = [:id, :short_identifier, :description, :date, :is_hidden, :show_total].freeze
 
     # Sends the contents of the specified grade entry form
     # Requires: id
@@ -20,7 +20,7 @@ module Api
     def index
       grade_entry_forms = get_collection(GradeEntryForm.includes(:grade_entry_items)) || return
 
-      include_args = { only: DEFAULT_FIELDS, include: { grade_entry_items: { only: [:name, :out_of] } } }
+      include_args = { only: DEFAULT_FIELDS, include: { grade_entry_items: { only: [:id, :name, :out_of] } } }
 
       respond_to do |format|
         format.xml do
@@ -28,6 +28,109 @@ module Api
           render xml: xml
         end
         format.json { render json: grade_entry_forms.to_json(include_args) }
+      end
+    end
+
+    # create a new grade entry form
+    # Requires:
+    #   :short_identifier
+    # Optional:
+    #   :description, :date, :is_hidden
+    #   grade_entry_items:
+    #     :name, :out_of, :bonus
+    def create
+      if has_missing_params?([:short_identifier])
+        # incomplete/invalid HTTP params
+        render 'shared/http_status', locals: { code: '422', message:
+          HttpStatusHelper::ERROR_CODE['message']['422'] }, status: 422
+        return
+      end
+
+      # check if there is an existing assignment
+      form = GradeEntryForm.find_by_short_identifier(params[:short_identifier])
+      unless form.nil?
+        render 'shared/http_status', locals: { code: '409', message:
+          'Grade Entry Form already exists' }, status: 409
+        return
+      end
+
+      ApplicationRecord.transaction do
+        create_params = params.permit(*DEFAULT_FIELDS)
+        create_params[:is_hidden] ||= false
+        create_params[:description] ||= ''
+        new_form = GradeEntryForm.new(create_params)
+        unless new_form.save
+          render 'shared/http_status', locals: { code: '500', message:
+            new_form.errors.full_messages.first }, status: 500
+          raise ActiveRecord::Rollback
+        end
+
+        params[:grade_entry_items]&.each&.with_index do |column_params, i|
+          column_params = column_params.permit(:name, :out_of, :bonus).to_h.symbolize_keys
+          grade_item = new_form.grade_entry_items.build(**column_params, position: i + 1)
+          unless grade_item.save
+            render 'shared/http_status', locals: { code: '500', message:
+              grade_item.errors.full_messages.first }, status: 500
+            raise ActiveRecord::Rollback
+          end
+        end
+        render 'shared/http_status', locals: { code: '200', message:
+          HttpStatusHelper::ERROR_CODE['message']['200'] }, status: 200
+      end
+    end
+
+    # create a new grade entry form
+    # params:
+    #   :short_identifier, :description, :date, :is_hidden
+    #   grade_entry_items:
+    #     :id, :name, :out_of, :bonus
+    #
+    # if the grade_entry_items id param is set, an existing item will be
+    # updated, otherwise a new grade_entry_item will be created
+    def update
+      # check if there is an existing assignment
+      form = GradeEntryForm.find_by_id(params[:id])
+      if form.nil?
+        render 'shared/http_status', locals: { code: '404', message:
+          'Grade Entry Form not found' }, status: 404
+        return
+      end
+
+      ApplicationRecord.transaction do
+        update_params = params.permit(*DEFAULT_FIELDS)
+        unless form.update(update_params)
+          render 'shared/http_status', locals: { code: '500', message:
+            form.errors.full_messages.first }, status: 500
+          raise ActiveRecord::Rollback
+        end
+
+        position = form.grade_entry_items.count
+        params[:grade_entry_items]&.each do |column_params|
+          if column_params[:id].nil?
+            column_params = column_params.permit(:name, :out_of, :bonus).to_h.symbolize_keys
+            grade_item = form.build(**column_params, position: position += 1)
+            unless grade_item.save
+              render 'shared/http_status', locals: { code: '500', message:
+                grade_item.errors.full_messages.first }, status: 500
+              raise ActiveRecord::Rollback
+            end
+          else
+            column_params = column_params.permit(:id, :name, :out_of, :bonus).to_h.symbolize_keys
+            grade_item = form.grade_entry_items.where(id: column_params[:id]).first
+            if grade_item.nil?
+              render 'shared/http_status', locals: { code: '404', message:
+                "Grade Entry Item with id=#{column_params[:id]} not found" }, status: 404
+              raise ActiveRecord::Rollback
+            end
+            unless grade_item.update(column_params)
+              render 'shared/http_status', locals: { code: '500', message:
+                grade_item.errors.full_messages.first }, status: 500
+              raise ActiveRecord::Rollback
+            end
+          end
+        end
+        render 'shared/http_status', locals: { code: '200', message:
+          HttpStatusHelper::ERROR_CODE['message']['200'] }, status: 200
       end
     end
 
