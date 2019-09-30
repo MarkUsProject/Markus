f00366ef38e64eaa75b20e65a77b219c7e2a1c40
Alex Shih <alex.alex.shih@gmail.com>
2019-09-30 00:06:19 -0400

    updated levels model validations

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,8 @@
 class Level < ApplicationRecord
   belongs_to :rubric_criterion
 
+  validates_numericality_of :number, only_integer: true, greater_than_or_equal_to: 0, allow_nil: true
+  validates_numericality_of :mark, greater_than_or_equal_to: 0
+
+
 end
