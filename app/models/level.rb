de88db7ba40eec72b5410363afeab374a2797d37
Alex Shih <alex.alex.shih@gmail.com>
2019-09-30 00:16:52 -0400

    fixed linting errors

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,8 +1,8 @@
+# Level represent a level within a Rubric Criterion
 class Level < ApplicationRecord
   belongs_to :rubric_criterion
 
   validates_numericality_of :number, only_integer: true, greater_than_or_equal_to: 0, allow_nil: true
   validates_numericality_of :mark, greater_than_or_equal_to: 0
-
 
 end
