2fa6a062825da482465216745c412ea74e9da5db
Alex Shih <alex.alex.shih@gmail.com>
2019-10-01 23:08:16 -0400

    added validations

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,6 +2,11 @@
 class Level < ApplicationRecord
   belongs_to :rubric_criterion
 
+  validates :name, presence: true
+  validates :number, presence: true
+  validates :description, presence: true
+  validates :mark, presence: true
+
   validates_numericality_of :number, only_integer: true, greater_than_or_equal_to: 0, allow_nil: true
   validates_numericality_of :mark, greater_than_or_equal_to: 0
 
