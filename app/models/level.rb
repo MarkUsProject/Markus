caa55bbd247324ee1bfea0752e421b4d05440628
Alex Shih <alex.alex.shih@gmail.com>
2019-10-10 11:55:54 -0400

    finished set default levels method

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,7 +1,5 @@
 # Level represent a level within a Rubric Criterion
 class Level < ApplicationRecord
-  belongs_to :rubric_criterion
-
   validates :name, presence: true
   validates :number, presence: true
   validates :description, presence: true
@@ -9,5 +7,4 @@ class Level < ApplicationRecord
 
   validates_numericality_of :number, only_integer: true, greater_than_or_equal_to: 0, allow_nil: true
   validates_numericality_of :mark, greater_than_or_equal_to: 0
-
 end
