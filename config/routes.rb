d612e144182ab424bd695c9862ac6e1b2a770829
David Liu <david@cs.toronto.edu>
2019-10-25 20:00:37 -0400

    grace-period-deductions: Fix deletion and change route to DELETE.

--- config/routes.rb
+++ config/routes.rb
@@ -212,7 +212,7 @@ Rails.application.routes.draw do
             get 'download_zip'
             delete 'cancel_remark_request'
             post 'add_extra_mark'
-            post 'delete_grace_period_deduction'
+            delete 'delete_grace_period_deduction'
             get 'next_grouping'
             post 'remove_extra_mark'
             post 'set_released_to_students'
@@ -377,7 +377,7 @@ Rails.application.routes.draw do
       end
 
       member do
-        post 'delete_grace_period_deduction'
+        delete 'delete_grace_period_deduction'
       end
     end
 
