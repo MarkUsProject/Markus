0d0bb7cfca59555ad5826c53640bddcabbe97e34
Alex Shih <alex.alex.shih@gmail.com>
2019-11-08 10:43:43 -0500

    removed usage of constants within rubric criteria controller

--- app/controllers/rubric_criteria_controller.rb
+++ app/controllers/rubric_criteria_controller.rb
@@ -6,11 +6,11 @@ class RubricCriteriaController < ApplicationController
     @assignment = Assignment.find(params[:assignment_id])
     file_out = MarkusCsv.generate(@assignment.get_criteria(:all, :rubric)) do |criterion|
       criterion_array = [criterion.name, criterion.max_mark]
-      (0..RubricCriterion::RUBRIC_LEVELS - 1).each do |i|
-        criterion_array.push(criterion['level_' + i.to_s + '_name'])
+      (0..criterion.levels.length - 1).each do |i|
+        criterion_array.push(criterion.levels[i].name)
       end
-      (0..RubricCriterion::RUBRIC_LEVELS - 1).each do |i|
-        criterion_array.push(criterion['level_' + i.to_s + '_description'])
+      (0..criterion.levels.length - 1).each do |i|
+        criterion_array.push(criterion.levels[i].description)
       end
       criterion_array
     end
