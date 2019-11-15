09f1b8c9daa0005307698910e408f64c56e74c61
Misha Schwartz <misha.schwartz@gmail.com>
2019-11-15 10:16:36 -0500

    autotest-cancel-job: add error message

--- app/jobs/autotest_cancel_job.rb
+++ app/jobs/autotest_cancel_job.rb
@@ -1,6 +1,12 @@
 class AutotestCancelJob < ApplicationJob
   queue_as MarkusConfigurator.autotest_cancel_queue
 
+  def self.on_complete_js(_status)
+    'window.BatchTestRunTable.fetchData'
+  end
+
+  def self.show_status(_status); end
+
   def perform(host_with_port, test_run_ids)
     if Rails.application.config.action_controller.relative_url_root.nil?
       markus_address = host_with_port
@@ -11,30 +17,27 @@ class AutotestCancelJob < ApplicationJob
     server_command = MarkusConfigurator.autotest_server_command
     server_params = { markus_address: markus_address, run_ids: test_run_ids }
 
-    begin
-      if server_username.nil?
-        # local cancellation with no authentication
-        cancel_command = [server_command, 'cancel', '-j', JSON.generate(server_params)]
-        output, status = Open3.capture2e(*cancel_command)
-        if status.exitstatus != 0
+    if server_username.nil?
+      # local cancellation with no authentication
+      cancel_command = [server_command, 'cancel', '-j', JSON.generate(server_params)]
+      output, status = Open3.capture2e(*cancel_command)
+      if status.exitstatus != 0
+        raise output
+      end
+    else
+      # local or remote cancellation with authentication
+      server_host = MarkusConfigurator.autotest_server_host
+      Net::SSH.start(server_host, server_username, auth_methods: ['publickey']) do |ssh|
+        cancel_command = "#{server_command} cancel -j '#{JSON.generate(server_params)}'"
+        output = ssh.exec!(cancel_command)
+        if output.exitstatus != 0
           raise output
         end
-      else
-        # local or remote cancellation with authentication
-        server_host = MarkusConfigurator.autotest_server_host
-        Net::SSH.start(server_host, server_username, auth_methods: ['publickey']) do |ssh|
-          cancel_command = "#{server_command} cancel -j '#{JSON.generate(server_params)}'"
-          output = ssh.exec!(cancel_command)
-          if output.exitstatus != 0
-            raise output
-          end
-        end
       end
-      # TODO: Use output for something?
-    rescue StandardError => e
-      # TODO: Where to show failure? (when there is a way to show failure, the following code should not be ensured)
-    ensure
-      TestRun.find(test_run_ids).each { |test_run| test_run.update(time_to_service: -1) }
     end
+    TestRun.find(test_run_ids).each { |test_run| test_run.update(time_to_service: -1) }
+    # TODO: Use output for something?
+  rescue StandardError => e
+    self.status[:error_message] = e.message
   end
 end
