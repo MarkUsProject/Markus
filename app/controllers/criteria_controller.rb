26e63bff89bd7a6cdae89101df10658f0f63026c
Misha Schwartz <misha.schwartz@gmail.com>
2019-11-25 16:20:35 -0500

    criteria: warn when overriding marks with yml upload

--- app/controllers/criteria_controller.rb
+++ app/controllers/criteria_controller.rb
@@ -148,44 +148,45 @@ class CriteriaController < ApplicationController
       flash_message(:error, e.message)
     else
       if data[:type] == '.yml'
-        assignment.get_criteria.each(&:destroy)
+        ApplicationRecord.transaction do
+          assignment.get_criteria.each(&:destroy)
 
-        # Create criteria based on the parsed data.
-        successes = 0
-        pos = 1
-        crit_format_errors = []
-        data[:contents].each do |criterion_yml|
-          type = criterion_yml[1]['type']
-          begin
-            if type.casecmp('rubric') == 0
-              criterion = RubricCriterion.load_from_yml(criterion_yml)
-            elsif type.casecmp('flexible') == 0
-              criterion = FlexibleCriterion.load_from_yml(criterion_yml)
-            elsif type.casecmp('checkbox') == 0
-              criterion = CheckboxCriterion.load_from_yml(criterion_yml)
-            else
-              raise RuntimeError
+          # Create criteria based on the parsed data.
+          successes = 0
+          pos = 1
+          crit_format_errors = []
+          data[:contents].each do |criterion_yml|
+            type = criterion_yml[1]['type']
+            begin
+              if type.casecmp('rubric') == 0
+                criterion = RubricCriterion.load_from_yml(criterion_yml)
+              elsif type.casecmp('flexible') == 0
+                criterion = FlexibleCriterion.load_from_yml(criterion_yml)
+              elsif type.casecmp('checkbox') == 0
+                criterion = CheckboxCriterion.load_from_yml(criterion_yml)
+              else
+                raise RuntimeError
+              end
+
+              criterion.assignment_id = assignment.id
+              criterion.position = pos
+              criterion.save!
+              pos += 1
+              successes += 1
+            rescue ActiveRecord::RecordInvalid # E.g., both visibility options are false.
+              crit_format_errors << criterion_yml[0]
+            rescue RuntimeError # An error occurred.
+              crit_format_errors << criterion_yml[0]
             end
-
-            criterion.assignment_id = assignment.id
-            criterion.position = pos
-            criterion.save!
-            pos += 1
-            successes += 1
-          rescue ActiveRecord::RecordInvalid # E.g., both visibility options are false.
-            crit_format_errors << criterion_yml[0]
-          rescue RuntimeError # An error occurred.
-            crit_format_errors << criterion_yml[0]
           end
-        end
-        unless crit_format_errors.empty?
-          flash_message(:error,
-                        I18n.t('criteria.errors.invalid_format') + ' ' +
-                          crit_format_errors.join(', '))
-        end
-        if successes > 0
-          flash_message(:success,
-                        I18n.t('upload_success', count: successes))
+          unless crit_format_errors.empty?
+            flash_message(:error, "#{I18n.t('criteria.errors.invalid_format')} #{crit_format_errors.join(', ')}")
+            raise ActiveRecord::Rollback
+          end
+          if successes > 0
+            flash_message(:success,
+                          I18n.t('upload_success', count: successes))
+          end
         end
       end
     end
