1f5e0611cb2a40095e0b238c5b207b7adee295a3
mishaschwartz <misha.schwartz@gmail.com>
2019-12-20 09:31:39 -0500

    csv: make sure all columns are shown in csv download of marks spreadsheet (#4290)
    Co-authored-by: David Liu <david@cs.toronto.edu>

--- app/models/grade_entry_form.rb
+++ app/models/grade_entry_form.rb
@@ -169,9 +169,11 @@ class GradeEntryForm < ApplicationRecord
     MarkusCSV.generate(students, headers) do |user_name, total_grade|
       row = [user_name]
       if grade_data.key? user_name
-        # Take grades sorted by position.
-        student_grades = grade_data[user_name].sort_by { |x| x[1] }
-                                              .map { |x| x[2].nil? ? '' : x[2] }
+        student_grades = Array.new(num_items, '')
+        grade_data[user_name].each do |g|
+          grade_index = g[1] - 1
+          student_grades[grade_index] = g[2].nil? ? '' : g[2]
+        end
         row.concat(student_grades)
         row << (total_grade.nil? ? '' : total_grade) if self.show_total
       else
