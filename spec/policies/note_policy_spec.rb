8a03a0da2f18a3bc5bff48fc35ab18c5f3cf4aa5
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-01-16 11:48:55 -0500

    Added new Note policy and Note policy tests

--- spec/policies/note_policy_spec.rb
+++ spec/policies/note_policy_spec.rb
@@ -12,7 +12,7 @@ describe NotePolicy do
   describe 'When the user is TA' do
     subject { described_class.new(note, user: user) }
     let(:user) { build(:ta) }
-    let(:note) {create(:note)}
+    let(:note) { create(:note) }
     context 'TA can manage the notes' do
       it { is_expected.to pass :manage? }
     end
@@ -21,10 +21,9 @@ describe NotePolicy do
       it { is_expected.not_to pass :update?, because_of: :ensure_can_modify? }
     end
     context 'When TA is editing or updating their own notes' do
-      let(:note) {build(:note, user: user)}
+      let(:note) { build(:note, user: user) }
       it { is_expected.to pass :edit? }
       it { is_expected.to pass :update? }
     end
   end
 end
-
