d6e8b093f8eef1875f872484cdc25c2014ce9121
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-01-16 14:28:27 -0500

    style changes

--- app/policies/note_policy.rb
+++ app/policies/note_policy.rb
@@ -1,14 +1,18 @@
 class NotePolicy < ApplicationPolicy
   default_rule :manage?
+
   def manage?
     user.ta? || user.admin?
   end
+
   def ensure_can_modify?
     user.admin? || user.id == record.creator_id
   end
+
   def edit?
     check?(:ensure_can_modify?)
   end
+
   def update?
     check?(:ensure_can_modify?)
   end
