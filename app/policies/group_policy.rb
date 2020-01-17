f620d0a09cca15ca729d0d4cad33a090a69d0143
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-01-17 15:58:24 -0500

    Added

---
+++ app/policies/group_policy.rb
@@ -0,0 +1,11 @@
+class GroupPolicy < ApplicationPolicy
+  default_rule :manage?
+  alias_rule :create?, :destroy?, :delete_rejected?, :disinvite_member?, :invite_member?, :accept_invitation?, :decline_invitation?, to: :student_manage?
+  def student_manage?
+    user.student?
+  end
+
+  def manage?
+    user.admin?
+  end
+end
