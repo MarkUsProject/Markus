639db615203914f7f65611ddcb49122e2e584117
David Liu <david@cs.toronto.edu>
2020-02-05 12:37:01 -0500

    pdfjs: Update to v2.3.200, install with webpacker.
    Fixes #4357.

--- app/javascript/packs/result.js
+++ app/javascript/packs/result.js
@@ -4,3 +4,11 @@ import 'javascripts/react_config';
 
 import { makeResult } from 'javascripts/Components/Result/result';
 window.makeResult = makeResult;
+
+import * as pdfjs from 'pdfjs-dist';
+import * as pdfjsViewer from 'pdfjs-dist/web/pdf_viewer';
+
+window.pdfjs = pdfjs;
+window.pdfjsViewer = pdfjsViewer;
+
+import 'pdfjs-dist/web/pdf_viewer.css';
