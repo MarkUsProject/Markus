04dfbd0eb0a7548066b623008506e9fc6dd92326
Alex Shih <alex.alex.shih@gmail.com>
2020-03-01 15:47:32 -0500

    updated creation of levels

--- spec/factories/rubric_criteria.rb
+++ spec/factories/rubric_criteria.rb
@@ -5,14 +5,7 @@ FactoryBot.define do
     max_mark { 4.0 }
     ta_visible { true }
     peer_visible { false }
+    5.times.each { |i| create(:level, rubric_criterion: criteria, mark: i) }
     sequence(:position)
-  end
-
-  factory :rubric_with_levels, parent: :rubric_criterion do
-    after(:build) do |criteria| # called by both create and build
-      5.times.each { |i|
-        create(:level, rubric_criterion: criteria, mark: i)
-      }
-    end
   end
 end
