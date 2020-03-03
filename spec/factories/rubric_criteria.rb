35b1b85089996d5f66cb3153cb4d0a22764c9d41
Alex Shih <alex.alex.shih@gmail.com>
2020-03-02 23:12:37 -0500

    changed rubric criteria to rubric criterion

--- spec/factories/rubric_criteria.rb
+++ spec/factories/rubric_criteria.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     peer_visible { false }
     sequence(:position)
     after(:create) do |criterion|
-      5.times.each { |i| create(:level, rubric_criteria: criterion, mark: i) }
+      5.times.each { |i| create(:level, rubric_criterion: criterion, mark: i) }
     end
   end
 end
