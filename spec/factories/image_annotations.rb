9d823067965109d07257213018a9411701a430fc
Misha Schwartz <misha.schwartz@gmail.com>
2020-03-19 17:23:23 -0400

    rspec: add tests for get_data method

--- spec/factories/image_annotations.rb
+++ spec/factories/image_annotations.rb
@@ -1,6 +1,20 @@
 require 'faker'
 
 FactoryBot.define do
-  factory :image_annotation do
+  factory :annotation do
+    association :annotation_text
+    association :submission_file
+    association :creator, factory: :admin
+    association :result, factory: :complete_result
+    sequence(:annotation_number)
+    creator_type { 'Admin' }
+    is_remark { false }
+
+    factory :image_annotation, class: ImageAnnotation do
+      x1 { 1 }
+      x2 { 2 }
+      y1 { 1 }
+      y2 { 2 }
+    end
   end
 end
