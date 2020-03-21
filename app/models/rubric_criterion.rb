840decedae1588bbc31c57e5b4a6529d240aa914
Alex Shih <alex.alex.shih@gmail.com>
2020-03-21 15:12:30 -0400

    removed unused update max mark function

--- app/models/rubric_criterion.rb
+++ app/models/rubric_criterion.rb
@@ -14,7 +14,6 @@ class RubricCriterion < Criterion
 
   has_many :levels, -> { order(:mark) }, inverse_of: :rubric_criterion, dependent: :destroy
   accepts_nested_attributes_for :levels, allow_destroy: true
-  before_validation :update_max_mark
   validate :validate_max_mark
 
   belongs_to :assignment, counter_cache: true
@@ -37,18 +36,6 @@ class RubricCriterion < Criterion
       errors.add(:max_mark, "Max mark of rubric criterion should not be greater than max level mark")
     end
   end
-
-  # def update_max_mark
-  #   max = 0
-  #   self.levels.each do |level|
-  #     if level.changed.include? 'mark'
-  #       max = level.mark if level.mark > max
-  #     end
-  #   end
-  #   # max mark does not need to be updated if it is larger than maximum level mark
-  #   return if max < self.max_mark
-  #   self.max_mark = max
-  # end
 
   def update_assigned_groups_count
     result = []
