a44b5162d504db30e0b2f12c6dbde20392b5f5ef
ignaspa <ignaspan@gmail.com>
2020-03-21 23:16:49 -0400

    mailer spec some tests pass

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,40 +1,40 @@
 RSpec.describe NotificationMailer, type: :mailer do
   describe 'release_email' do
-    before(:all) do
-      @user = create(:user)
-      @fake_assignment = create(:user)
-      @grouping = create(:grouping, assignment: fake_assignment)
-      byebug
-      @mail = described_class.with(user: user, grouping: grouping).release_email.deliver_now
-      byebug
+    before(:each) do
+      @user = create(:student)
+      @fake_assignment= create(:assignment)
+      @grouping = create(:grouping, assignment: @fake_assignment)
+      @submission = create(:submission, submission_version_used: true, grouping: @grouping)
+      @grouping.reload
+
+      @mail = described_class.with(user: @user, grouping: @grouping).release_email.deliver_now
     end
 
     it 'renders the subject' do
-      byebug
-      subject_line = 'MarkUs Notification (' + Rails.configuration.course_name + ') Your marks for A2 have released!'
-      expect(mail.subject).to eq(subject_line)
+      subject_line = 'MarkUs Notification (' + Rails.configuration.course_name + ') Your marks for ' +
+          @fake_assignment.short_identifier + ' have released!'
+      expect(@mail.subject).to eq(subject_line)
     end
 
     it 'renders the receiver email' do
-      puts mail.body.to_s
-      expect(mail.to).to eq([user.email])
+      expect(@mail.to).to eq([user.email])
     end
 
     it 'renders the sender email' do
-      expect(mail.from).to eq(['noreply@markus.com'])
+      expect(@mail.from).to eq(['noreply@markus.com'])
     end
 
     # not sure what these do
     it 'renders the student name in the body of the email.' do
-      expect(mail.body.to_s).to match(/"#{user.first_name} #{user.last_name}"/)
+      expect(@mail.body.to_s).to match(/"#{@user.first_name} #{@user.last_name}"/)
     end
 
     it 'renders the disclaimer in the body of the email.' do
-      expect(mail.body.to_s).to match(/This is an automated email. Please do not reply./)
+      expect(@mail.body.to_s).to match(/"This is an automated email. Please do not reply."/)
     end
 
     it 'renders the assignment in the body of the email.' do
-      expect(mail.body.to_s).to match("#{fake_assignment.short_identifier}")
+      expect(@mail.body.to_s).to match("#{@fake_assignment.short_identifier}")
     end
   end
 end
