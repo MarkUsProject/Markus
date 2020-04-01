dca31d1266bb2f849bc41327e741c80bba7afe03
Misha Schwartz <misha.schwartz@gmail.com>
2020-04-01 15:37:38 -0400

    rspec: add api test for get_test_files method

--- spec/controllers/api/assignments_controller_spec.rb
+++ spec/controllers/api/assignments_controller_spec.rb
@@ -264,5 +264,66 @@ describe Api::AssignmentsController do
         expect(response.status).to eq(404)
       end
     end
+    context 'GET get_test_files' do
+      let(:dirs) { [] }
+      let(:files) { {} }
+      before :each do
+        FileUtils.mkdir_p assignment.autotest_files_dir
+        dirs.each { |dir_path| FileUtils.mkdir_p File.join(assignment.autotest_files_dir, dir_path) }
+        files.each do |file_path, content|
+          File.write(File.join(assignment.autotest_files_dir, file_path.to_s), content)
+        end
+      end
+      after :each do
+        FileUtils.rm_rf assignment.autotest_files_dir
+      end
+      it 'should be successful' do
+        get :get_test_files, params: { id: assignment.id }
+        expect(response.status).to eq(200)
+      end
+      context 'when there are no test files' do
+        it 'should return a zip containing no files' do
+          get :get_test_files, params: { id: assignment.id }
+          check_zip_file_count(response.body, 0, 0)
+        end
+      end
+      context 'when there is a single test file' do
+        let(:files) { {'test.txt': 'test_content'} }
+        it 'should return a zip containing one file' do
+          get :get_test_files, params: { id: assignment.id }
+          check_zip_file_count(response.body, 1, 0)
+        end
+      end
+      context 'when there is a single test directory' do
+        let(:dirs) { ['test_dir'] }
+        it 'should return a zip containing one file' do
+          get :get_test_files, params: { id: assignment.id }
+          check_zip_file_count(response.body, 0, 1)
+        end
+      end
+      context 'when there is a file in a subdirectory' do
+        let(:dirs) { ['test_dir'] }
+        let(:files) { {'test_dir/test.txt': 'test_content'} }
+        it 'should return a zip containing one file and one directory' do
+          get :get_test_files, params: { id: assignment.id }
+          check_zip_file_count(response.body, 1, 1)
+        end
+      end
+    end
   end
+end
+
+def check_zip_file_count(content, file_count, dir_count)
+  files, dirs = 0, 0
+  Zip::InputStream.open(StringIO.new(content)) do |io|
+    while (entry = io.get_next_entry)
+      if entry.name_is_directory?
+        dirs += 1
+      else
+        files += 1
+      end
+    end
+  end
+  expect(files).to eq file_count
+  expect(dirs).to eq dir_count
 end
