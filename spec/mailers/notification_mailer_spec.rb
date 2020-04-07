0765991b851431aa34b93e7a4e9163b05cec73c0
ignaspa <ignaspan@gmail.com>
2020-04-06 22:10:46 -0400

    fixes var access in shared examples

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -44,9 +44,10 @@ RSpec.describe NotificationMailer, type: :mailer do
     let(:user) { create(:student) }
     let(:grade_entry_form) { create(:grade_entry_form_with_data) }
     let(:grade_entry_student) { grade_entry_form.grade_entry_students.find_or_create_by(user: user) }
-    let(:mail) { described_class.with(student: grade_entry_student, form: grade_entry_form)
-                                .release_spreadsheet_email
-                                .deliver_now }
+    let(:mail)
+      described_class.with(student: grade_entry_student, form: grade_entry_form)
+                     .release_spreadsheet_email
+                     .deliver_now
 
     it 'renders the subject' do
       subject_line = 'MarkUs Notification (' + Rails.configuration.course_name + ') Your marks for ' +
@@ -76,9 +77,10 @@ RSpec.describe NotificationMailer, type: :mailer do
     let(:invited) { create(:student) }
     let(:fake_assignment) { create(:assignment) }
     let(:grouping) { create(:grouping, assignment: fake_assignment) }
-    let(:mail) { described_class.with(invited: invited, inviter: inviter, grouping: grouping)
-                                .grouping_invite_email
-                                .deliver_now }
+    let(:mail)
+      described_class.with(invited: invited, inviter: inviter, grouping: grouping)
+                     .grouping_invite_email
+                     .deliver_now
 
     it 'renders the subject' do
       subject_line = 'MarkUs Notification (' + Rails.configuration.course_name + ') You have been invited to a group!'
