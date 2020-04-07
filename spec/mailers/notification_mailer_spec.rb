57e1d53053713b2857bc5e6cb57a31bb3df53e86
ignaspa <ignaspan@gmail.com>
2020-04-06 22:02:48 -0400

    fixes var access in shared examples

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,114 +1,104 @@
 RSpec.describe NotificationMailer, type: :mailer do
-  shared_examples_for "an email" do |mail|
-    it 'render the disclaimer in the body of the email.' do
+  RSpec.shared_examples "an email" do
+    it 'renders the disclaimer in the body of the email.' do
       expect(mail.body.to_s).to include('This is an automated email. Please do not reply.')
     end
 
-    it 'render the sender email' do
+    it 'renders the sender email' do
       expect(mail.from).to eq(['noreply@markus.com'])
     end
   end
 
   describe 'release_email' do
-    before(:each) do
-      @user = create(:student)
-      @fake_assignment = create(:assignment)
-      @grouping = create(:grouping, assignment: @fake_assignment)
-      @submission = create(:submission, submission_version_used: true, grouping: @grouping)
-      @grouping.reload
-      @mail = described_class.with(user: @user, grouping: @grouping).release_email.deliver_now
+    let(:user){ create(:student) }
+    let(:fake_assignment){ create(:assignment) }
+    let(:grouping){ create(:grouping, assignment: fake_assignment) }
+    let(:mail) do
+      create(:submission, submission_version_used: true, grouping: grouping)
+      grouping.reload
+      described_class.with(user: user, grouping: grouping).release_email.deliver_now
     end
 
     it 'renders the subject' do
       subject_line = 'MarkUs Notification (' + Rails.configuration.course_name + ') Your marks for ' +
-          @fake_assignment.short_identifier + ' have been released!'
-      expect(@mail.subject).to eq(subject_line)
+          fake_assignment.short_identifier + ' have been released!'
+      expect(mail.subject).to eq(subject_line)
     end
 
     it 'renders the receiver email' do
-      expect(@mail.to).to eq([@user.email])
+      expect(mail.to).to eq([user.email])
     end
 
     it 'renders the student name in the body of the email.' do
-      expect(@mail.body.to_s).to include("#{@user.first_name} #{@user.last_name}")
+      expect(mail.body.to_s).to include("#{user.first_name} #{user.last_name}")
     end
 
     it 'renders the assignment in the body of the email.' do
-      expect(@mail.body.to_s).to include(@fake_assignment.short_identifier.to_s)
+      expect(mail.body.to_s).to include(fake_assignment.short_identifier.to_s)
     end
 
-    it_should_behave_like "an email", @mail
+    include_examples 'an email'
   end
 
   describe 'release_spreadsheet_email' do
-    before(:each) do
-      @user = create(:student)
-      @grade_entry_form = create(:grade_entry_form_with_data)
-      @grade_entry_student = @grade_entry_form.grade_entry_students.find_or_create_by(user: @user)
-      @mail = described_class.with(student: @grade_entry_student, form: @grade_entry_form)
-                             .release_spreadsheet_email.deliver_now
-    end
+    let(:user){ create(:student) }
+    let(:grade_entry_form){ create(:grade_entry_form_with_data) }
+    let(:grade_entry_student){ grade_entry_form.grade_entry_students.find_or_create_by(user: user) }
+    let(:mail){ described_class.with(student: grade_entry_student, form: grade_entry_form)
+                           .release_spreadsheet_email.deliver_now }
 
     it 'renders the subject' do
       subject_line = 'MarkUs Notification (' + Rails.configuration.course_name + ') Your marks for ' +
-      @grade_entry_form.short_identifier + ' have been released!'
-      expect(@mail.subject).to eq(subject_line)
+      grade_entry_form.short_identifier + ' have been released!'
+      expect(mail.subject).to eq(subject_line)
     end
 
     it 'renders the receiver email' do
-      expect(@mail.to).to eq([@grade_entry_student.user.email])
+      expect(mail.to).to eq([grade_entry_student.user.email])
     end
 
     it 'renders the student name in the body of the email.' do
-      first_name = @grade_entry_student.user.first_name
-      last_name = @grade_entry_student.user.last_name
-      expect(@mail.body.to_s).to include("#{first_name} #{last_name}")
+      first_name = grade_entry_student.user.first_name
+      last_name = grade_entry_student.user.last_name
+      expect(mail.body.to_s).to include("#{first_name} #{last_name}")
     end
 
     it 'renders the spreadsheet name in the body of the email.' do
-      expect(@mail.body.to_s).to include(@grade_entry_form.short_identifier.to_s)
+      expect(mail.body.to_s).to include(grade_entry_form.short_identifier.to_s)
     end
 
-    it_should_behave_like "an email", @mail
+    include_examples 'an email'
   end
 
   describe 'grouping_invite_email' do
-    before(:each) do
-      @inviter = create(:student)
-      @invited = create(:student)
-      @fake_assignment = create(:assignment)
-      @grouping = create(:grouping, assignment: @fake_assignment)
-      @mail = described_class.with(invited: @invited, inviter: @inviter, grouping: @grouping)
-                             .grouping_invite_email.deliver_now
-    end
+    let(:inviter){ create(:student) }
+    let(:invited){ create(:student) }
+    let(:fake_assignment){ create(:assignment) }
+    let(:grouping){ create(:grouping, assignment: fake_assignment) }
+    let(:mail){ described_class.with(invited: invited, inviter: inviter, grouping: grouping)
+                           .grouping_invite_email.deliver_now }
 
     it 'renders the subject' do
       subject_line = 'MarkUs Notification (' + Rails.configuration.course_name + ') You have been invited to a group!'
-      expect(@mail.subject).to eq(subject_line)
+      expect(mail.subject).to eq(subject_line)
     end
 
     it 'renders the receiver email' do
-      expect(@mail.to).to eq([@invited.email])
-    end
-
-    it 'renders the sender email' do
-      expect(@mail.from).to eq(['noreply@markus.com'])
+      expect(mail.to).to eq([invited.email])
     end
 
     it 'renders the inviter name in the body of the email.' do
-      first_name = @inviter.first_name
-      last_name = @inviter.last_name
-      expect(@mail.body.to_s).to include("#{first_name} #{last_name}")
+      first_name = inviter.first_name
+      last_name = inviter.last_name
+      expect(mail.body.to_s).to include("#{first_name} #{last_name}")
     end
 
     it 'renders the invitee name in the body of the email.' do
-      first_name = @invited.first_name
-      last_name = @invited.last_name
-      expect(@mail.body.to_s).to include("#{first_name} #{last_name}")
+      first_name = invited.first_name
+      last_name = invited.last_name
+      expect(mail.body.to_s).to include("#{first_name} #{last_name}")
     end
 
-    it 'renders the disclaimer in the body of the email.' do
-      expect(@mail.body.to_s).to include('This is an automated email. Please do not reply.')
-    end
+    include_examples 'an email'
   end
 end
