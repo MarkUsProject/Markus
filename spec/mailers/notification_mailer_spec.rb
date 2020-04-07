43de47931d078aa1aa639958fac326a5ba10c8dd
ignaspa <ignaspan@gmail.com>
2020-04-07 16:57:57 -0400

    Fixes interpretation of apostrophe

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -7,16 +7,26 @@ RSpec.describe NotificationMailer, type: :mailer do
     it 'renders the sender email' do
       expect(mail.from).to eq(['noreply@markus.com'])
     end
+
+    it 'renders the recipient greeting in the body of the email.' do
+      first_name = recipient.first_name
+      last_name = recipient.last_name
+      expect(mail.body.to_s.gsub("&#39;", "'")).to include("Hello #{first_name} #{last_name},")
+    end
+
+    it 'renders the recipient email' do
+      expect(mail.to).to eq([recipient.email])
+    end
   end
 
   describe 'release_email' do
-    let(:user) { create(:student) }
+    let(:recipient) { create(:student, first_name: "Ilya", last_name: "O'Lenna") }
     let(:fake_assignment) { create(:assignment) }
     let(:grouping) { create(:grouping, assignment: fake_assignment) }
     let(:mail) do
       create(:submission, submission_version_used: true, grouping: grouping)
       grouping.reload
-      described_class.with(user: user, grouping: grouping).release_email.deliver_now
+      described_class.with(user: recipient, grouping: grouping).release_email.deliver_now
     end
 
     it 'renders the subject' do
@@ -25,15 +35,8 @@ RSpec.describe NotificationMailer, type: :mailer do
       expect(mail.subject).to eq(subject_line)
     end
 
-    it 'renders the receiver email' do
-      expect(mail.to).to eq([user.email])
-    end
-
-    it 'renders the student name in the body of the email.' do
-      expect(mail.body.to_s).to include("#{user.first_name} #{user.last_name}")
-    end
-
     it 'renders the assignment in the body of the email.' do
+      puts mail.body.to_s
       expect(mail.body.to_s).to include(fake_assignment.short_identifier.to_s)
     end
 
@@ -41,9 +44,9 @@ RSpec.describe NotificationMailer, type: :mailer do
   end
 
   describe 'release_spreadsheet_email' do
-    let(:user) { create(:student) }
+    let(:recipient) { create(:student) }
     let(:grade_entry_form) { create(:grade_entry_form_with_data) }
-    let(:grade_entry_student) { grade_entry_form.grade_entry_students.find_or_create_by(user: user) }
+    let(:grade_entry_student) { grade_entry_form.grade_entry_students.find_or_create_by(user: recipient) }
     let(:mail) do
       described_class.with(student: grade_entry_student, form: grade_entry_form)
                      .release_spreadsheet_email
@@ -56,16 +59,6 @@ RSpec.describe NotificationMailer, type: :mailer do
       expect(mail.subject).to eq(subject_line)
     end
 
-    it 'renders the receiver email' do
-      expect(mail.to).to eq([grade_entry_student.user.email])
-    end
-
-    it 'renders the student name in the body of the email.' do
-      first_name = grade_entry_student.user.first_name
-      last_name = grade_entry_student.user.last_name
-      expect(mail.body.to_s).to include("#{first_name} #{last_name}")
-    end
-
     it 'renders the spreadsheet name in the body of the email.' do
       expect(mail.body.to_s).to include(grade_entry_form.short_identifier.to_s)
     end
@@ -75,11 +68,11 @@ RSpec.describe NotificationMailer, type: :mailer do
 
   describe 'grouping_invite_email' do
     let(:inviter) { create(:student) }
-    let(:invited) { create(:student) }
+    let(:recipient) { create(:student) }
     let(:fake_assignment) { create(:assignment) }
     let(:grouping) { create(:grouping, assignment: fake_assignment) }
     let(:mail) do
-      described_class.with(invited: invited, inviter: inviter, grouping: grouping)
+      described_class.with(invited: recipient, inviter: inviter, grouping: grouping)
                      .grouping_invite_email
                      .deliver_now
     end
@@ -89,20 +82,10 @@ RSpec.describe NotificationMailer, type: :mailer do
       expect(mail.subject).to eq(subject_line)
     end
 
-    it 'renders the receiver email' do
-      expect(mail.to).to eq([invited.email])
-    end
-
     it 'renders the inviter name in the body of the email.' do
       first_name = inviter.first_name
       last_name = inviter.last_name
-      expect(mail.body.to_s).to include("#{first_name} #{last_name}")
-    end
-
-    it 'renders the invitee name in the body of the email.' do
-      first_name = invited.first_name
-      last_name = invited.last_name
-      expect(mail.body.to_s).to include("#{first_name} #{last_name}")
+      expect(mail.body.to_s.gsub("&#39;", "'")).to include("#{first_name} #{last_name}")
     end
 
     include_examples 'an email'
