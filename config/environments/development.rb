63ec9acdefd87fe8011b28aed179c5d7e4094822
David Liu <david@cs.toronto.edu>
2020-04-12 11:55:28 -0400

    mailer: modify default configuration in dev and prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -253,18 +253,19 @@ Markus::Application.configure do
   ###################################################################
   # Email Notifications
   ###################################################################
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'example.com',
-      user_name:            'example email',
-      password:             'example password',
-      authentication:       'plain',
-      enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method = :test
+  #config.action_mailer.smtp_settings = {
+  #    address:              'smtp.gmail.com',
+  #    port:                 587,
+  #    domain:               'example.com',
+  #    user_name:            'example email',
+  #    password:             'example password',
+  #    authentication:       'plain',
+  #    enable_starttls_auto: true
+  #}
   config.action_mailer.default_url_options = {host: 'localhost:3000'}
   config.action_mailer.asset_host = 'http://localhost:3000'
+  config.action_mailer.perform_deliveries = true
 
   ###################################################################
   # Resque queues
