030c6387e65fc8540f7c0be6d062247fd1c9ba8b
Alex Shih <alex.alex.shih@gmail.com>
2020-04-21 15:26:01 -0400

    fixed hound comment

--- app/models/rubric_criterion.rb
+++ app/models/rubric_criterion.rb
@@ -54,7 +54,7 @@ class RubricCriterion < Criterion
     scale = new_max / old_max
     self.levels.each do |level|
       # don't scale levels that the user has manually changed
-      unless level.changed.include? 'mark' or level.mark.nil?
+      unless level.changed.include? 'mark' || level.mark.nil?
         level.update_attributes(mark: (level.mark * scale).round(2))
         # don't validate in case updating level mark overlaps another mark temporarily
         level.save(validate: false)
