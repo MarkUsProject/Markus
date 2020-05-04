288e488440d7b413c49ba7cfcb909e78d1c10d63
David Liu <david@cs.toronto.edu>
2020-05-04 13:54:14 -0400

    test: Fix flaky level tests

--- spec/factories/levels.rb
+++ spec/factories/levels.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :level do
-    name { Faker::Lorem.word }
+    sequence(:name) { |i| "Level #{i}" }
     description { Faker::Lorem.word }
     rubric_criterion
   end
