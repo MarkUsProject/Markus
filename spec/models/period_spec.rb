c12261ac8e69173e0d6a700e9ae5e3dfdf91effc
Misha Schwartz <misha.schwartz@gmail.com>
2020-05-07 16:14:57 -0400

    rpsec: update tests

--- spec/models/period_spec.rb
+++ spec/models/period_spec.rb
@@ -1,6 +1,6 @@
 describe Period do
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:hours) }
+    subject { create :period }
     it { is_expected.to have_db_column(:interval) }
     it { is_expected.to belong_to(:submission_rule) }
 
@@ -11,43 +11,46 @@ describe Period do
     it { is_expected.not_to allow_value(-1).for(:hours) }
     it { is_expected.not_to allow_value(-100).for(:hours) }
     it { is_expected.not_to allow_value(nil).for(:hours) }
+    it { is_expected.not_to allow_value('').for(:hours) }
   end
 
   context 'A penalty decay period' do
-    before {@period = Period.create(submission_rule_type: 'PenaltyDecayPeriodSubmissionRule')}
+    let(:rule) { create :penalty_decay_period_submission_rule }
+    let(:period) { create :period, submission_rule: rule }
 
     it 'validate presence of deduction' do
-      #no deduction is set
-      expect(@period.valid?).to be false
+      period.deduction = nil
+      expect(period.valid?).to be false
     end
 
     it 'validate numericality of deduction' do
-      @period.deduction = 'string'
-      expect(@period.valid?).to be false
+      period.deduction = 'string'
+      expect(period.valid?).to be false
     end
 
     it 'validate presence of interval' do
-      #no interval is set
-      expect(@period.valid?).to be false
+      period.interval = nil
+      expect(period.valid?).to be false
     end
 
     it 'validate numericality of interval' do
-      @period.interval = 'string'
-      expect(@period.valid?).to be false
+      period.interval = 'string'
+      expect(period.valid?).to be false
     end
   end
 
   context 'A penalty period' do
-    before {@period = Period.create(submission_rule_type: 'PenaltyPeriodSubmissionRule')}
+    let(:rule) { create :penalty_period_submission_rule }
+    let(:period) { create :period, submission_rule: rule }
 
     it 'validate presence of deduction' do
-      #no deduction is set
-      expect(@period.valid?).to be false
+      period.deduction = nil
+      expect(period.valid?).to be false
     end
 
     it 'validate numericality of deduction' do
-      @period.deduction = 'string'
-      expect(@period.valid?).to be false
+      period.deduction = 'string'
+      expect(period.valid?).to be false
     end
   end
 end
