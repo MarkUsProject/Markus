23b39e39eba0786dbe55e95692cb78c896bb2290
Ignas Panero Armoska <ignaspan@gmail.com>
2020-05-18 18:31:22 -0400

    Fixes flexible criteria factory

--- spec/factories/flexible_criteria.rb
+++ spec/factories/flexible_criteria.rb
@@ -8,15 +8,7 @@ FactoryBot.define do
     sequence(:position)
   end
 
-  factory :flexible_criterion_with_annotation_category do
-    sequence(:name) { |n| "Flexible criterion #{n}" }
-    association :assignment, factory: :assignment
-    association :annotation_category, factory: :annotation_category
-    max_mark { 1.0 }
-    ta_visible { true }
-    peer_visible { false }
-    sequence(:position)
-
+  factory :flexible_criterion_with_annotation_category, parent: :flexible_criterion do
     after(:create) do |flexible_criterion|
       flexible_criterion.annotation_categories
                         .create(:annotation_category, flexible_criterion_id: flexible_criterion.id)
