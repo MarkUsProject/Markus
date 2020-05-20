2e712ab51cbd364e99df76ee7ac5bd162c74be98
ANUSHIYAPRIYA <anu.antony247@gmail.com>
2020-05-20 14:50:49 -0400

    Renamed migration file

--- db/migrate/20200506170130_create_grader_permission.rb
+++ db/migrate/20200506170130_create_grader_permission.rb
@@ -1,4 +1,4 @@
-class GraderPermission < ActiveRecord::Migration[6.0]
+class CreateGraderPermission < ActiveRecord::Migration[6.0]
   def self.up
     create_table :grader_permission do |t|
       t.column :description, :text
