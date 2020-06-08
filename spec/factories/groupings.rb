8fe6969e357357ef4dbf95ef41a4068e6ab30145
David Liu <david@cs.toronto.edu>
2020-06-07 22:51:46 -0400

    test: Fix incorrect test factory and fix failing test.
    A result is created when a submission is created, so we do not need to
    create an incomplete result manually in
    :grouping_with_inviter_and_submission.

--- spec/factories/groupings.rb
+++ spec/factories/groupings.rb
@@ -12,7 +12,7 @@ FactoryBot.define do
 
   factory :grouping_with_inviter_and_submission, parent: :grouping_with_inviter do
     after(:create) do |g|
-      create(:incomplete_result, submission: create(:version_used_submission, grouping: g))
+      create(:version_used_submission, grouping: g)
     end
   end
 end
