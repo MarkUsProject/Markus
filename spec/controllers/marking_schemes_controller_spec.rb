7eb579b6f54265e6fa0648d378bf9c000629ba54
Robert Tan <robert.bm.tan@hotmail.com>
2020-06-08 16:46:31 -0400

    Complete spec tests for marknig schemes controller

--- spec/controllers/marking_schemes_controller_spec.rb
+++ spec/controllers/marking_schemes_controller_spec.rb
@@ -4,36 +4,169 @@ describe MarkingSchemesController do
   let(:grade_entry_form_with_data) { create(:grade_entry_form_with_data) }
   let(:assignment) { create(:assignment) }
   let(:assignment_with_criteria_and_results) { create(:assignment_with_criteria_and_results) }
+  let(:admin) { create(:admin) }
 
-  context '#index' do
-    it 'returns correct JSON data' do
-      byebug
-      marking_scheme = create(:marking_scheme, assessments: [assignment, assignment_with_criteria_and_results])
-      p marking_scheme
+  describe 'An unauthenticated and unauthorized user' do
+    context '#index' do
+      it 'should respond with redirect' do
+        get :index
+        is_expected.to respond_with :redirect
+      end
+    end
+
+    context '#new' do
+      it 'should respond with redirect' do
+        post :new
+        is_expected.to respond_with :redirect
+      end
+    end
+
+    context '#populate' do
+      it 'should respond with redirect' do
+        get :populate
+        is_expected.to respond_with :redirect
+      end
     end
   end
 
-  context '#populate' do
+  describe 'An authorized user' do
+    context '#populate' do
+      it 'returns correct JSON data' do
+        create(:marking_scheme, assessments: [
+            grade_entry_form,
+            grade_entry_form_with_data,
+            assignment,
+            assignment_with_criteria_and_results
+        ])
+        get_as admin, :populate, format: :json
 
-  end
+        expected_keys = %w[id name assignment_weights grade_entry_form_weights edit_link delete_link]
+        expect(response.parsed_body).is_a? Hash
+        expect(response.parsed_body.keys).to match_array %w[data columns]
+        expect(response.parsed_body['data']).to_not be_empty
 
-  context '#create' do
+        data_hash = response.parsed_body['data'][0]
+        expect(data_hash.keys).to match_array expected_keys
 
-  end
+        assignment_weights = data_hash['assignment_weights']
+        expect(assignment_weights.keys).to match_array [assignment.id.to_s, assignment_with_criteria_and_results.id.to_s]
 
-  context '#update' do
+        grade_entry_form_weights = data_hash['grade_entry_form_weights']
+        expect(grade_entry_form_weights.keys).to match_array [grade_entry_form.id.to_s, grade_entry_form_with_data.id.to_s]
+      end
+    end
 
-  end
+    context '#create' do
+      it 'creates a marking scheme with marking weights' do
+        params = {
+            'marking_scheme': {
+                'name': 'Test Marking Scheme',
+                'marking_weights_attributes': {
+                    '0': {'id': assignment, 'weight': 1},
+                    '1': {'id': assignment_with_criteria_and_results, 'weight': 2}
+                }
+            }
+        }
 
-  context '#new' do
+        post_as admin, :create, params: params
+        marking_scheme = MarkingScheme.first
+        marking_weights = marking_scheme.marking_weights
+        expect(marking_scheme.name).to eq 'Test Marking Scheme'
+        expect(marking_weights.size).to eq 2
 
-  end
+        expected_ids = [assignment.id, assignment_with_criteria_and_results.id]
+        expect(marking_weights.map { |mw| mw.assessment_id }).to match_array expected_ids
+      end
+    end
 
-  context '#edit' do
+    context '#update' do
+      it 'updates an existing marking scheme with new marking weights' do
+        create(:marking_scheme, assessments: [
+            grade_entry_form,
+            grade_entry_form_with_data,
+            assignment,
+            assignment_with_criteria_and_results
+        ])
 
-  end
+        params = {
+            'id': MarkingScheme.first.id,
+            'marking_scheme': {
+                'name': 'Test Marking Scheme 2',
+                'marking_weights_attributes': {
+                    '0': {'id': assignment, 'weight': 2.5},
+                    '1': {'id': assignment_with_criteria_and_results, 'weight': 3.5},
+                    '2': {'id': grade_entry_form, 'weight': 1.5},
+                    '3': {'id': grade_entry_form_with_data, 'weight': 0}
+                }
+            }
+        }
 
-  context '#destroy' do
+        post_as admin, :update, params: params
 
+        marking_scheme = MarkingScheme.first
+        marking_weights = marking_scheme.marking_weights
+        expect(marking_scheme.name).to eq 'Test Marking Scheme 2'
+        expect(marking_weights.size).to eq 4
+
+        expected_weights = [2.5, 3.5, 1.5, 0]
+        expect(marking_weights.map { |mw| mw.weight }).to match_array expected_weights
+      end
+    end
+
+    context '#new' do
+      before(:each) do
+        get_as admin, :new, format: :js
+      end
+
+      it 'should render the new template' do
+        is_expected.to render_template(:new)
+      end
+
+      it 'should respond with success' do
+        is_expected.to respond_with(:success)
+      end
+    end
+
+    context '#edit' do
+      before(:each) do
+        create(:marking_scheme, assessments: [
+            grade_entry_form,
+            grade_entry_form_with_data,
+            assignment,
+            assignment_with_criteria_and_results
+        ])
+        post_as admin,
+                :edit,
+                params: { id: MarkingScheme.first.id },
+                format: :js
+      end
+
+      it 'should render the edit template' do
+        is_expected.to render_template(:edit)
+      end
+
+      it 'should respond with success' do
+        is_expected.to respond_with(:success)
+      end
+    end
+
+    context '#destroy' do
+      it ' should be able to delete the marking scheme' do
+        create(:marking_scheme, assessments: [
+            grade_entry_form,
+            grade_entry_form_with_data,
+            assignment,
+            assignment_with_criteria_and_results
+        ])
+
+        ms = MarkingScheme.first
+        delete_as admin,
+                  :destroy,
+                  params: { id: ms.id },
+                  format: :js
+        is_expected.to respond_with(:success)
+        expect { MarkingScheme.find(ms.id) }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
   end
 end
