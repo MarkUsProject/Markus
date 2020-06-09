d6929c22f42e90732ea85dd01cfb3ecee8cd91bf
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-06-08 22:20:04 -0400

    Houndbot changes

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -11,48 +11,47 @@ class AnnotationsController < ApplicationController
     submission_file = submission.submission_files.find(params[:submission_file_id])
     begin
       authorize! with: AnnotationPolicy
-      base_attributes = {
-        submission_file_id: submission_file.id,
-        is_remark: submission.has_remark?,
-        annotation_text_id: params[:annotation_text_id],
-        annotation_number: result.annotations.size + 1,
-        creator: current_user,
-        result_id: params[:result_id]
-      }
-
-      if submission_file.is_supported_image?
-        @annotation = result.annotations.create(
-          type: 'ImageAnnotation',
-          x1: params[:x1],
-          y1: params[:y1],
-          x2: params[:x2],
-          y2: params[:y2],
-          **base_attributes
-        )
-      elsif submission_file.is_pdf?
-        @annotation = result.annotations.create(
-          type: 'PdfAnnotation',
-          x1: params[:x1],
-          y1: params[:y1],
-          x2: params[:x2],
-          y2: params[:y2],
-          page: params[:page],
-          **base_attributes
-        )
-      else
-        @annotation = result.annotations.create(
-          type: 'TextAnnotation',
-          line_start: params[:line_start],
-          line_end: params[:line_end],
-          column_start: params[:column_start],
-          column_end: params[:column_end],
-          **base_attributes
-        )
-      end
-      render :create
     rescue ActionPolicy::Unauthorized => e
       flash_message(:error, e.message)
     end
+    base_attributes = {
+      submission_file_id: submission_file.id,
+      is_remark: submission.has_remark?,
+      annotation_text_id: params[:annotation_text_id],
+      annotation_number: result.annotations.size + 1,
+      creator: current_user,
+      result_id: params[:result_id]
+    }
+    if submission_file.is_supported_image?
+      @annotation = result.annotations.create(
+        type: 'ImageAnnotation',
+        x1: params[:x1],
+        y1: params[:y1],
+        x2: params[:x2],
+        y2: params[:y2],
+        **base_attributes
+      )
+    elsif submission_file.is_pdf?
+      @annotation = result.annotations.create(
+        type: 'PdfAnnotation',
+        x1: params[:x1],
+        y1: params[:y1],
+        x2: params[:x2],
+        y2: params[:y2],
+        page: params[:page],
+        **base_attributes
+      )
+    else
+      @annotation = result.annotations.create(
+        type: 'TextAnnotation',
+        line_start: params[:line_start],
+        line_end: params[:line_end],
+        column_start: params[:column_start],
+        column_end: params[:column_end],
+        **base_attributes
+      )
+    end
+    render :create
   end
 
   def new
--- app/policies/course_summary_policy.rb
+++ app/policies/course_summary_policy.rb
@@ -1,7 +1,8 @@
 class CourseSummaryPolicy < ApplicationPolicy
   alias_rule :index?, :populate?, to: :view_course_summary?
   def view_course_summary?
-    user.admin? || (user.ta? && (allowed_to?(:download_csv_grades_report?) || allowed_to?(:marking_schemes?))) || (user.student?)
+    user.admin? || (user.ta? &&
+        (allowed_to?(:download_csv_grades_report?) || allowed_to?(:marking_schemes?))) || (user.student?)
   end
 
   def download_csv_grades_report?
