2a3ca74d6331ec0d5e414aa870e2eb4b1605c6db
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-06-08 22:20:04 -0400

    Houndbot changes

--- app/policies/annotation_policy.rb
+++ app/policies/annotation_policy.rb
@@ -1,18 +1,18 @@
 class AnnotationPolicy < ApplicationPolicy
   default_rule :manage?
   def add_existing_annotation?
-    user.admin? || (user.ta? && check?(:is_ta_allowed?))
+    user.admin? || (user.ta? && check?(:ta_allowed?))
   end
 
   def manage?
-    user.admin? || (user.ta? && check?(:is_ta_allowed?)) || (authorized?(user) && check?(:is_reviewer_allowed?))
+    user.admin? || (user.ta? && check?(:ta_allowed?)) || (authorized?(user) && check?(:reviewer_allowed?))
   end
 
-  def is_ta_allowed?
+  def ta_allowed?
     allowed_to?(:create_delete_annotations?, with: GraderPermissionPolicy)
   end
 
-  def is_reviewer_allowed?
+  def reviewer_allowed?
     assignment = result.submission.assignment
     assignment.has_peer_review && user.is_reviewer_for?(assignment.pr_assignment, result)
   end
