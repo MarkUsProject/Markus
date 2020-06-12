45ff952ee2e075ee151580aa30923462e7322097
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-06-11 23:29:55 -0400

    Made changes based on the second review comments

--- app/models/grader_permission.rb
+++ app/models/grader_permission.rb
@@ -1,6 +1,5 @@
 # Contains the grader permissions for a particular grader
 class GraderPermission < ApplicationRecord
-  self.table_name = 'grader_permissions'
   belongs_to :user
   validates_presence_of :user_id
 end
