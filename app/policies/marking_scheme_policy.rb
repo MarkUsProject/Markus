f42ca434c03d0307f92a66320de192c1fb536011
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-06-16 21:39:41 -0400

    test: Added tests and factories for marking schemes controller

--- app/policies/marking_scheme_policy.rb
+++ app/policies/marking_scheme_policy.rb
@@ -1,6 +1,5 @@
 class MarkingSchemePolicy < ApplicationPolicy
-  alias_rule :index?, :populate?, :create?, :update?, :new?,
-             :edit?, :destroy?, to: :manage?
+  default_rule :manage?
 
   def manage?
     user.admin? || (user.ta? && allowed_to?(:manage_marking_schemes?, with: GraderPermissionPolicy))
---
+++ spec/factories/marking_schemes.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :marking_scheme, class: MarkingScheme do
+    name { 'Scheme' }
+  end
+end
