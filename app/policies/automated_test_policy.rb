ca507cf2c3c878eda085b63d9f27f21e31c0e129
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-06-22 14:28:31 -0400

    controller: Changed the target record in authorization

--- app/policies/automated_test_policy.rb
+++ app/policies/automated_test_policy.rb
@@ -3,10 +3,10 @@ class AutomatedTestPolicy < ApplicationPolicy
   alias_rule :student_interface?, :get_test_runs_students?, :execute_test_run?, to: :student_access?
 
   def student_access?
-    record.student?
+    user.student?
   end
 
   def manage?
-    record.admin? || (record.ta? && allowed_to?(:manage_assignments?, with: GraderPermissionPolicy))
+    user.admin? || (user.ta? && allowed_to?(:manage_assignments?, with: GraderPermissionPolicy))
   end
 end
