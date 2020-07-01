857a32f646a3475866cd1f24a9a1bf9a69284f8a
Ignas Panero Armoska <29822176+ignaspa@users.noreply.github.com>
2020-07-01 10:34:03 -0400

    models: change hash keys for annotation texts stats to be cleaner

--- app/models/annotation_text.rb
+++ app/models/annotation_text.rb
@@ -19,13 +19,13 @@ class AnnotationText < ApplicationRecord
     # TODO: simplify second join once creator is no longer polymoprhic
     applications = self.annotations.joins(result: { grouping: :group })
                        .joins('INNER JOIN users ON annotations.creator_id = users.id')
-                       .pluck_to_hash('results.id',
-                                      'groupings.assessment_id',
-                                      'results.submission_id',
-                                      'groups.group_name',
-                                      'users.first_name',
-                                      'users.last_name',
-                                      'users.user_name')
+                       .pluck_to_hash('results.id AS result_id',
+                                      'groupings.assessment_id AS assignment_id',
+                                      'results.submission_id AS submission_id',
+                                      'groups.group_name AS group_name',
+                                      'users.first_name AS first_name',
+                                      'users.last_name AS last_name',
+                                      'users.user_name AS user_name')
     stats = {
       num_times_used: self.annotations.count,
       uses: applications
