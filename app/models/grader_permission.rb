02c7d260cd757fd1e68fe0e5c8ff80264a9928fd
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-07-08 14:43:11 -0400

    model: Added validation for user in grader permission model

--- app/models/grader_permission.rb
+++ app/models/grader_permission.rb
@@ -2,4 +2,12 @@
 class GraderPermission < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id
+  validate :user_must_be_a_grader
+
+  def user_must_be_a_grader
+    return unless user && !user.is_a?(Ta)
+
+    errors.add('base', 'User must be a grader')
+    false
+  end
 end
