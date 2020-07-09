354d1c3a483b9ae8023c75a0b4bf3000d9ace2f5
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-07-08 23:23:42 -0400

    test: Added tests for grader permission model

--- spec/models/grader_permission_spec.rb
+++ spec/models/grader_permission_spec.rb
@@ -5,4 +5,24 @@ describe GraderPermission do
   context 'validates the presence of user id' do
     it { is_expected.to validate_presence_of(:user_id) }
   end
+  describe 'Validating the user' do
+    context 'When the user is admin' do
+      let(:user) { create(:admin) }
+      it 'should raise an invalid record error' do
+        expect { create :grader_permission, user_id: user.id }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+    context 'When the user is grader' do
+      let(:user) { create(:ta) }
+      it 'should not raise an error' do
+        expect(create(:grader_permission, user_id: user.id)).to be_valid
+      end
+    end
+    context 'When the user is student' do
+      let(:user) { create(:student) }
+      it 'should raise an invalid record error' do
+        expect { create :grader_permission, user_id: user.id }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
