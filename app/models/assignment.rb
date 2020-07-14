3c7e17e8316deb4c7b8a0339bd73d3cc4d22176a
Ignas Panero Armoska <ignaspan@gmail.com>
2020-07-14 15:48:56 -0400

    models: add upcoming helper method to assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -144,6 +144,15 @@ class Assignment < Assessment
     sections_past
   end
 
+  def upcoming(current_user)
+    grouping = current_user.accepted_grouping_for(self.id)
+    due_date = grouping&.due_date
+    if due_date.nil?
+      due_date = assignment.section_due_date(current_user.section)
+    end
+    due_date > Time.current
+  end
+
   # Whether or not this grouping is past its due date for this assignment.
   def grouping_past_due_date?(grouping)
     return past_all_due_dates? if grouping.nil?
