8d228b8aeba88b5c945d81b8465cb8ea3b526558
David Liu <david@cs.toronto.edu>
2020-07-29 22:44:17 -0400

    results: Fix event handling for PDF annotations

--- app/assets/javascripts/Components/Result/pdf_viewer.jsx
+++ app/assets/javascripts/Components/Result/pdf_viewer.jsx
@@ -8,14 +8,16 @@ export class PDFViewer extends React.Component {
   }
 
   componentDidMount() {
+    this.eventBus = new pdfjsViewer.EventBus();
     this.pdfViewer = new pdfjsViewer.PDFViewer({
+      eventBus: this.eventBus,
       container: this.pdfContainer.current,
       // renderer: 'svg',  TODO: investigate why some fonts don't render with SVG
     });
     window.pdfViewer = this;  // For fixing display when pane width changes
 
-    document.addEventListener('pagesinit', this.ready_annotations);
-    document.addEventListener('pagesloaded', this.refresh_annotations);
+    this.eventBus.on('pagesinit', this.ready_annotations);
+    this.eventBus.on('pagesloaded', this.refresh_annotations);
 
     if (this.props.url) {
       this.loadPDFFile();
@@ -52,8 +54,7 @@ export class PDFViewer extends React.Component {
       box.style.width   = '0';
       box.style.height  = '0';
     }
-    document.removeEventListener('pagesinit', this.ready_annotations);
-    document.removeEventListener('pagesloaded', this.refresh_annotations);
+    this.eventBus = null;
     window.pdfViewer = undefined;
   }
 
