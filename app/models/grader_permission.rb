5ac4470b9078db97094da1c8d15fce9bdf61f9b6
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-07-29 23:48:40 -0400

    models: Added validation for association between user and grader permission

--- app/models/grader_permission.rb
+++ app/models/grader_permission.rb
@@ -3,6 +3,7 @@ class GraderPermission < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id
   validate :user_must_be_a_grader
+  validates_associated :user
 
   def user_must_be_a_grader
     return unless user && !user.is_a?(Ta)
