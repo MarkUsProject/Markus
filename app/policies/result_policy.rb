630ab9d1fe1c73f2983d484d027ad0b2ae83db6b
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-08-06 14:59:47 -0400

    manage_extensions: Removed delete_grace_period_deduction permission

--- app/policies/result_policy.rb
+++
@@ -1,7 +0,0 @@
-# Policy for Results controller.
-class ResultPolicy < ApplicationPolicy
-  # Only admin and authorized grader can delete the grace credit deduction.
-  def delete_grace_period_deduction?
-    user.admin? || (user.ta? && allowed_to?(:manage_extensions?, with: GraderPermissionPolicy))
-  end
-end
