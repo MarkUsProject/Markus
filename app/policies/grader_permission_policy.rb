21ba740e024c4c01fdef224fc92ee229dccd2d85
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-08-21 10:54:27 -0400

    Added manage_course_grades policy

--- app/policies/grader_permission_policy.rb
+++ app/policies/grader_permission_policy.rb
@@ -1,19 +1,11 @@
 # Contains policy for all the grader permissions in the grader_permissions table
 class GraderPermissionPolicy < ApplicationPolicy
-  def create_delete_annotations?
-    user.grader_permission.create_delete_annotations
-  end
-
   def manage_submissions?
     user.grader_permission.manage_submissions
   end
 
   def manage_assessments?
     user.grader_permission.manage_assessments
-  end
-
-  def manage_extensions?
-    user.grader_permission.manage_extensions
   end
 
   def manage_course_grades?
