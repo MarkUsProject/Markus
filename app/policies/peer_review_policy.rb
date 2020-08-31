8c68cd955477cb2a480ab086134c23a008f53276
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-08-31 11:49:57 -0400

    policy: Renamed assign_reviewers? name in peer review policy

--- app/policies/peer_review_policy.rb
+++ app/policies/peer_review_policy.rb
@@ -1,9 +1,9 @@
 # Policy for Peer reviews controller
 class PeerReviewPolicy < ApplicationPolicy
-  alias_rule :index?, :populate?, :assign_groups?, to: :assign_reviewers?
+  default_rule :manage?
 
   # Only admin and authorized grader can manage reviewers.
-  def assign_reviewers?
+  def manage?
     allowed_to?(:manage_assessments?, with: GraderPermissionPolicy)
   end
 end
--- spec/policies/peer_review_policy_spec.rb
+++ spec/policies/peer_review_policy_spec.rb
@@ -3,20 +3,20 @@ describe PeerReviewPolicy do
   describe 'When the user is admin' do
     subject { described_class.new(user: user) }
     let(:user) { build(:admin) }
-    context 'Admin can assign or unassign reviewers' do
-      it { is_expected.to pass :assign_reviewers? }
+    context 'Admin can manage peer reviews and reviewers' do
+      it { is_expected.to pass :manage? }
     end
   end
   describe 'When the user is TA' do
     subject { described_class.new(user: user) }
     # By default all the grader permissions are set to false
     let(:user) { create(:ta) }
-    context 'When TA is allowed to assign or unassign reviewers' do
+    context 'When TA is allowed to manage reviewers' do
       let(:user) { create(:ta, manage_assessments: true) }
-      it { is_expected.to pass :assign_reviewers? }
+      it { is_expected.to pass :manage? }
     end
     context 'When TA is not allowed to assign or unassign reviewers' do
-      it { is_expected.not_to pass :assign_reviewers? }
+      it { is_expected.not_to pass :manage? }
     end
   end
 end
