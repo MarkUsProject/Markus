1acfb622748a2214fadd3fd3aa295486aef00211
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-09-01 17:07:14 -0400

    controller: Combined 2 authorization into one in peer reviews controller

--- spec/policies/peer_review_policy_spec.rb
+++ spec/policies/peer_review_policy_spec.rb
@@ -3,20 +3,26 @@ describe PeerReviewPolicy do
   describe 'When the user is admin' do
     subject { described_class.new(user: user) }
     let(:user) { build(:admin) }
-    context 'Admin can manage peer reviews and reviewers' do
+    context 'Admin can manage reviewers' do
+      it { is_expected.to pass :manage_reviewers? }
+    end
+    context 'Admin can manage peer reviews' do
       it { is_expected.to pass :manage? }
     end
   end
-  describe 'When the user is TA' do
+  describe 'When the user is grader' do
     subject { described_class.new(user: user) }
     # By default all the grader permissions are set to false
     let(:user) { create(:ta) }
-    context 'When TA is allowed to manage reviewers' do
+    context 'When grader is allowed to manage reviewers' do
       let(:user) { create(:ta, manage_assessments: true) }
-      it { is_expected.to pass :manage? }
+      it { is_expected.to pass :manage_reviewers? }
     end
-    context 'When TA is not allowed to assign or unassign reviewers' do
-      it { is_expected.not_to pass :manage? }
+    context 'When grader is not allowed to assign or unassign reviewers' do
+      it { is_expected.not_to pass :manage_reviewers? }
+    end
+    context 'Grader cannot manage peer reviews' do
+      it { is_expected.not_to pass :manage?}
     end
   end
 end
