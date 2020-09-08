415b79677be06b78fa6d27c3ca185eb3157833d9
Misha Schwartz <misha.schwartz@gmail.com>
2020-09-08 13:13:30 -0400

    rspec: attempt to fix a flaky test

--- spec/controllers/api/groups_controller_spec.rb
+++ spec/controllers/api/groups_controller_spec.rb
@@ -478,15 +478,16 @@ describe Api::GroupsController do
       let(:grouping) { create :grouping, assignment: assignment }
       let(:submission) { create :version_used_submission, grouping: grouping }
       let(:submission_file) { create :submission_file, submission: submission }
-      let!(:annotation) { create :text_annotation, submission_file: submission_file }
+      let(:annotation) { create :text_annotation, submission_file: submission_file }
       let(:response_type) { 'application/xml' }
       before do
         request.env['HTTP_ACCEPT'] = response_type
+        annotation
         get :annotations, params: { assignment_id: assignment.id, id: grouping.id }
       end
       it 'should get annotations for the given group' do
         content = Hash.from_xml(response.body)
-        expect(content['annotations']['annotation']['content']).to eq annotation.annotation_text.content
+        expect(content.dig('annotations', 'annotation', 'content')).to eq annotation.annotation_text.content
       end
       it 'should respond with 200' do
         expect(response.status).to eq(200)
