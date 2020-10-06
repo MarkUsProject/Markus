184768a593bcd65d6a919db6949e9c0fe0dea988
ANUSHIYAPRIYA <59573462+ANUSHIYAPRIYA@users.noreply.github.com>
2020-10-05 20:35:13 -0400

    models: get all the grouping data except the fake student grouping

--- app/models/test_student.rb
+++ app/models/test_student.rb
@@ -1,5 +1,5 @@
+# TestStudent - fake student, used for running tests on solution files
 class TestStudent < User
-  # TestStudent - fake student, used for running tests on solution files
   has_many :accepted_groupings,
            -> { where 'memberships.membership_status' => [StudentMembership::STATUSES[:accepted], StudentMembership::STATUSES[:inviter]] },
            class_name: 'Grouping',
