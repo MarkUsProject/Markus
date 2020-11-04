5549673e1994bf17d0b07e394a0ea88bc04d5e6c
Ignas Panero Armoska <ignaspa@protonmail.com>
2020-11-04 02:01:34 -0500

    JSON: reformat theme colors object

--- app/assets/javascripts/theme_colors.js
+++ app/assets/javascripts/theme_colors.js
@@ -1,50 +1,52 @@
-export const dark_mode_colors = {
-  primary_one: '#67abfa',
-  primary_two: '#243427',
-  primary_three: '#75a278',
-  background_main: '#161616',
-  background_support: '#333d3e',
-  line: '#b1b1b1',
-  sharp_line: '#e5e5e5',
-  heavy_error: '#ff7575',
-  error_light: '#a20000',
-  heavy_alert: '#927c40',
-  light_alert: '#48372c',
-  annotation_holder: '#48372c',
-  heavy_success: '#b9e586',
-  light_success: '#154100',
-  comments_color: 'var(--heavy_success)',
-  key_words_color: 'var(--primary_one)',
-  strings_color: 'var(--heavy_error)'
-};
-export const light_mode_colors = {
-  primary_one: '#245185',
-  primary_two: '#cee3ea',
-  primary_three: '#4c7c51',
-  background_main: '#fff',
-  background_support: '#cee3ea',
-  line: '#464646',
-  sharp_line: '#000',
-  heavy_error: '#a20000',
-  error_light: '#ffc2c2',
-  heavy_alert: '#ffd452',
-  annotation_holder: '#ffd452',
-  light_alert: '#ffe9ac',
-  heavy_success: '#246700',
-  light_success: '#b9e586',
-  comments_color: 'var(--heavy_success)',
-  key_words_color: 'var(--primary_one)',
-  strings_color: 'var(--heavy_error)',
+export const themes = {
+  dark: {
+    annotation_holder: '#48372c',
+    background_main: '#161616',
+    background_support: '#333d3e',
+    comments_color: 'var(--heavy_success)',
+    error_light: '#a20000',
+    heavy_alert: '#927c40',
+    heavy_error: '#ff7575',
+    heavy_success: '#b9e586',
+    key_words_color: 'var(--primary_one)',
+    light_alert: '#48372c',
+    light_success: '#154100',
+    line: '#b1b1b1',
+    primary_one: '#67abfa',
+    primary_two: '#243427',
+    primary_three: '#75a278',
+    sharp_line: '#e5e5e5',
+    strings_color: 'var(--heavy_error)'
+  },
+  light: {
+    annotation_holder: '#ffd452',
+    background_main: '#fff',
+    background_support: '#cee3ea',
+    comments_color: 'var(--heavy_success)',
+    error_light: '#ffc2c2',
+    heavy_alert: '#ffd452',
+    heavy_error: '#a20000',
+    heavy_success: '#246700',
+    key_words_color: 'var(--primary_one)',
+    light_alert: '#ffe9ac',
+    light_success: '#b9e586',
+    line: '#464646',
+    primary_one: '#245185',
+    primary_two: '#cee3ea',
+    primary_three: '#4c7c51',
+    sharp_line: '#000',
+    strings_color: 'var(--heavy_error)',
+  }
 };
 
 export function set_theme(theme) {
   if (theme === 'dark') {
-    Object.keys(dark_mode_colors).forEach(color => {
-      document.documentElement.style.setProperty('--' + color, dark_mode_colors[color]);
+    Object.keys(themes['dark']).forEach(color => {
+      document.documentElement.style.setProperty('--' + color, themes['dark'][color]);
     });
   } else {
-    Object.keys(light_mode_colors).forEach(color => {
-      document.documentElement.style.setProperty('--' + color, light_mode_colors[color]);
+    Object.keys(themes['light']).forEach(color => {
+      document.documentElement.style.setProperty('--' + color, themes['light'][color]);
     });
   }
-};
+}
