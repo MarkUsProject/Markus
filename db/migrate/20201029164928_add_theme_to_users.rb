c962143de6655d0ee8dee6cf77c4b9f44f696325
Ignas Panero Armoska <ignaspa@protonmail.com>
2020-11-12 13:31:16 -0500

    migration: change user theme to enum

--- db/migrate/20201029164928_add_theme_to_users.rb
+++ db/migrate/20201029164928_add_theme_to_users.rb
@@ -1,5 +1,5 @@
 class AddThemeToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :theme, :string, default: 'light', null: false
+    add_column :users, :theme, :integer, default: 1, null: false
   end
 end
