470d3b941c175d08f782f44cc2e9097dc5b5c85b
Robert Tan <robert.tan@mail.utoronto.ca>
2020-11-26 16:27:36 +0000

    Hound style comments

--- spec/models/grouping_spec.rb
+++ spec/models/grouping_spec.rb
@@ -1061,13 +1061,21 @@ describe Grouping do
         end
 
         it 'returns false when before section due date' do
-          SectionDueDate.create(section: @section, assignment: @assignment, due_date: Time.zone.parse('July 24 2009 5:00PM'))
+          SectionDueDate.create(
+              section: @section,
+              assignment: @assignment,
+              due_date: Time.zone.parse('July 24 2009 5:00PM')
+          )
           submit_file_at_time(@assignment, @group, 'test', 'July 20 2009 5:00PM', 'my_file', 'Hello, World!')
           expect(@grouping.submitted_after_collection_date?).to be false
         end
 
         it 'returns false when after section duedate' do
-          SectionDueDate.create(section: @section, assignment: @assignment, due_date: Time.zone.parse('July 18 2009 5:00PM'))
+          SectionDueDate.create(
+              section: @section,
+              assignment: @assignment,
+              due_date: Time.zone.parse('July 18 2009 5:00PM')
+          )
           submit_file_at_time(@assignment, @group, 'test', 'July 20 2009 5:00PM', 'my_file', 'Hello, World!')
           expect(@grouping.reload.submitted_after_collection_date?).to be true
         end
@@ -1100,13 +1108,21 @@ describe Grouping do
         end
 
         it 'returns false when before section due_date' do
-          SectionDueDate.create(section: @section, assignment: @assignment, due_date: Time.zone.parse('July 30 2009 5:00PM'))
+          SectionDueDate.create(
+              section: @section,
+              assignment: @assignment,
+              due_date: Time.zone.parse('July 30 2009 5:00PM')
+          )
           submit_file_at_time(@assignment, @group, 'test', 'July 28 2009 1:00PM', 'my_file', 'Hello, World!')
           expect(@grouping.reload.submitted_after_collection_date?).to be false
         end
 
         it 'returns true when after section due_date' do
-          SectionDueDate.create(section: @section, assignment: @assignment, due_date: Time.zone.parse('July 20 2009 5:00PM'))
+          SectionDueDate.create(
+              section: @section,
+              assignment: @assignment,
+              due_date: Time.zone.parse('July 20 2009 5:00PM')
+          )
           submit_file_at_time(@assignment, @group, 'test', 'July 28 2009 1:00PM', 'my_file', 'Hello, World!')
           expect(@grouping.submitted_after_collection_date?).to be true
         end
