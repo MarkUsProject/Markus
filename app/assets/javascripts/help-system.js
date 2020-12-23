1ab7f167737af45315b7e91aef19e19e6cb43b86
david-yz-liu <david@cs.toronto.edu>
2020-12-23 11:15:45 -0500

    css: Refactor help system css and grader permissions css

--- app/assets/javascripts/help-system.js
+++ app/assets/javascripts/help-system.js
@@ -1,12 +1,5 @@
 $(document).ready(() => {
-  $('.help-message-box').hide();
-  $('.help-message-title').hide();
-  $('.help-break').hide();
-
-  $('.help, .title-help').click((event) => {
-    const help_section = $(event.currentTarget).attr('class').split(' ')[1];
-    $('.help-message-box').filter('.' + help_section).toggle();
-    $('.help-message-title').filter('.' + help_section).toggle();
-    $('.help-break').filter('.' + help_section).toggle();
+  $('.help, .title-help, .inline-help').click(event => {
+    $(event.currentTarget).children().toggle();
   });
 });
