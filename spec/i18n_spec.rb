602fc8ebb610564edfcd20419c92a1b75b313e98
Misha Schwartz <misha.schwartz@gmail.com>
2021-01-05 09:13:20 -0500

    rspec: add test for normalized keys

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'I18n' do
   let(:i18n) { I18n::Tasks::BaseTask.new }
   let(:missing_keys) { i18n.missing_keys(locales: ['en']) }
   let(:unused_keys) { i18n.unused_keys }
+  let(:non_normalized) { i18n.non_normalized_paths }
 
   it 'does not have missing English keys' do
     expect(missing_keys)
@@ -16,5 +17,10 @@ RSpec.describe 'I18n' do
   it 'does not have unused keys' do
     expect(unused_keys).to be_empty,
                            "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+  end
+  it 'has normalized keys' do
+    expect(non_normalized).to be_empty,
+                              "#{non_normalized.count} non normalized keys, run `i18n-tasks health' to show them."
+                              " run `i18n-tasks normalize' to fix them"
   end
 end
