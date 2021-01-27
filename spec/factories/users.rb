2047305ea392f2ee6239b2a0fdece013228136a4
Mike Kang <mike.kang@mail.utoronto.ca>
2021-01-27 17:42:57 -0500

    Changed how fakerbot creates username so that separators only use underscore and hyphen. If we don't do this, then fakerbot will create an illegal useranme from time to time with the usage of apostrophes.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:user_name) { |n| Faker::Internet.user_name + n.to_s }
+    sequence(:user_name) { |n| Faker::Internet.username(separators: %w(_-)) }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
