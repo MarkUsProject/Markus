4bca212011e361ff0fc872ccf6ba40036cb6af72
Mike Kang <mike.kang@mail.utoronto.ca>
2021-01-27 18:00:16 -0500

    fixes for hound changes again

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,7 @@ class User < ApplicationRecord
   validates_uniqueness_of   :id_number, :allow_nil => true
   validates_inclusion_of    :time_zone, :in => ActiveSupport::TimeZone.all.map(&:name)
   validates                 :user_name, format: { with: /\A[a-zA-Z0-9\-_]+\z/,
-                                                  message: 'user_name must be alphanumeric, a hyphen, or an underscore'
-                                                }
-
+                                                  message: 'user_name must be alphanumeric, hyphen, or underscore' }
   after_initialize :set_display_name, :set_time_zone
 
   validates_format_of       :type,          with: /\AStudent|Admin|Ta|TestServer\z/
