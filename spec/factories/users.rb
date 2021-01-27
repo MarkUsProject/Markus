079207f8399882f23ff64c21cdd3ed5a07b5418a
Mike Kang <mike.kang@mail.utoronto.ca>
2021-01-27 17:56:10 -0500

    fixed sylistic changes that hound was barking about.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:user_name) { |n| Faker::Internet.username(separators: %w(_-)) }
+    sequence(:user_name) { Faker::Internet.username(separators: %w[_ -]) }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
