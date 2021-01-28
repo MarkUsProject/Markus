ad767edb1711c76361c7025edb4afec1606e528e
Mike Kang <mike.kang@mail.utoronto.ca>
2021-01-28 00:43:44 -0500

    restored to original code and commiting before branch switch

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:user_name) { Faker::Internet.username(separators: %w[_ -]) }
+    sequence(:user_name) { |n| Faker::Internet.user_name + n.to_s }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
