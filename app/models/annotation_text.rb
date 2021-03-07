a4dc820f29eac268fd00835875089c73b52f408e
Jonathan Chen <jc09310@gmail.com>
2021-03-06 22:36:34 -0500

    Tried to fix indentation Hound issue

--- app/models/annotation_text.rb
+++ app/models/annotation_text.rb
@@ -5,7 +5,7 @@ class AnnotationText < ApplicationRecord
 
   after_update :update_mark_deductions,
                unless: ->(t) {
-                  t.annotation_category.nil? || t.annotation_category.changes_to_save.key?('flexible_criterion_id')
+                          t.annotation_category.nil? || t.annotation_category.changes_to_save.key?('flexible_criterion_id')
                        }
   before_update :check_if_released, unless: ->(t) { t.deduction.nil? }
   before_destroy :check_if_released, unless: ->(t) { t.deduction.nil? }
