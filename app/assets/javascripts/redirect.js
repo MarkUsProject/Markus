65a49b0c2119ed4bb118f05450ff0865f396d622
Misha Schwartz <misha.schwartz@gmail.com>
2021-04-12 11:19:41 -0400

    js: add callback after session expired modal is rendered

--- app/assets/javascripts/redirect.js
+++ app/assets/javascripts/redirect.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
   $(document).ajaxError((event, xhr) => {
     if (xhr.status === 403) {
-      session_expired_modal.open()
+      session_expired_modal.open();
+      $('#session-expired-modal-close').click(function() {
+        session_expired_modal.close();
+        window.location.reload();
+      });
     }
-  });
-  $('#session-expired-modal-close').click(function() {
-    session_expired_modal.close();
-    window.location.reload();
   });
 });
