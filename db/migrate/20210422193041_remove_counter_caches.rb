b1d9765d83265ac5541097cabde4aef8d603bd7b
Misha Schwartz <misha.schwartz@gmail.com>
2021-04-22 17:03:37 -0400

    counter cache: remove annotation_texts_count

--- db/migrate/20210422193041_remove_counter_caches.rb
+++ db/migrate/20210422193041_remove_counter_caches.rb
@@ -4,6 +4,7 @@ class RemoveCounterCaches < ActiveRecord::Migration[6.0]
     remove_column :groupings, :notes_count
     remove_column :assessments, :notes_count
     remove_column :assessments, :groupings_count
+    remove_column :annotation_categories, :annotation_texts_count
   end
 
   def self.down
@@ -11,5 +12,6 @@ class RemoveCounterCaches < ActiveRecord::Migration[6.0]
     add_column :groupings, :notes_count, :integer, default: 0
     add_column :assessments, :notes_count, :integer, default: 0
     add_column :assessments, :groupings_count, :integer, default: 0
+    add_column :annotation_categories, :annotation_texts_count, :integer, default: 0
   end
 end
