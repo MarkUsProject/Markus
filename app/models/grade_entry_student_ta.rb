31e6033d4d88e1f2be4499a0e3a42f98f00157e8
Alexander <xander4444@icloud.com>
2021-05-31 11:48:06 -0400

    fixed spacing

--- app/models/grade_entry_student_ta.rb
+++ app/models/grade_entry_student_ta.rb
@@ -14,7 +14,6 @@ class GradeEntryStudentTa < ApplicationRecord
     grade_entry_student_ids =
       GradeEntryStudent.where(id: Array(grade_entry_student_ids)).pluck(:id)
     ta_ids = Ta.where(id: Array(ta_ids)).pluck(:id)
-
     # Get all existing associations to avoid violating the unique constraint.
     existing_values = GradeEntryStudentTa
       .where(grade_entry_student_id: grade_entry_student_ids, ta_id: ta_ids)
