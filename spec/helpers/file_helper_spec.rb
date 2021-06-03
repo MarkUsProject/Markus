71f49f664338cbd33f31dbe3ede048c9c84adf03
Alexander <xander4444@icloud.com>
2021-06-03 17:01:35 -0400

    added tests for get_file_type and get_file_comment

--- spec/helpers/file_helper_spec.rb
+++ spec/helpers/file_helper_spec.rb
@@ -35,4 +35,108 @@ describe FileHelper do
       end
     end
   end
+
+  context 'A .java Submission file' do
+    it 'return java' do
+      expect(FileHelper.get_file_type('filename.java')).to eq('java')
+    end
+    it 'return java comment' do
+      expect(FileHelper.get_comment_syntax('filename.java')).to eq(%w[/* */])
+    end
+  end
+
+  context 'A .rb Submission file' do
+    it 'return ruby' do
+      expect(FileHelper.get_file_type("filename.rb")).to eq('ruby')
+    end
+    it 'return ruby comment' do
+      expect(FileHelper.get_comment_syntax("filename.rb")).to eq(%W[=begin\n \n=end])
+    end
+  end
+
+  context 'A .py Submission file' do
+    it 'return python' do
+      expect(FileHelper.get_file_type('filename.py')).to eq('python')
+    end
+    it 'return python comment' do
+      expect(FileHelper.get_comment_syntax('filename.py')).to eq(%w[""" """])
+    end
+  end
+
+  context 'A .js Submission file' do
+    it 'return javascript' do
+      expect(FileHelper.get_file_type('filename.js')).to eq('javascript')
+    end
+    it 'return javascript comment' do
+      expect(FileHelper.get_comment_syntax('filename.js')).to eq(%w[/* */])
+    end
+  end
+
+  context 'A .html Submission file' do
+    it 'return html' do
+      expect(FileHelper.get_file_type('filename.html')).to eq('html')
+    end
+    it 'return html comment' do
+      expect(FileHelper.get_comment_syntax('filename.html')).to eq(%w[<!-- -->])
+    end
+  end
+
+  context 'A .css Submission file' do
+    it 'return css' do
+      expect(FileHelper.get_file_type('filename.css')).to eq('css')
+    end
+    it 'return css comment' do
+      expect(FileHelper.get_comment_syntax('filename.css')).to eq(%w[/* */])
+    end
+  end
+
+  context 'A .c Submission file' do
+    it 'return c' do
+      expect(FileHelper.get_file_type('filename.c')).to eq('c')
+    end
+    it 'return c comment' do
+      expect(FileHelper.get_comment_syntax('filename.c')).to eq(%w[/* */])
+    end
+  end
+
+  context 'A .tex Submission file' do
+    it 'return tex' do
+      expect(FileHelper.get_file_type('filename.tex')).to eq('tex')
+    end
+
+    it 'return a tex comment' do
+      expect(FileHelper.get_comment_syntax('filename.tex')).to eq(%w[## ##])
+    end
+  end
+
+  context 'A no extension Submission file' do
+    it 'return a unknown file extension' do
+      expect(FileHelper.get_file_type('filename')).to eq('unknown')
+    end
+    it 'return generic comment' do
+      expect(FileHelper.get_comment_syntax('filename')).to eq(%w[## ##])
+    end
+  end
+
+  context 'A supported image' do
+    before(:each) do
+      # currently supported formats: ['.jpeg', '.jpg', '.gif', '.png']
+      @jpeg_file = 'filename.jpeg'
+      @jpg_file = 'filename.jpg'
+      @gif_file = 'filename.gif'
+      @png_file = 'filename.png'
+      @heic_file = 'filename.heic'
+      @heif_file = 'filename.heif'
+      @unsupported_file = 'filename.bmp'
+    end
+
+    it 'returns \'image\' when checking file type' do
+      expect(FileHelper.get_file_type(@jpeg_file)).to eq 'image'
+      expect(FileHelper.get_file_type(@jpg_file)).to eq 'image'
+      expect(FileHelper.get_file_type(@gif_file)).to eq 'image'
+      expect(FileHelper.get_file_type(@png_file)).to eq 'image'
+      expect(FileHelper.get_file_type(@heic_file)).to eq 'image'
+      expect(FileHelper.get_file_type(@heif_file)).to eq 'image'
+    end
+  end
 end
