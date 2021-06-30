8fe9426d81b687a41e9d0b3bf18f634ec21f913e
Alexander <xander4444@icloud.com>
2021-06-29 20:14:34 -0400

    fixed lines and time zone error

--- spec/controllers/automated_tests_controller_spec.rb
+++ spec/controllers/automated_tests_controller_spec.rb
@@ -90,14 +90,14 @@ describe AutomatedTestsController do
       end
       context 'files data' do
         it 'should include assignment files' do
-          expected_time = Time.new(2021)
+          current_time = Time.new(2021)
           allow_any_instance_of(Assignment).to receive(:autotest_files).and_return ['file.txt']
           allow_any_instance_of(Pathname).to receive(:exist?).and_return true
-          allow(File).to receive(:mtime).and_return expected_time
-          allow_any_instance_of(SessionHandler).to receive(:time_zone).and_return "Eastern Time (US & Canada)"
+          allow(File).to receive(:mtime).and_return current_time
           subject
           url = download_file_assignment_automated_tests_url(assignment_id: assignment.id, file_name: 'file.txt')
-          data = [{ key: 'file.txt', submitted_date: I18n.l(expected_time.in_time_zone("Eastern Time (US & Canada)")), size: 1, url: url }.transform_keys(&:to_s)]
+          data = [{ key: 'file.txt', submitted_date: I18n.l(current_time),
+                    size: 1, url: url }.transform_keys(&:to_s)]
           expect(JSON.parse(response.body)['files']).to eq(data)
         end
         it 'should include directories' do
@@ -108,8 +108,8 @@ describe AutomatedTestsController do
           expect(JSON.parse(response.body)['files']).to eq(data)
         end
         it 'should include nested files' do
-          expected_time = Time.new(2021)
-          allow(File).to receive(:mtime).and_return expected_time
+          current_time = Time.new(2021)
+          allow(File).to receive(:mtime).and_return current_time
           allow_any_instance_of(Assignment).to receive(:autotest_files).and_return %w[some_dir some_dir/file.txt]
           allow_any_instance_of(Pathname).to receive(:exist?).and_return true
           allow_any_instance_of(Pathname).to receive(:directory?).and_wrap_original do |m, *_args|
@@ -118,8 +118,8 @@ describe AutomatedTestsController do
           subject
           url = download_file_assignment_automated_tests_url(assignment_id: assignment.id,
                                                              file_name: 'some_dir/file.txt')
-          data = [{ key: 'some_dir/' }, { key: 'some_dir/file.txt',
-                                          submitted_date: I18n.l(expected_time), size: 1, url: url }]
+          data = [{ key: 'some_dir/' }, { key: 'some_dir/file.txt', submitted_date: I18n.l(current_time),
+                                          size: 1, url: url }]
           expect(JSON.parse(response.body)['files']).to eq(data.map { |h| h.transform_keys(&:to_s) })
         end
       end
