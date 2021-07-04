18e779aeb563af7ecdb7566c816d6e19cbf69856
Junru Lin <junru.lin@mail.utoronto.ca>
2021-07-04 10:41:09 +0800

    creating grade_distribution_graph_data method

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -309,6 +309,39 @@ class AssignmentsController < ApplicationController
     end
   end
 
+  # Return the chart data of assignment grade distributions
+  def grade_distribution_graph_data
+    assignment = Assignment.find(params[:id])
+    labels = []
+    (0..19).each { |i| labels.push((5 * i).to_s + '-' + (5 * i + 5).to_s) }
+    datasets = [
+      {
+        label: '2',
+        data: assignment.grade_distribution_array(),
+        backgroundColor: [
+          'rgba(36, 81, 133, 1)',
+        ],
+        borderColor: [
+          'rgba(36, 81, 133, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ]
+    data = {labels: labels, datasets: datasets}
+    options = {
+      scales: {
+        xAxes: [{
+              ticks: {
+                min: 0,
+                max: 100,
+                stepSize: 10,
+              }
+            }]
+      }
+    }
+    render json: {data: data, options: options}
+  end
+
   # Refreshes the grade distribution graph
   def refresh_graph
     @assignment = Assignment.find(params[:id])
