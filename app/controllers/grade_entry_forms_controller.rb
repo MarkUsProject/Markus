91261fe3314ef96bb69e897c5f55cdd3b5fdb5ca
Alexander <xander4444@icloud.com>
2021-07-12 13:24:34 -0400

    resolved houndbot comments

--- app/controllers/grade_entry_forms_controller.rb
+++ app/controllers/grade_entry_forms_controller.rb
@@ -226,7 +226,7 @@ class GradeEntryFormsController < ApplicationController
 
   def grade_distribution_data
     new_labels = ['0 - 5']
-    new_labels += ((1..19).map {|i| (i * 5 + 1).to_s + ' - ' + (i * 5 + 5).to_s})
+    new_labels += ((1..19).map { |i| (i * 5 + 1).to_s + ' - ' + (i * 5 + 5).to_s })
     grade_entry_form = GradeEntryForm.find(params[:id])
     render json: { grade_distribution: grade_entry_form.grade_distribution_array, labels: new_labels }
   end
