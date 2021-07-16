f487fe40329ad2b89a062ebfe42f808fba53ae07
Misha Schwartz <misha.schwartz@gmail.com>
2021-07-16 08:55:24 -0400

    style: style fixes

--- app/helpers/routing_helper.rb
+++ app/helpers/routing_helper.rb
@@ -1,5 +1,5 @@
+# Helpers for routing related tasks
 module RoutingHelper
-
   # Return the request referer parsed as a hash that can be passed directly to
   # redirect_to. If the referer is not a url that can be redirected to, return
   # an empty hash.
@@ -7,7 +7,7 @@ module RoutingHelper
     referer_url = request.referer || ''
     referer = URI(referer_url)
     relative_url = Rails.application.config.action_controller.relative_url_root
-    referer.path = referer.path.gsub(/^#{relative_url}\/?/, '/')
+    referer.path = referer.path.gsub(%r{^#{relative_url}/?}, '/')
     begin
       options = Rails.application.routes.recognize_path(referer.to_s)
     rescue ActionController::RoutingError
