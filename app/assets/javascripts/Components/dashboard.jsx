8c99006916b415d78a64182f4017cd3ef0ed7ba6
Junru Lin <junru.lin@mail.utoronto.ca>
2021-07-19 17:44:00 +0800

    improve the code based on the feedbacks

--- app/assets/javascripts/Components/dashboard.jsx
+++ app/assets/javascripts/Components/dashboard.jsx
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 import { Bar } from 'react-chartjs-2';
 
 
-class Assignment extends React.Component {
+class AssignmentChart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -18,67 +18,59 @@ class Assignment extends React.Component {
         groupings_size: null
       },
       data: { // Fake data, from react-chartjs-2/example/src/charts/GroupedBar.js
-        labels: ['1', '2', '3', '4', '5', '6'],
-        datasets: [
-          {
-            label: '# of Red Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: 'rgb(255, 99, 132)',
-          },
-          {
-            label: '# of Blue Votes',
-            data: [2, 3, 20, 5, 1, 4],
-            backgroundColor: 'rgb(54, 162, 235)',
-          },
-          {
-            label: '# of Green Votes',
-            data: [3, 10, 13, 15, 22, 30],
-            backgroundColor: 'rgb(75, 192, 192)',
-          },
-        ],
+        labels: [],
+        datasets: [],
       }
     }
   }
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.assessment_id !== this.props.assessment_id) {
-      $.ajax({
-        url: Routes.chart_data_assignment_path(this.props.assessment_id),
-        dataType: 'json',
-      }).then(res => this.setState({data: res.data, summary: res.summary}))
+      this.fetchData();
     }
   }
 
+  fetchData = () => {
+    $.ajax({
+      url: Routes.chart_data_assignment_path(this.props.assessment_id),
+      dataType: 'json',
+    }).then(res => this.setState({data: res.data, summary: res.summary}))
+  };
+
   render() {
     return (
-    <div>
-      <div className='flex-row'>
-        <div id='assignment_<%= assignment.id %>_graph'>
-          <Bar data={this.state.data} />
-        </div>
-        <div className='flex-row-expand'>
-          <div className="grid-2-col">
-            <span> {I18n.t('average')} </span>
-            <span> {this.state.summary.average} </span>
-
-            <span> {I18n.t('median')} </span>
-            <span> {this.state.summary.median} </span>
-
-            <span> {I18n.t('assignments_submitted')} </span>
-            <span> {this.state.summary.num_submissions_collected} / {this.state.summary.groupings_size}</span>
-
-            <span> {I18n.t('assignments_graded')} </span>
-            <span> {this.state.summary.num_submissions_graded} / {this.state.summary.groupings_size}</span>
-
-            <span> {I18n.t('num_failed')} </span>
-            <span> {this.state.summary.num_fails} </span>
-
-            <span> {I18n.t('num_zeros')} </span>
-            <span> {this.state.summary.num_zeros} </span>
+      <div>
+        <div className='flex-row'>
+          <div id='assignment_<%= assignment.id %>_graph'>
+            <Bar data={this.state.data} />
           </div>
+          <div className='flex-row-expand'>
+            <div className="grid-2-col">
+              <span> {I18n.t('average')} </span>
+              <span> {this.state.summary.average} </span>
+
+              <span> {I18n.t('median')} </span>
+              <span> {this.state.summary.median} </span>
+
+              <span> {I18n.t('assignments_submitted')} </span>
+              <span> {this.state.summary.num_submissions_collected} / {this.state.summary.groupings_size}</span>
+
+              <span> {I18n.t('assignments_graded')} </span>
+              <span> {this.state.summary.num_submissions_graded} / {this.state.summary.groupings_size}</span>
+
+              <span> {I18n.t('num_failed')} </span>
+              <span> {this.state.summary.num_fails} </span>
+
+              <span> {I18n.t('num_zeros')} </span>
+              <span> {this.state.summary.num_zeros} </span>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     );
   }
 }
@@ -181,12 +173,6 @@ class Dashboard extends React.Component {
         });
         // Commented this one out for now.
         // this.getGradeEntryFormColumnBreakdown();
-      } else if (this.state.assessment_type === 'Assignment') {
-        // TODO
-        // $.ajax({
-        //   url: Routes.grade_distribution_graph_data_assignment_path(this.state.assessment_id),
-        //   dataType: 'json',
-        // }).then(res => this.setState({data: res}))
       }
     }
   }
@@ -195,7 +181,7 @@ class Dashboard extends React.Component {
     if (this.state.display_course_summary) {
       return <Bar data={this.state.data} />;
     } else if (this.state.assessment_type === 'Assignment') {
-      return <Assignment assessment_id={this.state.assessment_id}/>;
+      return <AssignmentChart assessment_id={this.state.assessment_id}/>;
     } else if (this.state.assessment_type === 'GradeEntryForm') {
       return (
         <div>
