32bfc228567a4692ecbebc15ada12d0a0b5c69a4
David Liu <david@cs.toronto.edu>
2021-08-15 21:47:50 -0400

    Update width, height, and legend style for DataChart

--- app/assets/javascripts/Components/Helpers/data_chart.jsx
+++ app/assets/javascripts/Components/Helpers/data_chart.jsx
@@ -3,12 +3,20 @@ import {render} from 'react-dom';
 import { Bar } from 'react-chartjs-2';
 
 export class DataChart extends React.Component {
+  static defaultProps = {
+    legend: true,
+    width: 'auto',
+    height: 500
+  };
 
   render() {
     let options = {
-      responsive: false,
-      legend: {
-        display: this.props.legend
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: this.props.legend
+        },
       },
       scales: {
         y: {
@@ -40,7 +48,13 @@ export class DataChart extends React.Component {
     };
 
     return (
-        <Bar id={'term_marks'} data={this.props} options={options} width={this.props.width} height={500} display={'inline-flex'} margin={'10px'}/>
+        <Bar
+          id={'term_marks'}
+          data={this.props}
+          options={options}
+          width={this.props.width}
+          height={this.props.height}
+          style={{display: 'inline-flex', margin: '10px'}}/>
     );
   }
 }
