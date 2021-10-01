b0adf1a21933f91a62f4fc89330cf0626fd92de5
mshwed <matt@jumboprivacy.com>
2021-10-01 16:00:02 -0400

    autotest: updated criterion id for autotest test groups

--- app/helpers/automated_tests_helper.rb
+++ app/helpers/automated_tests_helper.rb
@@ -35,6 +35,7 @@ module AutomatedTestsHelper
   end
 
   def update_test_groups_from_specs(assignment, test_specs)
+    criterion = Criterion.find_by assessment_id: assignment.id
     test_specs_path = assignment.autotest_settings_file
     # create/modify test groups based on the autotest specs
     test_group_ids = []
@@ -49,7 +50,7 @@ module AutomatedTestsHelper
           test_group_id = extra_data_specs['test_group_id']
           display_output = extra_data_specs['display_output'] || TestGroup.display_outputs.keys.first
           test_group_name = extra_data_specs['name'] || TestGroup.model_name.human
-          criterion_id = nil
+          criterion_id = criterion.id
           unless extra_data_specs['criterion'].nil?
             criterion_id = criteria_map[extra_data_specs['criterion']]
             if criterion_id.nil?
