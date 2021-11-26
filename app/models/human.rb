559188753d60f55422c0dee5648b937874b8a733
Samuel Maldonado <sam.maldonado@cognitivescience.co>
2021-11-26 12:15:36 -0500

    update layouts for course landing page and add tests for human.visible_courses

--- app/models/human.rb
+++ app/models/human.rb
@@ -2,9 +2,9 @@
 class Human < User
   has_many :roles, foreign_key: :user_id, inverse_of: :human
   validates_presence_of :roles, unless: -> { self.new_record? }
+  has_many :courses, through: :roles
 
   def visible_courses
-    courses = Course.joins(:roles).where('roles.id': self.roles)
-    courses.where.not('roles.type': 'Student').or(courses.where('courses.is_hidden': false, 'roles.hidden': false))
+    self.courses.where.not('roles.type': 'Student').or(self.courses.where('courses.is_hidden': false, 'roles.hidden': false))
   end
 end
