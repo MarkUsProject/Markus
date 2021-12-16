11cd341cc0ab9a5fa32bf06d9adbe3a4ab211776
Gabriel Anover <gabino6821@gmail.com>
2021-12-16 11:28:37 -0500

    Fixed houndbot comments

--- app/assets/javascripts/Components/Result/url_viewer.jsx
+++ app/assets/javascripts/Components/Result/url_viewer.jsx
@@ -15,9 +15,9 @@ export class URLViewer extends React.Component {
 
   render() {
     return (
-      <div id="url_container" key={"url_container"}>
+      <div id="url-container" key={"url_container"}>
         <iframe
-          id="url_display"
+          id="url-display"
           src={this.state.url}
           allowFullScreen
         />
