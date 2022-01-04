9e29c23b2ac68b2433609b73c764887f4af6bcff
Gabriel Anover <gabino6821@gmail.com>
2022-01-04 13:11:07 -0500

    Removed unnecessary code in URLViewer

--- app/assets/javascripts/Components/Result/url_viewer.jsx
+++ app/assets/javascripts/Components/Result/url_viewer.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
 export class URLViewer extends React.Component {
-  state = {
-    url: "https://www.youtube.com/embed/dQw4w9WgXcQ", // Sample Youtube embed link
-    //url: "https://drive.google.com/file/d/1yQh8bzo4IzmtSt-qZ0Vb-BzzEXsp45v_/preview" // sample google drive embed link
-    //url: "https://play.library.utoronto.ca/watch/4493e554354a7e51bdf90f3197185b5f" // sample mymedia link
-  };
-
-  componentDidMount() {
-    //TODO: Uncomment when url backend is working
-    //this.setState({
-    //  url: this.props.url
-    //})
+  constructor() {
+    super();
+    //TODO: Remove state url and replace its usage with url props when backend is working
+    this.state = {
+      url: "https://www.youtube.com/embed/dQw4w9WgXcQ", // Sample Youtube embed link
+      //url: "https://drive.google.com/file/d/1yQh8bzo4IzmtSt-qZ0Vb-BzzEXsp45v_/preview" // sample google drive embed link
+      //url: "https://play.library.utoronto.ca/watch/4493e554354a7e51bdf90f3197185b5f" // sample mymedia link
+    };
   }
 
   render() {
