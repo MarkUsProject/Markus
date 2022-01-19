88b3ca94fa8efd3c02582b0ce37a3683c4769232
David Liu <david@cs.toronto.edu>
2022-01-19 21:09:21 +0000

    responders: Fix configuration for responders gem (#5764)
    * role error: add interpolation options for instructors and update tests
    
    Co-authored-by: Misha Schwartz <misha.schwartz@gmail.com>

--- config/initializers/responders.rb
+++ config/initializers/responders.rb
@@ -1,4 +1,4 @@
 # Configuration for the 'responders' gem
 
 Rails.application.config.app_generators.scaffold_controller :responders_controller
-Responders::FlashResponder.flash_keys = [:success, :failure]
+Responders::FlashResponder.flash_keys = [:success, :error]
