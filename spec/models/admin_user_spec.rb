d0b5f0756a759b6322849a6eb3e55e4c368b8af8
Ziyuan (Jerry) Zhang <zyjerryzhang@gmail.com>
2022-01-25 16:42:46 -0500

    add tests for existing admin, use constant in AdminUser

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,10 +1,22 @@
 describe AdminUser do
-  let(:testuser) { AdminUser.find_or_create }
   it 'return type should be User' do
+    testuser = AdminUser.find_or_create
     expect(testuser).to be_a_kind_of(User)
   end
-  it 'first_name and last_name should equal to the specified values' do
-    expect(testuser.first_name).to eq('admin')
-    expect(testuser.last_name).to eq('admin')
+  context 'no existing admin' do
+    it 'first_name and last_name should equal to the specified values' do
+      testuser = AdminUser.find_or_create
+      expect(testuser.first_name).to eq('admin')
+      expect(testuser.last_name).to eq('admin')
+    end
+  end
+  context 'existing admin' do
+    it 'first_name and last_name should ot be changed for existing admin' do
+      adminuser = AdminUser.create(user_name: AdminUser::ADMIN_USERNAME, first_name: 'jerry', last_name: 'foo')
+      testuser = AdminUser.find_or_create
+      expect(testuser.first_name).to eq('jerry')
+      expect(testuser.last_name).to eq('foo')
+      expect(testuser).to eq(adminuser)
+    end
   end
 end
