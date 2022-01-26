eed95f7a78473a8391c9606b3f201c2f9f9f7308
Ziyuan (Jerry) Zhang <zyjerryzhang@gmail.com>
2022-01-26 18:42:50 -0500

    refactor tests, fix grammar error

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -4,15 +4,17 @@ describe AdminUser do
     it 'returns an instance of AdminUser' do
       expect(admin_user).to be_an_instance_of(AdminUser)
     end
-    it 'ensures the returned AdminUser api_key is not null' do
+    it 'ensures the returned AdminUser api_key is not nil' do
       expect(admin_user.api_key).not_to be(nil)
     end
     context 'when there is no existing AdminUser' do
-      it 'will create a new AdminUser' do
+      it 'creates a new AdminUser' do
         expect { AdminUser.find_or_create }.to change { AdminUser.count }.by(1)
       end
-      it 'will set first_name and last_name to the default values' do
+      it 'sets first_name to the default value' do
         expect(admin_user.first_name).to eq('admin')
+      end
+      it 'sets last_name to the default value' do
         expect(admin_user.last_name).to eq('admin')
       end
     end
@@ -20,16 +22,17 @@ describe AdminUser do
       let!(:existing_admin_user) do
         AdminUser.create(user_name: AdminUser::ADMIN_USERNAME, first_name: 'jerry', last_name: 'foo')
       end
-      let(:returned_user) { AdminUser.find_or_create }
-      it 'will not create a new AdminUser' do
+      it 'does not create a new AdminUser' do
         expect { AdminUser.find_or_create }.not_to(change { AdminUser.count })
       end
-      it 'will return the existing AdminUser' do
-        expect(returned_user).to eq(existing_admin_user)
+      it 'returns the existing AdminUser' do
+        expect(admin_user).to eq(existing_admin_user)
       end
-      it 'will leave first_name and last_name unchanged' do
-        expect(returned_user.first_name).to eq('jerry')
-        expect(returned_user.last_name).to eq('foo')
+      it 'leaves first_name unchanged' do
+        expect(admin_user.first_name).to eq('jerry')
+      end
+      it 'leaves last_name unchanged' do
+        expect(admin_user.last_name).to eq('foo')
       end
     end
   end
