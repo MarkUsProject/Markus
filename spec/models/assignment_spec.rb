611b17c2525460e20e5730b164f9ede4a3e48548
Sophie Sun <sophiesun2810@gmail.com>
2022-02-03 11:02:23 -0500

    unsave changes that should be on AddingTest branch

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -2227,41 +2227,6 @@ describe Assignment do
     end
   end
 
-  describe '#get_num_valid' do
-    before :each do
-      @assignment = create(:assignment, assignment_properties_attributes: { group_min: 2, group_max: 3 })
-      @groupings = Array.new(4) { create(:grouping, assignment: @assignment) }
-    end
-
-    context 'When two groups meet min size requirement' do
-      before :each do
-        create(:accepted_student_membership, grouping: @groupings.first)
-        create(:inviter_student_membership, grouping: @groupings.first)
-        create(:student_membership, grouping: @groupings.second)
-        create(:student_membership, grouping: @groupings.second)
-      end
-      it 'should return # of valid groups that meet size requirement' do
-        expect(@assignment.get_num_valid).to eq(2)
-      end
-    end
-    context 'When one group meets min size requirement and another is instructor approved' do
-      before :each do
-        create(:accepted_student_membership, grouping: @groupings.first)
-        create(:inviter_student_membership, grouping: @groupings.first)
-        @groupings.second.update_attribute(:instructor_approved, true)
-      end
-      it 'should return # of valid groups that meet size requirement or instructor approved' do
-        expect(@assignment.get_num_valid).to eq(2)
-      end
-    end
-
-    context 'When no group is valid' do
-      it 'should return # of valid groups' do
-        expect(@assignment.get_num_valid).to eq(0)
-      end
-    end
-  end
-
   describe '#get_num_marked' do
     let(:instructor) { create(:instructor) }
     let(:ta) { create(:ta) }
