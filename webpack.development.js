addc9bd43c67f28a2655a03c066dc0070788f07b
David Liu <david@cs.toronto.edu>
2022-02-06 23:31:39 -0500

    webpack: update webpack development source-map type

--- webpack.development.js
+++ webpack.development.js
@@ -3,7 +3,7 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "eval-source-map",
+  devtool: "inline-source-map",
   watch: true,
   watchOptions: {
     ignored: /node_modules/,
