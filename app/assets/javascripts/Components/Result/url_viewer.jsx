e499d65e593fa48116f935379bfc73cc74ad4277
Gabriel Anover <gabino6821@gmail.com>
2022-02-08 13:02:59 -0500

    Reworked URL view based on feedback

--- app/assets/javascripts/Components/Result/url_viewer.jsx
+++ app/assets/javascripts/Components/Result/url_viewer.jsx
@@ -4,8 +4,8 @@ export class URLViewer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: "",
       embeddedURL: "",
+      isInvalidUrl: false,
     };
   }
 
@@ -20,38 +20,49 @@ export class URLViewer extends React.Component {
   }
 
   configDisplay = () => {
+    const url = this.parseURL(this.props.externalUrl);
+    if (!url) {
+      this.setState({isInvalidUrl: true});
+      return;
+    }
+    switch (url.hostname) {
+      case "docs.google.com":
+      case "drive.google.com":
+        this.configureGoogleDrivePreview(url);
+        break;
+      case "www.youtube.com":
+      case "youtu.be":
+        this.configureOEmbedPreview(url.toString(), "https://www.youtube.com/oembed");
+        break;
+      default:
+        this.setState({embeddedURL: ""});
+    }
+    this.setState({isInvalidUrl: false});
+  };
+
+  /*
+   * Takes the content from a markusurl and checks to see if it is valid HTTP(S) url.
+   * Returns a url object if it is valid and nothing otherwise.
+   */
+  parseURL = urlStr => {
     try {
-      const url = new URL(this.props.externalUrl);
-      const youtube_view_is_set = this.configureOEmbedPreview(
-        url.toString(),
-        "https://www.youtube.com/oembed"
-      );
-      if (!youtube_view_is_set) {
-        switch (url.hostname) {
-          case "docs.google.com":
-          case "drive.google.com":
-            this.configureGoogleDrivePreview(url);
-            break;
-          default:
-            this.setState({embeddedURL: ""});
-        }
+      const url = new URL(urlStr);
+      if ((url.protocol === "http:" || url.protocol === "https:") && url.hostname !== "") {
+        return url;
       }
-      this.setState({url: this.props.externalUrl});
     } catch (e) {
-      this.setState({
-        url: "",
-        embeddedURL: "",
-      });
+      //Invalid URL
     }
   };
 
   configureGoogleDrivePreview = url => {
     const path = url.pathname.split("/");
     if (path[1] === "forms") {
-      url.pathname = url.pathname.replace(/(\/[^\/]+)$/, "/viewform");
+      path[path.length - 1] = "/viewform";
     } else {
-      url.pathname = url.pathname.replace(/(\/[^\/]+)$/, "/preview");
+      path[path.length - 1] = "/preview";
     }
+    url.pathname = path.join("/");
     this.setState({
       embeddedURL: url.toString(),
     });
@@ -64,43 +75,42 @@ export class URLViewer extends React.Component {
         this.setState({
           embeddedURL: match[1],
         });
-        return true;
       }
     });
-    return false;
   };
 
   renderPreviewDisplay = () => {
+    const errorMessage = message => {
+      return <div className="url-message-display">{message}</div>;
+    };
     if (this.state.embeddedURL !== "") {
       return (
         <iframe className="url-display" src={this.state.embeddedURL} allowFullScreen>
-          <div className="url-message-display">{I18n.t("submissions.url_display_error")}</div>
+          {errorMessage(I18n.t("submissions.url_display_error"))}
         </iframe>
       );
-    } else if (this.state.url !== "") {
-      const url_host = new URL(this.props.externalUrl).hostname;
-      return (
-        <div className="url-message-display">
-          {I18n.t("submissions.unsupported_url", {host: url_host})}
-        </div>
-      );
+    } else if (this.state.isInvalidUrl) {
+      return errorMessage(I18n.t("submissions.invalid_url", {item: "This"}));
+    } else {
+      return errorMessage(I18n.t("submissions.url_display_error"));
     }
   };
 
   render() {
-    if (this.state.url !== "") {
-      return (
-        <div className="url-container">
-          <div className="link-bar">
-            <a className="link-display" href={this.state.url} target="_blank">
-              {this.state.url}
+    return (
+      <div className="url-container">
+        <div className="link-display">
+          {/* Make Invalid URLs unclickable */}
+          {this.state.isInvalidUrl ? (
+            this.props.externalUrl
+          ) : (
+            <a href={this.props.externalUrl} target="_blank">
+              {this.props.externalUrl}
             </a>
-          </div>
-          <div className="display-area">{this.renderPreviewDisplay()}</div>
+          )}
         </div>
-      );
-    } else {
-      return <div className="url-message-display">{this.props.externalUrl}</div>;
-    }
+        <div className="display-area">{this.renderPreviewDisplay()}</div>
+      </div>
+    );
   }
 }
