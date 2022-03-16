29ab535a68307e89ea478c125c20495ad307ae52
Gabriel Anover <gabino6821@gmail.com>
2022-03-16 14:45:31 -0400

    Added courses list to settings page

--- app/javascript/application_webpack.js
+++ app/javascript/application_webpack.js
@@ -122,5 +122,7 @@ import {makeStarterFileManager} from "javascripts/Components/starter_file_manage
 window.makeStarterFileManager = makeStarterFileManager;
 import {makeNotesTable} from "javascripts/Components/notes_table";
 window.makeNotesTable = makeNotesTable;
+import {makeAdminCourseList} from "javascripts/Components/admin_course_list";
+window.makeAdminCourseList = makeAdminCourseList;
 import {makeCourseList} from "javascripts/Components/course_list";
 window.makeCourseList = makeCourseList;
