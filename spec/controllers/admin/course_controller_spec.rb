03c70d9f8ea09d7fba0b747a9e0c8f70daec63f6
Gabriel Anover <gabino6821@gmail.com>
2022-03-21 17:25:46 -0400

    Added tests for new changes

--- spec/controllers/admin/course_controller_spec.rb
+++ spec/controllers/admin/course_controller_spec.rb
@@ -1,8 +1,11 @@
 describe Admin::CoursesController do
   describe '#index' do
+    let!(:course1) { create(:course) }
+    let!(:course2) { create(:course) }
+    let!(:course3) { create(:course) }
     shared_examples 'user with unauthorized access' do
       it 'responds with 403' do
-        get_as user, :index, format: 'js'
+        get_as user, :index, format: 'json'
         expect(response).to have_http_status(403)
       end
     end
@@ -20,13 +23,48 @@ describe Admin::CoursesController do
     end
     context 'Admin' do
       let(:user) { create(:admin_user) }
-      it 'responds with 200 when sending javascript' do
-        get_as user, :index, format: 'js'
-        expect(response).to have_http_status(200)
+      context 'when sending javascript' do
+        it 'responds with 200' do
+          get_as user, :index, format: 'js'
+          expect(response).to have_http_status(200)
+        end
       end
-      it 'responds with 404 when sending html' do
-        get_as user, :index, format: 'html'
-        expect(response).to have_http_status(404)
+      context 'when sending html' do
+        it 'responds with 404' do
+          get_as user, :index, format: 'html'
+          expect(response).to have_http_status(404)
+        end
+      end
+      context 'when sending json' do
+        it 'responds with 200' do
+          get_as user, :index, format: 'json'
+          expect(response).to have_http_status(200)
+        end
+        it 'sends the appropriate data' do
+          get_as user, :index, format: 'json'
+          received_data = JSON.parse(response.body).map(&:symbolize_keys)
+          expected_data = [
+            {
+              id: course1.id,
+              name: course1.name,
+              display_name: course1.display_name,
+              is_hidden: course1.is_hidden
+            },
+            {
+              id: course2.id,
+              name: course2.name,
+              display_name: course2.display_name,
+              is_hidden: course2.is_hidden
+            },
+            {
+              id: course3.id,
+              name: course3.name,
+              display_name: course3.display_name,
+              is_hidden: course3.is_hidden
+            }
+          ]
+          expect(received_data).to match_array(expected_data)
+        end
       end
     end
   end
