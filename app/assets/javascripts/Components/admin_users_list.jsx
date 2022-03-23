61d36cc70466f6a486aa9dc79cbb4852e123d528
Gabriel Anover <gabino6821@gmail.com>
2022-03-23 19:03:04 -0400

    Created users react list and added it to admin/users index page

---
+++ app/assets/javascripts/Components/admin_users_list.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {render} from "react-dom";
+import ReactTable from "react-table";
+
+class AdminUsersList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
+    $.ajax({
+      url: Routes.admin_users_path(),
+      dataType: "json",
+    }).then(data => {
+      console.log(data);
+      this.setState({users: data, loading: false});
+    });
+  };
+
+  columns = [
+    {
+      Header: I18n.t("activerecord.attributes.user.user_name"),
+      accessor: "user_name",
+      id: "user_name",
+      minWidth: 120,
+    },
+    {
+      Header: I18n.t("activerecord.attributes.user.first_name"),
+      accessor: "first_name",
+      minWidth: 120,
+    },
+    {
+      Header: I18n.t("activerecord.attributes.user.last_name"),
+      accessor: "last_name",
+      minWidth: 120,
+    },
+    {
+      Header: I18n.t("activerecord.attributes.user.email"),
+      accessor: "email",
+      minWidth: 150,
+    },
+    {
+      Header: I18n.t("activerecord.attributes.user.user_type"),
+      accessor: "type",
+      minWidth: 90,
+    },
+    {
+      Header: I18n.t("activerecord.attributes.user.id_number"),
+      accessor: "id_number",
+      minWidth: 90,
+      className: "number",
+    },
+    {
+      Header: I18n.t("actions"),
+      accessor: "id",
+      minWidth: 70,
+      Cell: ({value}) => <a href="#">{I18n.t("edit")}</a>,
+      sortable: false,
+      filterable: false,
+    },
+  ];
+
+  render() {
+    return (
+      <ReactTable
+        data={this.state.users}
+        columns={this.columns}
+        filterable
+        defaultSorted={[{id: "user_name"}]}
+        loading={this.state.loading}
+      />
+    );
+  }
+}
+
+export function makeAdminUsersList(elem, props) {
+  render(<AdminUsersList {...props} />, elem);
+}
