85844e7b7465b1ca739f3ebfbf15eb6d262f7686
Gabriel Anover <gabino6821@gmail.com>
2022-04-05 18:55:13 -0400

    made appropriate changes to controller tests

--- spec/controllers/instructors_controller_spec.rb
+++ spec/controllers/instructors_controller_spec.rb
@@ -29,21 +29,21 @@ describe InstructorsController do
       it_behaves_like 'role is from a different course' do
         subject do
           post_as new_role, :create,
-                  params: { course_id: course.id, role: { end_user: { user_name: end_user.user_name } } }
+                  params: { course_id: course.id, role: { user: { user_name: end_user.user_name } } }
         end
       end
       it 'be able to create Instructor' do
         post_as instructor,
                 :create,
-                params: { course_id: course.id, role: { end_user: { user_name: end_user.user_name } } }
-        expect(course.instructors.joins(:end_user).where('users.user_name': end_user.user_name)).to exist
+                params: { course_id: course.id, role: { user: { user_name: end_user.user_name } } }
+        expect(course.instructors.joins(:user).where('users.user_name': end_user.user_name)).to exist
         expect(response).to redirect_to action: 'index'
       end
       context 'when a end_user does not exist' do
         let(:end_user) { build :end_user }
         subject do
           post_as instructor, :create,
-                  params: { course_id: course.id, role: { end_user: { user_name: end_user.user_name } } }
+                  params: { course_id: course.id, role: { user: { user_name: end_user.user_name } } }
         end
         it 'should not create a Ta' do
           instructor
@@ -59,12 +59,12 @@ describe InstructorsController do
       it_behaves_like 'role is from a different course' do
         subject do
           post_as new_role, :update,
-                  params: { course_id: course.id, id: role, role: { end_user: { user_name: end_user.user_name } } }
+                  params: { course_id: course.id, id: role, role: { user: { user_name: end_user.user_name } } }
         end
       end
       subject do
         post_as instructor, :update,
-                params: { course_id: course.id, id: role, role: { end_user: { user_name: new_end_user.user_name } } }
+                params: { course_id: course.id, id: role, role: { user: { user_name: new_end_user.user_name } } }
       end
       context 'when the new user exists' do
         let(:new_end_user) { create :end_user }
@@ -76,9 +76,9 @@ describe InstructorsController do
       context 'when the user does not exist' do
         let(:new_end_user) { build :end_user }
         it 'should not change the user' do
-          old_user = role.end_user
+          old_user = role.user
           subject
-          expect(role.reload.end_user).to eq(old_user)
+          expect(role.reload.user).to eq(old_user)
         end
         it 'should display an error message' do
           subject
