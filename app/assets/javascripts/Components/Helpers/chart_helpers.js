cffc223902073e082a342d4f30305a347988d348
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-04-06 17:50:02 -0400

    restore color settings

--- app/assets/javascripts/Components/Helpers/chart_helpers.js
+++ app/assets/javascripts/Components/Helpers/chart_helpers.js
@@ -3,18 +3,34 @@ import * as I18n from "i18n-js";
 export function chartScales() {
   return {
     x: {
+      grid: {
+        borderColor: "#98c7ff", // --primary_one
+        color: "#b1b1b140", // --gridline
+      },
       title: {
         display: true,
         text: I18n.t("main.grade"),
+        color: "#b1b1b1", // --line
       },
       min: 0,
       max: 100,
-      ticks: {autoSkip: false},
+      ticks: {
+        autoSkip: false,
+        color: "#b1b1b1", // --line
+      },
     },
     y: {
+      grid: {
+        borderColor: "#98c7ff", // --primary_one
+        color: "#b1b1b140", // --gridline
+      },
       title: {
         display: true,
         text: I18n.t("main.frequency"),
+        color: "#b1b1b1", // --line
+      },
+      ticks: {
+        color: "#b1b1b1", // --line
       },
     },
   };
