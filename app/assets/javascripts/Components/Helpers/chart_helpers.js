019f1da3ce9723c01ba2c5d4d6aea0a7108967d8
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-04-06 00:42:01 -0400

    convert chartScales to a function

--- app/assets/javascripts/Components/Helpers/chart_helpers.js
+++ app/assets/javascripts/Components/Helpers/chart_helpers.js
@@ -1,35 +1,21 @@
 import * as I18n from "i18n-js";
 
-export const chartScales = {
-  x: {
-    grid: {
-      borderColor: "#98c7ff", // --primary_one
-      color: "#b1b1b140", // --gridline
+export function chartScales() {
+  return {
+    x: {
+      title: {
+        display: true,
+        text: I18n.t("main.grade"),
+      },
+      min: 0,
+      max: 100,
+      ticks: {autoSkip: false},
     },
-    title: {
-      display: true,
-      text: I18n.t("main.grade"),
-      color: "#b1b1b1", // --gridline
+    y: {
+      title: {
+        display: true,
+        text: I18n.t("main.frequency"),
+      },
     },
-    min: 0,
-    max: 100,
-    ticks: {
-      autoSkip: false,
-      color: "#b1b1b1", // --gridline
-    },
-  },
-  y: {
-    grid: {
-      borderColor: "#98c7ff", // --primary_one
-      color: "#b1b1b140", // --gridline
-    },
-    title: {
-      display: true,
-      text: I18n.t("main.frequency"),
-      color: "#b1b1b1", // --gridline
-    },
-    ticks: {
-      color: "#b1b1b1", // --gridline
-    },
-  },
-};
+  };
+}
