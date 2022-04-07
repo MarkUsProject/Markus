c993ae24c06fed79a3c8082b74a21f7be4ecd272
Tianji Zhang <zhang.sh.tianji@gmail.com>
2022-04-07 11:34:00 -0400

    change to use styling color variable

--- app/assets/javascripts/Components/Helpers/chart_helpers.js
+++ app/assets/javascripts/Components/Helpers/chart_helpers.js
@@ -1,36 +1,40 @@
 import * as I18n from "i18n-js";
 
 export function chartScales() {
+  let bars = document.documentElement.style.getPropertyValue("--primary_one");
+  let ticksColor = document.documentElement.style.getPropertyValue("--line");
+  let labelColor = document.documentElement.style.getPropertyValue("--line");
+  let gridLineColor = document.documentElement.style.getPropertyValue("--gridline");
   return {
     x: {
       grid: {
-        borderColor: "#98c7ff", // --primary_one
-        color: "#b1b1b140", // --gridline
+        borderColor: bars,
+        color: gridLineColor,
       },
       title: {
         display: true,
         text: I18n.t("main.grade"),
-        color: "#b1b1b1", // --line
+        color: labelColor,
       },
       min: 0,
       max: 100,
       ticks: {
         autoSkip: false,
-        color: "#b1b1b1", // --line
+        color: ticksColor,
       },
     },
     y: {
       grid: {
-        borderColor: "#98c7ff", // --primary_one
-        color: "#b1b1b140", // --gridline
+        borderColor: bars,
+        color: gridLineColor,
       },
       title: {
         display: true,
         text: I18n.t("main.frequency"),
-        color: "#b1b1b1", // --line
+        color: labelColor,
       },
       ticks: {
-        color: "#b1b1b1", // --line
+        color: ticksColor,
       },
     },
   };
