9a2c2d54ccd44b777c3333a36746c5f9415bc14a
Gabriel Anover <gabino6821@gmail.com>
2022-04-12 19:00:42 -0400

    Added tests for validations

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,4 +2,5 @@
 class Instructor < Role
   after_create { Repository.get_class.update_permissions }
   after_destroy { Repository.get_class.update_permissions }
+  validate :associated_user_is_an_end_user, unless: -> { self.admin? }
 end
