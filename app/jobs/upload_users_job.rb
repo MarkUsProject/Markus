d3fa136183141686862ca8a1de52231a96634a65
Gabriel Anover <gabino6821@gmail.com>
2022-04-18 11:22:59 -0400

    simplified upload job

--- app/jobs/upload_users_job.rb
+++ app/jobs/upload_users_job.rb
@@ -1,26 +1,19 @@
 class UploadUsersJob < ApplicationJob
+  USER_FIELDS = [:user_name, :last_name, :first_name, :id_number, :email].freeze
+
   def self.on_complete_js(_status)
     'window.location.reload.bind(window.location)'
   end
 
   def perform(user_class, data, encoding)
     progress.total = data.lines.count
-    user_indices = {
-      user_name: EndUser::CSV_ORDER.index(:user_name) || 0,
-      last_name: EndUser::CSV_ORDER.index(:last_name) || 1,
-      first_name: EndUser::CSV_ORDER.index(:first_name) || 2,
-      id_number: EndUser::CSV_ORDER.index(:id_number) || 3,
-      email: EndUser::CSV_ORDER.index(:email) || 4
-    }
     user_class.transaction do
       MarkusCsv.parse(data, encoding: encoding, skip_blanks: true, row_sep: :auto) do |row|
-        user_data = {
-          user_name: row[user_indices[:user_name]]&.strip,
-          last_name: row[user_indices[:last_name]]&.strip,
-          first_name: row[user_indices[:first_name]]&.strip,
-          id_number: row[user_indices[:id_number]]&.strip,
-          email: row[user_indices[:email]]&.strip
-        }
+        user_data = {}
+        USER_FIELDS.each_with_index do |field, index|
+          row_index = EndUser::CSV_ORDER.index(field) || index
+          user_data[field] = row[row_index]&.strip
+        end
         next if user_data[:user_name].blank? || user_data[:last_name].blank? || user_data[:first_name].blank?
         user = user_class.find_or_initialize_by(user_data)
         raise "#{user_data[:user_name]}: #{user.errors.full_messages.join('; ')}" unless user.save
