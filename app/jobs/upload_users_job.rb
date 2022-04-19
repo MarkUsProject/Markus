4cf9338af9af919368a5cc60da4c2746ce386f54
Gabriel Anover <gabino6821@gmail.com>
2022-04-19 16:19:22 -0400

    Simplified End User row association and fixed bug with updating users

--- app/jobs/upload_users_job.rb
+++ app/jobs/upload_users_job.rb
@@ -9,12 +9,8 @@ class UploadUsersJob < ApplicationJob
     progress.total = data.lines.count
     user_class.transaction do
       MarkusCsv.parse(data, encoding: encoding, skip_blanks: true, row_sep: :auto) do |row|
-        user_data = {}
-        USER_FIELDS.each_with_index do |field, index|
-          row_index = EndUser::CSV_ORDER.index(field) || index
-          user_data[field] = row[row_index]&.strip
-        end
-        user = user_class.find_or_initialize_by(user_name: user_data[:user_name])
+        user_data = EndUser::CSV_ORDER.zip(row).to_h
+        user = user_class.find_by(user_name: user_data[:user_name]) || user_class.new(user_data)
         user.assign_attributes(user_data)
         raise "#{user_data[:user_name]}: #{user.errors.full_messages.join('; ')}" unless user.save
         progress.increment
