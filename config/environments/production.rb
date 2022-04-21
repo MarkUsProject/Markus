079a4be848351fbf54dbebd9453dc9b89d300829
Gabriel Anover <gabino6821@gmail.com>
2022-04-21 19:02:26 -0400

    Migrated changes to new branch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,9 @@
 Markus::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+                                          email: {
+                                            email_prefix: '[ERROR] ',
+                                            sender_address: %("MarkUs Exception Notifier" <notifier@example.com>),
+                                            exception_recipients: %w[exceptions@example.com]
+                                          }
 end
