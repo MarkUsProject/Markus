371207e708c686d7e2f3ab12e8eb7a34c47d37c8
Gabriel Anover <gabino6821@gmail.com>
2022-04-21 19:06:45 -0400

    Added ability to enable/disable notification gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,11 @@
 Markus::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  Rails.application.config.middleware.use ExceptionNotification::Rack,
-                                          email: {
-                                            email_prefix: '[ERROR] ',
-                                            sender_address: %("MarkUs Exception Notifier" <notifier@example.com>),
-                                            exception_recipients: %w[exceptions@example.com]
-                                          }
+  if Settings.exception_notification.enabled
+    Rails.application.config.middleware.use ExceptionNotification::Rack,
+                                            email: {
+                                              email_prefix: '[ERROR] ',
+                                              sender_address: %("MarkUs Exception Notifier" <notifier@example.com>),
+                                              exception_recipients: %w[exceptions@example.com]
+                                            }
+  end
 end
