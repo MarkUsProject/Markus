5e36a0cbbea16a9ae8d9d566a1ecaa9f6051f7ce
Gabriel Anover <gabino6821@gmail.com>
2022-04-28 18:02:45 -0400

    added check for user type

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -5,6 +5,7 @@ Rails.application.config.after_initialize do
     # Modify CSP for Performance Gem
     RailsPerformance::RailsPerformanceController.class_eval do
       include SessionHandler
+      before_action :check_user_not_authorized
 
       content_security_policy do |p|
         p.style_src :self, "'unsafe-inline'"
@@ -12,6 +13,16 @@ Rails.application.config.after_initialize do
                           "'unsafe-inline'",
                           'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js',
                           'https://code.highcharts.com'
+      end
+
+      protected
+
+      def check_user_not_authorized
+        unless real_user&.admin_user?
+          render 'shared/http_status',
+                 formats: [:html], locals: { code: '403', message: HttpStatusHelper::ERROR_CODE['message']['403'] },
+                 status: :forbidden, layout: false
+        end
       end
     end
 
@@ -21,7 +32,6 @@ Rails.application.config.after_initialize do
       config.duration = Settings.session_timeout.seconds
       # default path where to mount gem
       config.mount_at = '/admin/performance'
-      config.verify_access_proc = proc { @real_user&.admin_user? }
     end
   end
 end
