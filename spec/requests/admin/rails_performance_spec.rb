8ba834ae2fa959c9823c265a379707a4bec0f41f
Gabriel Anover <gabino6821@gmail.com>
2022-04-29 18:03:21 -0400

    Added tests to ensure gem is loaded properly

---
+++ spec/requests/admin/rails_performance_spec.rb
@@ -0,0 +1,46 @@
+describe 'Rails Performance dashboard authorization', type: :request do
+  context 'when the user is not authenticated' do
+    it 'returns a 403 status code' do
+      get '/admin/performance'
+      expect(response).to have_http_status :forbidden
+    end
+  end
+
+  context 'when the user is authenticated' do
+    before :each do
+      allow_any_instance_of(ActionDispatch::Request::Session).to receive(:[]).with(:auth_type)
+                                                                             .and_return('local')
+      allow_any_instance_of(ActionDispatch::Request::Session).to receive(:[]).with(:real_user_name)
+                                                                             .and_return(user.user_name)
+      get '/admin/performance'
+    end
+
+    context 'and is an admin' do
+      let(:user) { create(:admin_user) }
+      it 'returns a 200 status code' do
+        expect(response).to redirect_to('/admin/performance')
+      end
+    end
+
+    context 'and is an instructor' do
+      let(:user) { create(:instructor) }
+      it 'returns a 403 status code' do
+        expect(response).to have_http_status :forbidden
+      end
+    end
+
+    context 'and is a TA' do
+      let(:user) { create(:ta) }
+      it 'returns a 403 status code' do
+        expect(response).to have_http_status :forbidden
+      end
+    end
+
+    context 'and is a student' do
+      let(:user) { create(:student) }
+      it 'returns a 403 status code' do
+        expect(response).to have_http_status :forbidden
+      end
+    end
+  end
+end
