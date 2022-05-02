b2ee19cd077e3957d87ea00889e5a3853d16568e
Gabriel Anover <gabino6821@gmail.com>
2022-05-02 13:31:04 -0400

    Fixed timeout and home links bugs

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -1,37 +1,37 @@
 require 'rails_performance'
 
 Rails.application.config.after_initialize do
-  if defined?(RailsPerformance)
-    # Modify CSP for Performance Gem
-    RailsPerformance::RailsPerformanceController.class_eval do
-      include SessionHandler
-      before_action :check_user_not_authorized
+  # Modify CSP for Performance Gem
+  RailsPerformance::RailsPerformanceController.class_eval do
+    include SessionHandler
+    before_action :check_user_not_authorized
 
-      content_security_policy do |p|
-        p.style_src :self, "'unsafe-inline'"
-        p.script_src_elem :self,
-                          "'unsafe-inline'",
-                          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js',
-                          'https://code.highcharts.com'
-      end
-
-      protected
-
-      def check_user_not_authorized
-        unless real_user&.admin_user?
-          render 'shared/http_status',
-                 formats: [:html], locals: { code: '403', message: HttpStatusHelper::ERROR_CODE['message']['403'] },
-                 status: :forbidden, layout: false
-        end
-      end
+    content_security_policy do |p|
+      p.style_src :self, "'unsafe-inline'"
+      p.script_src_elem :self,
+                        "'unsafe-inline'",
+                        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js',
+                        'https://code.highcharts.com'
     end
 
+    protected
+
+    def check_user_not_authorized
+      unless real_user&.admin_user?
+        render 'shared/http_status',
+               formats: [:html], locals: { code: '403', message: HttpStatusHelper::ERROR_CODE['message']['403'] },
+               status: :forbidden, layout: false
+      end
+    end
+  end
+
+  if defined?(RailsPerformance)
     RailsPerformance.setup do |config|
       config.enabled = true
       config.redis = Redis::Namespace.new(Rails.root.to_s)
-      config.duration = Settings.session_timeout.seconds
-      # default path where to mount gem
+      config.duration = 3.hours
       config.mount_at = '/admin/performance'
+      config.home_link = ENV.fetch('RAILS_RELATIVE_URL_ROOT')
     end
   end
 end
