a814af2ccb982c4fd8706bcc0f653bb5c90c3b50
Gabriel Anover <gabino6821@gmail.com>
2022-05-11 17:49:04 -0400

    fixed redis configuration

--- config/initializers/rails_performance.rb
+++ config/initializers/rails_performance.rb
@@ -31,7 +31,6 @@ Rails.application.config.after_initialize do
   if defined?(RailsPerformance)
     RailsPerformance.setup do |config|
       config.enabled = Settings.rails_performance.enabled
-      config.redis = Redis::Namespace.new(Rails.root.to_s)
       config.duration = Settings.rails_performance.duration.minutes
       config.mount_at = '/admin/performance'
       config.home_link = ENV.fetch('RAILS_RELATIVE_URL_ROOT') { '/' }
