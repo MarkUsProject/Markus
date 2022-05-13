dc84761b1c2b37122f40e86b4a82211b3727fbc1
Samuel Maldonado <sam.maldonado@cognitivescience.co>
2022-05-13 17:34:48 -0400

    normalize i18n keys and update tests

--- spec/controllers/lti_controller_spec.rb
+++ spec/controllers/lti_controller_spec.rb
@@ -1,10 +1,10 @@
 describe LtiController do
   let(:instructor) { create :instructor }
-  describe 'get_config', :get_config do
+  describe 'get_config', :get_canvas_config do
     it 'should respond with success when not logged in' do
       is_expected.to respond_with(:success)
     end
-    before { get_as instructor, :get_config }
+    before { get_as instructor, :get_canvas_config }
     it 'should respond with success when logged in' do
       is_expected.to respond_with(:success)
     end
