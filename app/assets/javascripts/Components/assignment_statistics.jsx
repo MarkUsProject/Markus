41a6e7906dcefaec1cb3a2391e69befe1e001827
Gabriel Anover <gabino6821@gmail.com>
2022-05-13 19:15:23 -0400

    Added additional styling and statistics

--- app/assets/javascripts/Components/assignment_statistics.jsx
+++ app/assets/javascripts/Components/assignment_statistics.jsx
@@ -80,51 +80,91 @@ class AssignmentStatistics extends React.Component {
     }
   }
 
-  render() {
-    const grader_distribution_graph = (
-      <div>
-        <h3>{I18n.t("grader_distribution")}</h3>
-        <Bar
-          data={this.state.ta_grade_distribution.data}
-          options={this.state.ta_grade_distribution.options}
-          width="400"
-          height="350"
-        />
-        <p>
-          <a
-            href={Routes.grader_summary_course_assignment_graders_path(
-              this.props.course_id,
-              this.props.assessment_id
-            )}
-          >
-            {I18n.t("activerecord.models.ta.other")}
-          </a>
-        </p>
-      </div>
-    );
+  grader_distribution_graph = () => {
+    if (this.state.ta_grade_distribution.data.datasets.length !== 0) {
+      return (
+        <div>
+          <h3>{I18n.t("grader_distribution")}</h3>
+          <Bar
+            data={this.state.ta_grade_distribution.data}
+            options={this.state.ta_grade_distribution.options}
+          />
+          <p>
+            <a
+              href={Routes.grader_summary_course_assignment_graders_path(
+                this.props.course_id,
+                this.props.assessment_id
+              )}
+            >
+              {I18n.t("activerecord.models.ta.other")}
+            </a>
+          </p>
+        </div>
+      );
+    }
+  };
 
+  render() {
     return (
       <div>
-        <Bar
-          data={this.state.assignment_grade_distribution.data}
-          options={this.state.assignment_grade_distribution.options}
-          width="500"
-          height="450"
-        />
-        {this.state.ta_grade_distribution.data.datasets.length !== 0
-          ? grader_distribution_graph
-          : ""}
+        <div className="grid-2-col assignment-summary-data-dashboard">
+          <AssignmentSummaryValue
+            statistic={I18n.t("average")}
+            value={this.state.summary.average}
+          />
+          <AssignmentSummaryValue statistic={I18n.t("median")} value={this.state.summary.median} />
+          <AssignmentSummaryValue
+            statistic={I18n.t("num_failed")}
+            value={this.state.summary.num_fails}
+          />
+          <AssignmentSummaryValue
+            statistic={I18n.t("num_zeros")}
+            value={this.state.summary.num_zeros}
+          />
+          <AssignmentSummaryPercentage
+            statisticText={I18n.t("assignments_submitted")}
+            progress={this.state.summary.num_submissions_collected}
+            total={this.state.summary.groupings_size}
+          />
+          <AssignmentSummaryPercentage
+            statisticText={I18n.t("assignments_graded")}
+            progress={this.state.summary.num_submissions_graded}
+            total={this.state.summary.groupings_size}
+          />
+        </div>
+        <div className="bar-graph">
+          <Bar
+            data={this.state.assignment_grade_distribution.data}
+            options={this.state.assignment_grade_distribution.options}
+          />
+        </div>
+        {this.grader_distribution_graph()}
       </div>
     );
   }
 }
 
-class AssignmentStatisticsValue extends React.Component {
+class AssignmentSummaryValue extends React.Component {
   render() {
     return (
       <div>
-        <p>{this.props.statistic}:</p>
-        <p className={"assignment-stat-value"}>{this.props.value}</p>
+        <span className="assignment-summary-text">{this.props.statistic}:</span>
+        <span className="assignment-summary-value">{(this.props.value || 0).toFixed(2)}%</span>
+      </div>
+    );
+  }
+}
+
+class AssignmentSummaryPercentage extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="summary-progress-circle">
+          {(this.props.progress / this.props.total || 0).toFixed(2) * 100}%
+        </div>
+        <span>
+          {this.props.statisticText}: {this.props.progress} / {this.props.total}
+        </span>
       </div>
     );
   }
