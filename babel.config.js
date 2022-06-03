d276865144675598e6a47e55150fd7e94c3a94e8
David Liu <david@cs.toronto.edu>
2022-06-03 10:59:51 -0400

    deps: Update babel to use core-js 3 (#6043)

--- babel.config.js
+++ babel.config.js
@@ -30,7 +30,7 @@ module.exports = function (api) {
         {
           forceAllTransforms: true,
           useBuiltIns: "entry",
-          corejs: 2,
+          corejs: "3.22",
           modules: false,
           exclude: ["transform-typeof-symbol"],
         },
