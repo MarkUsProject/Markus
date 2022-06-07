f7f7d842f5dfc060da0ee37d565dfd1d1451d346
David Liu <david@cs.toronto.edu>
2022-06-07 01:15:30 -0400

    Updates to make tests with with host browser

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -117,8 +117,8 @@ RSpec.configure do |config|
 end
 
 Capybara.configure do |config|
-  config.app_host = 'http://localhost:3000/csc108'
+  config.app_host = "http://localhost:#{ENV.fetch('CAPYBARA_SERVER_PORT')}"
   config.server_host = ENV.fetch('CAPYBARA_SERVER_HOST')
   config.server_port = ENV.fetch('CAPYBARA_SERVER_PORT')
-  config.run_server = false
+  config.server = :puma
 end
--- spec/system/main/login_spec.rb
+++ spec/system/main/login_spec.rb
@@ -24,7 +24,7 @@ describe 'logging in', type: :system do
     visit '/'
     click_link(I18n.t('main.login_with', name: Settings.local_auth_login_name ||
       I18n.t('main.local_authentication_default_name')), href: nil)
-    fill_in('user_login', with: 'instructor', id: 'user_login')
+    fill_in('user_login', with: user.user_name, id: 'user_login')
     fill_in('user_password', with: 'x', id: 'user_password')
     click_button(I18n.t('main.log_in'), name: 'commit')
     expect(page).to have_current_path('/courses')
