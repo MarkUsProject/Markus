7d0765f2b3ffb3f9c84239fbf543d7db70c0957d
Gabriel Anover <gabino6821@gmail.com>
2022-06-13 13:35:49 -0400

    Added debugging code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,6 +96,8 @@ RSpec.configure do |config|
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
 
+    puts(ENV.fetch('CHROMEDRIVER_URL'))
+
     driven_by :selenium, using: :chrome, screen_size: [1400, 1400],
                          options: {
                            browser: :remote,
