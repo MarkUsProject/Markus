5ee3eb24ddf28874dfb8f65e28f712457c8f2529
Gabriel Anover <gabino6821@gmail.com>
2022-06-14 09:02:02 -0400

    updated messages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,8 +44,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 Capybara.configure do |config|
-  # config.server_host = ENV.fetch('CAPYBARA_SERVER_HOST', '0.0.0.0')
-  # config.server_port = ENV.fetch('CAPYBARA_SERVER_PORT', '3434')
+  config.server_host = ENV.fetch('CAPYBARA_SERVER_HOST', '0.0.0.0')
+  config.server_port = ENV.fetch('CAPYBARA_SERVER_PORT', '3434')
   config.default_max_wait_time = 30
   config.server = :puma
 end
@@ -91,7 +91,7 @@ RSpec.configure do |config|
 
   config.before type: :system do
     # Disable UI system tests by default when testing locally
-    skip('System Tests Disabled') unless ENV.fetch('ENABLE_UI_TESTING', nil) == 'true'
+    skip('UI system testing is currently disabled') unless ENV.fetch('ENABLE_UI_TESTING', nil) == 'true'
 
     # Configure the selenium webdriver for system tests
     browser = ENV.fetch('DISABLE_HEADLESS_UI_TESTING', nil) == 'true' ? :chrome : :headless_chrome
