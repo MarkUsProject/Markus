b66ac15ad501b9d6c6849e4ed6bdfcfcfb26bbf2
Gabriel Anover <gabino6821@gmail.com>
2022-06-15 15:54:37 -0400

    Configure driver globally

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,6 +28,7 @@ require 'action_policy/rspec'
 require 'action_policy/rspec/dsl'
 require 'capybara/rspec'
 require 'selenium/webdriver'
+require 'byebug'
 # Loads lib repo stuff.
 require 'time-warp'
 
@@ -41,6 +42,13 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
+end
+
+Capybara.register_driver :selenium_remote_chrome do |app|
+  chrome_options = Selenium::WebDriver::Chrome::Options.new(args: ['--no-sandbox', '--disable-gpu',
+                                                                   '--window-size=1400,1400'])
+  chrome_options.add_argument('--headless') unless ENV.fetch('DISABLE_HEADLESS_UI_TESTING', nil) == 'true'
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: 'http://localhost:9515', capabilities: [chrome_options])
 end
 
 Capybara.configure do |config|
@@ -91,19 +99,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.before type: :system do
-    # Disable UI system tests by default when testing locally
-    skip('UI system testing is currently disabled') unless ENV.fetch('ENABLE_UI_TESTING', nil) == 'true'
-
-    # Configure the selenium webdriver for system tests
-    browser = ENV.fetch('DISABLE_HEADLESS_UI_TESTING', nil) == 'true' ? :chrome : :headless_chrome
-    driven_by :selenium, using: browser, screen_size: [1400, 1400],
-                         options: {
-                           browser: :remote,
-                           url: 'http://localhost:9515',
-                           capabilities: [
-                             Selenium::WebDriver::Chrome::Options.new(args: ['--no-sandbox', '--disable-gpu'])
-                           ]
-                         }
+    # Override the default driver used by rspec system tests
+    driven_by :selenium_remote_chrome
   end
 
   config.after :each do |test|
