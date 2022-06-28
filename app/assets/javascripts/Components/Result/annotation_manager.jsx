4701d67a91830045f8833cbc4ed29edc8e3ddc9d
Ziyuan (Jerry) Zhang <67441706+TheTallJerry@users.noreply.github.com>
2022-06-27 21:21:35 -0400

    react: use React.Fragment to prevent key warning (#6056)

--- app/assets/javascripts/Components/Result/annotation_manager.jsx
+++ app/assets/javascripts/Components/Result/annotation_manager.jsx
@@ -8,45 +8,47 @@ export class AnnotationManager extends React.Component {
   }
 
   render() {
-    return [
-      <button key="new_annotation_button" onClick={this.props.newAnnotation} title="Shift + N">
-        {I18n.t("helpers.submit.create", {
-          model: I18n.t("activerecord.models.annotation.one"),
-        })}
-      </button>,
-      <ul className="tags" key="annotation_categories">
-        {this.props.categories.map(cat => (
-          <li
-            className="annotation_category"
-            id={`annotation_category_${cat.id}`}
-            key={cat.id}
-            onMouseDown={e => e.preventDefault()}
-          >
-            {cat.annotation_category_name}
-            <div id={`annotation_text_list_${cat.id}`}>
-              <ul>
-                {cat.texts.map(text => (
-                  <li
-                    key={`annotation_text_${text.id}`}
-                    id={`annotation_text_${text.id}`}
-                    onClick={e => {
-                      e.preventDefault();
-                      this.props.addExistingAnnotation(text.id);
-                    }}
-                    onMouseDown={e => e.preventDefault()}
-                    title={text.content}
-                  >
-                    <span className={"text-content"}>{text.content.slice(0, 70)}</span>
-                    <span className={"red-text"}>
-                      {!text.deduction ? "" : "-" + text.deduction}
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </li>
-        ))}
-      </ul>,
-    ];
+    return (
+      <React.Fragment>
+        <button key="new_annotation_button" onClick={this.props.newAnnotation} title="Shift + N">
+          {I18n.t("helpers.submit.create", {
+            model: I18n.t("activerecord.models.annotation.one"),
+          })}
+        </button>
+        <ul className="tags" key="annotation_categories">
+          {this.props.categories.map(cat => (
+            <li
+              className="annotation_category"
+              id={`annotation_category_${cat.id}`}
+              key={cat.id}
+              onMouseDown={e => e.preventDefault()}
+            >
+              {cat.annotation_category_name}
+              <div id={`annotation_text_list_${cat.id}`}>
+                <ul>
+                  {cat.texts.map(text => (
+                    <li
+                      key={`annotation_text_${text.id}`}
+                      id={`annotation_text_${text.id}`}
+                      onClick={e => {
+                        e.preventDefault();
+                        this.props.addExistingAnnotation(text.id);
+                      }}
+                      onMouseDown={e => e.preventDefault()}
+                      title={text.content}
+                    >
+                      <span className={"text-content"}>{text.content.slice(0, 70)}</span>
+                      <span className={"red-text"}>
+                        {!text.deduction ? "" : "-" + text.deduction}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
   }
 }
