c5c0b92f46940239eebdeab868a2453fdb923520
Gabriel Anover <gabino6821@gmail.com>
2022-06-28 20:04:54 -0400

    Added defaults to migration

--- db/migrate/20220624220107_add_api_submit_to_assignment_properties.rb
+++ db/migrate/20220624220107_add_api_submit_to_assignment_properties.rb
@@ -1,5 +1,5 @@
 class AddApiSubmitToAssignmentProperties < ActiveRecord::Migration[7.0]
   def change
-    add_column :assignment_properties, :api_submit, :boolean
+    add_column :assignment_properties, :api_submit, :boolean, default: false, null: false
   end
 end
