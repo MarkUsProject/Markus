94b53f20556847c0999eee56ac1c4f4f18c78706
Dhairya Khara <50108381+Dhairya-Khara@users.noreply.github.com>
2022-07-24 18:58:12 -0400

    views: Improve styling on HTML error pages (e.g. 404, 403) (#6101)
    * display nav bar
    * include "Go Home" button

--- Changelog.md
+++ Changelog.md
@@ -54,7 +54,7 @@
 - Added emoji annotations for graders and removed `control+click` quick annotations. (#6093)
 - Sorted courses on the dashboard. (#6099)
 - Introduce standalone ruby script as an alternative method to checking for repository access (#5736)
-- Added summary statistics for criteria (#6100)
+- Improved UI on 404 and 403 pages to match the style on MarkUs' dashboard. (#6101)
 - Pass group name and starter files to the autotester when running tests (#6104)
 - Disabled admin editing of course name and allowed instructors to edit display name (#6111)
 - Changed nav bar layout by moving the MarkUs logo beside the course name on the top bar (#6115)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -42,11 +42,15 @@ class ApplicationController < ActionController::Base
   end
 
   def page_not_found
-    render 'shared/http_status',
-           formats: [:html],
-           locals: { code: '404', message: HttpStatusHelper::ERROR_CODE['message']['404'] },
-           status: :not_found,
-           layout: false
+    if current_user
+      current_role unless current_course.nil?
+      render 'shared/error_page',
+             locals: { code: '404', message: HttpStatusHelper::ERROR_CODE['message']['404'] },
+             status: :not_found,
+             layout: 'content'
+    else
+      redirect_to root_path
+    end
   end
 
   protected
@@ -121,9 +125,14 @@ class ApplicationController < ActionController::Base
   end
 
   def user_not_authorized
-    render 'shared/http_status',
-           formats: [:html], locals: { code: '403', message: HttpStatusHelper::ERROR_CODE['message']['403'] },
-           status: :forbidden, layout: false
+    if current_user
+      render 'shared/error_page',
+             locals: { code: '403', message: HttpStatusHelper::ERROR_CODE['message']['403'] },
+             status: :forbidden,
+             layout: 'content'
+    else
+      redirect_to root_path
+    end
   end
 
   # Render 403 if the current user is switching roles and they try to view a route for a different course
