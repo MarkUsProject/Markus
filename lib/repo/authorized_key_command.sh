c37e63f9013e2e696fe08aa37d16f322575223de
Misha Schwartz <misha.schwartz@gmail.com>
2022-08-02 14:47:52 -0400

    ssh: ssh daemon gets authorized keys directly from database and ensures that the user has permission to access the correct url

--- lib/repo/authorized_key_command.sh
+++ lib/repo/authorized_key_command.sh
@@ -1,29 +1,11 @@
 #!/usr/bin/env bash
 
-# This script finds all .authorized_keys files associated with markus instances
-# and writes their content to stdout.
+# This script finds all .authorized_keys lines associated with markus instances
+# from each instance's database and writes their content to stdout.
 # This file should be called by the ssh daemon's AuthorizedKeysCommand.
 
-# shellcheck disable=SC1090
-source "${HOME}/.ssh/rc"
+HOME_DIR=${1:-${HOME}}
 
-# The MARKUS_REPO_LOC_PATTERN environment variable must be set
-#
-# It is highly recommended that the MARKUS_REPO_LOC_PATTERN variable sourced from the "${HOME}/.ssh/rc"
-# file (see above). You may also hard-code it in this file directly if you wish or source them from another
-# file. If you do either, you must modify this file accordingly.
-[[ -z ${MARKUS_REPO_LOC_PATTERN} ]] && echo 'ERROR: MARKUS_REPO_LOC_PATTERN not set' 1>&2 && exit 1
-
-# Convert the MARKUS_REPO_LOC_PATTERN to an actual path that points to a repository on disk.
-# This function replaces '(instance)' with a * glob pattern and then appends '.authorized_keys'.
-# This allows this script to find all .authorized_keys files associated
-# with all MarkUs instances.
-#
-# For example:
-#  echo $(authorized_key_paths)  # /some/path/*/repos/.authorized_keys
-authorized_key_paths() {
-  echo "${MARKUS_REPO_LOC_PATTERN//(instance)/*}/.authorized_keys"
-}
-
-# shellcheck disable=SC2046
-cat $(authorized_key_paths)
+while IFS= read -r service; do
+  psql service="${service}" -qtA -c 'SELECT get_authorized_keys()'
+done < <(sed -n "s/^\[\(.*\)\]\s*$/\1/p" "${HOME_DIR}/.pg_service.conf")
