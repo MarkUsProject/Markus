fe6ab844b34e8d6dbca2956ca97ca781ebe1602c
Misha Schwartz <misha.schwartz@gmail.com>
2022-08-09 14:20:12 -0400

    gem/node update: update to i18n-js v4

--- config/initializers/i18n_js.rb
+++ config/initializers/i18n_js.rb
@@ -1,10 +1,7 @@
 # Configuration for the 'i18n-js' gem
 
-# Generate public/javascripts/{i18n.js,translations.js}
-Rails.application.config.middleware.use I18n::JS::Middleware
-
-# Explicitly whitelist available locales for i18n-js.
-I18n.available_locales = Settings.i18n.available_locales
-
-# Set default locale.
-I18n.default_locale = Settings.i18n.default_locale
+Rails.application.config.after_initialize do
+  require "i18n-js/listen"
+  # This will only run in development.
+  I18nJS.listen
+end
