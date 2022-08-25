11823b4795d9c7fae336c1e9df7da64b1685f36b
Misha Schwartz <misha.schwartz@gmail.com>
2022-08-25 14:30:43 -0400

    db: do not dynamically change the database schema

---
+++ spec/db/database_identifier_spec.rb
@@ -0,0 +1,8 @@
+describe 'Database Identifier Function' do
+  let(:db_id) do
+    ActiveRecord::Base.connection.execute('SELECT database_identifier()')[0]['database_identifier']
+  end
+  it 'should return a string that contains the current database' do
+    expect(db_id).to include(ActiveRecord::Base.connection.current_database)
+  end
+end
