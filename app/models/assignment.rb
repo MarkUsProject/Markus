2e94776d0e663d76e29d6e1196dfd5bc2175e0ea
mishaschwartz <misha.schwartz@gmail.com>
2022-10-28 15:46:44 -0400

    style: do not use private send (#6301)

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -705,7 +705,7 @@ class Assignment < Assessment
         result = g.current_result
         marks = result.nil? ? {} : result.mark_hash
         g.accepted_students.each do |s|
-          other_info = Student::CSV_ORDER.map { |field| s.__send__(field) }
+          other_info = Student::CSV_ORDER.map { |field| s.public_send(field) }
           row = [g.group.group_name] + other_info
           if result.nil?
             row += Array.new(2 + self.ta_criteria.count, nil)
