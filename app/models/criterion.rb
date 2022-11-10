268a5522a80306832438f1f358c56db1f56daf4a
Misha Schwartz <misha.schwartz@gmail.com>
2022-11-10 13:51:41 -0500

    criterion: handle case where assignment may not be assigned yet

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -168,7 +168,7 @@ class Criterion < ApplicationRecord
   end
 
   def results_unreleased?
-    return true if self.assignment.released_marks.empty?
+    return true if self.assignment&.released_marks.blank?
 
     errors.add(:base, 'Cannot update criterion once results are released.')
     false
