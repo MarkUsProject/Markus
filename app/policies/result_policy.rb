5e147e516db697941ad1eaf490781ad2b6f812bf
Daniel Dervishi <58835213+DanielDervishi@users.noreply.github.com>
2022-11-13 22:17:00 -0500

    results: TAs are no longer able to access urls for results they haven't been assigned (#6321)
    Resolves #6251.

--- app/policies/result_policy.rb
+++ app/policies/result_policy.rb
@@ -12,8 +12,7 @@ class ResultPolicy < ApplicationPolicy
   authorize :from_codeviewer, :select_file, :view_token, optional: true
 
   def view?
-    role.instructor? ||
-      role.ta? ||
+    check?(:manage_submissions?, role) || check?(:assigned_grader?, record.grouping) ||
       (!record.grouping.assignment.release_with_urls && check?(:member?, record.submission.grouping)) ||
       check?(:view_with_result_token?)
   end
@@ -40,11 +39,11 @@ class ResultPolicy < ApplicationPolicy
   end
 
   def grade?
-    role.instructor? || role.ta?
+    check?(:manage_submissions?, role) || check?(:assigned_grader?, record.grouping)
   end
 
   def review?
-    role.instructor? || role.ta? || (
+    check?(:manage_submissions?, role) || check?(:assigned_grader?, record.grouping) || (
       record&.submission&.assignment&.has_peer_review &&
           role.is_reviewer_for?(record&.submission&.assignment&.pr_assignment, record)
     )
@@ -59,7 +58,7 @@ class ResultPolicy < ApplicationPolicy
   end
 
   def download?
-    role.instructor? || role.ta? || (
+    check?(:manage_submissions?, role) || check?(:assigned_grader?, record.grouping) || (
       from_codeviewer && role.is_reviewer_for?(record.submission.grouping.assignment.pr_assignment, record)
     ) || (
       record.submission.grouping.accepted_students.ids.include?(role.id) && (
