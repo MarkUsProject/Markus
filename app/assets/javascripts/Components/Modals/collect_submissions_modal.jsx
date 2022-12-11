ed550dcb353c594186f2d39d0d7f852cd3906c4f
David Liu <david@cs.toronto.edu>
2022-12-11 09:01:20 -0500

    UI updates for collect-before-due

--- app/assets/javascripts/Components/Modals/collect_submissions_modal.jsx
+++ app/assets/javascripts/Components/Modals/collect_submissions_modal.jsx
@@ -10,6 +10,7 @@ class CollectSubmissionsModal extends React.Component {
     super(props);
     this.state = {
       override: this.props.override,
+      collect_time: "collect_due_date",
       collect_current: false,
       apply_late_penalty: true,
     };
@@ -23,8 +24,9 @@ class CollectSubmissionsModal extends React.Component {
     event.preventDefault();
     this.props.onSubmit(
       this.state.override,
-      this.state.collect_current,
-      this.state.apply_late_penalty
+      this.state.collect_time === "collect_current",
+      // Always apply late penalty when collecting based on due date
+      this.state.apply_late_penalty || this.state.collect_time === "collect_due_date"
     );
   };
 
@@ -32,8 +34,8 @@ class CollectSubmissionsModal extends React.Component {
     this.setState({override: event.target.checked});
   };
 
-  handleCollectCurrentChange = event => {
-    this.setState({collect_current: event.target.checked});
+  handleCollectTimeChange = event => {
+    this.setState({collect_time: event.target.value});
   };
 
   handleApplyLatePenaltyChange = event => {
@@ -62,45 +64,61 @@ class CollectSubmissionsModal extends React.Component {
         <form onSubmit={this.onSubmit}>
           <div className={"modal-container-vertical"}>
             <p>{this.warningText()}</p>
-            <p>
-              <label>
-                <input type="checkbox" name="override" onChange={this.handleOverrideChange} />
-                &nbsp;
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: I18n.t("submissions.collect.override_existing_html"),
-                  }}
-                />
-              </label>
-            </p>
-            <p>
-              <label>
-                <input
-                  type="checkbox"
-                  name="collect_current"
-                  onChange={this.handleCollectCurrentChange}
-                />
-                &nbsp;
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: I18n.t("submissions.collect.collect_current_html"),
-                  }}
-                />
-              </label>
-            </p>
-            <p>
-              <label>
-                <input
-                  type="checkbox"
-                  defaultChecked={this.state.apply_late_penalty}
-                  name="apply_late_penalty"
-                  onChange={this.handleApplyLatePenaltyChange}
-                />
-              </label>
-              &nbsp;
-              <span>{I18n.t("submissions.collect.apply_late_penalty")}</span>
-            </p>
-
+            <fieldset>
+              <legend>{I18n.t("submissions.collect.collection_time")}</legend>
+              <p>
+                <label htmlFor={"collect_due_date"}>
+                  <input
+                    type={"radio"}
+                    name={"collect_time"}
+                    value={"collect_due_date"}
+                    checked={this.state.collect_time === "collect_due_date"}
+                    onChange={this.handleCollectTimeChange}
+                  />
+                  {I18n.t("submissions.collect.collect_due_date")}
+                </label>
+              </p>
+              <p>
+                <label htmlFor={"collect_current"}>
+                  <input
+                    type={"radio"}
+                    name={"collect_time"}
+                    value={"collect_current"}
+                    checked={this.state.collect_time === "collect_current"}
+                    onChange={this.handleCollectTimeChange}
+                  />
+                  {I18n.t("submissions.collect.collect_current")}
+                </label>
+              </p>
+            </fieldset>
+            <fieldset>
+              <legend>{I18n.t("submissions.collect.collection_options")}</legend>
+              <p>
+                <label>
+                  <input type="checkbox" name="override" onChange={this.handleOverrideChange} />
+                  &nbsp;
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: I18n.t("submissions.collect.override_existing_html"),
+                    }}
+                  />
+                </label>
+              </p>
+              {this.state.collect_time === "collect_current" && (
+                <p>
+                  <label>
+                    <input
+                      type="checkbox"
+                      defaultChecked={this.state.apply_late_penalty}
+                      name="apply_late_penalty"
+                      onChange={this.handleApplyLatePenaltyChange}
+                    />
+                  </label>
+                  &nbsp;
+                  <span>{I18n.t("submissions.collect.apply_late_penalty")}</span>
+                </p>
+              )}
+            </fieldset>
             <section className={"modal-container dialog-actions"}>
               <input type="submit" value={I18n.t("submissions.collect.submit")} />
             </section>
