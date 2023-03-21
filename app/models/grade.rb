5db83ecaf5a50609a8b920c98b8e91c6b5b306d6
Nina Huang <nina.hhhuang@gmail.com>
2023-03-21 09:16:51 -0400

    added test cases and made changes to grade model

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -7,6 +7,5 @@ class Grade < ApplicationRecord
 
   validate :courses_should_match
   validates :grade,
-            numericality: { greater_than_or_equal_to: 0,
-                            allow_nil: true }
+            numericality: { allow_nil: true }
 end
