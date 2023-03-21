6c0aae8c59da55462351f42f99e0cbd60103f971
Nina Huang <nina.hhhuang@gmail.com>
2023-03-21 10:15:39 -0400

    Allowed negative grades in test cases

--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -9,9 +9,9 @@ describe Grade do
   it { is_expected.to allow_value(0.0).for(:grade) }
   it { is_expected.to allow_value(1.5).for(:grade) }
   it { is_expected.to allow_value(100.0).for(:grade) }
-  it { is_expected.to_not allow_value(-0.5).for(:grade) }
-  it { is_expected.to_not allow_value(-1.0).for(:grade) }
-  it { is_expected.to_not allow_value(-100.0).for(:grade) }
+  it { is_expected.to allow_value(-0.5).for(:grade) }
+  it { is_expected.to allow_value(-1.0).for(:grade) }
+  it { is_expected.to allow_value(-100.0).for(:grade) }
 
   it { is_expected.to belong_to(:grade_entry_item) }
   it { is_expected.to belong_to(:grade_entry_student) }
