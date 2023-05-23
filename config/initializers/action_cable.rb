98e2fde4764bae3d6175ce03b96bb46070f7699b
Daniel Dervishi <d.dervishi@mail.utoronto.ca>
2023-05-23 13:35:35 -0400

    Delete old initializer file

--- config/initializers/action_cable.rb
+++
@@ -1,4 +0,0 @@
-# Markus::Application.configure do
-#   # Settings specified here will take precedence over those in config/application.rb
-#   config.action_cable.url = "#{config.action_controller.relative_url_root}/cable"
-# end
