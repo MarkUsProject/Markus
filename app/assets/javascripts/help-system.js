97c9655da556f5e219e4d217d872080ec67dd9fe
Daniel Dervishi <d.dervishi@mail.utoronto.ca>
2023-07-10 11:13:52 -0400

    Addressed Comments

--- app/assets/javascripts/help-system.js
+++ app/assets/javascripts/help-system.js
@@ -1,12 +1,10 @@
 (function () {
   const domContentLoadedCB = () => {
-    $(".help, .title-help, .inline-help").click(event => {
-      $(".help, .title-help, .inline-help")
-        .click(event => {
-          $(event.currentTarget).children("p").toggle();
-        })
-        .prepend(HELP_ICON_HTML);
-    });
+    $(".help, .title-help, .inline-help")
+      .click(event => {
+        $(event.currentTarget).children("p").toggle();
+      })
+      .prepend(HELP_ICON_HTML);
   };
 
   if (document.readyState === "loading") {
