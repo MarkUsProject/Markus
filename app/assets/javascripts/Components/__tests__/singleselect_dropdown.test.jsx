ddc69c858208c58cd04dd59e8cb6bebb8941af35
Daniel Dervishi <d.dervishi@mail.utoronto.ca>
2023-07-17 11:41:32 -0400

    updated tests

--- app/assets/javascripts/Components/__tests__/singleselect_dropdown.test.jsx
+++ app/assets/javascripts/Components/__tests__/singleselect_dropdown.test.jsx
@@ -5,9 +5,10 @@ import {SingleSelectDropDown} from "../../DropDownMenu/SingleSelectDropDown.js";
 describe("SingleSelectDropdown", () => {
   let props;
   let onChange = jest.fn();
+  let options = ["a", "b", "c"];
   beforeEach(() => {
     props = {
-      options: ["a", "b", "c"],
+      options: options,
       selected: "",
       select: onChange,
       defaultValue: "",
@@ -15,40 +16,54 @@ describe("SingleSelectDropdown", () => {
     render(<SingleSelectDropDown {...props} />);
   });
 
-  it("should load the default value as the currently selected option", () => {
-    expect(screen.getByTestId("selection")).toHaveTextContent("");
-  });
-  it("should not load the dropdown options when the dropdown has not been clicked", () => {
-    expect(screen.queryByText("a")).toBeNull();
-    expect(screen.queryByText("b")).toBeNull();
-    expect(screen.queryByText("c")).toBeNull();
+  describe("when the dropdown is closed", () => {
+    describe("when the dropdown is first rendered", () => {
+      it("should load the default value as the currently selected option", () => {
+        expect(screen.getByTestId("selection")).toHaveTextContent("");
+      });
+    });
+    it("should not load the dropdown options", () => {
+      expect(screen.queryByText("a")).toBeNull();
+      expect(screen.queryByText("b")).toBeNull();
+      expect(screen.queryByText("c")).toBeNull();
+    });
+    it("should render a down arrow within the dropdown", () => {
+      expect(screen.getByTestId("arrow-down")).toBeInTheDocument();
+    });
   });
 
-  it("should load the dropdown options when it has been clicked", () => {
-    fireEvent.click(screen.getByTestId("dropdown"));
-    expect(screen.getByText("a")).toBeInTheDocument();
-    expect(screen.getByText("b")).toBeInTheDocument();
-    expect(screen.getByText("c")).toBeInTheDocument();
-  });
-  it("should open a dropdown only with the options specified", () => {
-    fireEvent.click(screen.getByTestId("dropdown"));
-    expect(screen.getByText("a")).toBeInTheDocument();
-    expect(screen.getByText("b")).toBeInTheDocument();
-    expect(screen.getByText("c")).toBeInTheDocument();
-    expect(screen.getAllByRole("listitem").length).toBe(3);
-  });
-  it("should render a down arrow within the dropdown when it is closed", () => {
-    expect(screen.getByTestId("arrow-down")).toBeInTheDocument();
-  });
-  it("should render a upward arrow within the dropdown when it is open", () => {
-    fireEvent.click(screen.getByTestId("dropdown"));
-    expect(screen.getByTestId("arrow-up")).toBeInTheDocument();
-  });
-  it("should close the dropdown when already expanded and clicked again", () => {
-    fireEvent.click(screen.getByTestId("dropdown"));
-    fireEvent.click(screen.getByTestId("dropdown"));
-    expect(screen.queryByText("a")).toBeNull();
-    expect(screen.queryByText("b")).toBeNull();
-    expect(screen.queryByText("c")).toBeNull();
+  describe("when the dropdown is opened", () => {
+    describe("when there are options available", () => {
+      it("should open a dropdown only with the options specified", () => {
+        fireEvent.click(screen.getByTestId("dropdown"));
+        expect(screen.getByText("a")).toBeInTheDocument();
+        expect(screen.getByText("b")).toBeInTheDocument();
+        expect(screen.getByText("c")).toBeInTheDocument();
+        expect(screen.getAllByRole("listitem").length).toBe(3);
+      });
+    });
+    describe("No options available", () => {
+      beforeAll(() => {
+        options = [];
+      });
+      it("should show no options available when there aren't any options to choose from", () => {
+        fireEvent.click(screen.getByTestId("dropdown"));
+        expect(screen.getByText("No options available")).toBeInTheDocument();
+        expect(screen.queryByText("a")).toBeNull();
+        expect(screen.queryByText("b")).toBeNull();
+        expect(screen.queryByText("c")).toBeNull();
+      });
+    });
+    it("should render a upward arrow within the dropdown", () => {
+      fireEvent.click(screen.getByTestId("dropdown"));
+      expect(screen.getByTestId("arrow-up")).toBeInTheDocument();
+    });
+    it("should close the dropdown when clicked again", () => {
+      fireEvent.click(screen.getByTestId("dropdown"));
+      fireEvent.click(screen.getByTestId("dropdown"));
+      expect(screen.queryByText("a")).toBeNull();
+      expect(screen.queryByText("b")).toBeNull();
+      expect(screen.queryByText("c")).toBeNull();
+    });
   });
 });
