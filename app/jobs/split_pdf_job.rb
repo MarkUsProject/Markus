7191ea6b7d14b06d86254103341abba320d68317
Anis Singh <anisnylesingh@gmail.com>
2023-08-08 15:19:49 -0400

    Correctly invoke exam matcher package.

--- app/jobs/split_pdf_job.rb
+++ app/jobs/split_pdf_job.rb
@@ -258,12 +258,9 @@ class SplitPdfJob < ApplicationJob
         img.destroy!
         student_info.destroy!
 
-        # python_exe = Rails.application.config.python
-        # read_chars_py_file = Rails.root.join('lib/scanner/run_scanner.py').to_s
         exam_matcher_exe = "#{Rails.application.config.python} -m markus_exam_matcher"
         char_type = exam_template.cover_fields == 'id_number' ? 'digit' : 'letter'
-        # stdout, status = Open3.capture2(python_exe, read_chars_py_file, student_info_file, char_type)
-        stdout, status = Open3.capture2(exam_matcher_exe, student_info_file, char_type)
+        stdout, status = Open3.capture2("#{exam_matcher_exe} #{student_info_file} #{char_type}")
         parsed = stdout.strip.split("\n")
 
         next unless status.success? && parsed.length == 1
