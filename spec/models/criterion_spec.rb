560f396b80f06fa98f8cd063251f993afeae7660
Bruce Liu <liuxb904@gmail.com>
2023-10-06 19:49:02 -0400

    Minor edits

--- spec/models/criterion_spec.rb
+++ spec/models/criterion_spec.rb
@@ -1,9 +1,9 @@
 describe Criterion do
   let(:assignment) { create :assignment_with_criteria_and_results }
-  context 'when uploading criteria from yaml' do
+  describe 'reset marking states' do
     before('resets marking states with at least one result') do
       assignment.current_results do |result|
-        instance_variable_set(result.marking_state, Result::MARKING_STATES[:complete])
+        result.update(marking_state: Result::MARKING_STATES[:complete])
       end
       Criterion.reset_marking_states(assignment.id)
     end
