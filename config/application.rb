4721a764eca859b7e7219c95c1d83265f66262b4
Samuel Maldonado <sam.maldonado@cognitivescience.co>
2023-10-26 13:48:35 -0400

    add websocket config to settings

--- config/application.rb
+++ config/application.rb
@@ -129,6 +129,8 @@ module Markus
 
     config.action_cable.url = "#{config.relative_url_root}/cable"
 
+    config.action_cable.allowed_request_origins = Settings.web_socket_allowed_request_origins
+
     # TODO: review initializers 01 and 02
     # TODO review markus custom config format
     # TODO handle namespaces properly for app/lib
