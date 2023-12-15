57220f8ba45578174cf4bcf30f16beac9198d3c7
David Liu <david@cs.toronto.edu>
2023-12-14 23:07:01 -0500

    Updates for Rails 7.1

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-begin
-  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-rescue LoadError
-  # Do nothing
-end
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
