c8ddb8904055d50af5af97e2d2df16f6cddb3fdf
David Liu <david@cs.toronto.edu>
2023-12-14 23:16:49 -0500

    Restore bootsnap require handling

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+begin
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+rescue LoadError
+  # Do nothing
+end
