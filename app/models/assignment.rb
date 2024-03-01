df24fc554728ee09b3e58e530f6ce5b2d3ff9848
Mimis Chlympatsos <mimis.chlympatsos@mail.utoronto.ca>
2024-03-01 16:44:02 -0500

    association-improvement
    Added dependent: :destroy to all the associations that needed it (in the Assignment and Assessment models), changed the order of associations in Assignment to make groups-and-groupings first (this is the first thing  that should be checked when deleting an assignment), created a new locale string for use in the AnnotationCategory model, and added another rescue block in the #destroy action of assignments controller to handle other potential complications that could arise in assignment deletion. This last one is temporary, since we would ideally like to perform a more targetted handling of different exceptions (but for now this prevents crashing and gives an idea of the problem).

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -8,6 +8,10 @@ class Assignment < Assessment
 
   validates :due_date, presence: true
 
+  # this has to be before :peer_reviews or it throws a HasManyThroughOrderError
+  has_many :groupings, foreign_key: :assessment_id, inverse_of: :assignment
+  has_many :groups, through: :groupings, dependent: :restrict_with_exception
+
   has_one :assignment_properties,
           dependent: :destroy,
           inverse_of: :assignment,
@@ -32,12 +36,14 @@ class Assignment < Assessment
   has_many :ta_criteria,
            -> { where(ta_visible: true).order(:position) },
            class_name: 'Criterion',
+           dependent: :destroy,
            inverse_of: :assignment,
            foreign_key: :assessment_id
 
   has_many :peer_criteria,
            -> { where(peer_visible: true).order(:position) },
            class_name: 'Criterion',
+           dependent: :destroy,
            inverse_of: :assignment,
            foreign_key: :assessment_id
 
@@ -57,24 +63,27 @@ class Assignment < Assessment
   accepts_nested_attributes_for :assignment_files, allow_destroy: true
   validates_associated :assignment_files
 
-  # this has to be before :peer_reviews or it throws a HasManyThroughOrderError
-  has_many :groupings, foreign_key: :assessment_id, inverse_of: :assignment
   # Assignments can now refer to themselves, where this is null if there
   # is no parent (the same holds for the child peer reviews)
   belongs_to :parent_assignment,
              class_name: 'Assignment', optional: true, inverse_of: :pr_assignment, foreign_key: :parent_assessment_id
-  has_one :pr_assignment, class_name: 'Assignment', foreign_key: :parent_assessment_id, inverse_of: :parent_assignment
-  has_many :peer_reviews, through: :groupings
-  has_many :pr_peer_reviews, through: :parent_assignment, source: :peer_reviews
+  has_one :pr_assignment,
+          class_name: 'Assignment',
+          dependent: :destroy,
+          foreign_key: :parent_assessment_id,
+          inverse_of: :parent_assignment
+  has_many :peer_reviews, dependent: :destroy, through: :groupings
+  has_many :pr_peer_reviews, through: :parent_assignment, source: :peer_reviews, dependent: :destroy
 
-  has_many :current_submissions_used, through: :groupings,
-                                      source: :current_submission_used
+  has_many :current_submissions_used,
+           through: :groupings,
+           source: :current_submission_used,
+           dependent: :destroy
 
-  has_many :ta_memberships, through: :groupings
-  has_many :student_memberships, through: :groupings
+  has_many :ta_memberships, through: :groupings, dependent: :destroy
+  has_many :student_memberships, through: :groupings, dependent: :destroy
 
-  has_many :submissions, through: :groupings
-  has_many :groups, through: :groupings, dependent: :restrict_with_exception
+  has_many :submissions, through: :groupings, dependent: :destroy
 
   has_many :notes, as: :noteable, dependent: :destroy
 
@@ -82,7 +91,7 @@ class Assignment < Assessment
 
   has_many :starter_file_groups, dependent: :destroy, inverse_of: :assignment, foreign_key: :assessment_id
 
-  has_many :tas, -> { distinct }, through: :ta_memberships, source: :role
+  has_many :tas, -> { distinct }, through: :ta_memberships, source: :role, dependent: :destroy
 
   before_save do
     @prev_assessment_section_property_ids = assessment_section_properties.ids
