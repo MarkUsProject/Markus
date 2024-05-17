c5710750ba883f42b3b401e425f8371ea0188000
Utku Egemen Umut <121046082+umututku03@users.noreply.github.com>
2024-05-16 22:43:37 -0400

    Update text_annotation rspec test

--- spec/models/text_annotation_spec.rb
+++ spec/models/text_annotation_spec.rb
@@ -4,8 +4,50 @@ describe TextAnnotation do
 
     it { is_expected.to validate_presence_of(:line_start) }
     it { is_expected.to validate_presence_of(:line_end) }
+    it { is_expected.to validate_presence_of(:column_start) }
+    it { is_expected.to validate_presence_of(:column_end) }
+
+    it { is_expected.to allow_value(1).for(:line_start) }
+    it { is_expected.to allow_value(1).for(:line_end) }
+    it { is_expected.to allow_value(0).for(:column_start) }
+    it { is_expected.to allow_value(0).for(:column_end) }
+
     it { is_expected.to allow_value(10).for(:line_start) }
     it { is_expected.to allow_value(10).for(:line_end) }
+    it { is_expected.to allow_value(5).for(:column_start) }
+    it { is_expected.to allow_value(5).for(:column_end) }
+
+    it { is_expected.not_to allow_value(0).for(:line_start) }
+    it { is_expected.not_to allow_value(0).for(:line_end) }
+    it { is_expected.not_to allow_value(-1).for(:line_start) }
+    it { is_expected.not_to allow_value(-1).for(:line_end) }
+    it { is_expected.not_to allow_value(-1).for(:column_start) }
+    it { is_expected.not_to allow_value(-1).for(:column_end) }
+
+    it { is_expected.not_to allow_value(nil).for(:line_start) }
+    it { is_expected.not_to allow_value(nil).for(:line_end) }
+    it { is_expected.not_to allow_value(nil).for(:column_start) }
+    it { is_expected.not_to allow_value(nil).for(:column_end) }
+
+    it 'is invalid with line_start less than 1' do
+      expect(subject).not_to allow_value(0).for(:line_start)
+      expect(subject).not_to allow_value(-10).for(:line_start)
+    end
+
+    it 'is invalid with line_end less than 1' do
+      expect(subject).not_to allow_value(0).for(:line_end)
+      expect(subject).not_to allow_value(-10).for(:line_end)
+    end
+
+    it 'is invalid with column_start less than 0' do
+      expect(subject).not_to allow_value(-1).for(:column_start)
+      expect(subject).not_to allow_value(-10).for(:column_start)
+    end
+
+    it 'is invalid with column_end less than 0' do
+      expect(subject).not_to allow_value(-1).for(:column_end)
+      expect(subject).not_to allow_value(-10).for(:column_end)
+    end
 
     include_examples 'course associations'
 
@@ -15,10 +57,5 @@ describe TextAnnotation do
 
       it_behaves_like 'gets annotation data'
     end
-
-    # TODO: Change Model.
-    # We should not allow negative values vor lines
-    # should_not allow_value(-1).for(:line_start)
-    # should_not allow_value(-1).for(:line_end)
   end
 end
