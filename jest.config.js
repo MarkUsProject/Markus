912947f3e038c4cc4fd551e0c9e0be2004a270f2
David Liu <david@cs.toronto.edu>
2024-08-01 21:04:48 -0400

    Fix mime import and ignore for jest (cjs) transform

--- jest.config.js
+++ jest.config.js
@@ -175,7 +175,7 @@ module.exports = {
   transform: {"\\.[jt]sx?$": "babel-jest"},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ["/node_modules/(?!i18n-js)", "\\.pnp\\.[^\\/]+$"],
+  transformIgnorePatterns: ["/node_modules/(?!i18n-js|mime)", "\\.pnp\\.[^\\/]+$"],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
