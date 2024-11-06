051d44d4dccd9df31de3e7052e6f2dde79053d7b
ch-iv <icheppy101@gmail.com>
2024-11-05 19:40:41 -0500

    Refactor FileViewer

--- app/assets/javascripts/Components/Result/file_viewer.jsx
+++ app/assets/javascripts/Components/Result/file_viewer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import heic2any from "heic2any";
 
 import {ImageViewer} from "./image_viewer";
 import {TextViewer} from "./text_viewer";
@@ -9,122 +8,18 @@ import {BinaryViewer} from "./binary_viewer";
 import {URLViewer} from "./url_viewer";
 
 export class FileViewer extends React.Component {
-  // this.props.result_id is used as a flag for the component to
-  // know whether it is displaying within the result view.
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: "",
-      type: "",
-      url: "",
-      loading: true,
-    };
-  }
-
-  componentDidMount() {
-    if (!this.props.result_id || this.props.selectedFile !== null) {
-      this.set_submission_file(this.props.selectedFile);
-    } else {
-      this.setState({loading: false});
-    }
-  }
-
-  // Manually manage a change of selectedFile, as this requires fetching new file data.
-  shouldComponentUpdate(nextProps) {
-    if (!!this.props.result_id && this.props.selectedFile !== nextProps.selectedFile) {
-      this.set_submission_file(nextProps.selectedFile);
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      !this.props.result_id &&
-      (prevProps.selectedFile !== this.props.selectedFile ||
-        prevProps.submission_id !== this.props.submission_id ||
-        prevProps.selectedFileURL !== this.props.selectedFileURL)
-    ) {
-      this.set_submission_file("");
-    }
-    // Update file type for use in the submission file panel
-    if (
-      typeof this.props.handleFileTypeUpdate === "function" &&
-      prevState.type !== this.state.type
-    ) {
-      this.props.handleFileTypeUpdate(this.state.type);
-    }
-  }
-
-  isNotebook(type) {
-    return type === "jupyter-notebook";
-  }
-
-  getFileUrl = submission_file_id => {
-    if (!!this.props.selectedFileURL) {
-      return this.props.selectedFileURL;
-    } else {
-      return Routes.download_file_course_assignment_submission_path(
-        this.props.course_id,
-        this.props.assignment_id,
-        this.props.submission_id,
-        {
-          select_file_id: submission_file_id,
-          show_in_browser: true,
-          from_codeviewer: true,
-        }
-      );
-    }
+  state = {
+    loading: false,
+    errorMessage: null,
   };
 
-  /*
-   * Update the contents being displayed with the given submission file id.
-   */
-  set_submission_file = (submission_file_id, force_text) => {
-    if (
-      (!this.props.result_id && this.props.selectedFile === null) ||
-      submission_file_id === null
-    ) {
-      this.setState({loading: false, type: ""});
-      return;
-    }
-    force_text = !!force_text;
+  setLoading(loading) {
+    this.setState({loading: loading});
+  }
 
-    // TODO: is this the right spot to remove these? Should it be done earlier?
-    $(".annotation_text_display").each(function () {
-      this.remove();
-    });
-
-    this.setState({loading: true, url: this.getFileUrl(submission_file_id)}, () => {
-      if (
-        this.props.selectedFileType === "image" ||
-        this.props.selectedFileType === "pdf" ||
-        this.isNotebook(this.props.selectedFileType)
-      ) {
-        this.setState({type: this.props.selectedFileType});
-      } else {
-        const delimiter = this.props.selectedFileURL.includes("?") ? "&" : "?";
-        fetch(
-          this.props.selectedFileURL +
-            delimiter +
-            new URLSearchParams({
-              preview: true,
-              force_text: force_text,
-            })
-        )
-          .then(response => (response.ok ? response.text() : Promise.reject()))
-          .then(text => {
-            this.setState({
-              content: text.replace(/\r?\n/gm, "\n"),
-              type: this.props.selectedFileType,
-              loading: false,
-            });
-          });
-      }
-    });
-  };
+  setErrorMessage(errorMessage) {
+    this.setState({errorMessage: errorMessage});
+  }
 
   render() {
     const commonProps = {
@@ -133,14 +28,16 @@ export class FileViewer extends React.Component {
       released_to_students: this.props.released_to_students,
       resultView: !!this.props.result_id,
       course_id: this.props.course_id,
-      key: `${this.state.type}-viewer`,
+      key: `${this.props.selectedFileType}-viewer`,
     };
 
-    if (this.state.loading) {
+    if (this.state.errorMessage) {
+      return <p>{this.state.errorMessage}</p>;
+    } else if (this.state.loading) {
       return I18n.t("working");
-    } else if (this.state.type === "image") {
+    } else if (this.props.selectedFileType === "image") {
       return <ImageViewer url={this.state.url} mime_type={this.props.mime_type} {...commonProps} />;
-    } else if (this.state.type === "pdf") {
+    } else if (this.props.selectedFileType === "pdf") {
       return (
         <PDFViewer
           url={this.state.url}
@@ -148,7 +45,7 @@ export class FileViewer extends React.Component {
           {...commonProps}
         />
       );
-    } else if (this.isNotebook(this.state.type)) {
+    } else if (this.props.selectedFileType === "jupyter-notebook") {
       return (
         <NotebookViewer
           url={this.state.url}
@@ -156,20 +53,22 @@ export class FileViewer extends React.Component {
           {...commonProps}
         />
       );
-    } else if (this.state.type === "binary") {
+    } else if (this.props.selectedFileType === "binary") {
       return (
         <BinaryViewer
+          url={this.state.url}
           content={this.state.content}
           getAnyway={() => this.set_submission_file(this.props.selectedFile, true)}
           {...commonProps}
         />
       );
-    } else if (this.state.type === "markusurl") {
+    } else if (this.props.selectedFileType === "markusurl") {
       return <URLViewer externalUrl={this.state.content} {...commonProps} />;
-    } else if (this.state.type !== "") {
+    } else if (this.props.selectedFileType !== "") {
       return (
         <TextViewer
-          type={this.state.type}
+          url={this.props.selectedFileURL}
+          type={this.props.selectedFileType}
           content={this.state.content}
           focusLine={this.props.focusLine}
           {...commonProps}
