f9a18ea58fd202d392a9c48a3b3099d2f94bfcad
ch-iv <icheppy101@gmail.com>
2024-11-07 22:59:03 -0500

    Pass the correct URL to child viewer components

--- app/assets/javascripts/Components/Result/file_viewer.jsx
+++ app/assets/javascripts/Components/Result/file_viewer.jsx
@@ -29,6 +29,7 @@ export class FileViewer extends React.Component {
       resultView: !!this.props.result_id,
       course_id: this.props.course_id,
       key: `${this.props.selectedFileType}-viewer`,
+      url: this.props.selectedFileURL,
       setLoadingCallback: this.setLoading,
       setErrorMessageCallback: this.setErrorMessage,
     };
@@ -38,27 +39,14 @@ export class FileViewer extends React.Component {
     } else if (this.state.loading) {
       return I18n.t("working");
     } else if (this.props.selectedFileType === "image") {
-      return <ImageViewer url={this.state.url} mime_type={this.props.mime_type} {...commonProps} />;
+      return <ImageViewer mime_type={this.props.mime_type} {...commonProps} />;
     } else if (this.props.selectedFileType === "pdf") {
-      return (
-        <PDFViewer
-          url={this.state.url}
-          annotationFocus={this.props.annotationFocus}
-          {...commonProps}
-        />
-      );
+      return <PDFViewer annotationFocus={this.props.annotationFocus} {...commonProps} />;
     } else if (this.props.selectedFileType === "jupyter-notebook") {
-      return (
-        <NotebookViewer
-          url={this.state.url}
-          annotationFocus={this.props.annotationFocus}
-          {...commonProps}
-        />
-      );
+      return <NotebookViewer annotationFocus={this.props.annotationFocus} {...commonProps} />;
     } else if (this.props.selectedFileType === "binary") {
       return (
         <BinaryViewer
-          url={this.state.url}
           content={this.state.content}
           getAnyway={() => this.set_submission_file(this.props.selectedFile, true)}
           {...commonProps}
@@ -69,9 +57,7 @@ export class FileViewer extends React.Component {
     } else if (this.props.selectedFileType !== "") {
       return (
         <TextViewer
-          url={this.props.selectedFileURL}
           type={this.props.selectedFileType}
-          content={this.state.content}
           focusLine={this.props.focusLine}
           {...commonProps}
         />
