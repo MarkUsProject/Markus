38bc2f2d45f84183f842a555c02892774522a1b8
David Liu <david@cs.toronto.edu>
2024-12-24 13:32:44 -0500

    Fix tests again

--- app/helpers/download_helper.rb
+++ app/helpers/download_helper.rb
@@ -9,7 +9,7 @@ module DownloadHelper
   # Wrapper around +send_data+ which forces the +disposition+ optional argument to
   # 'attachment' since this method  should only be used to download the data as a file.
   def send_data_download(data, options = {})
-    options = { **options, type: Rack::Mime.mime_type(options[:filename]) } if options.key?(:filename)
+    options = { **options, type: Rack::Mime.mime_type(File.extname(options[:filename])) } if options.key?(:filename)
     send_data data, **options, disposition: 'attachment'
   end
 end
