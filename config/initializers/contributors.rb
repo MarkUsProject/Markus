f589c9097306dadb0772a55949673cf2f61bfcc3
Omid Hemmati <omidhemmati79@gmail.com>
2025-01-12 05:32:30 -0500

    created tests for read_contributors method

--- config/initializers/contributors.rb
+++ config/initializers/contributors.rb
@@ -1,10 +1,12 @@
-def read_contributors
-  contributors_file = Rails.root.join('doc/markus-contributors.txt')
-  if File.exist?(contributors_file)
-    File.read(contributors_file).split("\n")
-  else
-    []
+class Contributors
+  def self.read_contributors
+    contributors_file = Rails.root.join('doc/markus-contributors.txt')
+    if File.exist?(contributors_file)
+      File.read(contributors_file).split("\n")
+    else
+      []
+    end
   end
 end
 
-Rails.configuration.markus_contributors = read_contributors
+Rails.configuration.markus_contributors = Contributors.read_contributors
