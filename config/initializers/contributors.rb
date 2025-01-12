a9fb1c2f0d465d5a1aebc9742f28cd8f8ead8fa1
Omid Hemmati <omidhemmati79@gmail.com>
2025-01-12 18:21:30 -0500

    updated initializer logic to store contributors as a string

--- config/initializers/contributors.rb
+++ config/initializers/contributors.rb
@@ -2,9 +2,10 @@ class Contributors
   def self.read_contributors
     contributors_file = Rails.root.join('doc/markus-contributors.txt')
     if File.exist?(contributors_file)
-      File.read(contributors_file).split("\n")
+      contributors_array = File.read(contributors_file).split("\n")
+      contributors_array.join(', ')
     else
-      []
+      ''
     end
   end
 end
--- spec/initializers/contributors_spec.rb
+++ spec/initializers/contributors_spec.rb
@@ -4,9 +4,9 @@ describe '.read_contributors' do
 
   let(:contributors) { ['David Liu', 'Pranav Rao', 'Ivan Chepelev', 'Omid Hemmati'] }
 
-  it 'should return an empty array if the contributors file does not exist' do
+  it 'should return an empty string if the contributors file does not exist' do
     allow(File).to receive(:exist?).and_return(false)
-    expect(subject).to eq([])
+    expect(subject).to eq('')
   end
 
   it 'should return the contributors if the contributors file exists' do
@@ -14,6 +14,6 @@ describe '.read_contributors' do
       exist?: true,
       read: contributors.join("\n")
     )
-    expect(subject).to eq(contributors)
+    expect(subject).to eq(contributors.join(', '))
   end
 end
