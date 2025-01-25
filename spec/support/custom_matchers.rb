19e5e31d5e445021d45043ca6cc5dc1cc4f9c419
Raine-Yang-UofT <raine.yang@mail.utoronto.ca>
2025-01-25 18:14:01 -0500

    refactor: remove I18n checking of custom matcher

--- spec/support/custom_matchers.rb
+++ spec/support/custom_matchers.rb
@@ -1,6 +1,5 @@
-RSpec::Matchers.define :have_message do |msg, **interpolations|
+RSpec::Matchers.define :have_message do |expected|
   match do |actual|
-    expected = get_expected_text(msg, **interpolations)
     actual_messages = Array(actual).map { |m| strip_html(m.to_s) }
     actual_messages.any? { |m| m.strip == strip_html(expected).strip }
   end
@@ -11,9 +10,8 @@ RSpec::Matchers.define :have_message do |msg, **interpolations|
   end
 end
 
-RSpec::Matchers.define :contain_message do |msg, **interpolations|
+RSpec::Matchers.define :contain_message do |expected|
   match do |actual|
-    expected = get_expected_text(msg, **interpolations)
     actual_messages = Array(actual).map { |m| strip_html(m.to_s) }
     actual_messages.any? { |m| m.include?(strip_html(expected)) }
   end
@@ -22,27 +20,6 @@ RSpec::Matchers.define :contain_message do |msg, **interpolations|
     actual_stripped = Array(actual).map { |m| strip_html(m.to_s) }
     "expected that #{actual_stripped.inspect} would contain message #{expected.inspect}"
   end
-end
-
-def get_expected_text(msg, **interpolations)
-  if translation_exists?(msg)
-    I18n.t(msg, **interpolations)
-  else
-    interpolate_string(msg.to_s, **interpolations)
-  end
-end
-
-def translation_exists?(key)
-  I18n.exists?(key)
-rescue StandardError
-  false
-end
-
-def interpolate_string(text, **interpolations)
-  interpolations.each do |key, value|
-    text = text.gsub("%{#{key}}", value.to_s)
-  end
-  text
 end
 
 def strip_html(text)
