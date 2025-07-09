fb60fac597708745392e85cd8f3f8642c979929d
freyazjiner <149322974+jinerfreya@users.noreply.github.com>
2025-07-08 22:51:25 -0400

    updated some namings and new test cases

--- app/javascript/Components/Result/annotation_manager.jsx
+++ app/javascript/Components/Result/annotation_manager.jsx
@@ -5,19 +5,18 @@ export class AnnotationManager extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <button key="new_annotation_button" title="Shift + N">
+        <button key="new_annotation_button" onClick={this.props.newAnnotation} title="Shift + N">
           {I18n.t("helpers.submit.create", {
             model: I18n.t("activerecord.models.annotation.one"),
           })}
         </button>
-
         <ul className="tags" key="annotation_categories">
           {this.props.categories.map(cat => (
             <DropDownMenu
               key={cat.id}
               header={cat.annotation_category_name}
               items={cat.texts}
-              addExistingAnnotation={this.props.addExistingAnnotation}
+              onItemClick={this.props.addExistingAnnotation}
             />
           ))}
         </ul>
--- app/javascript/Components/Result/dropdown_menu.jsx
+++ app/javascript/Components/Result/dropdown_menu.jsx
@@ -6,15 +6,15 @@ import safe_marked from "../../common/safe_marked";
 export class DropDownMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {onHover: false};
+    this.state = {expanded: false};
   }
 
   handleMouseEnter = () => {
-    this.setState({onHover: true});
+    this.setState({expanded: true});
   };
 
   handleMouseLeave = () => {
-    this.setState({onHover: false});
+    this.setState({expanded: false});
   };
 
   render() {
@@ -27,27 +27,25 @@ export class DropDownMenu extends React.Component {
       >
         <div className="dropdown-header">{this.props.header}</div>
 
-        {this.state.onHover && (
-          <div className="list">
-            <ul>
-              {this.props.items.map(item => (
-                <li
-                  key={item.id}
-                  id={item.id}
-                  data-testid={`item-${item.id}`}
-                  onMouseEnter={() => this.props.addExistingAnnotation(item.id)}
-                  onMouseDown={e => e.preventDefault()}
-                  title={item.content}
-                >
-                  <span
-                    className={"text-content"}
-                    dangerouslySetInnerHTML={{__html: safe_marked(item.content).slice(0, 70)}}
-                  />
-                  <span className={"red-text"}>{!item.deduction ? "" : "-" + item.deduction}</span>
-                </li>
-              ))}
-            </ul>
-          </div>
+        {this.state.expanded && (
+          <ul>
+            {this.props.items.map(item => (
+              <li
+                key={item.id}
+                data-testid={`item-${item.id}`}
+                onClick={e => {
+                  e.preventDefault();
+                  this.props.addExistingAnnotation(item.id);
+                }}
+              >
+                <span
+                  className={"text-content"}
+                  dangerouslySetInnerHTML={{__html: safe_marked(item.content).slice(0, 70)}}
+                />
+                <span className={"red-text"}>{!item.deduction ? "" : "-" + item.deduction}</span>
+              </li>
+            ))}
+          </ul>
         )}
       </li>
     );
