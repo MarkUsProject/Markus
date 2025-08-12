325a19eff5dd041afc4a6391399e4cab611b9aee
freyazjiner <149322974+jinerfreya@users.noreply.github.com>
2025-08-12 11:51:20 -0400

    add tests for the subcomponent

--- app/javascript/Components/__tests__/assignment_summary.test.jsx
+++ app/javascript/Components/__tests__/assignment_summary.test.jsx
@@ -171,3 +171,96 @@ describe("For the AssignmentSummaryTable's display of an assignment without auto
     });
   });
 });
+
+describe("For the AssignmentSummaryTable's subcomponent behavior", () => {
+  let groups_sample;
+  beforeEach(async () => {
+    groups_sample = [
+      {
+        group_name: "group_0001",
+        section: null,
+        members: [
+          ["c9nielse", "Nielsen", "Carl", true],
+          ["c8szyman", "Szymanowski", "Karol", true],
+        ],
+        tags: [],
+        graders: [["c9varoqu", "Nelle", "Varoquaux"]],
+        marking_state: "released",
+        final_grade: 9.0,
+        criteria: {1: 0.0},
+        max_mark: "21.0",
+        result_id: 15,
+        submission_id: 15,
+        total_extra_marks: null,
+      },
+      {
+        group_name: "group_0002",
+        section: "LEC0101",
+        members: [
+          ["c8debuss", "Debussy", "Claude", false],
+          ["c8holstg", "Holst", "Gustav", false],
+        ],
+        tags: [],
+        graders: [],
+        marking_state: "released",
+        final_grade: 6.0,
+        criteria: {1: 2.0},
+        max_mark: "21.0",
+        result_id: 5,
+        submission_id: 5,
+        total_extra_marks: null,
+      },
+    ];
+    fetch.mockReset();
+    fetch.mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce({
+        data: groups_sample,
+        criteriaColumns: [
+          {
+            Header: "dolores",
+            accessor: "criteria.1",
+            className: "number",
+            headerClassName: "",
+          },
+        ],
+        numAssigned: 2,
+        numMarked: 2,
+        ltiDeployments: [],
+      }),
+    });
+
+    await act(async () => {
+      render(
+        <AssignmentSummaryTable
+          assignment_id={1}
+          course_id={1}
+          is_instructor={false}
+          lti_deployments={[]}
+        />
+      );
+    });
+  });
+
+  it("should initially hide grader subcomponent content", () => {
+    expect(
+      screen.queryByText(I18n.t("activerecord.models.ta", {count: 2}))
+    ).not.toBeInTheDocument();
+  });
+
+  it("should show grader subcomponent when expanded", async () => {
+    expect(screen.getByText("▶")).toBeInTheDocument();
+
+    await act(async () => {
+      fireEvent.click(screen.getByText("▶"));
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText("▼")).toBeInTheDocument();
+    });
+
+    await waitFor(() => {
+      expect(screen.getByText(I18n.t("activerecord.models.ta", {count: 2}))).toBeInTheDocument();
+    });
+  });
+});
