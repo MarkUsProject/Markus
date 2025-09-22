6120d9b076733e623d6a92e03ade6e3feb89cb5e
Donny Wong <donnywong@cs.toronto.edu>
2025-09-22 14:19:54 -0400

    updated code for improvement and fixes.

--- app/controllers/lti_deployments_controller.rb
+++ app/controllers/lti_deployments_controller.rb
@@ -183,11 +183,12 @@ class LtiDeploymentsController < ApplicationController
 
   def create_course
     if LtiConfig.respond_to?(:allowed_to_create_course?) && !LtiConfig.allowed_to_create_course?(record)
+      @title = I18n.t('lti.course_creation_denied')
       @message = format(
         Settings.lti.unpermitted_new_course_message,
         course_name: record.lms_course_name
       )
-      render 'unpermitted_course_creation', status: :unprocessable_entity
+      render 'message', status: :forbidden
       return
     end
 
