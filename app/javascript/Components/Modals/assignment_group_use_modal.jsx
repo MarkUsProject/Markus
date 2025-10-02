32c7d47ce40141e6401060455f42a23c90581f7e
freyazjiner <jinerz77@gmail.com>
2025-10-02 16:35:44 -0400

    fixed the submit button

--- app/javascript/Components/Modals/assignment_group_use_modal.jsx
+++ app/javascript/Components/Modals/assignment_group_use_modal.jsx
@@ -13,13 +13,23 @@ export default class AssignmentGroupUseModal extends React.Component {
     Modal.setAppElement("body");
   }
 
+  componentDidUpdate(prevProps) {
+    if (this.props.isOpen && !prevProps.isOpen) {
+      if (this.props.cloneAssignments.length > 0) {
+        this.setState({assignmentId: this.props.cloneAssignments[0].id});
+      } else {
+        this.setState({assignmentId: ""});
+      }
+    }
+  }
+
   handleChange = event => {
     this.setState({assignmentId: event.target.value});
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    if (window.confirm(I18n.t("groups.delete_groups_linked"))) {
+    if (this.state.assignmentId && window.confirm(I18n.t("groups.delete_groups_linked"))) {
       this.props.onSubmit(this.state.assignmentId);
     }
   };
@@ -37,7 +47,7 @@ export default class AssignmentGroupUseModal extends React.Component {
           {I18n.t("groups.assignment_to_use")}
           <select
             id="assignment-group-select"
-            value={this.state.assignmentId || ""}
+            value={this.state.assignmentId}
             onChange={event => this.handleChange(event)}
           >
             {this.props.cloneAssignments.map(assignment => (
@@ -47,7 +57,11 @@ export default class AssignmentGroupUseModal extends React.Component {
             ))}
           </select>
           <div className={"dialog-actions"}>
-            <button className="button" type="submit" disabled={!this.state.assignmentId}>
+            <button
+              className="button"
+              type="submit"
+              disabled={!this.state.assignmentId || this.props.cloneAssignments.length === 0}
+            >
               {I18n.t("save")}
             </button>
             <button
