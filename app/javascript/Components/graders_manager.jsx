e5c00efbf2d0b4a6f824260fb45bb83224c875bb
freyazjiner <jinerz77@gmail.com>
2025-10-06 21:06:16 -0400

    Fixed the search for Name in Graders page

--- app/javascript/Components/graders_manager.jsx
+++ app/javascript/Components/graders_manager.jsx
@@ -492,7 +492,18 @@ class RawGradersTable extends React.Component {
     },
     {
       Header: I18n.t("activerecord.attributes.user.full_name"),
+      accessor: "first_name",
+      id: "full_name",
       Cell: row => `${row.original.first_name} ${row.original.last_name}`,
+      filterMethod: (filter, row) => {
+        if (filter.value) {
+          const fullName = `${row._original.first_name} ${row._original.last_name}`.toLowerCase();
+          return fullName.includes(filter.value.toLowerCase());
+        } else {
+          return true;
+        }
+      },
+      sortable: true,
       minWidth: 170,
     },
     {
