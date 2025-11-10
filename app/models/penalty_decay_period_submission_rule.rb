485c3b0daf41082baf397a8969e3c11bc554a6f6
Freya Zhang <149322974+freyazjiner@users.noreply.github.com>
2025-11-09 21:47:51 -0500

    Added penalty_type option to submission rules (#7709)
    Penalty-based submission rules now support three `penalty_type` options:
    
    - percentage: percentage of assignment total (the default and current behaviour)
    - percentage_of_mark: percentage of earned mark
    - points: raw mark value

--- app/models/penalty_decay_period_submission_rule.rb
+++ app/models/penalty_decay_period_submission_rule.rb
@@ -1,13 +1,19 @@
 class PenaltyDecayPeriodSubmissionRule < SubmissionRule
   # This message will be dislayed to Students on viewing their file manager
   # after the due date has passed, but before the calculated collection date.
+  validates :penalty_type,
+            presence: true,
+            inclusion: { in: [ExtraMark::PERCENTAGE, ExtraMark::POINTS, ExtraMark::PERCENTAGE_OF_MARK] }
+
   def overtime_message(grouping)
     # How far are we into overtime?
     overtime_hours = calculate_overtime_hours_from(Time.current, grouping)
     # Calculate the penalty that the grouping will suffer
     potential_penalty = calculate_penalty(overtime_hours)
+    penalty_suffix = penalty_type || ExtraMark::PERCENTAGE
 
-    I18n.t 'penalty_decay_period_submission_rules.overtime_message', potential_penalty: potential_penalty
+    I18n.t "penalty_decay_period_submission_rules.overtime_message_#{penalty_suffix}",
+           potential_penalty: potential_penalty
   end
 
   def apply_submission_rule(submission)
@@ -17,12 +23,18 @@ class PenaltyDecayPeriodSubmissionRule < SubmissionRule
     result = submission.get_original_result
     overtime_hours = calculate_overtime_hours_from(submission.revision_timestamp, submission.grouping)
     penalty_amount = calculate_penalty(overtime_hours)
+    penalty_suffix = penalty_type || ExtraMark::PERCENTAGE
     if penalty_amount.positive?
-      ExtraMark.create(result: result,
-                       extra_mark: -penalty_amount,
-                       unit: ExtraMark::PERCENTAGE,
-                       description: I18n.t('penalty_decay_period_submission_rules.extramark_description',
-                                           overtime_hours: overtime_hours.round(2), penalty_amount: penalty_amount))
+      ExtraMark.create(
+        result: result,
+        extra_mark: -penalty_amount,
+        unit: self.penalty_type,
+        description: I18n.t(
+          "penalty_decay_period_submission_rules.extramark_description_#{penalty_suffix}",
+          overtime_hours: overtime_hours.round(2),
+          penalty_amount: penalty_amount
+        )
+      )
     end
 
     submission
