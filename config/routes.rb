f8d8bbc79fe6afa2c224ef1a592ce4c529d871ae
Naragod <mateo.naranjo@mail.utoronto.ca>
2025-11-12 15:35:11 -0500

    ISSUE-7298: Clean up code

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
             put 'add_tag'
             put 'remove_tag'
             post 'collect_submission'
-            post 'add_test_results'
+            post 'add_test_run'
           end
           resources :submission_files, only: [:index, :create] do
             collection do
