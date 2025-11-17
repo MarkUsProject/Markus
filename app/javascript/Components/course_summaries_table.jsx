350f4b706cf6c4d27bbf9d19e7b9409a0d31aa4e
Liz <elizabethfj.liu@mail.utoronto.ca>
2025-11-16 22:04:18 -0500

    migrated to use react-table v8

--- app/javascript/Components/course_summaries_table.jsx
+++ app/javascript/Components/course_summaries_table.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactTable from "react-table";
+import Table from "./table/table";
+import {createColumnHelper} from "@tanstack/react-table";
 
 export class CourseSummaryTable extends React.Component {
   constructor(props) {
@@ -15,107 +16,136 @@ export class CourseSummaryTable extends React.Component {
     marking_schemes: [],
     data: [],
   };
+  nameColumns() {
+    const columnHelper = createColumnHelper();
+    return [
+      columnHelper.accessor("hidden", {
+        id: "hidden",
+        enableSorting: false,
+        enableHiding: false,
+        enableColumnFilter: true,
+        cell: () => null,
+        header: () => null,
+        size: 0,
+      }),
+      columnHelper.accessor("user_name", {
+        header: () => I18n.t("activerecord.attributes.user.user_name"),
+        enableColumnFilter: true,
+      }),
+      columnHelper.accessor("first_name", {
+        header: () => I18n.t("activerecord.attributes.user.first_name"),
+        enableColumnFilter: true,
+      }),
+      columnHelper.accessor("last_name", {
+        header: () => I18n.t("activerecord.attributes.user.last_name"),
+        enableColumnFilter: true,
+      }),
+    ];
+  }
 
-  nameColumns = [
-    {
-      id: "hidden",
-      accessor: "hidden",
-      filterMethod: (filter, row) => {
-        return filter.value || !row.hidden;
-      },
-      className: "rt-hidden",
-      headerClassName: "rt-hidden",
-      resizable: false,
-      width: 0,
-    },
-    {
-      Header: I18n.t("activerecord.attributes.user.user_name"),
-      accessor: "user_name",
-      filterable: true,
-    },
-    {
-      Header: I18n.t("activerecord.attributes.user.first_name"),
-      accessor: "first_name",
-      filterable: true,
-    },
-    {
-      Header: I18n.t("activerecord.attributes.user.last_name"),
-      accessor: "last_name",
-      filterable: true,
-    },
-  ];
+  // nameColumns = [
+  //   {
+  //     id: "hidden",
+  //     accessor: "hidden",
+  //     filterMethod: (filter, row) => {
+  //       return filter.value || !row.hidden;
+  //     },
+  //     className: "rt-hidden",
+  //     headerClassName: "rt-hidden",
+  //     resizable: false,
+  //     width: 0,
+  //   },
+  //   {
+  //     Header: I18n.t("activerecord.attributes.user.user_name"),
+  //     accessor: "user_name",
+  //     filterable: true,
+  //   },
+  //   {
+  //     Header: I18n.t("activerecord.attributes.user.first_name"),
+  //     accessor: "first_name",
+  //     filterable: true,
+  //   },
+  //   {
+  //     Header: I18n.t("activerecord.attributes.user.last_name"),
+  //     accessor: "last_name",
+  //     filterable: true,
+  //   },
+  // ];
 
   updateShowHidden = event => {
     let showHidden = event.target.checked;
     let filtered = [];
-    for (let i = 0; i < this.state.filtered.length; i++) {
-      if (this.state.filtered[i].id !== "hidden") {
-        filtered.push(this.state.filtered[i]);
-      }
-    }
+    // for (let i = 0; i < this.state.filtered.length; i++) {
+    //   if (this.state.filtered[i].id !== "hidden") {
+    //     filtered.push(this.state.filtered[i]);
+    //   }
+    // }
+    let columnFilters = this.state.columnFilters.filter(f => f.id !== "hidden");
+
     if (!showHidden) {
-      filtered.push({id: "hidden", value: false});
+      columnFilters.push({id: "hidden", value: false});
     }
-    this.setState({filtered, showHidden});
+    this.setState({showHidden, columnFilters});
   };
 
   dataColumns = () => {
-    let columns = [];
-    this.props.assessments.map(data => {
-      columns.push({
-        accessor: `assessment_marks.${data["id"]}.mark`,
-        Header: data["name"],
-        minWidth: 50,
-        className: "number",
-        headerStyle: {textAlign: "right"},
-      });
+    const columnHelper = createColumnHelper();
+
+    const columns = [];
+    this.props.assessments.forEach(data => {
+      columns.push(
+        columnHelper.accessor(`assessment_marks.${data.id}.mark`, {
+          header: () => data.name,
+          cell: info => info.getValue(),
+        })
+      );
     });
-    this.props.marking_schemes.map(data => {
-      columns.push({
-        accessor: `weighted_marks.${data["id"]}.mark`,
-        Header: data["name"],
-        minWidth: 50,
-        className: "number",
-        headerStyle: {textAlign: "right"},
-      });
+    this.props.marking_schemes.forEach(data => {
+      columns.push(
+        columnHelper.accessor(`weighted_marks.${data.id}.mark`, {
+          header: () => data.name,
+          cell: info => info.getValue(),
+        })
+      );
     });
     return columns;
   };
 
   render() {
-    return [
-      !this.props.student && (
-        <div key="show-hidden" style={{height: "2em"}}>
-          <input
-            id="show_hidden"
-            name="show_hidden"
-            type="checkbox"
-            checked={this.state.showHidden}
-            onChange={this.updateShowHidden}
-            style={{marginLeft: "5px", marginRight: "5px"}}
-          />
-          <label htmlFor="show_hidden">{I18n.t("students.display_inactive")}</label>
-        </div>
-      ),
-      <ReactTable
-        key="course-summary-table"
-        data={this.props.data}
-        columns={
-          this.props.student ? this.dataColumns() : this.nameColumns.concat(this.dataColumns())
-        }
-        defaultSorted={[
-          {
-            id: "user_name",
-          },
-        ]}
-        loading={this.props.loading}
-        filtered={this.state.filtered}
-        onFilteredChange={filtered => this.setState({filtered})}
-        className={"auto-overflow"}
-        getNoDataProps={() => ({
-          loading: this.props.loading,
-        })}
-      />,
-    ];
+    const columns = this.props.student
+      ? this.dataColumns()
+      : [...this.nameColumns(), ...this.dataColumns()];
+
+    return (
+      <>
+        {!this.props.student && (
+          <div key="show-hidden" style={{height: "2em"}}>
+            <input
+              id="show_hidden"
+              name="show_hidden"
+              type="checkbox"
+              checked={this.state.showHidden}
+              onChange={this.updateShowHidden}
+              style={{marginLeft: "5px", marginRight: "5px"}}
+            />
+            <label htmlFor="show_hidden">{I18n.t("students.display_inactive")}</label>
+          </div>
+        )}
+        <Table
+          data={this.props.data}
+          columns={columns}
+          columnFilters={this.state.columnFilters}
+          initialState={{sorting: [{id: "user_name", desc: false}]}}
+          loading={this.props.loading}
+          filtered={this.state.filtered}
+          onFilteredChange={columnFilters => this.setState({columnFilters})}
+          className={"auto-overflow"}
+          getNoDataProps={() => ({
+            loading: this.props.loading,
+          })}
+        />
+        ,
+      </>
+    );
   }
 }
