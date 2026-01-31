6ff0bf81983912f2f13fa2041ea0c1b346cbf41c
Karl-Michaud <karl.michaud@mail.utoronto.ca>
2026-01-30 20:27:15 -0500

    Forgot to modify this one

--- app/models/result.rb
+++ app/models/result.rb
@@ -459,7 +459,7 @@ class Result < ApplicationRecord
     end
 
     if nil_marks || num_marks < criteria.count
-      errors.add(:base, I18n.t('results.criterion_incomplete_error'))
+      errors.add(:base, :criterion_incomplete_error)
       throw(:abort)
     end
     true
