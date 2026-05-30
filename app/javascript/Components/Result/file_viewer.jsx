2005f450767c92a63820043fe3c0337fb2c417b8
David Liu <david@cs.toronto.edu>
2026-05-30 14:33:34 -0400

    Refactored SubmissionFilePanel subcomponents into functional style

--- app/javascript/Components/Result/file_viewer.jsx
+++ app/javascript/Components/Result/file_viewer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useRef, useEffect, useCallback} from "react";
+import {Grid} from "react-loader-spinner";
 
 import {ImageViewer} from "./image_viewer";
 import {TextViewer} from "./text_viewer";
@@ -7,96 +8,96 @@ import {HTMLViewer} from "./html_viewer";
 import {BinaryViewer} from "./binary_viewer";
 import {URLViewer} from "./url_viewer";
 
-export class FileViewer extends React.Component {
-  state = {
-    loading: false,
-    errorMessage: null,
-  };
+export const FileViewer = React.memo(function FileViewer(props) {
+  const [loading, setLoadingState] = useState(false);
+  const [errorMessage, setErrorMessageState] = useState(null);
+  const mounted = useRef(false);
 
-  mounted = false;
-
-  setLoading = loading => {
-    if (this.mounted) {
-      this.setState({loading: loading});
-    }
-  };
-
-  setErrorMessage = message => {
-    if (this.mounted) {
-      this.setState({errorMessage: message});
-    }
-  };
-
-  componentDidMount() {
-    this.mounted = true;
-  }
-
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props !== prevProps) {
-      this.setState({
-        loading: false,
-        errorMessage: null,
-      });
-    }
-  }
-
-  getViewer() {
-    const commonProps = {
-      submission_file_id: this.props.selectedFile,
-      annotations: this.props.annotations ? this.props.annotations : [],
-      released_to_students: this.props.released_to_students,
-      resultView: !!this.props.result_id,
-      course_id: this.props.course_id,
-      key: `${this.props.selectedFileType}-viewer`,
-      url: this.props.selectedFileURL,
-      setLoadingCallback: this.setLoading,
-      setErrorMessageCallback: this.setErrorMessage,
+  useEffect(() => {
+    mounted.current = true;
+    return () => {
+      mounted.current = false;
     };
+  }, []);
 
-    if (this.props.selectedFileType === "image") {
-      return <ImageViewer mime_type={this.props.mime_type} {...commonProps} />;
-    } else if (this.props.selectedFileType === "pdf") {
-      return <PDFViewer annotationFocus={this.props.annotationFocus} {...commonProps} />;
-    } else if (
-      this.props.selectedFileType === "jupyter-notebook" ||
-      (this.props.selectedFileType === "rmarkdown" && this.props.rmd_convert_enabled)
-    ) {
-      return <HTMLViewer annotationFocus={this.props.annotationFocus} {...commonProps} />;
-    } else if (this.props.selectedFileType === "binary") {
-      return <BinaryViewer {...commonProps} />;
-    } else if (this.props.selectedFileType === "markusurl") {
-      return <URLViewer {...commonProps} />;
-    } else if (this.props.selectedFileType !== "") {
-      return (
-        <TextViewer
-          type={
-            this.props.selectedFileType === "rmarkdown" ? "markdown" : this.props.selectedFileType
-          }
-          focusLine={this.props.focusLine}
-          {...commonProps}
-        />
-      );
-    } else {
-      return "";
+  const setLoading = useCallback(value => {
+    if (mounted.current) {
+      setLoadingState(value);
     }
-  }
+  }, []);
 
-  render() {
-    const viewer = this.getViewer();
-    const outerDivStyle = {
-      display: this.state.loading || this.state.errorMessage ? "none" : "block",
-      height: "100%",
-    };
-    return (
-      <React.Fragment>
-        <div style={outerDivStyle}>{viewer}</div>
-        {this.state.errorMessage && <p>{this.state.errorMessage}</p>}
-        {this.state.loading && !this.state.errorMessage && <p>{I18n.t("working")}</p>}
-      </React.Fragment>
+  const setErrorMessage = useCallback(message => {
+    if (mounted.current) {
+      setErrorMessageState(message);
+    }
+  }, []);
+
+  useEffect(() => {
+    setLoadingState(true);
+    setErrorMessageState(null);
+  }, [props.selectedFile, props.selectedFileURL, props.selectedFileType]);
+
+  const commonProps = {
+    submission_file_id: props.selectedFile,
+    annotations: props.annotations ?? [],
+    released_to_students: props.released_to_students,
+    resultView: !!props.result_id,
+    course_id: props.course_id,
+    key: `${props.selectedFileType}-viewer`,
+    url: props.selectedFileURL,
+    setLoadingCallback: setLoading,
+    setErrorMessageCallback: setErrorMessage,
+  };
+
+  let viewer;
+  if (props.selectedFileType === "image") {
+    viewer = <ImageViewer mime_type={props.mime_type} {...commonProps} />;
+  } else if (props.selectedFileType === "pdf") {
+    viewer = <PDFViewer annotationFocus={props.annotationFocus} {...commonProps} />;
+  } else if (
+    props.selectedFileType === "jupyter-notebook" ||
+    (props.selectedFileType === "rmarkdown" && props.rmd_convert_enabled)
+  ) {
+    viewer = <HTMLViewer annotationFocus={props.annotationFocus} {...commonProps} />;
+  } else if (props.selectedFileType === "binary") {
+    viewer = <BinaryViewer {...commonProps} />;
+  } else if (props.selectedFileType === "markusurl") {
+    viewer = <URLViewer {...commonProps} />;
+  } else if (props.selectedFileType !== "") {
+    viewer = (
+      <TextViewer
+        type={props.selectedFileType === "rmarkdown" ? "markdown" : props.selectedFileType}
+        focusLine={props.focusLine}
+        {...commonProps}
+      />
     );
+  } else {
+    viewer = "";
   }
-}
+
+  const outerDivStyle = {
+    display: loading || errorMessage ? "none" : "block",
+    height: "100%",
+  };
+
+  return (
+    <React.Fragment>
+      <div style={outerDivStyle}>{viewer}</div>
+      {errorMessage && <p>{errorMessage}</p>}
+      {loading && !errorMessage && (
+        <div className="loading-spinner">
+          <Grid
+            visible={true}
+            height="25"
+            width="25"
+            color="#31649B"
+            aria-label="grid-loading"
+            radius="12.5"
+            wrapperStyle={{}}
+            wrapperClass="grid-wrapper"
+          />
+        </div>
+      )}
+    </React.Fragment>
+  );
+});
