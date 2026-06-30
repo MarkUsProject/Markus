f7c443c39c9277c4badca0c0f98396b5039ab8db
mrafie1 <rafie.muhammad2007@gmail.com>
2026-06-30 05:01:36 -0230

    Removed previous changes to use getByTestId

--- app/javascript/Components/__tests__/annotation_usage_panel.test.jsx
+++ app/javascript/Components/__tests__/annotation_usage_panel.test.jsx
@@ -61,7 +61,7 @@ describe("For the AnnotationUsagePanel's group name search", () => {
   });
 
   it("becomes case-sensitive when the toggle is checked", () => {
-    fireEvent.click(screen.getByRole("checkbox"));
+    fireEvent.click(screen.getByTestId("group_name_case_sensitive"));
 
     const groupSearch = screen.getByRole("textbox", {
       name: `${I18n.t("search")} ${I18n.t("activerecord.models.submission.one")}`,
