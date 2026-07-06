24b2ff408f60bf90cf3c9842495eac50db73aa7f
Muhammad <rafie.muhammad2007@gmail.com>
2026-07-06 14:14:31 -0400

    Migrated AnnotationUsagePanel to react table v8 (#8021)

--- app/javascript/Components/table/table_row.jsx
+++ app/javascript/Components/table/table_row.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import TableCell from "./table_cell";
 import {SELECTION_COLUMN_ID} from "./table";
 
-function TableRow({row, isSelected, isExpanded, renderSubComponent}) {
+function TableRow({row, isSelected, isGrouped, isExpanded, renderSubComponent}) {
   return (
     <div className="rt-tr-group" role="rowgroup">
       <div className="rt-tr -odd" role="row">
@@ -15,13 +15,14 @@ function TableRow({row, isSelected, isExpanded, renderSubComponent}) {
             <TableCell
               cell={cell}
               isSelected={cellSelection}
+              isExpanded={isExpanded}
               key={cell.id}
               width={cell.column.getSize()}
             />
           );
         })}
       </div>
-      {isExpanded && <div>{renderSubComponent({row})}</div>}
+      {isExpanded && !isGrouped && <div>{renderSubComponent({row})}</div>}
     </div>
   );
 }
