b3c8507c8b6ba0695484746f27f6c36e3c0144a1
mrafie1 <rafie.muhammad2007@gmail.com>
2026-07-13 12:38:56 -0400

    refactored child channel class to include auto-auth

--- app/channels/groups_channel.rb
+++ app/channels/groups_channel.rb
@@ -1,20 +1,17 @@
 class GroupsChannel < ApplicationCable::Channel
   def subscribed
-    course = Course.find_by(id: params[:course_id])
-    assignment = course&.assignments&.find_by(id: params[:assignment_id])
-    role = Role.find_by(user: current_user, course: course)
-    if assignment.nil? || role.nil?
-      reject
-      return
-    end
-    unless allowed_to?(:manage?, assignment, context: { real_user: current_user, role: role })
-      reject
-      return
-    end
     stream_for current_user
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def implicit_authorization_target
+    assignment
+  end
+
+  def authorization_rule
+    :manage?
   end
 end
