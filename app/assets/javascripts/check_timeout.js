f675973b2925a2fcab114251af4938ef436f5697
mrafie1 <rafie.muhammad2007@gmail.com>
2026-07-21 21:10:19 -0400

    update check_timeout logic

--- app/assets/javascripts/check_timeout.js
+++ app/assets/javascripts/check_timeout.js
@@ -1,9 +1,33 @@
 (function () {
+  let intervalId;
   const domContentLoadedCB = () => {
-    setInterval(() => {
-      $.get(Routes.check_timeout_main_index_path());
+    startPolling();
+  };
+
+  const startPolling = () => {
+    if (intervalId) return;
+
+    intervalId = setInterval(() => {
+      checkTimeout();
     }, 120000);
   };
 
+  const stopPolling = () => {
+    clearInterval(intervalId);
+    intervalId = null;
+  };
+
+  const checkTimeout = () => {
+    $.get(Routes.check_timeout_main_index_path());
+  };
+
   document.addEventListener("DOMContentLoaded", domContentLoadedCB);
+  document.addEventListener("visibilitychange", () => {
+    if (document.hidden) {
+      stopPolling();
+    } else {
+      checkTimeout();
+      startPolling();
+    }
+  });
 })();
