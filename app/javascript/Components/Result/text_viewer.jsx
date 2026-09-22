1df127ad449f5f8dd59dd417642373e80e023706
mrafie1 <rafie.muhammad2007@gmail.com>
2026-09-22 12:33:09 -0400

    simplify bug fix

--- app/javascript/Components/Result/text_viewer.jsx
+++ app/javascript/Components/Result/text_viewer.jsx
@@ -105,15 +105,6 @@ export class TextViewer extends React.PureComponent {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const fileChanged =
-      this.props.submission_file_id !== prevProps.submission_file_id ||
-      this.props.url !== prevProps.url;
-
-    if (fileChanged && this.state.preview_file) {
-      this.setState({preview_file: false});
-      return;
-    }
-
     if (this.props.url && this.props.url !== prevProps.url) {
       // The URL has updated, so the content needs to be fetched using the new URL.
       this.fetchContent(this.props.url)
@@ -166,10 +157,8 @@ export class TextViewer extends React.PureComponent {
    * 3. Scroll to line numbered this.props.focusLine
    */
   ready_annotations = () => {
-    if (!this.raw_content.current) {
-      if (this.state.preview_file) {
-        this.setState({preview_file: false});
-      }
+    if (this.state.preview_file) {
+      this.setState({preview_file: false});
       return;
     }
 
